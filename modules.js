fa3d7bc046c64f9a81b1413388e949eedd410b09
robit.alec <robit.alec@gmail.com>
2021-02-12 00:56:13 +0000

    Created modules.js.

---
+++ modules.js
@@ -0,0 +1,23 @@
+exports.aggregateMonthlyYears = function(years, images, reducer) {
+  return ee.ImageCollection.fromImages(
+    years.map(function(yr) {
+      return months.map(function(mnth) {
+        return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
+                     .filter(ee.Filter.calendarRange(mnth, mnth, 'month'))
+                     .reduce(reducer)
+                     .set('year', yr)
+                     .set('month', mnth);
+        });
+  }).flatten()
+  );
+};
+
+exports.aggregateYears = function(years, images, reducer) {
+  return ee.ImageCollection.fromImages(
+    years.map(function(yr) {
+      return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
+                   .reduce(reducer)
+                   .set('year', yr);
+    })
+  );
+};
