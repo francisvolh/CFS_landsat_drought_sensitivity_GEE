152e712e7b3798f689378ecafec585589e9237f3
robit.alec <robit.alec@gmail.com>
2021-02-12 01:53:40 +0000

    Updated modules/cmi.js (added 1 line, deleted 1 line).

--- modules/cmi.js
+++ modules/cmi.js
@@ -59,7 +59,7 @@ exports.calcPET = function(img, dem) {
     '93 * VPD * KTRF * (2.71828182846 ** (ELEV / 9300))', {
       'VPD': img.select('VPD'),
       'KTRF': img.select('KTRF'),
-      'ELEV': dem.select('elevation')
+      'ELEV': ee.ImageCollection("NRCan/CDEM").select('elevation').mosaic()
     }).rename('PET'));
 };
 
