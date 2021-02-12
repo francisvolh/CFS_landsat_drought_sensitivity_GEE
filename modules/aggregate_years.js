079c40028b2a90b5111b90f3ce936a4a4cd7651c
robit.alec <robit.alec@gmail.com>
2021-02-12 00:48:56 +0000

    Updated modules/aggregate_years.js (added 7 lines, deleted 5 lines).

--- modules/aggregate_years.js
+++ modules/aggregate_years.js
@@ -1,7 +1,9 @@
 exports.aggregateYears = function(years, images, reducer) {
-  years.map(function(yr) {
-    return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
-                 .reduce(reducer)
-                 .set('year', yr);
-  });
+  ee.ImageCollection.fromImages(
+    years.map(function(yr) {
+      return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
+                   .reduce(reducer)
+                   .set('year', yr);
+    });
+  )
 };
