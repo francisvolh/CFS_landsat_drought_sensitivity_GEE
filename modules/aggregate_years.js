615cb2cabeebd31eeedffcc3a1f69ef7e799276a
robit.alec <robit.alec@gmail.com>
2021-02-12 00:49:00 +0000

    Updated modules/aggregate_years.js (added 2 lines, deleted 2 lines).

--- modules/aggregate_years.js
+++ modules/aggregate_years.js
@@ -4,6 +4,6 @@ exports.aggregateYears = function(years, images, reducer) {
       return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
                    .reduce(reducer)
                    .set('year', yr);
-    });
-  )
+    })
+  );
 };
