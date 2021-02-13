6ea667ee75527361427885f50401c943bf3c71d8
robit.alec <robit.alec@gmail.com>
2021-02-13 03:37:56 +0000

    Updated modules/cmi-terraclimate.js (added 56 lines, deleted 56 lines).

--- modules/cmi-terraclimate.js
+++ modules/cmi-terraclimate.js
@@ -16,74 +16,74 @@
 // vs 	Wind-speed at 10m 	0 	2923 	m/s 	0.01 
 
 // Functions
-// exports.calcETMAX = function(img) {
-//   return img.addBands(
-//     img.expression(
-//     '0.61078 * (2.71828182846 ** (17.269 * tmax / (237.3 + tmax)))', {
-//       'tmax': img.select('tmmx')
-//     }).rename('ETMAX'));
-// };
+exports.calcETMAX = function(img) {
+  return img.addBands(
+    img.expression(
+    '0.61078 * (2.71828182846 ** (17.269 * tmax / (237.3 + tmax)))', {
+      'tmax': img.select('tmmx')
+    }).rename('ETMAX'));
+};
 
-// exports.calcETMIN = function(img) {
-//   return img.addBands(
-//     img.expression(
-//     '0.61078 * (2.71828182846 ** (17.269 * tmin / (237.3 + tmin)))', {
-//       'tmin': img.select('tmmn')
-//     }).rename('ETMIN'));
-// };
+exports.calcETMIN = function(img) {
+  return img.addBands(
+    img.expression(
+    '0.61078 * (2.71828182846 ** (17.269 * tmin / (237.3 + tmin)))', {
+      'tmin': img.select('tmmn')
+    }).rename('ETMIN'));
+};
 
-// exports.calcETDEW = function(img) {
-//   return img.addBands(
-//     img.expression(
-//     '0.61078 * (2.71828182846 ** (17.269 * (tmin - 2.5) / (237.3 + tmin - 2.5)))', {
-//       'tmin': img.select('tmmn')
-//     }).rename('ETDEW'));
-// };
+exports.calcETDEW = function(img) {
+  return img.addBands(
+    img.expression(
+    '0.61078 * (2.71828182846 ** (17.269 * (tmin - 2.5) / (237.3 + tmin - 2.5)))', {
+      'tmin': img.select('tmmn')
+    }).rename('ETDEW'));
+};
 
 
-// exports.calcVPD = function(img) {
-//   return img.addBands(
-//     img.expression(
-//     '0.5 * (ETMAX + ETMIN) - ETDEW', {
-//       'ETMAX': img.select('ETMAX'),
-//       'ETMIN': img.select('ETMIN'),
-//       'ETDEW': img.select('ETDEW')
-//     }).rename('VPD'));
-// };
+exports.calcVPD = function(img) {
+  return img.addBands(
+    img.expression(
+    '0.5 * (ETMAX + ETMIN) - ETDEW', {
+      'ETMAX': img.select('ETMAX'),
+      'ETMIN': img.select('ETMIN'),
+      'ETDEW': img.select('ETDEW')
+    }).rename('VPD'));
+};
 
-// exports.calcTAVG515 = function(img) {
-//   return img.addBands(
-//     img.expression(
-//     '(((tmin + tmax) / 2) + 5) / 15', {
-//       'tmin': img.select('tmmn'),
-//       'tmax': img.select('tmmx')
-//     }).rename('TAVG515'));
-// };
+exports.calcTAVG515 = function(img) {
+  return img.addBands(
+    img.expression(
+    '(((tmin + tmax) / 2) + 5) / 15', {
+      'tmin': img.select('tmmn'),
+      'tmax': img.select('tmmx')
+    }).rename('TAVG515'));
+};
 
-// exports.calcKTRF = function(img) {
-//   return img.addBands(
-//     img.select('TAVG515')
-//       .where(img.select('TAVG515').lt(0), 0)
-//       .where(img.select('TAVG515').gt(1), 1)
-//       .rename('KTRF'));
-// };
+exports.calcKTRF = function(img) {
+  return img.addBands(
+    img.select('TAVG515')
+      .where(img.select('TAVG515').lt(0), 0)
+      .where(img.select('TAVG515').gt(1), 1)
+      .rename('KTRF'));
+};
 
-// exports.calcPET = function(img, dem) {
-//   return img.addBands(
-//     img.expression(
-//     '93 * VPD * KTRF * (2.71828182846 ** (ELEV / 9300))', {
-//       'VPD': img.select('vpd'),
-//       'KTRF': img.select('KTRF'),
-//       'ELEV': ee.Image("MERIT/DEM/v1_0_3")
-//       // ee.ImageCollection("NRCan/CDEM").select('elevation').mosaic()
-//     }).rename('PET'));
-// };
+exports.calcPET = function(img, dem) {
+  return img.addBands(
+    img.expression(
+    '93 * VPD * KTRF * (2.71828182846 ** (ELEV / 9300))', {
+      'VPD': img.select('VPD'),
+      'KTRF': img.select('KTRF'),
+      'ELEV': ee.Image("MERIT/DEM/v1_0_3")
+      // ee.ImageCollection("NRCan/CDEM").select('elevation').mosaic()
+    }).rename('PET'));
+};
  
 exports.calcCMI = function(img) {
   return img.addBands(
     img.expression(
     '(PREC - PET) / 10', {
       'PREC': img.select('pr'),
-      'PET': img.select('pet')
+      'PET': img.select('PET')
     }).rename('CMI'));
 };
