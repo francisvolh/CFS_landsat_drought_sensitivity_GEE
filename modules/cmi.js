5fa32543fbef523f6981357bb13c04b7d97a7a72
robit.alec <robit.alec@gmail.com>
2021-02-15 19:52:55 +0000

    Updated modules/cmi.js (added 1 line, deleted 1 line).

--- modules/cmi.js
+++ modules/cmi.js
@@ -56,7 +56,7 @@ exports.calcKTRF = function(img) {
 exports.calcPET = function(img, dem) {
   return img.addBands(
     img.expression(
-    '93 * VPD * KTRF * (2.71828182846 ** (ELEV / 9300))', {
+    '93 * VPD * KTRF * (2.71828182846 ** (1.0 * ELEV / 9300))', {
       'VPD': img.select('VPD'),
       'KTRF': img.select('KTRF'),
       'ELEV': ee.Image("MERIT/DEM/v1_0_3")
