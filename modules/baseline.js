97e019f91738f9d6e8139a571b3d7598d6b10be2
robit.alec <robit.alec@gmail.com>
2021-02-26 20:26:46 +0000

    Updated modules/baseline.js (added 8 lines, deleted 7 lines).

--- modules/baseline.js
+++ modules/baseline.js
@@ -13,28 +13,29 @@ exports.gtPercentile = function(img, cmiBand, percentileBand) {
 
 exports.antecedentPercentile = function(years, images, band, percentiles) {
   return ee.ImageCollection.fromImages(years.map(function(yr) {
-    var ante12max = ee.Date.fromYMD(yr, antemax, 1);
+    // antemax - July 1
+    var ante12max = ee.Date.fromYMD(yr, 6, 1);
     
     var band3 = band + '_ante3';
     var band6 = band + '_ante6';
     var band12 = band + '_ante12';
     
     return ee.Image([
-      // Antecedent: 3
+      // Antecedent: 3 (months 3-6)
       images.filter(ee.Filter.eq('year', yr))
-              .filter(ee.Filter.rangeContains('month', ante3min, antemax))
+              .filter(ee.Filter.rangeContains('month', 3, 6))
               .select([band], [band3])
               .reduce(ee.Reducer.percentile(percentiles)),
                
-      // Antecedent: 6
+      // Antecedent: 6 (months 1-6)
       images.filter(ee.Filter.eq('year', yr))
-              .filter(ee.Filter.rangeContains('month', ante6min, antemax))
+              .filter(ee.Filter.rangeContains('month', 1, 6))
               .select([band], [band6])
               .reduce(ee.Reducer.percentile(percentiles)),
                
-      // Antecedent: 12
+      // Antecedent: 12 (months 6-6 year previous)
       images.filter(ee.Filter.date(ante12max.advance(-1, 'year'),
-                                      ante12max))
+                                   ante12max))
               .select([band], [band12])
               .reduce(ee.Reducer.percentile(percentiles))
       ]).set('year', yr);
