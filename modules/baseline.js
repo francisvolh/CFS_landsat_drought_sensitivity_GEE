f07e754dce5853739519d4a978832490e2d8c67c
robit.alec <robit.alec@gmail.com>
2021-02-26 22:21:24 +0000

    Updated modules/baseline.js (added 3 lines, deleted 3 lines).

--- modules/baseline.js
+++ modules/baseline.js
@@ -51,9 +51,9 @@ exports.gtPercentileBands3 = function(percentileImages, cmiImages, cmiBand) {
     var filtCMI = cmiImages.filter(ee.Filter.eq('year', yr));
 
     return filtCMI.map(function(cmiImg) {
-      return ee.Image.cat([cmiImg.select(cmiBand),
-                           cmiImg.select(cmiBand)
-                                 .gt(percentImg)])
+      return cmiImg.select(cmiBand)
+                   .addBands(cmiImg.select(cmiBand)
+                                   .gt(percentImg));
     })
   }).flatten();
 };
