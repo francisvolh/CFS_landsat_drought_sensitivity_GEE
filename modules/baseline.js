08a8293270a53307b9c88c4072556b2a05f1367d
robit.alec <robit.alec@gmail.com>
2021-02-26 21:54:38 +0000

    Updated modules/baseline.js (added 2 lines, deleted 1 line).

--- modules/baseline.js
+++ modules/baseline.js
@@ -36,7 +36,8 @@ exports.gtPercentileBands = function(cmiImages, cmiBand, percentileImages) {
 exports.gtPercentileBands2 = function(years, cmiImages, cmiBand, percentileImages) {
   // ee.ImageCollection.fromImages
   (years.map(function(yr) {
-    var filtCMI = cmiImages.filter(ee.Filter.eq('year', yr));
+    // var filtCMI = 
+    return cmiImages.filter(ee.Filter.eq('year', yr));
     
     // var filterPerc = ee.Image(percentileImages.filter(ee.Filter.eq('year', yr)).first());
     // return percentileImages.filter(ee.Filter.eq('year', yr));
