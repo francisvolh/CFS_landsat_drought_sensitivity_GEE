3a4b9855a13bd7a45d0d7ae4203ee81424676d23
robit.alec <robit.alec@gmail.com>
2021-02-26 21:23:55 +0000

    Updated modules/baseline.js (added 1 line, deleted 1 line).

--- modules/baseline.js
+++ modules/baseline.js
@@ -28,7 +28,7 @@ exports.gtPercentileBands = function(cmiImages, cmiBand, percentileImages) {
                 'cmi': cmiImg.select(cmiBand),
                 'percentile': percentImg.select(band)
                 }).rename(outname)));
-        }));
+        })).flatten();
       }));
     })
   );
