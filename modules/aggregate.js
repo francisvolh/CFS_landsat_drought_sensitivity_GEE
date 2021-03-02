91ffa02319d1a2502dfb7bfdd641798cc2475646
robit.alec <robit.alec@gmail.com>
2021-03-02 00:57:42 +0000

    Updated modules/aggregate.js (added 17 lines, deleted 1 line).

--- modules/aggregate.js
+++ modules/aggregate.js
@@ -1,9 +1,25 @@
-exports.aggregateMY = function(years, minmonth, maxmonth, images, reducer) {
+exports.aggregateMRange = function(years, minmonth, maxmonth, images, reducer) {
   return ee.ImageCollection.fromImages(
     years.map(function(yr) {
       return months.map(function(mnth) {
         return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
                      .filter(ee.Filter.calendarRange(minmonth, maxmonth, 'month'))
+                     .reduce(reducer)
+                     .set('year', yr)
+                     .set('month', mnth)
+                     .set('system:time_start',
+                          ee.Date.fromYMD(yr, mnth, 1));
+        });
+  }).flatten()
+  );
+};
+
+exports.aggregateMY = function(years, months, images, reducer) {
+  return ee.ImageCollection.fromImages(
+    years.map(function(yr) {
+      return months.map(function(mnth) {
+        return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
+                     .filter(ee.Filter.calendarRange(mnth, mnth, 'month'))
                      .reduce(reducer)
                      .set('year', yr)
                      .set('month', mnth)
