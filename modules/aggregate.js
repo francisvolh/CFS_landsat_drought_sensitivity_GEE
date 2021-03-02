0b730f65fdf853288b39b0c8f8ecdec0c66238f9
robit.alec <robit.alec@gmail.com>
2021-03-02 00:56:01 +0000

    Updated modules/aggregate.js (added 2 lines, deleted 2 lines).

--- modules/aggregate.js
+++ modules/aggregate.js
@@ -1,9 +1,9 @@
-exports.aggregateMY = function(years, months, images, reducer) {
+exports.aggregateMY = function(years, minmonth, maxmonth, images, reducer) {
   return ee.ImageCollection.fromImages(
     years.map(function(yr) {
       return months.map(function(mnth) {
         return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
-                     .filter(ee.Filter.calendarRange(mnth, mnth, 'month'))
+                     .filter(ee.Filter.calendarRange(minmonth, maxmonth, 'month'))
                      .reduce(reducer)
                      .set('year', yr)
                      .set('month', mnth)
