446124a4821c278237c6f3c307d6da9450b24447
robit.alec <robit.alec@gmail.com>
2021-03-02 01:27:44 +0000

    Updated modules/baseline.js (added 1 line, deleted 1 line).

--- modules/baseline.js
+++ modules/baseline.js
@@ -32,7 +32,7 @@ exports.antecedentMeans = function(images, band, years) {
 
 
 // Percentiles across years
-exports.antecedentPercentile = function(images, percentiles, images, band, percentiles) {
+exports.antecedentPercentiles = function(images, percentiles, images, band, percentiles) {
   return ee.ImageCollection.fromImages(years.map(function(yr) {
     // antemax - July 1
     var ante12max = ee.Date.fromYMD(yr, 7, 1);
