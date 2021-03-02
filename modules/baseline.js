1527d17471fc7a0e3bd6485d24851df77586076a
robit.alec <robit.alec@gmail.com>
2021-03-02 01:53:42 +0000

    Updated modules/baseline.js (added 1 line, deleted 1 line).

--- modules/baseline.js
+++ modules/baseline.js
@@ -35,7 +35,7 @@ exports.antecedentMeans = function(images, band, years) {
 exports.gtPercentiles = function(means, percentiles) {
   //ee.ImageCollection.fromImages(
   return percentiles.map(function(percent) {
-    var base = means.reduce(ee.Reducer.percentile(percent));
+    var base = means.reduce(ee.Reducer.percentile([percent]));
     return means.map(function(img) {return img.subtract(base)});
   });
 };
