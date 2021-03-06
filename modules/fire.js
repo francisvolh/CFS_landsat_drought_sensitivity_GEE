055b3560d3ebdebb45ee4ff9f0aa604bcde8b417
robit.alec <robit.alec@gmail.com>
2021-03-06 03:52:43 +0000

    Updated modules/fire.js (added 3 lines, deleted 3 lines).

--- modules/fire.js
+++ modules/fire.js
@@ -1,8 +1,8 @@
 exports.fireMasks = function(fires, years) {
   return ee.ImageCollection.fromImages(years.map(function(yr) {
+    var yrMin5 = yr - 5
+
     return ee.Image([
-      var yrMin5 = yr - 5
-      
       fires.filter(ee.Filter.rangeContains('YEAR', yrMin5, yr))
            .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
            .rename('fire-in-last-5-years')
