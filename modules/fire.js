be02db086b9fc4329ecde2994b6fd3a4f2f8f6d1
robit.alec <robit.alec@gmail.com>
2021-03-06 03:54:51 +0000

    Updated modules/fire.js (added 9 lines, deleted 7 lines).

--- modules/fire.js
+++ modules/fire.js
@@ -1,11 +1,13 @@
 exports.fireMasks = function(fires, years) {
   return ee.ImageCollection.fromImages(years.map(function(yr) {
-    var yrMin5 = yr - 5
-    return ee.Image(yrMin5)
-    // return ee.Image([
-    //   fires.filter(ee.Filter.rangeContains('YEAR', yrMin5, yr))
-    //       .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
-    //       .rename('fire-in-last-5-years')
-    //   ]).set('year', yr);
+    var date = ee.Date.fromYMD(yr, 1, 1)
+    var y = date.get('year')
+    var ymin5 = date.advance(-5, 'year').get('year')
+    
+    return ee.Image([
+      fires.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
+          .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
+          .rename('fire-in-last-5-years')
+      ]).set('year', yr);
   }));
 };
