f7d91ec7edb0d007bc9a236a7f206c28a9721271
robit.alec <robit.alec@gmail.com>
2021-03-06 03:50:45 +0000

    Updated modules/fire.js (deleted 1 line).

--- modules/fire.js
+++ modules/fire.js
@@ -3,7 +3,6 @@ exports.fireMasks = function(fires, years) {
     return ee.Image([
       fires.filter(ee.Filter.rangeContains('YEAR', yr - 5, yr))
            .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
-           .eq(0)
            .rename('fire-in-last-5-years')
       ]).set('year', yr);
   }));
