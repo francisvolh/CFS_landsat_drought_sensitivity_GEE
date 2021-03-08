7ba32625cb1e3357d0da27ae78b9c4d695040135
robit.alec <robit.alec@gmail.com>
2021-03-08 02:24:00 +0000

    Updated modules/sensitivity.js (added 11 lines, deleted 8 lines).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -30,13 +30,16 @@ exports.maskDrought = function(veg, droughts, fires) {
     var fire = fires.filter(ee.Filter.eq('year', yr))
                     .first()
                     .eq(0);
+                    
+                    
+    var drought3 = base.select('CMI_gt_ante3_p10').eq(0)
+    var drought6 = base.select('CMI_gt_ante6_p10').eq(0)
+    var drought12 = base.select('CMI_gt_ante12_p10').eq(0)
+
+    var veg3 = veg.updateMask(drought3).select(['NDVI', 'EVI'], ['NDVI_3', 'EVI_3']);
+    var veg6 = veg.updateMask(drought6).select(['NDVI', 'EVI'], ['NDVI_6', 'EVI_6']);
+    var veg12 = veg.updateMask(drought12).select(['NDVI', 'EVI'], ['NDVI_12', 'EVI_12']);
     
-    var sumante = base.expression('ante3 + ante6 + ante12', {
-                                    'ante3': base.select('CMI_gt_ante3_p10'),
-                                    'ante6': base.select('CMI_gt_ante6_p10'),
-                                    'ante12': base.select('CMI_gt_ante12_p10')})
-                      .eq(3);
-    
-    return v.updateMask(fire).updateMask(sumante);
+    return ee.Image([veg3, veg6, veg12]).copyProperties(v);
   });
 };
