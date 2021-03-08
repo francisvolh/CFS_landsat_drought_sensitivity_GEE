9c9b745cfccdca6847264b9149e3d5ff336d420c
robit.alec <robit.alec@gmail.com>
2021-03-08 22:39:45 +0000

    Updated modules/sensitivity.js (added 15 lines, deleted 7 lines).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -8,14 +8,22 @@ exports.maskBaseline = function(veg, droughts, fires, percentiles) {
     var fire = fires.filter(ee.Filter.eq('year', yr))
                     .first()
                     .eq(0);
-                    
-    var sumante = base.expression('ante3 + ante6 + ante12', {
-                                    'ante3': base.select('CMI_gt_ante3_p10'),
-                                    'ante6': base.select('CMI_gt_ante6_p10'),
-                                    'ante12': base.select('CMI_gt_ante12_p10')})
-                      .eq(3);
+    return ee.Image.cat(
+      percentiles.map(function(percent) {
+        
+        var ante3band = 'CMI_gt_ante3_p' + percent;
+        var ante6band = 'CMI_gt_ante6_p' + percent;
+        var ante12band = 'CMI_gt_ante12_p' + percent;
+        
+        var sumante = base.expression('ante3 + ante6 + ante12', {
+          'ante3': base.select(ante3band),
+          'ante6': base.select(ante6band),
+          'ante12': base.select(ante12band)
+        }).eq(3);
     
-    return v.updateMask(fire).updateMask(sumante);
+       return v.updateMask(fire).updateMask(sumante);
+      })
+    )
   });
 };
 
