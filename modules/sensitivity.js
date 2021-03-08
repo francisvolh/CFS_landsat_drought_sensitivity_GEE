49476a3bb1423070b74cd5a9fb95d91c75e8b0cc
robit.alec <robit.alec@gmail.com>
2021-03-08 01:54:51 +0000

    Created modules/sensitivity.js.

---
+++ modules/sensitivity.js
@@ -0,0 +1,20 @@
+exports.maskEVI = function(veg, droughts, fires) {
+  return veg.map(function(v) {
+    var yr = v.date().get('year');
+    
+    var base = droughts.filter(ee.Filter.eq('year', yr))
+                       .first();
+                         
+    var fire = fires.filter(ee.Filter.eq('year', yr))
+                    .first()
+                    .eq(0);
+    
+    var sumante = base.expression('ante3 + ante6 + ante12', {
+                                    'ante3': base.select('CMI_gt_ante3_p10'),
+                                    'ante6': base.select('CMI_gt_ante6_p10'),
+                                    'ante12': base.select('CMI_gt_ante12_p10')})
+                      .eq(3);
+    
+    return v.updateMask(fire).updateMask(sumante);
+  });
+};
