1b0891be2b2fabd214d3e70a33c5df51856c0f0a
robit.alec <robit.alec@gmail.com>
2021-03-08 23:11:44 +0000

    Updated modules/sensitivity.js (added 10 lines, deleted 13 lines).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -13,29 +13,21 @@ exports.maskVeg = function(veg, droughts, fires, percentiles) {
                     
     return ee.Image(
       percentiles.map(function(percent) {
-        
         var ante3band = 'CMI_gt_ante3_p' + percent;
         var ante6band = 'CMI_gt_ante6_p' + percent;
         var ante12band = 'CMI_gt_ante12_p' + percent;
         
-        var ndviband = 'NDVI_base_p' + percent;
-        var eviband = 'EVI_base_p' + percent;
-        
-        var drought3band = 'CMI_gt_ante3_p' + percent;
-        var drought6band = 'CMI_gt_ante6_p' + percent;
-        var drought12band = 'CMI_gt_ante12_p' + percent;
-        
+        var drought3 = base.select(ante3band).eq(0);
+        var drought6 = base.select(ante6band).eq(0);
+        var drought12 = base.select(ante12band).eq(0);
+
         var ante3ndvi = 'NDVI_ante3_p' + percent;
         var ante6ndvi = 'NDVI_ante6_p' + percent;
         var ante12ndvi = 'NDVI_ante12_p' + percent;
         var ante3evi = 'EVI_ante3_p' + percent;
         var ante6evi = 'EVI_ante6_p' + percent;
         var ante12evi = 'EVI_ante12_p' + percent;
-
-        var drought3 = base.select(drought3band).eq(0);
-        var drought6 = base.select(drought6band).eq(0);
-        var drought12 = base.select(drought12band).eq(0);
-
+        
         var veg3 = v.updateMask(drought3)
                     .select(['NDVI', 'EVI'], [ante3ndvi, ante3evi]);
         var veg6 = v.updateMask(drought6)
@@ -43,11 +35,16 @@ exports.maskVeg = function(veg, droughts, fires, percentiles) {
         var veg12 = v.updateMask(drought12)
                      .select(['NDVI', 'EVI'], [ante12ndvi, ante12evi]);
         
+
+        
         var basemask = base.expression('ante3 + ante6 + ante12', {
           'ante3': base.select(ante3band),
           'ante6': base.select(ante6band),
           'ante12': base.select(ante12band)
         }).eq(3);
+        
+        var ndviband = 'NDVI_base_p' + percent;
+        var eviband = 'EVI_base_p' + percent;
         
         var baseveg = v.updateMask(basemask)
                        .select(['NDVI', 'EVI'], [ndviband, eviband]);
