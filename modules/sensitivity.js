295571f995c2bb44b86866e44a9820098d4ee9a8
robit.alec <robit.alec@gmail.com>
2021-03-11 19:13:38 +0000

    Updated modules/sensitivity.js (added 5 lines, deleted 1 line).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -67,7 +67,11 @@ exports.maskVeg = function(veg, droughts, fires, lc, percentiles) {
         // Update vegetation with baseline mask
         var baseveg = v.updateMask(basemask)
                        .rename([ndviband, eviband]);
-                      // TODO: add in count mask
+        
+        // Mask where counts < 3 for baseline only
+        var counts = baseveg.reduce(ee.Reducer.count())
+                            .gte(3);
+        baseveg = baseveg.updateMask(counts);
         
         // Return baseline and drought period NDVI/EVI measures
         return ee.Image([baseveg, veg3, veg6, veg12]).copyProperties(v);
