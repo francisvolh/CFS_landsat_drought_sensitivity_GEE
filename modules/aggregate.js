0064f20f0983908ae9c925ab5f8cda6cc75c8ab7
robit.alec <robit.alec@gmail.com>
2021-03-20 02:16:15 +0000

    Updated modules/aggregate.js (added 4 lines).

--- modules/aggregate.js
+++ modules/aggregate.js
@@ -38,6 +38,10 @@ exports.aggregateMRange = function(years, minmonth, maxmonth, images, reducer) {
   return ee.ImageCollection.fromImages(
     years.map(function(yr) {
       return months.map(function(mnth) {
+      // Filter images to year and min/max month
+      // Reduce with reducer provided
+      // Set year and pseudo date properties
+      // Return an image for each year
         return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
                      .filter(ee.Filter.calendarRange(minmonth, maxmonth, 'month'))
                      .reduce(reducer)
