036d8dad2f63efbf3a38ce702ba20990f8d14796
robit.alec <robit.alec@gmail.com>
2021-06-16 19:40:39 +0000

    Updated Calculate-drought-sensitivity-MODIS.js (added 5 lines).

--- Calculate-drought-sensitivity-MODIS.js
+++ Calculate-drought-sensitivity-MODIS.js
@@ -61,6 +61,11 @@ ctef = ctef.filter(ee.Filter.inList('REG_ID', ['CL13R02', 'CL13R03', 'CL13R04'])
 var minyear = 1980;
 var maxyear = 2019;
 
+// Set list of years and months
+var months = ee.List.sequence(1, 12);
+var years = ee.List.sequence(minyear, maxyear);
+
+
 var aggDaymet = cmiDaymet.prepDaymet(minyear, maxyear);
 
 
