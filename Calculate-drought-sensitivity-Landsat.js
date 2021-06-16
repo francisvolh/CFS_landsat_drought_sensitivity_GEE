155dea0749f4279050d085c8f40866fabfc5e3ae
robit.alec <robit.alec@gmail.com>
2021-06-16 21:02:18 +0000

    Updated Calculate-drought-sensitivity-Landsat.js (added 2 lines, deleted 2 lines).

--- Calculate-drought-sensitivity-Landsat.js
+++ Calculate-drought-sensitivity-Landsat.js
@@ -65,8 +65,8 @@ var means = baseline.antecedentMeans(aggDaymet, 'CMI', years);
 
 // Compare antecedent means to percentiles. Eg. mean CMI for ante 3 year 2011 vs full period 10%
 var drought = baseline.gtPercentile(means, percentiles);
-
-// Drop 1980 since there's no complete antecedent 12 period
+print(drought)
+// Drop before 1985 since there's no complete antecedent 12 period (1980) or 5 yr (1980-1985)
 drought = drought.filter(ee.Filter.neq('year', 1980));
 
 // Fire -------------------------------------------------------------
