1ae099563928c0b4252112d5a1fed3f3da15ef9f
robit.alec <robit.alec@gmail.com>
2021-06-16 19:44:58 +0000

    Updated modules/baseline.js (added 34 lines, deleted 16 lines).

--- modules/baseline.js
+++ modules/baseline.js
@@ -6,9 +6,10 @@ exports.antecedentMeans = function(images, band, years) {
     var ante12max = ee.Date.fromYMD(yr, 7, 1);
 
     // Setup output band names
-    var band3 = band + '_ante3';
-    var band6 = band + '_ante6';
-    var band12 = band + '_ante12';
+    var band3mo = band + '_ante3mo';
+    var band6mo = band + '_ante6mo';
+    var band12mo = band + '_ante12mo';
+    var band5yr = band + '_ante5yr';
 
     return ee.Image([
       // Antecedent: 3 (months 3-6)
@@ -16,15 +17,15 @@ exports.antecedentMeans = function(images, band, years) {
       // Reduce with mean reducer
       images.filter(ee.Filter.eq('year', yr))
             .filter(ee.Filter.rangeContains('month', 3, 6))
-            .select([band], [band3])
+            .select([band], [band3mo])
             .reduce(ee.Reducer.mean()),
 
-      // Antecedent: 6 (months 1-6)      
+      // Antecedent: 6 (months 1-6)
       // Filter to year, and within antecedent period
       // Reduce with mean reducer
       images.filter(ee.Filter.eq('year', yr))
             .filter(ee.Filter.rangeContains('month', 1, 6))
-            .select([band], [band6])
+            .select([band], [band6mo])
             .reduce(ee.Reducer.mean()),
 
       // Antecedent: 12 (months 6-6 year previous)
@@ -32,26 +33,43 @@ exports.antecedentMeans = function(images, band, years) {
       // Reduce with mean reducer
       images.filter(ee.Filter.date(ante12max.advance(-1, 'year'),
                                    ante12max))
-              .select([band], [band12])
+              .select([band], [band12mo])
+              .reduce(ee.Reducer.mean()),
+
+      // Antecedent: 5 (driest in previous 5 years)
+      ee.ImageCollection([
+        images.filter(ee.Filter.date(ante12max.advance(-1, 'year'), ante12max))
+              .reduce(ee.Reducer.mean()),
+        images.filter(ee.Filter.date(ante12max.advance(-2, 'year'), ante12max))
+              .reduce(ee.Reducer.mean()),
+        images.filter(ee.Filter.date(ante12max.advance(-3, 'year'), ante12max))
+              .reduce(ee.Reducer.mean()),
+        images.filter(ee.Filter.date(ante12max.advance(-4, 'year'), ante12max))
+              .reduce(ee.Reducer.mean()),
+        images.filter(ee.Filter.date(ante12max.advance(-5, 'year'), ante12max))
               .reduce(ee.Reducer.mean())
-      ]).set('year', yr);
+        ]).select([band], [band5yr])
+          .reduce(ee.Reducer.min())
   }));
 };
 
 // Compare percentile images for each antecedent period to each image's antecedent means
 exports.gtPercentile = function(means, percentiles) {
-  var percent3 = means.select(['CMI_ante3_mean'], ['CMI_gt_ante3']).reduce(ee.Reducer.percentile(percentiles));
-  var percent6 = means.select(['CMI_ante6_mean'], ['CMI_gt_ante6']).reduce(ee.Reducer.percentile(percentiles));
-  var percent12 = means.select(['CMI_ante12_mean'], ['CMI_gt_ante12']).reduce(ee.Reducer.percentile(percentiles));
+  var percent3 = means.select(['CMI_ante3mo_mean'], ['CMI_gt_ante3mo']).reduce(ee.Reducer.percentile(percentiles));
+  var percent6 = means.select(['CMI_ante6mo_mean'], ['CMI_gt_ante6mo']).reduce(ee.Reducer.percentile(percentiles));
+  var percent12 = means.select(['CMI_ante12mo_mean'], ['CMI_gt_ante12mo']).reduce(ee.Reducer.percentile(percentiles));
+  var percent5 = means.select(['CMI_ante5yr_min'], ['CMI_gt_ante5yr']).reduce(ee.Reducer.percentile(percentiles));
 
   return means.map(function(img){
     return ee.Image([
-      img.select('CMI_ante3_mean')
+      img.select('CMI_ante3mo_mean')
          .gt(percent3),
-      img.select('CMI_ante6_mean')
+      img.select('CMI_ante6mo_mean')
          .gt(percent6),
-      img.select('CMI_ante12_mean')
-         .gt(percent12)]).copyProperties(img);
+      img.select('CMI_ante12mo_mean')
+         .gt(percent12),
+      img.select('CMI_ante5yr_min')
+         .gt(percent5)]).copyProperties(img);
   });
 };
 
