86a2edb85bb6c5255dbbac271335e4c5384e8613
robit.alec <robit.alec@gmail.com>
2021-06-21 17:07:38 +0000

    Updated modules/l5-prep.js (added 13 lines).

--- modules/l5-prep.js
+++ modules/l5-prep.js
@@ -10,8 +10,8 @@
 exports.calcIndices = function(img) {
   return ee.Image([
     img.expression('(nir - red) / (nir + red)',
-                   {red: img.select('B3'),
-                    nir: img.select('B4')})
+                   {red: img.select('B4'),
+                    nir: img.select('B3')})
        .rename('NDVI'),
      img.expression('(nir - swir) / (nir + swir)',
                    {nir: img.select('B4'),
@@ -23,4 +23,17 @@ exports.calcIndices = function(img) {
                     nir: img.select('B4')})
        .rename('EVI'),
   ]).copyProperties(img).set({'system:time_start': img.get('system:time_start')});
+};
+
+
+exports.cloudMaskL457 = function(image) {
+  var qa = image.select('pixel_qa');
+  // If the cloud bit (5) is set and the cloud confidence (7) is high
+  // or the cloud shadow bit is set (3), then it's a bad pixel.
+  var cloud = qa.bitwiseAnd(1 << 5)
+                  .and(qa.bitwiseAnd(1 << 7))
+                  .or(qa.bitwiseAnd(1 << 3));
+  // Remove edge pixels that don't occur in all bands
+  var mask2 = image.mask().reduce(ee.Reducer.min());
+  return image.updateMask(cloud.not()).updateMask(mask2);
 };
