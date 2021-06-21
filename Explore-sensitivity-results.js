aa9b679fdd3ff4af1a4f9f917427cd656f3ce57c
robit.alec <robit.alec@gmail.com>
2021-06-21 18:49:41 +0000

    Updated Explore-sensitivity-results.js (added 1 line, deleted 1 line).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -51,7 +51,7 @@ function showPalette(name, palette) {
 
 
 // Map --------------------------------------------------------------------------
-Map.addLayer(ctef);
+Map.addLayer(ctef, null, 'ctef', false);
 
 // Set either sens_80_19 or sens_00_19
 var toview = sens_l5;
