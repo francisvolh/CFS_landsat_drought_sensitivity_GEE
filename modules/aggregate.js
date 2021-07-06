819e742a05b0fb84aa92dcc03898cdc4ef002ce8
robit.alec <robit.alec@gmail.com>
2021-07-06 04:56:50 +0000

    Updated modules/aggregate.js (added 2 lines, deleted 2 lines).

--- modules/aggregate.js
+++ modules/aggregate.js
@@ -34,8 +34,8 @@ exports.aggregateY = function(years, images, reducer) {
       // Return an image for each year
       return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
                    .reduce(reducer)
-                   .set('year', yr)
-                   .set('system:time_start', ee.Date.fromYMD(yr, 1, 1));
+                  // .set('year', yr)
+                  // .set('system:time_start', ee.Date.fromYMD(yr, 1, 1));
     })
   );
 };
