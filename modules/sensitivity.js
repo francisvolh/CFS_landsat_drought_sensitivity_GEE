1450ac99fe95a13cc0be2d608a120a6e65966838
robit.alec <robit.alec@gmail.com>
2021-07-06 17:32:38 +0000

    Updated modules/sensitivity.js (added 1 line, deleted 1 line).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -15,7 +15,7 @@ exports.splitDrought = function(veg, drought, antes, percentiles, indices) {
         // Loop over percentiles
         return percentiles.map(function(p) {
           // Loop over indices
-          indices.map(function(index) {
+          return indices.map(function(index) {
             // Set up band names
             var droughtmaskband = 'CMI_lt_ante' + ante + 'mo_p' + p;
             var antepindexband = index + '_ante' + ante + 'mo_p' + p;
