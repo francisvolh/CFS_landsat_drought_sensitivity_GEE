0a84e563eb89159f9e7f69ef8b3b7fa77c399f9d
robit.alec <robit.alec@gmail.com>
2021-07-06 03:16:57 +0000

    Updated modules/fire.js (added 17 lines, deleted 21 lines).

--- modules/fire.js
+++ modules/fire.js
@@ -1,25 +1,21 @@
 // Generate fire masks from fire polygons
-exports.generateFireMasks = function(firepols, years) {
-  // Return image collection from images for each year's 5 year fire
-  return ee.ImageCollection.fromImages(years.map(function(yr) {
-    // First of day of year
-    var date = ee.Date.fromYMD(yr, 1, 1);
-    
-    // Present year
-    var y = date.get('year');
-    
-    // 5 years previous
-    var ymin5 = date.advance(-4, 'year').get('year');
-    
-    // Filter fires within last 5 years
-    // Reduce to any non zero = anywhere there is a fire
-    // Result is 0 = no fire, 1 = fire
-    return ee.Image([
-      firepols.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
-              .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
-              .rename('fire-in-last-5-years')
-      ]).set('year', yr);
-  }));
+exports.generateFireMasks = function(firepols, yr) {
+  var date = ee.Date.fromYMD(yr, 1, 1);
+  
+  // Present year
+  var y = date.get('year');
+  
+  // 5 years previous
+  var ymin5 = date.advance(-4, 'year').get('year');
+  
+  // Filter fires within last 5 years
+  // Reduce to any non zero = anywhere there is a fire
+  // Result is 0 = no fire, 1 = fire
+  return ee.Image([
+    firepols.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
+            .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
+            .rename('fire-in-last-5-years')
+    ]).set('year', yr);
 };
 
 // Load NFDB fire polygons
