c67ef64d4e0edade3a04f2f5e42d39745056500e
robit.alec <robit.alec@gmail.com>
2021-07-06 03:44:10 +0000

    Updated modules/land-cover.js (added 10 lines, deleted 6 lines).

--- modules/land-cover.js
+++ modules/land-cover.js
@@ -24,15 +24,19 @@
 // 220	Permanent snow and ice
 // 230	Unclassified
 
-exports.lcMask = function() {
-  // Load GlobCover land cover
-  var lc = ee.Image("ESA/GLOBCOVER_L4_200901_200912_V2_3");
+// Load GlobCover land cover
+var lc = ee.Image("ESA/GLOBCOVER_L4_200901_200912_V2_3");
 
-  // Set land cover mask, only retaining if not one of
-  return lc.updateMask(
+// Set land cover mask, only retaining if not one of
+var lcmask = lc.updateMask(
     lc.expression('lc != 11 && lc != 14 && lc != 20 && lc != 30 && ' +
                   'lc != 120 && lc != 140 && lc != 150 && lc != 190 && ' +
                   'lc != 200 && lc != 210 && lc != 220 && lc != 230',
                   {lc: lc.select('landcover')}))
       .select('landcover');
-};
+
+
+
+exports.maskLc = function(img) {
+  img.updateMask(lcmask);
+}
