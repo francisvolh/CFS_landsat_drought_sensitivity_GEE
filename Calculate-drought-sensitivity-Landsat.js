4c4d71843c494fc38d330d99ad6b8868302509e2
robit.alec <robit.alec@gmail.com>
2021-07-12 18:43:05 +0000

    Updated Calculate-drought-sensitivity-Landsat.js (deleted 1 line).

--- Calculate-drought-sensitivity-Landsat.js
+++ Calculate-drought-sensitivity-Landsat.js
@@ -119,7 +119,6 @@ drought = drought.filter(ee.Filter.gt('year', 1980));
 // Landsat ----------------------------------------------------------
 // Merge L5 and L7
 var veg = l5.merge(l7);
-
 // Filter within min/max year and for July
 // Mask clouds, fires, land cover and calculate indices
 veg = veg
