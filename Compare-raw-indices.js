7d5c835741356522b043ed02cb2a5d8aed0ed04c
robit.alec <robit.alec@gmail.com>
2021-07-12 23:43:21 +0000

    Created Compare-raw-indices.js.

---
+++ Compare-raw-indices.js
@@ -0,0 +1,95 @@
+// Data -------------------------------------------------------------------------
+// Drought sensitivity
+var sens_modis = ee.Image('users/robitalec/CFS/dr-sens-modis-test-july-12');
+// var sens_00_19 = ee.Image('users/robitalec/CFS/drought-sensitivity-2000-2019');
+
+// Landsat
+var sens_land = ee.Image('users/robitalec/CFS/dr-sen-land-test-july-12');
+
+// Load CTEF regions
+var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
+// ctef = ctef.filter(ee.Filter.stringContains('ZONE_EN', 'Arctic').not());
+ctef = ctef.filter(ee.Filter.inList('REG_ID', ['CL13R02', 'CL13R03', 'CL13R04']));
+
+// Land cover mask function
+var lcmask = require('users/robitalec/CFS:modules/land-cover.js');
+
+
+// Palette ----------------------------------------------------------------------
+// Gena's palette functions
+var palettes = require('users/gena/packages:palettes');
+
+var pal = palettes.colorbrewer.RdBu[9].reverse();
+var min = -20; var max = 20;
+var viz = {min: min, max: max, palette: pal};
+
+function showPalette(name, palette) {
+  var image = ee.Image.pixelLonLat().select(0)
+    .clip(ee.Geometry.Rectangle({coords: [[min, 0], [max, 10]], geodesic: false}))
+    .visualize(viz);
+
+  print(name);
+  print(ui.Thumbnail(image));
+}
+// showPalette(min + '           0           ' + max, pal);
+
+
+// Map --------------------------------------------------------------------------
+var lcmask = lcmask.reverseMask();
+
+// Set either sens_80_19 or sens_00_19
+var toview = sens_modis;
+
+// Set the percentile to view
+// either 5, 10, or 20
+var p = 10;
+
+// Filter the band names before map. Comment any of these out to just map all bands
+var selectBands = toview.bandNames()
+                        .filter(ee.Filter.stringContains('item', 'p' + p))
+                        // .filter(ee.Filter.stringContains('item', 'NDVI'))
+                        // .filter(ee.Filter.stringContains('item', 'ante3'))
+
+
+// Note, there's a bit of server side logic here so the browser might hang briefly
+
+// Get the list of band names and add them all separately to the map
+// By default all are added, but not shown - so you'll need to select the one to view
+// After you view one, make sure to set it off so you are only seeing one later at a time
+// var bandList = selectBands.getInfo();
+// for (var i = 0; i < bandList.length; i++) {
+//   Map.addLayer(toview.select(bandList[i]), viz, bandList[i], false);
+// }
+
+Map.addLayer(ctef, null, 'ctef', false);
+
+// var band =
+Map.addLayer(sens_modis)
+Map.addLayer(sens_land)
+
+
+Map.addLayer(lcmask, null, 'lc', false);
+
+// Chart -------------------------------------------------------------------------
+// Select an image to print a histogram
+// var band = 'Sens_EVI_ante3_p10';
+
+// print(ui.Chart.image.histogram({
+//   image: toview.select(band),
+//   region: ctef,
+//   scale: 1e12,
+//   maxBuckets: 7
+// }));
+
+// Export ------------------------------------------------------------------------
+// var exp = {
+//   image: toview.select(band).visualize(viz),
+//   description: band,
+//   folder: 'Visuals',
+//   region: geometry2,
+//   scale: 5000,
+//   maxPixels: 2e9
+// };
+// Export.image.toDrive(exp);
+
+// Map.setOptions('SATELLITE')
