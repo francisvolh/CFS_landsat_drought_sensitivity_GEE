4457a087ccc3924a058457542b6352b4a7f571f1
robit.alec <robit.alec@gmail.com>
2021-07-12 21:00:09 +0000

    Updated Calculate-drought-sensitivity-MODIS.js (added 82 lines, deleted 87 lines).

--- Calculate-drought-sensitivity-MODIS.js
+++ Calculate-drought-sensitivity-MODIS.js
@@ -1,30 +1,37 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var geometry = 
-    /* color: #d63000 */
-    /* shown: false */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-169.947757868042, 74.19688274436743],
-          [-169.947757868042, 24.716404693071457],
-          [-49.71338286804201, 24.716404693071457],
-          [-49.71338286804201, 74.19688274436743]]], null, false),
-    geometry2 = 
-    /* color: #98ff00 */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-147.8210336191388, 70.07926665876009],
-          [-147.8210336191388, 47.928518609971924],
-          [-93.5925179941388, 47.928518609971924],
-          [-93.5925179941388, 70.07926665876009]]], null, false);
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// === Calculate Drought Sensitivity ===
+// --- MODIS ---
+// Alec L. Robitaille
+
+
+// Data -------------------------------------------------------------
+// CTEF regions
+var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
+
+var modis = ee.ImageCollection("MODIS/006/MOD13Q1");
+
+
+
+// Variables --------------------------------------------------------
+// Set min max year for daymet
+var minyear = 1980;
+var maxyear = 2019;
+
+// Set list of years and months
+var months = ee.List.sequence(1, 12);
+var years = ee.List.sequence(minyear, maxyear);
+
+// Set percentiles to use
+var percentiles = [1, 5, 10, 20];
+
+// Set antecedent periods
+var antes = [3, 6, 12];
+
+// Set indices
+var indices = ['NDVI', 'EVI'];
+// TODO: add NBR
+
+
+
 // Modules ----------------------------------------------------------
 // Load modules of functions
 
@@ -34,11 +41,11 @@ var agg = require('users/robitalec/CFS:modules/aggregate.js');
 // CMI functions
 var cmiDaymet = require('users/robitalec/CFS:modules/cmi-daymet.js');
 
-// Baseline functions
-var baseline = require('users/robitalec/CFS:modules/baseline.js');
-
 // Land cover mask function
 var lcmask = require('users/robitalec/CFS:modules/land-cover.js');
+
+// Baseline functions
+var baseline = require('users/robitalec/CFS:modules/baseline.js');
 
 // Fire functions
 var fire = require('users/robitalec/CFS:modules/fire.js');
@@ -50,26 +57,22 @@ var sensitivity = require('users/robitalec/CFS:modules/sensitivity.js');
 var palettes = require('users/gena/packages:palettes');
 
 
-// Data -------------------------------------------------------------
-// CTEF regions
-var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
+
+// Filter -----------------------------------------------------------
 // ctef = ctef.filter(ee.Filter.stringContains('ZONE_EN', 'Arctic').not());
 ctef = ctef.filter(ee.Filter.inList('REG_ID', ['CL13R02', 'CL13R03', 'CL13R04']));
 
-// Aggregate --------------------------------------------------------
-// Set min max year for daymet
-var minyear = 1980;
-var maxyear = 2019;
-
-// Set list of years and months
-var months = ee.List.sequence(1, 12);
-var years = ee.List.sequence(minyear, maxyear);
+// MODIS
+modis = modis
+  .filter(ee.Filter.calendarRange(minyear, maxyear, 'year'))
+  .filter(ee.Filter.calendarRange(7, 7, 'month'));
 
 
+
+// Daymet -----------------------------------------------------------
+// Aggregate
 var aggDaymet = cmiDaymet.prepDaymet(minyear, maxyear);
 
-
-// Calculate CMI ----------------------------------------------------
 // Calculate CMI (and ETMAX, ETMIN, ETDEW, VPD, TAVG 5, 15, KTRF and PET)
 aggDaymet = aggDaymet
   .map(cmiDaymet.calcETMAX)
@@ -81,52 +84,45 @@ aggDaymet = aggDaymet
   .map(cmiDaymet.calcPET)
   .map(cmiDaymet.calcCMI);
 
-
-// Calculate baseline -----------------------------------------------
-// Set percentiles to use. Javascript list.
-var percentiles = [1, 5, 10, 20];
-
+// Calculate baseline
 // Calculate antecedent means across years. Eg. mean CMI for antecedent 3 period across years
 var means = baseline.antecedentMeans(aggDaymet, 'CMI', years);
 
 // Compare antecedent means to percentiles. Eg. mean CMI for ante 3 year 2011 vs full period 10%
-var drought = baseline.gtPercentile(means, percentiles);
+var drought = baseline.ltPercentile(means, percentiles);
 
-// Drop 1980 since there's no complete antecedent 12 period
-drought = drought.filter(ee.Filter.neq('year', 1980));
-
-// Fire -------------------------------------------------------------
-// Load NFDB fire polygons
-var firepol = ee.FeatureCollection("users/robitalec/CFS/NFDB_poly");
-
-// Generate fire masks. Each year has a mask which represents fires in the last year
-var firemask = fire.fireMasks(firepol, years);
+// Drop before 1985 since there's no complete antecedent 12 period (1980) or 5 yr (1980-1985)
+drought = drought.filter(ee.Filter.gt('year', 1980));
 
 
-// Land Cover ------------------------------------------------------
-// Load GlobCover and mask
-var lc = lcmask.lcMask();
 
+// MODIS -----------------------------------------------------------
+var veg = modis;
 
-// EVI/NDVI ---------------------------------------------------------
-// Load MODIS EVI/NDVI
 // Filter within min/max year and for July
-var veg = ee.ImageCollection("MODIS/006/MOD13Q1")
-  .filter(ee.Filter.calendarRange(minyear, maxyear, 'year'))
-  .filter(ee.Filter.calendarRange(7, 7, 'month'));
+// Mask clouds, fires, land cover and calculate indices
+veg = veg
+  // TODO: set year
+  // TODO: calc indices
+  // TODO: mask clouds
+  .map(fire.maskFires)
+  .map(lcmask.maskLc);
 
-// Mask fire and land cover, return baseline and drought percentiles EVI/NDVI across years
-var maskveg = sensitivity.maskVeg(veg, drought, firemask, lc, percentiles);
+// Split vegetation indices into drought/non-drought pixels
+var splits = sensitivity.splitDrought(veg, drought, antes, percentiles, indices);
 
 // Reduce yearly measures to means of all years
-var means = maskveg.reduce(ee.Reducer.mean());
+var means = splits.reduce(ee.Reducer.mean());
+
+
 
 // Drought sensitivity -------------------------------------------
 // SP,T,L = [ (baseline EVIP – drought EVIP,T,L) / baseline EVIP ] x 100
-var droughtSens = sensitivity.droughtSensitivity(means, percentiles);
+var droughtSens = sensitivity.droughtSensitivity(means, antes, percentiles, indices);
 
 // Drought sensitivity prime (S’) = max across three antecedent periods
-var droughtSensPrime = sensitivity.droughtSensivitityPrime(droughtSens, percentiles);
+// var droughtSensPrime = sensitivity.droughtSensivitityPrime(droughtSens, percentiles);
+
 
 
 // Map ------------------------------------------------------------
@@ -144,16 +140,15 @@ var viz = {min: min, max: max, palette: pal};
 // }
 // showPalette(min + '           0           ' + max, palettes.colorbrewer.RdBu[5]);
 
+Map.addLayer(droughtSens.select('Sens_NDVI_ante3mo_p10'), viz);
+// Map.addLayer(droughtSensPrime.select('Sens_Prime_NBR_p20'));
 
-// Map.addLayer(droughtSens.select('Sensitivity_NDVI_ante3_p5'), viz);
-// Map.addLayer(droughtSens.select('Sensitivity_NDVI_ante6_p5'));
-// Map.addLayer(droughtSens.select('Sensitivity_NDVI_ante12_p5'));
-// Map.addLayer(droughtSensPrime.select('Sens_Prime_NDVI_p5'));
+
 
 // Export -------------------------------------------------------
 var exp = {
   image: droughtSens,
-  description: 'drought-sensitivity-' + minyear + '-' + maxyear,
+  description: 'drought-sensitivity-MODIS-' + minyear + '-' + maxyear,
   folder: 'CFS-drought-sensitivity',
   region: geometry,
   scale: 250,
@@ -163,7 +158,7 @@ Export.image.toDrive(exp);
 
 var exp = {
   image: droughtSens,
-  description: 'drought-sensitivity-' + minyear + '-' + maxyear,
+  description: 'drought-sensitivity-MODIS-' + minyear + '-' + maxyear,
   assetId: 'CFS/drought-sensitivity-' + minyear + '-' + maxyear,
   region: geometry,
   scale: 250,
