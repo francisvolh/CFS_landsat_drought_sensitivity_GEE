0bd5a6ff9ea1f66cc0072936ffe0fa35a7ad197a
robit.alec <robit.alec@gmail.com>
2021-07-12 22:11:03 +0000

    Updated Explore-sensitivity-results.js (added 1 line, deleted 1 line).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -90,7 +90,7 @@ var band = 'Sens_NDVI_ante3mo_p10'
 Map.addLayer(sens_modis.select(band), viz, 'MODIS')
 Map.addLayer(sens_land.select(band), viz, 'Landsat')
 
-Map.addLayer(sens_modis.select(band).minus(sens_land.select(band)), null, 'dif')
+Map.addLayer(sens_modis.select(band).subtract(sens_land.select(band)), null, 'dif')
 
 Map.addLayer(lcmask, null, 'lc', false);
 
