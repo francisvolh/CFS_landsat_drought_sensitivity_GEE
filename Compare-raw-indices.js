3ada1a32dd836a876c60b1cc8640cc126115cd04
robit.alec <robit.alec@gmail.com>
2021-07-14 04:02:10 +0000

    Updated Compare-raw-indices.js (added 22 lines).

--- Compare-raw-indices.js
+++ Compare-raw-indices.js
@@ -69,6 +69,28 @@ var sensitivity = require('users/robitalec/CFS:modules/sensitivity.js');
 // Gena's palette functions
 var palettes = require('users/gena/packages:palettes');
 
+// Define a function to convert from degrees to radians.
+function radians(img) {
+  return img.toFloat().multiply(Math.PI).divide(180);
+}
+
+// Define a function to compute a hillshade from terrain data
+// for the given sun azimuth and elevation.
+function hillshade(az, ze, slope, aspect) {
+  // Convert angles to radians.
+  var azimuth = radians(ee.Image(az));
+  var zenith = radians(ee.Image(ze));
+  // Note that methods on images are needed to do the computation.
+  // i.e. JavaScript operators (e.g. +, -, /, *) do not work on images.
+  // The following implements:
+  // Hillshade = cos(Azimuth - Aspect) * sin(Slope) * sin(Zenith) +
+  //     cos(Zenith) * cos(Slope)
+  return azimuth.subtract(aspect).cos()
+    .multiply(slope.sin())
+    .multiply(zenith.sin())
+    .add(
+      zenith.cos().multiply(slope.cos()));
+}
 
 
 // Filter -----------------------------------------------------------
