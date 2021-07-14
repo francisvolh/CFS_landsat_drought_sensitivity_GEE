aa63fe558f346f168eda8e903a0be499bc5ac2aa
robit.alec <robit.alec@gmail.com>
2021-07-14 23:19:47 +0000

    Updated modules/landsat-prep.js (added 1 line, deleted 1 line).

--- modules/landsat-prep.js
+++ modules/landsat-prep.js
@@ -35,7 +35,7 @@ exports.maskClouds = function(image) {
   // If the cloud bit (5) is set and the cloud confidence (7) is high
   // or the cloud shadow bit is set (3), then it's a bad pixel.
   var cloud = qa.bitwiseAnd(1 << 5)
-                  .and(qa.bitwiseAnd(1 << 7))
+                  .and(qa.bitwiseAnd(1 << 6))
                   .or(qa.bitwiseAnd(1 << 3));
   // Remove edge pixels that don't occur in all bands
   var mask2 = image.mask().reduce(ee.Reducer.min());
