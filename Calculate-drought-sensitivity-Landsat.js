da8ec7a95a739a2ae4a0edd4ec12666fee94c6f7
robit.alec <robit.alec@gmail.com>
2021-07-15 20:06:36 +0000

    Updated Calculate-drought-sensitivity-Landsat.js (added 2 lines, deleted 1 line).

--- Calculate-drought-sensitivity-Landsat.js
+++ Calculate-drought-sensitivity-Landsat.js
@@ -156,10 +156,11 @@ var splits = sensitivity.splitDrought(veg, drought, antes, percentiles, indices)
 var means = splits.reduce(ee.Reducer.mean());
 
 
+
 // Drought sensitivity -------------------------------------------
 // SP,T,L = [ (baseline EVIP – drought EVIP,T,L) / baseline EVIP ] x 100
 var droughtSens = sensitivity.droughtSensitivity(means, antes, percentiles, indices);
-print(droughtSens)
+
 // Drought sensitivity prime (S’) = max across three antecedent periods
 // var droughtSensPrime = sensitivity.droughtSensivitityPrime(droughtSens, percentiles);
 
