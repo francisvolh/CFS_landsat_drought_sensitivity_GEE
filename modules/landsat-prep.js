6cb84a3ef615a16b555469253bf45811e62e6877
robit.alec <robit.alec@gmail.com>
2021-07-15 20:18:55 +0000

    Updated modules/landsat-prep.js (added 1 line, deleted 1 line).

--- modules/landsat-prep.js
+++ modules/landsat-prep.js
@@ -88,5 +88,5 @@ exports.rescale = function(img) {
     img.select('sr_cloud_qa'),
     img.select('pixel_qa'),
     img.select('radsat_qa')
-  ]).copyProperties(img);
+  ]).copyProperties(img).set({'system:time_start': img.date().millis()});
 };
