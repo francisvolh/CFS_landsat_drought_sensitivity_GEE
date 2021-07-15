35202a94265a46c02c96c35610e562389a982a8d
robit.alec <robit.alec@gmail.com>
2021-07-15 21:17:36 +0000

    Updated modules/modis-prep.js (added 6 lines, deleted 1 line).

--- modules/modis-prep.js
+++ modules/modis-prep.js
@@ -10,7 +10,12 @@ exports.rescale = function(img) {
 //float?
 
 
-var water = ee.Image("MODIS/MOD44W/MOD44W_005_2000_02_24").select('water_mask');
+// var water = ee.Image("MODIS/MOD44W/MOD44W_005_2000_02_24").select('water_mask');
+var water = ee.Image("JRC/GSW1_3/GlobalSurfaceWater")
+                    .select('occurrence')
+                    .gt(0.7)
+                    .unmask()
+                    .not();
 exports.maskWater = function(img) {
   return img.updateMask(water.not());
 };
