db6a787fad7bedc7d809b05cee0c69164e28f534
robit.alec <robit.alec@gmail.com>
2021-07-16 02:16:19 +0000

    Created archive/Get-scatter-plots.js.

---
+++ archive/Get-scatter-plots.js
@@ -0,0 +1,160 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #d63000 */
+    /* shown: false */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-141.0367481146711, 64.76633228247655],
+          [-141.0367481146711, 61.84752382750249],
+          [-136.3126270209211, 61.84752382750249],
+          [-136.3126270209211, 64.76633228247655]]], null, false),
+    geometry2 = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.MultiPoint(
+        [[-139.95916965557532, 63.70435832848431],
+         [-139.08026340557532, 63.53350092779306],
+         [-138.26178196026282, 63.49675483765842],
+         [-137.70147922588782, 63.63125949888033],
+         [-137.03131321026282, 63.86451363545733],
+         [-137.53119113995032, 64.17971408308071],
+         [-138.15191867901282, 64.1126417845022],
+         [-139.26703098370032, 63.85483311775371],
+         [-140.23382785870032, 63.99487483284026],
+         [-139.50323703838782, 64.25616993332066],
+         [-138.57489231182532, 64.30384766782944],
+         [-138.37713840557532, 63.76755836210998]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// Data -------------------------------------------------------------------------
+// Drought sensitivity 
+var sens_modis = ee.Image('users/robitalec/CFS/drought-sensitivity-1980-2019_v2');
+
+// Landsat
+var sens_land = ee.Image('users/robitalec/CFS/drought-sensitivity-Landsat-1980-2019');
+
+// Load CTEF regions
+var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
+// ctef = ctef.filter(ee.Filter.stringContains('ZONE_EN', 'Arctic').not());
+ctef = ctef.filter(ee.Filter.inList('REG_ID', ['CL13R02', 'CL13R03', 'CL13R04']));
+
+// Land cover mask function
+var lcmask = require('users/robitalec/CFS:modules/land-cover.js');
+
+
+
+// Functions ---------------------------------------------------------------------
+// Gena's functions
+var palettes = require('users/gena/packages:palettes');
+var text = require('users/gena/packages:text');
+var gallery = require('users/gena/packages:gallery');
+
+var pal = palettes.colorbrewer.RdBu[9].reverse();
+var min = -20; var max = 20;
+var viz = {min: min, max: max, palette: pal, opacity:0.7};
+
+function showPalette(name, palette) {
+  var image = ee.Image.pixelLonLat().select(0)
+    .clip(ee.Geometry.Rectangle({coords: [[min, 0], [max, 10]], geodesic: false}))
+    .visualize(viz);
+
+  print(name);
+  print(ui.Thumbnail(image));
+}
+// showPalette(min + '           0           ' + max, pal);
+
+
+// Define a function to convert from degrees to radians.
+function radians(img) {
+  return img.toFloat().multiply(Math.PI).divide(180);
+}
+
+// Define a function to compute a hillshade from terrain data
+// for the given sun azimuth and elevation.
+function hillshade(az, ze, slope, aspect) {
+  // Convert angles to radians.
+  var azimuth = radians(ee.Image(az));
+  var zenith = radians(ee.Image(ze));
+  // Note that methods on images are needed to do the computation.
+  // i.e. JavaScript operators (e.g. +, -, /, *) do not work on images.
+  // The following implements:
+  // Hillshade = cos(Azimuth - Aspect) * sin(Slope) * sin(Zenith) +
+  //     cos(Zenith) * cos(Slope)
+  return azimuth.subtract(aspect).cos()
+    .multiply(slope.sin())
+    .multiply(zenith.sin())
+    .add(
+      zenith.cos().multiply(slope.cos()));
+}
+
+function summary(img) {
+  return img.reduceRegion({
+    reducer: ee.Reducer.mean(),
+      // ee.Reducer.min().combine(
+      // ee.Reducer.max()).combine(
+      //   ee.Reducer.mean()),
+    geometry: ctef,
+    bestEffort: true
+  });
+}
+
+
+
+// Process ----------------------------------------------------------------------
+// Compute terrain meaasures from the SRTM DEM.
+var terrain = ee.Algorithms.Terrain(ee.Image("USGS/GMTED2010"));
+var slope = radians(terrain.select('slope'));
+var aspect = radians(terrain.select('aspect'));
+
+// azimuth, zenith
+var az = 120
+var ze = 60
+var hill = hillshade(az, ze, slope, aspect);
+
+// diff
+var dif = sens_modis.select(band).subtract(sens_land.select(band));
+
+// Filter -----------------------------------------------------------------------
+var toview = sens_land;
+
+// Set the percentile to view
+// either 5, 10, or 20
+var p = 10;
+
+// Filter the band names before map. Comment any of these out to just map all bands
+var selectBands = toview.bandNames()
+                        // .filter(ee.Filter.stringContains('item', 'p' + p))
+                        .filter(ee.Filter.stringContains('item', 'NDVI'))
+                        // .filter(ee.Filter.stringContains('item', 'ante3'))
+
+// Or select band
+var band = 'Sens_NDVI_ante6mo_p10';
+
+// As a collection
+var ascol = ee.ImageCollection.fromImages(toview.select(selectBands).bandNames().map(function(name) { 
+  return sens_modis.select([name]).set({"name": ee.String(name)
+  });
+}));
+
+
+// Viz --------------------------------------------------------------------------
+var vizgallery = {min: min, max: max, palette: pal, opacity:1};
+
+// Chart -
+
+
+// combine two images
+print(sens_modis, sens_land)
+var x = band + '-MODIS'
+var y = band + '-Landsat'
+var img = ee.Image([ee.ImageCollection(sens_modis.select(band)).median().rename(x),
+                   ee.ImageCollection(sens_land.select(band)).median().rename(y)])
+var values = img.sample({region: ee.FeatureCollection.randomPoints(geometry, 1e3, 42).geometry(), scale: 30})
+print(values)
+var chart = ui.Chart.feature.byFeature(values, x, y)
+  .setChartType('ScatterChart')
+  .setOptions({ pointSize: 2, pointColor: 'red', width: 300, height: 300, titleX: x, titleY: y})
+print(chart)
