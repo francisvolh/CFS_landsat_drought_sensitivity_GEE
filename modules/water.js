8a234d6907daabdbd946f2035d1ee0ff62800c15
robit.alec <robit.alec@gmail.com>
2021-07-28 16:30:05 +0000

    Created modules/water.js.

---
+++ modules/water.js
@@ -0,0 +1,9 @@
+// var water = ee.Image("MODIS/MOD44W/MOD44W_005_2000_02_24").select('water_mask');
+var water = ee.Image("JRC/GSW1_3/GlobalSurfaceWater")
+                    .select('occurrence')
+                    .gt(0.7)
+                    .unmask()
+                    .not();
+exports.maskWater = function(img) {
+  return img.updateMask(water.not());
+}; 
