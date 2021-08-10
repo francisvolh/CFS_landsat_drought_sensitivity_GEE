4d8a47ba67a430b74d7fd8a5b5b663be77bd27ed
robit.alec <robit.alec@gmail.com>
2021-08-10 17:41:56 +0000

    Updated Explore-sensitivity-results.js (added 30 lines).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -234,6 +234,36 @@ var columns = 4;
 var acrosssensors = gallery.draw(ee.ImageCollection(imagesRGB), geometry.bounds(), rows, columns);
 Map.addLayer(acrosssensors, null, 'gallery: across sensor', false);
 
+// Across index, within sensor gallery strip
+var comb = ee.Image([sens_modis.select(forindex)])
+var combcol = ee.ImageCollection.fromImages(comb.bandNames().map(function(name) { 
+  return comb.select([name]).set({"name": ee.String(name)})
+}));
+var imagesRGB = combcol.map(function(img) {
+  var label = text.draw(img.get('name'), geolabel, Map.getScale(), {
+      fontSize:32, textColor: '000000', outlineColor: 'ffffff', outlineWidth: 1, outlineOpacity: 0.6});
+  return img.visualize(vizgallery).blend(label);
+});
+
+var rows = 3;
+var columns = 1;
+var acrossindex = gallery.draw(ee.ImageCollection(imagesRGB), geometry.bounds(), rows, columns);
+Map.addLayer(acrossindex, null, 'gallery: MODIS across index', false);
+
+var comb = ee.Image([sens_land.select(forindex)])
+var combcol = ee.ImageCollection.fromImages(comb.bandNames().map(function(name) { 
+  return comb.select([name]).set({"name": ee.String(name)})
+}));
+var imagesRGB = combcol.map(function(img) {
+  var label = text.draw(img.get('name'), geolabel, Map.getScale(), {
+      fontSize:32, textColor: '000000', outlineColor: 'ffffff', outlineWidth: 1, outlineOpacity: 0.6});
+  return img.visualize(vizgallery).blend(label);
+});
+
+var rows = 3;
+var columns = 1;
+var acrossindex = gallery.draw(ee.ImageCollection(imagesRGB), geometry.bounds(), rows, columns);
+Map.addLayer(acrossindex, null, 'gallery: Landsat across index', false);
 
 
 // Add all bands separately
