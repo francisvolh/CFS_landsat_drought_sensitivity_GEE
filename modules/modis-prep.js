a24669b53c4c79099619dd574e11ea334eb4e847
robit.alec <robit.alec@gmail.com>
2021-08-10 21:56:29 +0000

    Updated modules/modis-prep.js (added 20 lines, deleted 1 line).

--- modules/modis-prep.js
+++ modules/modis-prep.js
@@ -7,4 +7,23 @@ exports.rescale = function(img) {
             .copyProperties(img)
             .set('system:time_start', img.get('system:time_start'));
 };
-//float?
+
+
+exports.calcIndices = function(img) {
+  return ee.Image([
+    img.expression('(nir - red) / (nir + red)',
+                   {red: img.select('B3'),
+                    nir: img.select('B4')})
+       .rename('NDVI'),
+     img.expression('(nir - swir2) / (nir + swir2)',
+                   {nir: img.select('B4'),
+                    swir2: img.select('B7')})
+       .rename('NBR'),
+     img.expression('2.5 * ((nir - red) / (nir + 6 * red - 7.5 * blue + 1))',
+                   {blue: img.select('B1'),
+                    red: img.select('B3'),
+                    nir: img.select('B4')})
+       .rename('EVI'),
+     img.select('pixel_qa')
+  ]).copyProperties(img).set({'system:time_start': img.date().millis()});
+};
