8e41dc5a040efd29c88bc47c3e1ecc5c43585422
robit.alec <robit.alec@gmail.com>
2021-08-10 22:06:49 +0000

    Updated modules/modis-prep.js (added 1 line, deleted 12 lines).

--- modules/modis-prep.js
+++ modules/modis-prep.js
@@ -1,16 +1,5 @@
 // MODIS prep
 
- 
-exports.rescale = function(img) {
-  return img.multiply(0.0001)
-            .float()
-            .copyProperties(img)
-            .set('system:time_start', img.get('system:time_start'));
-};
-
-
-// MODIS prep
-
 
 exports.rescale = function(img) {
   return img.multiply(0.0001)
