4e6f0fd4191d742d4db0e64baf20c840bfa08e6a
robit.alec <robit.alec@gmail.com>
2021-08-17 15:52:54 +0000

    Updated modules/modis-prep.js (added 18 lines, deleted 2 lines).

--- modules/modis-prep.js
+++ modules/modis-prep.js
@@ -20,7 +20,7 @@ exports.maskWater = function(img) {
 
 
 // For MOD09A1
-// Name 	        Description 	                  (band) 	Wavelength    Scale 
+// Name 	        Description 	                  (band) 	Wavelength    Scale
 // sur_refl_b01 	Surface reflectance for band 1  Red 		620-670nm   	0.0001
 // sur_refl_b02 	Surface reflectance for band 2 	NIR 		841-876nm   	0.0001
 // sur_refl_b03 	Surface reflectance for band 3 	Blue 		459-479nm   	0.0001
@@ -28,7 +28,7 @@ exports.maskWater = function(img) {
 // sur_refl_b05 	Surface reflectance for band 5 	NIR  		1230-1250nm 	0.0001
 // sur_refl_b06 	Surface reflectance for band 6 	SWIR 		1628-1652nm 	0.0001
 // sur_refl_b07 	Surface reflectance for band 7 	SWIR 		2105-2155nm 	0.0001
-// QA 	Surface reflectance 500m band quality control flags 				
+// QA 	Surface reflectance 500m band quality control flags
 exports.calcIndices = function(img) {
   return ee.Image([
     img.expression('(nir - red) / (nir + red)',
@@ -46,4 +46,20 @@ exports.calcIndices = function(img) {
        .rename('EVI'),
      img.select('pixel_qa')
   ]).copyProperties(img).set({'system:time_start': img.date().millis()});
+};
+
+
+exports.rescale = function(img) {
+  return ee.Image([
+    img.select('sur_refl_b01').multiply(0.0001),
+    img.select('sur_refl_b02').multiply(0.0001),
+    img.select('sur_refl_b03').multiply(0.0001),
+    img.select('sur_refl_b04').multiply(0.0001),
+    img.select('sur_refl_b05').multiply(0.0001),
+    img.select('sur_refl_b06').multiply(0.0001),
+    img.select('sur_refl_b07').multiply(0.0001),
+    img.select('QA'),
+    img.select('StateQA')
+  ]).copyProperties(img)
+    .set({'system:time_start': img.get('system:time_start')});
 };
