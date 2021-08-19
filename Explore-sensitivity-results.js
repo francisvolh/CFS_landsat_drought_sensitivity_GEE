acb359ac1bf20efe21be0bc45b54825f28308ae4
robit.alec <robit.alec@gmail.com>
2021-08-19 16:26:58 +0000

    Updated Explore-sensitivity-results.js (added 2 lines, deleted 3 lines).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -316,9 +316,8 @@ print(chart)
 
 
 // delete me
-print('Compare MOD09Q1 with old MOD13Q1')
-print(sens_modis_old.bandNames())
 var index = 'NDVI'
+print('Compare MOD09Q1 with old MOD13Q1: ' + index)
 var band12 = 'Sens_' + index + '_ante12mo_p' + 10
 var x = band12 + '-MOD09Q1'
 var y = band12 + '-MODIS_old'
