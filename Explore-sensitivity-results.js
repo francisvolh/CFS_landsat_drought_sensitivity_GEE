8c56a32da98e3478ae5cde0096d511e5f2c57e4f
robit.alec <robit.alec@gmail.com>
2021-08-19 16:20:02 +0000

    Updated Explore-sensitivity-results.js (added 29 lines, deleted 4 lines).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -278,10 +278,35 @@ Map.addLayer(acrossindex, null, 'gallery: Landsat across index', false);
 // }
 
 // Charts -----------------------------------------------------------------------
-var x = band + '-MOD09Q1'
-var y = band + '-Landsat'
-var img = ee.Image([ee.ImageCollection(sens_modis.select(band)).median().rename(x),
-                   ee.ImageCollection(sens_land.select(band)).median().rename(y)])
+var index = 'NBR'
+var p = 10
+var band3 = 'Sens_' + index + '_ante3mo_p' + 10
+var band6 = 'Sens_' + index + '_ante6mo_p' + 10
+var band12 = 'Sens_' + index + '_ante12mo_p' + 10
+var x = band3 + '-MOD09Q1'
+var y = band3 + '-Landsat'
+var img = ee.Image([ee.ImageCollection(sens_modis.select(band3)).median().rename(x),
+                   ee.ImageCollection(sens_land.select(band3)).median().rename(y)])
+var values = img.sample({region: ee.FeatureCollection.randomPoints(geometry, 1e3, 42).geometry(), scale: 30})
+var chart = ui.Chart.feature.byFeature(values, x, y)
+  .setChartType('ScatterChart')
+  .setOptions({titleX: x, titleY: y})
+print(chart)
+
+var x = band6 + '-MOD09Q1'
+var y = band6 + '-Landsat'
+var img = ee.Image([ee.ImageCollection(sens_modis.select(band6)).median().rename(x),
+                   ee.ImageCollection(sens_land.select(band6)).median().rename(y)])
+var values = img.sample({region: ee.FeatureCollection.randomPoints(geometry, 1e3, 42).geometry(), scale: 30})
+var chart = ui.Chart.feature.byFeature(values, x, y)
+  .setChartType('ScatterChart')
+  .setOptions({titleX: x, titleY: y})
+print(chart)
+
+var x = band12 + '-MOD09Q1'
+var y = band12 + '-Landsat'
+var img = ee.Image([ee.ImageCollection(sens_modis.select(band12)).median().rename(x),
+                   ee.ImageCollection(sens_land.select(band12)).median().rename(y)])
 var values = img.sample({region: ee.FeatureCollection.randomPoints(geometry, 1e3, 42).geometry(), scale: 30})
 var chart = ui.Chart.feature.byFeature(values, x, y)
   .setChartType('ScatterChart')
