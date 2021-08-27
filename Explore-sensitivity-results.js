24b445f9521cd963bbc2c6cd0c03c550ac8c0a8a
robit.alec <robit.alec@gmail.com>
2021-08-27 16:05:48 +0000

    Updated Explore-sensitivity-results.js (added 2 lines, deleted 2 lines).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -57,11 +57,11 @@ print('Region selected: ' + region)
 
 // Data -------------------------------------------------------------------------
 // Drought sensitivity
-var sens_modis_old = ee.Image('users/robitalec/CFS/drought-sensitivity-MODIS-' + region);
+var sens_modis_old = ee.Image('users/robitalec/CFS/drought-sensitivity-MODIS-' + '2000_2012-' + region);
 var sens_modis = ee.Image('users/robitalec/CFS/drought-sensitivity-MOD09Q1-' + region);
 
 // Landsat
-var sens_land = ee.Image('users/robitalec/CFS/drought-sensitivity-Landsat-' + region);
+var sens_land = ee.Image('users/robitalec/CFS/drought-sensitivity-Landsat-' + '2000_2012-' + region);
 
 // Load CTEF regions
 var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
