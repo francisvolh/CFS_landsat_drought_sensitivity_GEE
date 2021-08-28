a0dbaa64069ecf1a7863414de3b45b6f4f6df6b9
robit.alec <robit.alec@gmail.com>
2021-08-28 00:21:15 +0000

    Updated modules/drought.js (added 1 line).

--- modules/drought.js
+++ modules/drought.js
@@ -47,6 +47,7 @@ var drought = baseline.ltPercentile(means, percentiles);
 // Drop since there's no complete antecedent 12 period (1980) or 5 yr (1980-1985)
 drought = drought.filter(ee.Filter.gt('year', 1980));
 
+
 exports.baselineCMI = function() {
   return drought
 }
