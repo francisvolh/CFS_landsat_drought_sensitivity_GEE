e134ed676ed99af9dc2dffdd6941dd577d562e49
robit.alec <robit.alec@gmail.com>
2021-08-30 17:02:30 +0000

    Updated Explore-sensitivity-results.js (added 2 lines, deleted 3 lines).

--- Explore-sensitivity-results.js
+++ Explore-sensitivity-results.js
@@ -211,7 +211,6 @@ Map.addLayer(ee.Image('users/robitalec/CFS/land-cover-mask'), null, 'lc', false)
 
 // Gallery ----------------------------------------------------------------------
 var selectBands = toview.bandNames()
-                        .filter(ee.Filter.stringContains('item', 'p1').not())
 
 // Within Landsat gallery strip
 var toview = sens_land
@@ -224,7 +223,7 @@ var imagesRGB = ascol.map(function(img) {
       fontSize:32, textColor: '000000', outlineColor: 'ffffff', outlineWidth: 1, outlineOpacity: 0.6});
   return img.visualize(vizgallery).blend(label);
 });
-
+print(toview.select(selectBands))
 var rows = 3;
 var columns = 3;
 var wisensor = gallery.draw(ee.ImageCollection(imagesRGB), geometry.bounds(), rows, columns);
