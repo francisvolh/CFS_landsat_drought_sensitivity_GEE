6b3cea97f6b4136e27392294e07c1c9db1e1aa40
robit.alec <robit.alec@gmail.com>
2021-08-30 21:49:05 +0000

    Updated Compare-raw-indices-again.js (added 2 lines, deleted 2 lines).

--- Compare-raw-indices-again.js
+++ Compare-raw-indices-again.js
@@ -158,8 +158,8 @@ vegM = vegM.select(['NDVI_mean', 'EVI_mean', 'NBR_mean'], ['NDVI', 'EVI', 'NBR']
 
 
 // Compare ---------------------------------------------------------
-var yr = 2007;
-var n = 1e3;
+var yr = 2003;
+var n = 1e4;
 
 var index = 'NDVI';
 var x = index + ' MODIS ' + yr;
