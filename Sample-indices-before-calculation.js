f14c36fe22324f5047030edaed40927c04e557e8
robit.alec <robit.alec@gmail.com>
2021-09-10 18:31:28 +0000

    Created Sample-indices-before-calculation.js.

---
+++ Sample-indices-before-calculation.js
@@ -0,0 +1,128 @@
+// === Calculate Drought Sensitivity ===
+// --- Landsat ---
+// Alec L. Robitaille
+
+
+var region = 'Yukon';
+print('Region set: ' + region);
+
+
+
+// Data -------------------------------------------------------------
+// CTEF regions
+var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
+
+
+var l5 = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR");
+var l7 = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR");
+
+
+// Variables --------------------------------------------------------
+// Set min max year for daymet
+var minyear = 1980;
+var maxyear = 2019;
+
+// Set list of years and months
+var months = ee.List.sequence(1, 12);
+var years = ee.List.sequence(minyear, maxyear);
+
+// Min/max years Landsat 5
+var minyearl5 = 1985;
+var maxyearl5 = 2012;
+var yearsl5 = ee.List.sequence(minyearl5, maxyearl5);
+
+// Min/max years Landsat 7
+var minyearl7 = 1999;
+var maxyearl7 = 2003;
+var yearsl7 = ee.List.sequence(minyearl7, maxyearl7);
+
+// Set percentiles to use
+var percentiles = [1, 5, 10, 20];
+
+// Set antecedent periods
+var antes = [3, 6, 12];
+
+// Set indices
+var indices = ['NDVI', 'NBR', 'EVI'];
+
+
+
+// Modules ----------------------------------------------------------
+// Load modules of functions
+
+// Landsat prep functions
+var landsatprep = require('users/robitalec/CFS:modules/landsat-prep.js');
+
+// Land cover mask function
+var lcmask = require('users/robitalec/CFS:modules/land-cover.js');
+
+// Fire functions
+var fire = require('users/robitalec/CFS:modules/fire.js');
+
+// Water functions
+var water = require('users/robitalec/CFS:modules/water.js');
+
+// Sensitivity
+var sensitivity = require('users/robitalec/CFS:modules/sensitivity.js');
+
+// Gena's palette functions
+var palettes = require('users/gena/packages:palettes');
+
+// Drought module
+var droughtModule = require('users/robitalec/CFS:modules/drought.js');
+
+
+
+// Filter -----------------------------------------------------------
+// ctef = ctef.filter(ee.Filter.stringContains('ZONE_EN', 'Arctic').not());
+ctef = ctef.filter(ee.Filter.inList('REG_ID', ['CL13R02', 'CL13R03', 'CL13R04']));
+
+if (region == 'Yukon') {
+  var geo = ctef;
+} else if (region == 'Alberta') {
+  var geo = alberta;
+}
+
+// Landsat 5
+l5 = l5
+  .filterBounds(geo)
+  .filter(ee.Filter.calendarRange(minyearl5, maxyearl5, 'year'))
+  .filter(ee.Filter.calendarRange(7, 7, 'month'));
+
+// Landsat 7
+l7 = l7
+  .filterBounds(geo)
+  .filter(ee.Filter.calendarRange(minyearl7, maxyearl7, 'year'))
+  .filter(ee.Filter.calendarRange(7, 7, 'month'));
+
+
+
+// Daymet -----------------------------------------------------------
+var drought = droughtModule.baselineCMI();
+
+
+
+// Landsat ----------------------------------------------------------
+// Merge L5 and L7
+var veg = l5.merge(l7)
+  // *** CHECKING LANDSAT v MODIS ***
+  .filter(ee.Filter.calendarRange(2000, 2012, 'year'))
+
+
+// Filter within min/max year and for July
+// Mask clouds, fires, land cover and calculate indices
+veg = veg
+  .map(landsatprep.rescale)
+  .map(landsatprep.setYear)
+  .map(landsatprep.calcIndices)
+  .map(landsatprep.maskClouds)
+  .map(water.maskWater)
+  .map(fire.maskFires)
+  .map(lcmask.maskLc);
+
+// Aggregate Landsat yearly, rename _mean bands
+veg = landsatprep.aggregateY(veg);
+veg = veg.select(['NDVI_mean', 'EVI_mean', 'NBR_mean'], ['NDVI', 'EVI', 'NBR']);
+
+// Split vegetation indices into drought/non-drought pixels
+var splits = sensitivity.splitDrought(veg, drought, antes, percentiles, indices);
