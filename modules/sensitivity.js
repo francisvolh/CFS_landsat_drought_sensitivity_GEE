24bfd371a13766471512cc44fc29b2ffd012940e
robit.alec <robit.alec@gmail.com>
2021-09-14 16:27:29 +0000

    Updated modules/sensitivity.js (added 1 line, deleted 1 line).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -56,7 +56,7 @@ exports.droughtSensitivity = function(means, antes, percentiles, indices) {
           var droughtband = id + '_drought' + '_mean';
           var sensband = 'Sens_' + id;
 
-          return means.expression('((baseline - drought)', {
+          return means.expression('(baseline - drought)', {
           // return means.expression('((baseline - drought) / baseline) * 100', {  
                                 baseline: means.select(baseband),
                                 drought: means.select(droughtband)
