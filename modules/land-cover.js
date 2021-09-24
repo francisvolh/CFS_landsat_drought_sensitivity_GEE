2a0fef0bace959f3a9c667689f74c55b5c4a35aa
robit.alec <robit.alec@gmail.com>
2021-09-24 00:26:58 +0000

    Updated modules/land-cover.js (added 1 line, deleted 1 line).

--- modules/land-cover.js
+++ modules/land-cover.js
@@ -31,7 +31,7 @@ var lc = ee.Image("ESA/GLOBCOVER_L4_200901_200912_V2_3");
 // Set land cover mask, only retaining if not one of
 var lcmask = lc.updateMask(
     lc.expression('lc != 11 && lc != 14 && lc != 20 && lc != 30 && ' +
-                  'lc != 120 && lc != 140 && lc != 150 && lc != 160 && lc != 170 && lc != 180 && lc != 190 && ' +
+                  'lc != 120 && lc != 140 && lc != 150 && lc != 10 && lc != 170 && lc != 180 && lc != 190 && ' +
                   'lc != 200 && lc != 210 && lc != 220 && lc != 230',
                   {lc: lc.select('landcover')}))
       .select('landcover');
