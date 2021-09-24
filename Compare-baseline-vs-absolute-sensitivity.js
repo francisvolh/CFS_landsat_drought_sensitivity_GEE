5dbc1426667ef60e7323b05252fecf0a088e3e26
robit.alec <robit.alec@gmail.com>
2021-09-24 00:59:14 +0000

    Updated Compare-baseline-vs-absolute-sensitivity.js (added 15 lines, deleted 15 lines).

--- Compare-baseline-vs-absolute-sensitivity.js
+++ Compare-baseline-vs-absolute-sensitivity.js
@@ -182,19 +182,19 @@ Map.addLayer(acrosssensors, null, 'gallery: Landsat relative (top) Landsat absol
 
 
 // Charts -----------------------------------------------------------------------
-var index = 'NBR'
-print('Compare Landsat and MODIS: ' + index)
-var p = 10
-var band3 = 'Sens_' + index + '_ante3mo_p' + p
-var band6 = 'Sens_' + index + '_ante6mo_p' + p
-var band12 = 'Sens_' + index + '_ante12mo_p' + p
-var x = band3 + '-MOD09Q1'
-var y = band3 + '-Landsat'
-var img = ee.Image([sens_modis.select(band3).rename(x),
-                    sens_land.select(band3).rename(y)])
-var values = img.sample({region: ee.FeatureCollection.randomPoints(geometry, 1e3, 42).geometry(), scale: 30})
-var chart = ui.Chart.feature.byFeature(values, x, y)
-  .setChartType('ScatterChart')
-  .setOptions({titleX: x, titleY: y, trendlines: {0:{}}})
-print(chart)
+// var index = 'NBR'
+// print('Compare Landsat and MODIS: ' + index)
+// var p = 10
+// var band3 = 'Sens_' + index + '_ante3mo_p' + p
+// var band6 = 'Sens_' + index + '_ante6mo_p' + p
+// var band12 = 'Sens_' + index + '_ante12mo_p' + p
+// var x = band3 + '-MOD09Q1'
+// var y = band3 + '-Landsat'
+// var img = ee.Image([sens_modis.select(band3).rename(x),
+//                     sens_land.select(band3).rename(y)])
+// var values = img.sample({region: ee.FeatureCollection.randomPoints(geometry, 1e3, 42).geometry(), scale: 30})
+// var chart = ui.Chart.feature.byFeature(values, x, y)
+//   .setChartType('ScatterChart')
+//   .setOptions({titleX: x, titleY: y, trendlines: {0:{}}})
+// print(chart)
 
