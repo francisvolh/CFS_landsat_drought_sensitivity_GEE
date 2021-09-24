a418b5ee1d17764d73a7befec421d259e834c979
robit.alec <robit.alec@gmail.com>
2021-09-24 01:04:52 +0000

    Updated Compare-baseline-vs-absolute-sensitivity.js (added 1 line, deleted 1 line).

--- Compare-baseline-vs-absolute-sensitivity.js
+++ Compare-baseline-vs-absolute-sensitivity.js
@@ -128,7 +128,7 @@ Map.addLayer(ee.Image(1), {palette:'747474'}, 'constant', false);
 Map.addLayer(ctef, null, 'ctef', false);
 
 // MODIS and Landsat, selected band
-Map.addLayer(sens_modis.select(band), viz, 'MODIS ' + band, false);
+Map.addLayer(sens_modis_absolute.select(band), viz, 'MODIS ' + band, false);
 Map.addLayer(sens_land.select(band), viz, 'Landsat ' + band, false);
 
 // Processed layers:
