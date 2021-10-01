e40b4ef2631d94da3a14c47e09717c406002ba3c
robit.alec <robit.alec@gmail.com>
2021-10-01 19:06:01 +0000

    Updated Sample-all-steps.js (added 6 lines, deleted 4 lines).

--- Sample-all-steps.js
+++ Sample-all-steps.js
@@ -126,7 +126,7 @@ var veg = l5.merge(l7)
 
 // Filter within min/max year and for July
 // Mask clouds, fires, land cover and calculate indices
-var veg_masked = veg
+veg = veg
   .map(landsatprep.rescale)
   .map(landsatprep.setYear)
   .map(landsatprep.calcIndices)
@@ -136,7 +136,7 @@ var veg_masked = veg
   .map(lcmask.maskLc);
 
 // Aggregate Landsat yearly, rename _mean bandsveg_years = landsatprep.aggregateY(veg_masked);
-var veg_years = veg_masked.select(['NDVI_mean', 'EVI_mean', 'NBR_mean'], ['NDVI', 'EVI', 'NBR']);
+var veg_years = veg.select(['NDVI_mean', 'EVI_mean', 'NBR_mean'], ['NDVI', 'EVI', 'NBR']);
 
 // Split vegetation indices into drought/non-drought pixels
 var splits = sensitivity.splitDrought(veg_years, drought, antes, percentiles, indices);
@@ -166,8 +166,10 @@ var points = ctef.map(function(ft) {
               });
 }).flatten();
 
-print(veg_masked.limit(2))
-print(veg_masked.limit(2).select(['NDVI', 'NBR', 'EVI'], ['raw_july_NDVI', 'raw_july_NBR', 'raw_july_EVI']))
+veg_masked = veg_masked.select(['NDVI', 'NBR', 'EVI'], ['raw_july_NDVI', 'raw_july_NBR', 'raw_july_EVI']);
+
+print(veg_years.limit(2))
+print(veg_years.limit(2).select(['NDVI', 'NBR', 'EVI'], ['agg_july_NDVI', 'agg_july_NBR', 'agg_july_EVI']))
 // veg_years
 // splits
 // means
