ed05630420f9b05f347ad43197c95cf008d39ac0
robit.alec <robit.alec@gmail.com>
2021-10-01 18:36:48 +0000

    Created Sample-all-steps.js.

---
+++ Sample-all-steps.js
@@ -0,0 +1,197 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = /* color: #d63000 */ee.Geometry.MultiPoint(
+        [[-120.01339170395872, 54.82097540279146],
+         [-116.94820615708372, 56.69970979965382],
+         [-118.67246694131055, 56.154441684771434],
+         [-136.91728237631563, 60.982984401796536],
+         [-140.69657925131563, 62.12368198955072],
+         [-140.76982845442157, 63.46615402037919],
+         [-137.26518978254657, 63.15528203910088],
+         [-137.78154720442157, 62.58922824931989]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+// === Sample all steps ===
+// --- Landsat ---
+// Alec L. Robitaille
+
+
+
+
+
+// Data -------------------------------------------------------------
+var geometry = /* color: #d63000 */ee.Geometry.MultiPoint(
+        [[-120.01339170395872, 54.82097540279146],
+         [-116.94820615708372, 56.69970979965382],
+         [-118.67246694131055, 56.154441684771434],
+         [-136.91728237631563, 60.982984401796536],
+         [-140.69657925131563, 62.12368198955072],
+         [-140.76982845442157, 63.46615402037919],
+         [-137.26518978254657, 63.15528203910088],
+         [-137.78154720442157, 62.58922824931989]]);
+
+// CTEF regions
+var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions')
+  .filterBounds(geometry);
+Map.addLayer(ctef)
+
+
+var l5 = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR");
+var l7 = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR");
+
+
+// Variables --------------------------------------------------------
+// Set min max year for daymet
+var minyear = 1980;
+var maxyear = 2019;
+
+// Set list of years and months
+var months = ee.List.sequence(1, 12);
+var years = ee.List.sequence(minyear, maxyear);
+
+// Min/max years Landsat 5
+var minyearl5 = 1985;
+var maxyearl5 = 2012;
+var yearsl5 = ee.List.sequence(minyearl5, maxyearl5);
+
+// Min/max years Landsat 7
+var minyearl7 = 1999;
+var maxyearl7 = 2003;
+var yearsl7 = ee.List.sequence(minyearl7, maxyearl7);
+
+// Set percentiles to use
+var percentiles = [1, 5, 10, 20];
+
+// Set antecedent periods
+var antes = [3, 6, 12];
+
+// Set indices
+var indices = ['NDVI', 'NBR', 'EVI'];
+
+
+
+// Modules ----------------------------------------------------------
+// Load modules of functions
+
+// Landsat prep functions
+var landsatprep = require('users/robitalec/CFS:modules/landsat-prep.js');
+
+// Land cover mask function
+var lcmask = require('users/robitalec/CFS:modules/land-cover.js');
+
+// Fire functions
+var fire = require('users/robitalec/CFS:modules/fire.js');
+
+// Water functions
+var water = require('users/robitalec/CFS:modules/water.js');
+
+// Sensitivity
+var sensitivity = require('users/robitalec/CFS:modules/sensitivity.js');
+
+// Gena's palette functions
+var palettes = require('users/gena/packages:palettes');
+
+// Drought module
+var droughtModule = require('users/robitalec/CFS:modules/drought.js');
+
+
+
+// Filter -----------------------------------------------------------
+var geo = ctef
+
+// Landsat 5
+l5 = l5
+  .filterBounds(geo)
+  .filter(ee.Filter.calendarRange(minyearl5, maxyearl5, 'year'))
+  .filter(ee.Filter.calendarRange(7, 7, 'month'));
+
+// Landsat 7
+l7 = l7
+  .filterBounds(geo)
+  .filter(ee.Filter.calendarRange(minyearl7, maxyearl7, 'year'))
+  .filter(ee.Filter.calendarRange(7, 7, 'month'));
+
+
+
+// Daymet -----------------------------------------------------------
+var drought = droughtModule.baselineCMI();
+
+
+
+// Landsat ----------------------------------------------------------
+// Merge L5 and L7
+var veg = l5.merge(l7)
+  // *** CHECKING LANDSAT v MODIS ***
+  .filter(ee.Filter.calendarRange(2000, 2012, 'year'))
+
+
+// Filter within min/max year and for July
+// Mask clouds, fires, land cover and calculate indices
+veg = veg
+  .map(landsatprep.rescale)
+  .map(landsatprep.setYear)
+  .map(landsatprep.calcIndices)
+  .map(landsatprep.maskClouds)
+  .map(water.maskWater)
+  .map(fire.maskFires)
+  .map(lcmask.maskLc);
+
+// Aggregate Landsat yearly, rename _mean bands
+veg = landsatprep.aggregateY(veg);
+veg = veg.select(['NDVI_mean', 'EVI_mean', 'NBR_mean'], ['NDVI', 'EVI', 'NBR']);
+
+// Split vegetation indices into drought/non-drought pixels
+var splits = sensitivity.splitDrought(veg, drought, antes, percentiles, indices);
+
+// Reduce yearly measures to means of all years
+var means = splits.reduce(ee.Reducer.mean());
+
+
+
+// Drought sensitivity -------------------------------------------
+// SP,T,L = [ (baseline EVIP – drought EVIP,T,L) / baseline EVIP ] x 100
+var droughtSens = sensitivity.droughtSensitivity(means, antes, percentiles, indices);
+
+// Drought sensitivity prime (S’) = max across three antecedent periods
+// var droughtSensPrime = sensitivity.droughtSensivitityPrime(droughtSens, percentiles);
+
+
+
+// Map ------------------------------------------------------------
+var pal = palettes.colorbrewer.RdBu[9];
+var min = -50; var max = 50;
+var viz = {min: min, max: max, palette: pal};
+
+// function showPalette(name, palette) {
+//   var image = ee.Image.pixelLonLat().select(0)
+//     .clip(ee.Geometry.Rectangle({ coords: [[0, 0], [100, 10]], geodesic: false }))
+//     .visualize({ min: 0, max: 100, palette: palette });
+
+//   print(name);
+//   print(ui.Thumbnail(image));
+// }
+// showPalette(min + '           0           ' + max, palettes.colorbrewer.RdBu[5]);
+
+// Map.addLayer(droughtSens.select('Sens_EVI_ante12mo_p10'), viz);
+// Map.addLayer(droughtSensPrime.select('Sens_Prime_NBR_p20'));
+
+
+
+// Export -------------------------------------------------------
+var exp = {
+  image: droughtSens,
+  description: 'drought-sensitivity-Landsat-' + '2000_2012-' + region + '_absolute',
+  folder: 'CFS-drought-sensitivity-Landsat-' + '2000_2012-' + region + '_absolute',
+  region: geo,
+  scale: 250,
+  maxPixels: 1e9
+};
+// Export.image.toDrive(exp);
+
+var exp = {
+  image: droughtSens,
+  description: 'drought-sensitivity-Landsat-' + '2000_2012-2-' + region + '_absolute',
+  assetId: 'CFS/drought-sensitivity-Landsat-' + '2000_2012-2-' + region + '_absolute',
+  region: geo,
+  scale: 30,
+  maxPixels: 1e9
+};
+Export.image.toAsset(exp);
