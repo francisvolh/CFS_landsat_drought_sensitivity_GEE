244b3b1ad2b025fe6f3ea8879ab41e61a518e482
robit.alec <robit.alec@gmail.com>
2021-10-01 22:07:51 +0000

    Updated Sample-all-steps.js (added 12 lines, deleted 5 lines).

--- Sample-all-steps.js
+++ Sample-all-steps.js
@@ -194,11 +194,18 @@ var points = ctef.map(function(ft) {
               });
 }).flatten();
 
-var forindex = toview.bandNames()
-                     .filter(ee.Filter.stringContains('item', 'p15'))
-                     .filter(ee.Filter.or(ee.Filter.stringContains('item', 'ante12mo'),
-                                          ee.Filter.stringContains('item', 'ante3mo')));
-
+var means_names = means.bandNames()
+                       .filter(ee.Filter.stringContains('item', 'p15'))
+                       .filter(ee.Filter.or(ee.Filter.stringContains('item', 'ante12mo'),
+                                            ee.Filter.stringContains('item', 'ante3mo')));
+                                            
+var drought_names = means.bandNames()
+                         .filter(ee.Filter.stringContains('item', 'p15'))
+                         .filter(ee.Filter.or(ee.Filter.stringContains('item', 'ante12mo'),
+                                              ee.Filter.stringContains('item', 'ante3mo')));
+                                            
+print(means_names)
+print(drought_names)
 
 // TODO filter them here
 // TODO append modis band names
