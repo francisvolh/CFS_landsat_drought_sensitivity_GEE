21c7177484378a84c009f42002ee3cc83a698cee
robit.alec <robit.alec@gmail.com>
2021-10-01 19:16:21 +0000

    Updated Sample-all-steps.js (added 3 lines, deleted 3 lines).

--- Sample-all-steps.js
+++ Sample-all-steps.js
@@ -168,11 +168,11 @@ var points = ctef.map(function(ft) {
 }).flatten();
 
 veg_masked = veg_masked.select(['NDVI', 'NBR', 'EVI'], ['raw_july_NDVI', 'raw_july_NBR', 'raw_july_EVI']);
-print(veg_masked.length())
+print(veg_masked.size())
 
 veg_years = veg_years.select(['NDVI', 'NBR', 'EVI'], ['agg_july_NDVI', 'agg_july_NBR', 'agg_july_EVI'])
-print(veg_years.length())
-print(veg_years.limit(2).select(['NDVI', 'NBR', 'EVI'], ['agg_july_NDVI', 'agg_july_NBR', 'agg_july_EVI']))
+print(veg_years.size())
+print(veg_years.limit(2))
 // veg_years
 // splits
 // means
