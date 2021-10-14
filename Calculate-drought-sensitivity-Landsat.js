0b7979bff257bc5bb108d2487c5a5862f16c25b2
robit.alec <robit.alec@gmail.com>
2021-10-14 15:37:03 +0000

    Updated Calculate-drought-sensitivity-Landsat.js (added 2 lines, deleted 2 lines).

--- Calculate-drought-sensitivity-Landsat.js
+++ Calculate-drought-sensitivity-Landsat.js
@@ -27,8 +27,8 @@ print('Region set: ' + region);
 var ctef = ee.FeatureCollection('users/robitalec/CFS/CTEF_Ecoregions');
 
 
-var l5 = ee.ImageCollection("LANDSAT/LT05/C01/T1_SR");
-var l7 = ee.ImageCollection("LANDSAT/LE07/C01/T1_SR");
+var l5 = ee.ImageCollection('LANDSAT/LT05/C02/T1_L2');
+var l7 = ee.ImageCollection('LANDSAT/LE07/C02/T1_L2');
 
 
 // Variables --------------------------------------------------------
