eb8a1c270bb652367ccdad7213b7bf2137dd030e
robit.alec <robit.alec@gmail.com>
2021-11-11 16:13:27 +0000

    Updated modules/drought.js (added 1 line, deleted 1 line).

--- modules/drought.js
+++ modules/drought.js
@@ -45,7 +45,7 @@ var means = baseline.antecedentMeans(cmi, 'CMI', years);
 exports.baselineCMI = function(percentiles) {
     // Compare antecedent means to percentiles. Eg. mean CMI for ante 3 year 2011 vs full period 10%
   var drought = baseline.ltPercentile(means, percentiles);
-  
+
   // Drop since there's no complete antecedent 12 period (1980) or 5 yr (1980-1985)
   drought = drought.filter(ee.Filter.gt('year', 1980));
   return drought;
