dd546d00e4c84b723fdd72749fd8470fb969c95b
robit.alec <robit.alec@gmail.com>
2021-11-11 16:03:23 +0000

    Updated modules/landsat-prep.js (deleted 10 lines).

--- modules/landsat-prep.js
+++ modules/landsat-prep.js
@@ -74,13 +74,3 @@ exports.aggregateY = function(images) {
     })
   );
 };
-
-
-
-// Applies scaling factors. From EE docs. 
-exports.rescale = function(image) {
-  var opticalBands = image.select('SR_B.').multiply(0.0000275).add(-0.2);
-  var thermalBand = image.select('ST_B6').multiply(0.00341802).add(149.0);
-  return image.addBands(opticalBands, null, true)
-              .addBands(thermalBand, null, true);
-};
