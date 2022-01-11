f4a608a71eb022c87abee4d575c7ca2ba2338440
robit.alec <robit.alec@gmail.com>
2022-01-11 03:50:01 +0000

    Updated modules/land-cover.js (added 1 line, deleted 1 line).

--- modules/land-cover.js
+++ modules/land-cover.js
@@ -26,7 +26,7 @@ var maskLandCover = function(img) {
     img.expression('lc == 0 && lc != 20 && lc != 31 && lc != 32 && ' +
                   'lc != 33 && lc != 40 && lc != 80 && lc != 81 && lc != 100',
                   {lc: img.select('b1')}))
-      .select('landcover');
+      .select('b1');
 };
 
 // Landsat prep functions
