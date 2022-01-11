3c00b36d2b4c20163c6f642df9e18003a30beaef
robit.alec <robit.alec@gmail.com>
2022-01-11 17:57:00 +0000

    Updated checks/Check-new-land-cover.js (added 17 lines, deleted 4 lines).

--- checks/Check-new-land-cover.js
+++ checks/Check-new-land-cover.js
@@ -12,13 +12,26 @@ var l5 = ee.ImageCollection('LANDSAT/LT05/C02/T1_L2');
 var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 var lcMask = lcmask.returnLandCover();
 
+
+
 // Filter -----------------------------------------------------------
-var year = 2012
+var year = 2012;
 
 l5 = l5.filter(ee.Filter.eq('year', year))
-       .filterBounds(geometry);
+       .filterBounds(geometry)
+       .first();
 
-lc = lc.filterDate(ee.Date.fromYMD(year, 1, 1), ee.Date.fromYMD(year + 1, 1, 1))
+lc = lc.filterDate(ee.Date.fromYMD(year, 1, 1), ee.Date.fromYMD(year + 1, 1, 1));
+
+
 
 // Map --------------------------------------------------------------
-Map.addLayer(lc, 'lc')
+Map.addLayer(lc, null, 'lc');
+
+Map.addLayer(lc.first().expression(
+  'lc != 0 && lc != 20 && lc != 31 && lc != 32 && ' +
+  'lc != 33 && lc != 40 && lc != 80 && lc != 81 && lc != 100',
+  {lc: lc.first().select('b1')}), null, 'masked lc');
+  
+Map.addLayer(l5, null, 'raw l5');
+Map.addLayer(lcmask.maskLandCover(l5), null, 'masked l5');
