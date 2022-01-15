9c3719aaaa4b7fd535229863b97b61c06ed7e423
robit.alec <robit.alec@gmail.com>
2022-01-15 19:48:52 +0000

    Updated checks/Compare-Hermosilla-2022-and-ABoVE-land-cover.js (added 1 line, deleted 1 line).

--- checks/Compare-Hermosilla-2022-and-ABoVE-land-cover.js
+++ checks/Compare-Hermosilla-2022-and-ABoVE-land-cover.js
@@ -50,7 +50,7 @@ Map.addLayer(above);
 
 
 
-// Sample random points
+// Sample random points, in large region to cover all classes
 var points = ee.FeatureCollection.randomPoints(geometry, 1000);
 var combined = ee.Image([hermo, above]);
 var combined_sample = combined.sampleRegions(points);
