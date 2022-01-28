53e1e28dee349a5812947dc8fcbb0231bf01f783
robit.alec <robit.alec@gmail.com>
2022-01-28 20:51:46 +0000

    Updated Map-land-cover-transitions.js (added 27 lines, deleted 48 lines).

--- Map-land-cover-transitions.js
+++ Map-land-cover-transitions.js
@@ -4,6 +4,15 @@
 
 
 
+// Function -------------------------------------------------------------------
+// Landsat prep functions
+var landsatprep = require('users/robitalec/CFS:modules/landsat-prep.js');
+
+// Land cover functions
+var land_cover = require('users/robitalec/CFS:modules/land-cover.js');
+
+
+
 // Images ---------------------------------------------------------------------
 // Hermosilla et al. 2022 land cover
 var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
@@ -11,37 +20,13 @@ var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 
 
 // Processing -----------------------------------------------------------------
-// Grab first year
-lc = lc.first();
+// TODO: mask fires
 
-// Remap
-var from = [0, 20, 31, 32, 33, 40, 50, 80, 81, 100, 210, 220, 230];
-var to =   [0, 1,  2,  3,  4,  5,  6,  7,  8,  9,   10,  11,  12 ];
-lc = lc.remap(from, to).rename('land_cover');
-
-// Also a masked version
-var masked_lc = lc.updateMask(lc.eq(1).or(lc.eq(10)));
+// Set years and mask classes
+lc = lc.map(landsatprep.setYear)
+       .map(maskClasses);
 
 
-
-// Stratified sample ----------------------------------------------------------
-// Full land cover
-var sampled_points = lc.addBands([ee.Image.pixelLonLat()]).stratifiedSample({
-  classBand: 'land-cover',
-  numPoints: 100,
-  region: geometry
-}).map(function(ft) {
-  return ft.setGeometry(ee.Geometry.Point([ft.get('longitude'), ft.get('latitude')]));
-});
-
-// Masked land cover
-var masked_sampled_points = masked_lc.addBands([ee.Image.pixelLonLat()]).stratifiedSample({
-  classBand: 'land-cover',
-  numPoints: 100,
-  region: geometry
-}).map(function(ft) {
-  return ft.setGeometry(ee.Geometry.Point([ft.get('longitude'), ft.get('latitude')]));
-});
 
 
 
@@ -56,13 +41,7 @@ var pal = palettes.crameri.batlow[25];
 // Land cover
 Map.addLayer(lc, {palette: pal}, 'land cover', false);
 
-// Sampled points
-print(sampled_points);
-Map.addLayer(sampled_points, null, 'sampled points', false);
 
 
 // Masked land cover
 Map.addLayer(masked_lc, {palette: pal}, 'masked land cover');
-
-// Sample points within masked land cover
-Map.addLayer(masked_sampled_points, null, 'sampled points within mask');
