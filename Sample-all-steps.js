df0705cbdf498a7bbc7cc697b029a0a242ba1420
robit.alec <robit.alec@gmail.com>
2022-01-28 03:51:43 +0000

    Updated Sample-all-steps.js (added 1 line, deleted 1 line).

--- Sample-all-steps.js
+++ Sample-all-steps.js
@@ -205,7 +205,7 @@ var sampled = images_to_sample.sampleRegions({
   collection: points, 
   properties: null, 
   scale: 30, 
-  projection: ee.Projection("EPSG:4326").atScale(1)
+  projection: ee.Projection("EPSG:4326").atScale(30)
 });
 // var sampled = points.map(function(ft) {
 //   return images_to_sample.sampleRegions(ft, null, 30);
