038f9957dea9660c9844d3791dcfe0d2d0d4268e
robit.alec <robit.alec@gmail.com>
2022-01-28 00:18:57 +0000

    Updated Test-stratified-land-cover-sampling.js (deleted 40 lines).

--- Test-stratified-land-cover-sampling.js
+++ Test-stratified-land-cover-sampling.js
@@ -22,43 +22,6 @@ lc = lc.first();
 
 var from = [0, 20, 31, 32, 33, 40, 50, 80, 81, 100, 210, 220, 230];
 var to =   [0, 1,  2,  3,  4,  5,  6,  7,  8,  9,   10,  11,  12 ];
-ca_lc_last = ca_lc_last.remap(from, to);
-
-
-var palettes = require('users/gena/packages:palettes')
-
-
-Map.addLayer(lc, {palette: palettes.colorbrewer.Dark2[13]}, 'land cover');
-
-//  0   Unclassified 
-//  20  Water 
-//  31  Snow/Ice 
-//  32  Rock/Rubble  
-//  33  Exposed/Barren Land 
-//  40  Bryoids 
-//  50  Shrubs 
-//  80  Wetland
-//  81  Wetland Treed
-//  100 Herbs 
-//  210 Coniferous  
-//  220 Broad Leaf  
-//  230 Mixedwood 
-
-var sampled_points = lc.stratifiedSample({
-  classBand: 'b1',
-  numPoints: 100,
-  region: geometry
-});
-// Load Hermosilla land cover
-var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
-
-
-
-lc = lc.first();
-
-
-var from = [0, 20, 31, 32, 33, 40, 50, 80, 81, 100, 210, 220, 230];
-var to =   [0, 1,  2,  3,  4,  5,  6,  7,  8,  9,   10,  11,  12 ];
 lc = lc.remap(from, to);
 
 
@@ -84,9 +47,6 @@ var sampled_points = lc.stratifiedSample({
   numPoints: 100,
   region: geometry
 });
-
-print(sampled_points)
-Map.addLayer(sampled_points);
 
 print(sampled_points)
 Map.addLayer(sampled_points);
