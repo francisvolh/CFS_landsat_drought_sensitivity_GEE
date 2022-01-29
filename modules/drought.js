73a6cf297908da1980d2cbe3863b2da00db0b453
robit.alec <robit.alec@gmail.com>
2022-01-29 18:33:25 +0000

    Updated modules/drought.js (added 1 line, deleted 1 line).

--- modules/drought.js
+++ modules/drought.js
@@ -1,6 +1,6 @@
 // Variables --------------------------------------------------------
 // Set percentiles to use
-var percentiles = [1, 5, 10, 20];
+var percentiles = [1, 5, 10, 15, 20];
 
 // Set min max year for daymet
 var minyear = 1980;
