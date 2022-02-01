334bc6455715c76c2e726c8295d7ff4559f52bf0
robit.alec <robit.alec@gmail.com>
2022-02-01 00:13:12 +0000

    Updated Sample-all-steps.js (added 11 lines).

--- Sample-all-steps.js
+++ Sample-all-steps.js
@@ -232,3 +232,14 @@ Export.table.toDrive({
   description: today + '_sampled-intermediate-landsat-YT-AB-BC',
   folder: 'Drought-sensitivity-refugia'
 });
+
+
+var exp = {
+  image: droughtSens.select('Sens_NDVI_ante12mo_p15'),
+  description: today + '_drought-sensitivity-Landsat-1985_2012-NDVI-12mo-p15-AB-sub',
+  folder: 'Drought-sensitivity-refugia',
+  region: albertasubsample,
+  scale: 1000,
+  maxPixels: 1e9
+};
+Export.image.toDrive(exp);
