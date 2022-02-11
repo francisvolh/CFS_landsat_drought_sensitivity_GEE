94dbc8435c9c4301799487cef403a937f9c8c118
robit.alec <robit.alec@gmail.com>
2022-02-11 01:36:08 +0000

    Created modules/get_collection.js.

---
+++ modules/get_collection.js
@@ -0,0 +1,41 @@
+/*
+Thin wrapper around LandTrendr API
+Generates a SR collection in a region, then transforms into spectral indices
+Alec L. Robitaille
+
+https://emapr.github.io/LT-GEE/api.html
+
+Kennedy, R.E., Yang, Z., Gorelick, N., Braaten, J., Cavalcante, L.,
+Cohen, W.B., Healey, S. (2018). Implementation of the LandTrendr Algorithm
+on Google Earth Engine. Remote Sensing. 10, 691.
+*/
+
+
+// Load LandTrendr API
+var ltgee = require('users/emaprlab/public:Modules/LandTrendr.js');
+
+// Set flags to mask
+var mask = ['cloud', 'shadow', 'snow', 'water', 'waterplus'];
+
+
+// Get collection of spectral indices
+var get_indices = function(min_year, max_year, min_mm_dd, max_mm_dd, region, indices) {
+	var collection = get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region);
+
+	return(ltgee.transformSRcollection(collection, indices));
+};
+exports.get_indices = get_indices;
+
+
+// Get collection of Landsat SR bands
+var get_SR = function(min_year, max_year, min_mm_dd, max_mm_dd, region) {
+	return(ltgee.buildSRcollection({
+		startYear: min_year,
+		endYear: max_year,
+		startDay: min_mm_dd,
+		endDay:  max_mm_dd,
+		aoi:  region,
+		maskThese: mask
+	}));
+};
+exports.get_SR = get_SR;
