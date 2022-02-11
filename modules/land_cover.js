f6e406370abe17805e1516d6c3f42160ec4e6b1b
robit.alec <robit.alec@gmail.com>
2022-02-11 22:41:16 +0000

    Updated modules/land_cover.js (added 52 lines, deleted 59 lines).

--- modules/land_cover.js
+++ modules/land_cover.js
@@ -1,4 +1,32 @@
 /*
+Land cover masks from Hermosilla et al. 2022 land cover
+Alec L. Robitaille
+
+
+High-resolution annual forest land cover maps for Canada's forested ecosystems (1984-2019)
+
+0 	Unclassified
+20 	Water
+31 	Snow/Ice
+32 	Rock/Rubble
+33 	Exposed/Barren Land
+40 	Bryoids
+50 	Shrubs
+80 	Wetland
+81 	Wetland Treed
+100 	Herbs
+210 	Coniferous
+220 	Broad Leaf
+230 	Mixedwood
+
+
+Hermosilla, T., Wulder, M.A., White, J.C., Coops, N.C., 2022. Land cover
+classification in an era of big and open data: Optimizing localized
+implementation and training data selection to improve mapping outcomes.
+Remote Sensing of Environment. No. 112780.
+DOI: https://doi.org/10.1016/j.rse.2022.112780 [Open Access]
+
+
 -- Excluded --
 0   Unclassified
 20  Water
@@ -17,73 +45,38 @@
 230 Mixedwood
 */
 
-// Data
+// Load utils
+var utils = require('users/robitalec/CFS:modules/utils.js');
+
 // Load Hermosilla land cover
 var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 
 
-
-// Functions
-// Mask land cover, only retaining if not one of
-var maskClasses = function(img) {
-  return img.updateMask(
-    img.expression('lc == 50 || lc == 81 || ' +
-                   'lc == 210 || lc == 220 || lc == 230',
-                  {lc: img.select('b1')}))
-      .select(['b1'], ['land_cover']);
+// Mask classes
+var mask_classes = function(img) {
+	return img.updateMask(
+		img.expression('lc == 50 || lc == 81 || lc == 210 || lc == 220 || lc == 230',
+		{lc: img.select('b1')})
+	).select(['b1'], ['land_cover'])
 };
+exports.mask_classes = mask_classes;
 
-exports.maskClasses = function(img) {
-  return maskClasses(img);
+
+// Get land cover collection
+var get_land_cover = function() {
+	lc = lc.map(utils.set_year)
+				 .map(mask_land_cover);
 };
-
-// Landsat prep functions
-var landsatprep = require('users/robitalec/CFS:modules/landsat-prep.js');
+exports.get_land_cover = get_land_cover;
 
 
-// Processing
-lc = lc.map(landsatprep.setYear)
-       .map(maskClasses);
-
-
-/// Exports
-exports.returnLandCover = function() {
-	return lc;
-};
-
-
-// exports.reverseMask = function() {
-//   return lcmask.mask().not().selfMask();
-// };
-exports.maskLandCover = function(img) {
+// Mask image with land cover
+var mask_land_cover = function(img) {
   var date = img.date();
   return img.updateMask(lc.filterDate(date, date.advance(1, 'year')).first());
 };
+exports.mask_land_cover = mask_land_cover;
 
-
-
-// Return 1985, 2002, 2019
-exports.returnLandCoverSample = function() {
- return ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2")
-              .map(landsatprep.setYear)
-              .filter(ee.Filter.inList('year', [1985, 2002, 2019]))
-              .toBands();
-};
-
-
-// Stratified sampling
-var lc_2002 = lc.filter(ee.Filter.eq('year', 2002)).first();
-
-// Note unless seed is changed, always same points
-exports.stratifiedSample = function(region, n_pts) {
-  return lc_2002.addBands([ee.Image.pixelLonLat()]).stratifiedSample({
-    classBand: 'land_cover',
-    numPoints: n_pts,
-    region: region
-  }).map(function(ft) {
-    return ft.setGeometry(ee.Geometry.Point([ft.get('longitude'), ft.get('latitude')]));
-  });
-};
 
 
 
