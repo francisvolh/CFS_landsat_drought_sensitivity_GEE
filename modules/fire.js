a54dbb9cde510a4e3b2bebad8014fe66dd791e61
robit.alec <robit.alec@gmail.com>
2022-02-11 02:39:07 +0000

    Updated modules/fire.js (added 36 lines, deleted 52 lines).

--- modules/fire.js
+++ modules/fire.js
@@ -1,5 +1,16 @@
-// Generate fire masks from fire polygons
-var generateFireMasks = function(firepols, yr) {
+/*
+Fire masks from NBAC fire data
+Alec L. Robitaille
+
+https://cwfis.cfs.nrcan.gc.ca/datamart
+https://cwfis.cfs.nrcan.gc.ca/downloads/nbac/nbac_2020_r9_20210810.shp.pdf
+*/
+
+// Load NBAC fire polygons
+var NBAC_fires = ee.FeatureCollection("users/robitalec/CFS/nbac_1986_to_2020_20210810");
+
+// Generate fire masks from NBAC - any fire in preceeding 5 years
+var five_year_fires = function(yr) {
   var date = ee.Date.fromYMD(yr, 1, 1);
 
   // Present year
@@ -12,46 +23,19 @@ var generateFireMasks = function(firepols, yr) {
   // Reduce to any non zero = anywhere there is a fire
   // Result is 0 = no fire, 1 = fire
   return ee.Image([
-    firepols.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
-            .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
-            .rename('fire-in-last-5-years')
+    NBAC_fires.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
+							.reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
+							.rename('fire-in-last-5-years')
     ]).set('year', yr);
 };
-
-// Load NFDB fire polygons
-var firepol = ee.FeatureCollection("users/robitalec/CFS/nbac_1986_to_2020_20210810");
+exports.five_year_fires = five_year_fires;
 
 
 // Mask fires
-exports.maskFires = function(img) {
+var mask_five_year_fires = function(img) {
   var yr = img.date().get('year');
 
-  var fire = generateFireMasks(firepol, yr).eq(0) //.first()?
+  var fire = five_year_fires(yr).eq(0);
   return(img.updateMask(fire));
-}
-
-
-// Archive -----
-// Generate fire masks from fire polygons
-exports.fireMasks = function(fires, years) {
-  // Return image collection from images for each year's 5 year fire
-  return ee.ImageCollection.fromImages(years.map(function(yr) {
-    // First of day of year
-    var date = ee.Date.fromYMD(yr, 1, 1);
-
-    // Present year
-    var y = date.get('year');
-
-    // 5 years previous
-    var ymin5 = date.advance(-4, 'year').get('year');
-
-    // Filter fires within last 5 years
-    // Reduce to any non zero = anywhere there is a fire
-    // Result is 0 = no fire, 1 = fire
-    return ee.Image([
-      fires.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
-          .reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
-          .rename('fire-in-last-5-years')
-      ]).set('year', yr);
-  }));
 };
+exports.mask_current_year = mask_current_year;
