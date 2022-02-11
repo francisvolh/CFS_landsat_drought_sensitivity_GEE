faff3b57f0fdc96e24a80871226c2a64ff7a0a79
robit.alec <robit.alec@gmail.com>
2022-02-11 02:17:04 +0000

    Updated modules/get_collection.js (added 8 lines, deleted 1 line).

--- modules/get_collection.js
+++ modules/get_collection.js
@@ -27,6 +27,13 @@ exports.get_SR = get_SR;
 var get_indices = function(min_year, max_year, min_mm_dd, max_mm_dd, region, indices) {
 	var collection = get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region);
 
-	return(ltgee.transformSRcollection(collection, indices));
+	return(ltgee.transformSRcollection(collection, indices)
+							.map(setYear)));
 };
 exports.get_indices = get_indices;
+
+
+// Set year
+exports.setYear = function(img) {
+  return img.set('year', img.date().get('year'));
+};
