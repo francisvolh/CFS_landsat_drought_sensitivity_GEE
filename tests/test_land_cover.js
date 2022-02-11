1e42e3df79de637b80539bb7a47cf520ef00ead6
robit.alec <robit.alec@gmail.com>
2022-02-11 22:42:00 +0000

    Updated tests/test_land_cover.js (added 12 lines).

--- tests/test_land_cover.js
+++ tests/test_land_cover.js
@@ -13,6 +13,18 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 var img = lc.first();
 
+
+// Test mask_classes
+// Usage: mask_classes(img)
+Map.addLayer(land_cover.mask_classes(img));
+
+
+// Test get_land_cover
+// Usage: get_land_cover()
+Map.addLayer(land_cover.get_land_cover());
+
+
 // Test mask_land_cover
 // Usage: mask_land_cover(img)
+var img = ee.Image.constant(1);
 Map.addLayer(land_cover.mask_land_cover(img));
