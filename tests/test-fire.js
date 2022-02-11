632cf59a1b8a506519d7194b57a0e4f1b8b9131b
robit.alec <robit.alec@gmail.com>
2022-02-11 02:37:52 +0000

    Created tests/test-fire.js.

---
+++ tests/test-fire.js
@@ -0,0 +1,21 @@
+/*
+Test modules/fire.js
+Alec L. Robitaille
+
+*/
+
+
+// Load fire module
+var fire = require('users/robitalec/CFS:modules/fire.js');
+
+
+// Geometry
+var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
+                                    [-105.988, 58.941], [-105.988, 59.673]]]);
+
+
+// Test five year fires
+// Usage: fire.five_year_fires(year)
+var five_year_fires = fire.five_year_fires(2013);
+print(five_year_fires);
+
