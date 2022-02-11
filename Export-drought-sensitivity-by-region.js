62963cd8608171141895cbfbf60943248c36d995
robit.alec <robit.alec@gmail.com>
2022-02-11 02:38:30 +0000

    Updated Export-drought-sensitivity-by-region.js (added 41 lines, deleted 1 line).

---
+++ Export-drought-sensitivity-by-region.js
@@ -0,0 +1,41 @@
+/*
+Fire masks from NBAC fire data
+Alec L. Robitaille
+
+https://cwfis.cfs.nrcan.gc.ca/datamart
+https://cwfis.cfs.nrcan.gc.ca/downloads/nbac/nbac_2020_r9_20210810.shp.pdf
+*/
+
+// Load NBAC fire polygons
+var NBAC_fires = ee.FeatureCollection("users/robitalec/CFS/nbac_1986_to_2020_20210810");
+
+// Generate fire masks from NBAC - any fire in preceeding 5 years
+var five_year_fires = function(yr) {
+  var date = ee.Date.fromYMD(yr, 1, 1);
+
+  // Present year
+  var y = date.get('year');
+
+  // 5 years previous
+  var ymin5 = date.advance(-4, 'year').get('year');
+
+  // Filter fires within last 5 years
+  // Reduce to any non zero = anywhere there is a fire
+  // Result is 0 = no fire, 1 = fire
+  return ee.Image([
+    NBAC_fires.filter(ee.Filter.rangeContains('YEAR', ymin5, y))
+							.reduceToImage(['YEAR'], ee.Reducer.anyNonZero())
+							.rename('fire-in-last-5-years')
+    ]).set('year', yr);
+};
+exports.five_year_fires = five_year_fires;
+
+
+// Mask fires
+var mask_five_year_fires = function(img) {
+  var yr = img.date().get('year');
+
+  var fire = five_year_fires(yr).eq(0)
+  return(img.updateMask(fire));
+}
+exports.mask_current_year = mask_current_year;
