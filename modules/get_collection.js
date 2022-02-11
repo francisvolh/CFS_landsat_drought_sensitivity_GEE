29ad33b559e2e2cacd763b4859b859a2d27487e6
robit.alec <robit.alec@gmail.com>
2022-02-11 02:22:03 +0000

    Updated modules/get_collection.js (added 2 lines, deleted 3 lines).

--- modules/get_collection.js
+++ modules/get_collection.js
@@ -26,10 +26,9 @@ exports.get_SR = get_SR;
 
 // Get collection of spectral indices
 var get_indices = function(min_year, max_year, min_mm_dd, max_mm_dd, region, indices) {
-	var collection = get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region)
-                      .map(setYear);
+	var collection = get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region);
 
-	return(ltgee.transformSRcollection(collection, indices));
+	return(ltgee.transformSRcollection(collection, indices).map(setYear));
 };
 exports.get_indices = get_indices;
 
