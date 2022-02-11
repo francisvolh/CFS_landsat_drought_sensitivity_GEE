a7ad4c744fb2e6068ffa32ce908c1d1633d6488f
robit.alec <robit.alec@gmail.com>
2022-02-11 01:52:31 +0000

    Updated tests/test_get_collection.js (added 2 lines, deleted 2 lines).

--- tests/test_get_collection.js
+++ tests/test_get_collection.js
@@ -20,8 +20,8 @@ print(sr_col);
 
 // Test indices collection
 // Usage: get_collection.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region, indices)
-var indices_col = get_collection.get_SR(2014, 2019, '06-15', '07-15', geometry, ['NDVI', 'EVI']);
+var indices_col = get_collection.get_indices(2014, 2019, '06-15', '07-15', geometry, ['NDVI', 'EVI']);
 print(indices_col);
 
 // Map
-Map.addLayer(geometry)
+Map.addLayer(geometry);
