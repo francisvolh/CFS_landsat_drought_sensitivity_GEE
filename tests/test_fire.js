d609068fb21f2ce6f661445aff6943f30740af41
robit.alec <robit.alec@gmail.com>
2022-02-11 02:43:02 +0000

    Updated tests/test_fire.js (added 11 lines, deleted 4 lines).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -14,12 +14,19 @@ var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
                                     [-105.988, 58.941], [-105.988, 59.673]]]);
 
 
-// Test five year fires
+// Test five_year_fires
 // Usage: fire.five_year_fires(year)
 var five_year_fires = fire.five_year_fires(2013);
 print(five_year_fires);
 
 
+// Test mask_five_year_fires
+// Usage: fire.mask_five_year_fires(img)
+var img = ee.Image.random().set('year', 2017);
+var mask_five_year_fires = fire.mask_five_year_fires(img);
+print(mask_five_year_fires);
+
 
 // Map
-Map.addLayer(five_year_fires)
+Map.addLayer(five_year_fires);
+Map.addLayer(mask_five_year_fires);
