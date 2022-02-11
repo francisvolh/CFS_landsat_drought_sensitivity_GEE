6ce60e477f0cef7c2bf8c1ae00fc3c692ab631ca
robit.alec <robit.alec@gmail.com>
2022-02-11 22:31:01 +0000

    Updated tests/test_land_cover.js (added 13 lines).

--- tests/test_land_cover.js
+++ tests/test_land_cover.js
@@ -3,3 +3,16 @@ Testing: modules/land_cover.js
 Alec L. Robitaille
 
 */
+
+
+// Load land cover module
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+
+
+// Load Hermosilla land cover
+var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
+var img = lc.first();
+
+// Test mask_land_cover
+// Usage: mask_land_cover(img)
+Map.addLayer(land_cover.mask_land_cover(img));
