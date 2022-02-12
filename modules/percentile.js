ec83c35c0abcd6527429857c31596db374ce662a
robit.alec <robit.alec@gmail.com>
2022-02-12 18:07:14 +0000

    Updated modules/percentile.js (added 6 lines, deleted 6 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -5,21 +5,21 @@ Alec L. Robitaille
 
 // Get percentile
 var get_percentile = function(images, percentile_list) {
-  var percentile_3mo = means.select(['CMI_ante3mo_mean'], ['CMI_ante3mo_lt'])
+  var percentile_3mo = images.select(['CMI_ante3mo_mean'], ['CMI_ante3mo_lt'])
                             .reduce(ee.Reducer.percentile(percentile_list));
 
-  // var percentile_6mo = means.select(['CMI_ante6mo_mean'], ['CMI_ante6mo_lt'])
+  // var percentile_6mo = images.select(['CMI_ante6mo_mean'], ['CMI_ante6mo_lt'])
   //                           .reduce(ee.Reducer.percentile(percentile_list));
 
-  var percentile_12mo = means.select(['CMI_ante12mo_mean'], ['CMI_ante12mo_lt'])
+  var percentile_12mo = images.select(['CMI_ante12mo_mean'], ['CMI_ante12mo_lt'])
                              .reduce(ee.Reducer.percentile(percentile_list));
 
-  var percentile_5yr = means.select(['CMI_ante5yr_mean_min'], ['CMI_ante5yr_lt'])
+  var percentile_5yr = images.select(['CMI_ante5yr_mean_min'], ['CMI_ante5yr_lt'])
                             .reduce(ee.Reducer.percentile(percentile_list));
-                            
+
   return(ee.Image([percentile_3mo, percentile_12mo, percentile_5yr]));
 };
-exports.get_percentile = get_percentile;
+
 
 // Compare percentile images for each antecedent period to each image's antecedent means
 var lt_percentile = function(means, percentile_list) {
