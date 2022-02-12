0d033777503663e05b9a5f3ac5e0dd4af3a13d59
robit.alec <robit.alec@gmail.com>
2022-02-12 17:47:25 +0000

    Created tests/test_percentile.js.

---
+++ tests/test_percentile.js
@@ -0,0 +1,42 @@
+/*
+Testing: modules/percentile.js
+Alec L. Robitaille
+*/
+
+// Load percentile module
+var percentile = require('users/robitalec/CFS:modules/percentile.js');
+
+// Load antecedent module
+var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
+
+// Load CMI module
+var cmi = require('users/robitalec/CFS:modules/cmi.js');
+
+// Load palettes module
+var palettes = require('users/gena/packages:palettes');
+var pal = palettes.colorbrewer.RdBu[5];
+
+// Load get_daymet module
+var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
+
+// Set years, months
+var years = ee.List.sequence(2010, 2015);
+var months = ee.List.sequence(5, 7);
+
+// Get Daymet collection
+var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
+
+// Calculate CMI
+var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
+
+// Antecedent means
+var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
+
+// Percentile list
+var percentile_list = [5, 10];
+
+// Test lt_percentile
+// Usage: lt_percentile(means, percentile_list)
+var lt_percent = percentile.lt_percentile(ante_means, percentile_list);
+print(lt_percent);
+Map.addLayer(ante_means.select('CMI_ante3mo_lt_p10'));
