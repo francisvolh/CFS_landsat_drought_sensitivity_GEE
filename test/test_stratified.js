6c5c71c80492d7c452c1bf7656511ed4b95cb589
robit.alec <robit.alec@gmail.com>
2022-02-12 00:54:01 +0000

    Created test/test_stratified.js.

---
+++ test/test_stratified.js
@@ -0,0 +1,17 @@
+/*
+Testing: modules/stratified.js
+Alec L. Robitaille
+
+*/
+
+
+// Load stratified module
+var stratified = require('users/robitalec/CFS:modules/stratified.js');
+
+// Load land cover module
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+
+// Load land cover image
+var lc = land_cover.get_land_cover().first();
+
+// Region
