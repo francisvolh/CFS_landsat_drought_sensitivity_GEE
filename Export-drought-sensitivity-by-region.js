9f2ce8fb82c608a8c3477dcbd7e97425dc08b4f2
robit.alec <robit.alec@gmail.com>
2022-02-12 19:55:47 +0000

    Updated Export-drought-sensitivity-by-region.js (deleted 2 lines).

--- Export-drought-sensitivity-by-region.js
+++ Export-drought-sensitivity-by-region.js
@@ -55,11 +55,9 @@ var percentile_images = percentile.get_percentile(ante_means, percentile_list);
 var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
 
 
-
 var sample = ecoregions.map(function(ft) {
 
 	var indices_col = get_landsat.get_indices(min_year, max_year, '07-01', '07-31', ft, ['NDVI', 'EVI', 'NBR']);
-
 
 
 })
