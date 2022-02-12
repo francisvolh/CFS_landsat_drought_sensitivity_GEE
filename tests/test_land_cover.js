4f65a0261f322a92a80424199dae3e1966ab64e0
robit.alec <robit.alec@gmail.com>
2022-02-12 00:03:35 +0000

    Updated tests/test_land_cover.js (added 4 lines, deleted 4 lines).

--- tests/test_land_cover.js
+++ tests/test_land_cover.js
@@ -11,11 +11,11 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 
 // Load Hermosilla land cover
 var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
-var img = lc.first();
+var lc_1985 = lc.first();
 
 // Test mask_classes
-// Usage: mask_classes(img)
-var masked_lc = land_cover.mask_classes(img);
+// Usage: mask_classes(lc_img)
+var masked_lc = land_cover.mask_classes(lc_1985);
 print(masked_lc);
 Map.addLayer(masked_lc, null, 'land_cover.mask_classes(img)');
 
@@ -29,7 +29,7 @@ Map.addLayer(lc_collection, null, 'land_cover.get_land_cover()');
 
 // Test mask_land_cover
 // Usage: mask_land_cover(img)
-var img = ee.Image.constant(1);
+var img = ee.Image('LANDSAT/LC08/C02/T1_L2/LC08_060016_20140910');
 var img_masked_lc = land_cover.mask_land_cover(img);
 print(img_masked_lc);
 Map.addLayer(img_masked_lc, null, 'land_cover.mask_land_cover(img)');
