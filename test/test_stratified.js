340609871b825f2972383d87a6920963debb7578
robit.alec <robit.alec@gmail.com>
2022-02-12 00:55:57 +0000

    Updated test/test_stratified.js (added 6 lines).

--- test/test_stratified.js
+++ test/test_stratified.js
@@ -15,3 +15,9 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var lc = land_cover.get_land_cover().first();
 
 // Region
+var geometry = ee.Geometry.Polygon([[[-96.188, 54.036], [-96.188, 51.866], [-92.233, 51.866], [-92.233, 54.036]]]);
+
+// Test stratified_sample
+// Usage: stratified.stratified_sample(img, band, region, n_pts)
+var strat = stratified.stratified_sample(lc, 'land_cover', geometry, 100);
+
