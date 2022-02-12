043f02b7b13cd0e832389dbac3400204d4b5dc4f
robit.alec <robit.alec@gmail.com>
2022-02-12 02:10:15 +0000

    Updated tests/test_cmi.js (added 2 lines, deleted 1 line).

--- tests/test_cmi.js
+++ tests/test_cmi.js
@@ -16,6 +16,7 @@ var daymet = ee.ImageCollection("NASA/ORNL/DAYMET_V4")
     .filterDate('2015-07-01', '2015-07-30')
     .mean();
 
+
 // Test calc_CMI
 // Usage: calc_CMI(img)
 var cmi_daymet = cmi.calc_CMI(daymet);
