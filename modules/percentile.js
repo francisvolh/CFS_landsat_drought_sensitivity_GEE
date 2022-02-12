2cb42521c11f73d2a3f9fe938ba1791aada3f540
robit.alec <robit.alec@gmail.com>
2022-02-12 18:19:32 +0000

    Updated modules/percentile.js (added 20 lines, deleted 19 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -5,24 +5,25 @@ Alec L. Robitaille
 
 // Get percentile
 var get_percentile = function(images, percentile_list) {
-  var percentile_3mo = images.select(['CMI_ante3mo_mean'], ['CMI_ante3mo'])
-                            .reduce(ee.Reducer.percentile(percentile_list));
-
-  // var percentile_6mo = images.select(['CMI_ante6mo_mean'], ['CMI_ante6mo'])
-  //                           .reduce(ee.Reducer.percentile(percentile_list));
-
-  var percentile_12mo = images.select(['CMI_ante12mo_mean'], ['CMI_ante12mo'])
-                             .reduce(ee.Reducer.percentile(percentile_list));
-
-  var percentile_5yr = images.select(['CMI_ante5yr_mean_min'], ['CMI_ante5yr'])
-                            .reduce(ee.Reducer.percentile(percentile_list));
-
-  return(ee.Image([percentile_3mo, percentile_12mo, percentile_5yr]));
+	return images.reduce(ee.Reducer.percentile(percentile_list));
 };
 exports.get_percentile = get_percentile;
 
 // Compare percentile images for each antecedent period to each image's antecedent means
-var lt_percentile = function(means, percentile_list) {
+var lt_percentile = function(img, percentile_images) {
+	return ee.Image([
+		img.select('CMI_ante3mo_mean')
+       .lt(percentile_images),
+		img.select('CMI_ante6mo_mean')
+       .lt(percentile_images),
+		img.select('CMI_ante12mo_mean')
+       .lt(percentile_images),
+		img.select('CMI_ante5yr_mean')
+       .lt(percentile_images)
+     ]).copyProperties(img);
+	])
+/*
+
   var percentile_3mo = means.select(['CMI_ante3mo_mean'], ['CMI_ante3mo_lt'])
                             .reduce(ee.Reducer.percentile(percentile_list));
 
@@ -53,6 +54,6 @@ var lt_percentile = function(means, percentile_list) {
       ]).copyProperties(img);
 	});
 
-	return means_lt_percentiles;
+	return means_lt_percentiles;*/
 };
 exports.lt_percentile = lt_percentile;
