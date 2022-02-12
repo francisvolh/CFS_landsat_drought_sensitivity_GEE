bbc456137256549c57ad618812f9ed7f6068ebb9
robit.alec <robit.alec@gmail.com>
2022-02-12 01:25:13 +0000

    Updated tests/test_stratified.js (added 4 lines, deleted 3 lines).

--- tests/test_stratified.js
+++ tests/test_stratified.js
@@ -11,15 +11,16 @@ var stratified = require('users/robitalec/CFS:modules/stratified.js');
 // Load land cover module
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 
-// Load land cover image
-var lc = land_cover.get_land_cover().first();
+// Load Hermosilla land cover
+var hermosilla_2022 = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
+var lc = hermosilla_2022.first();
 
 // Region
 var geometry = ee.Geometry.Polygon([[[-96.188, 54.036], [-96.188, 51.866], [-92.233, 51.866], [-92.233, 54.036]]]);
 
 // Test stratified_sample
 // Usage: stratified.stratified_sample(img, band, region, n_pts)
-var strat = stratified.stratified_sample(lc, 'land_cover', geometry, 10);
+var strat = stratified.stratified_sample(lc, 'b1', geometry, 10);
 print(strat);
 Map.addLayer(geometry);
 Map.addLayer(lc);
