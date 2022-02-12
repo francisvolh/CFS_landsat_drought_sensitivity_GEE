2ffa3ba34f79407bab793d628d64705d4696e1f3
robit.alec <robit.alec@gmail.com>
2022-02-12 00:21:40 +0000

    Updated tests/test_land_cover.js (added 1 line, deleted 1 line).

--- tests/test_land_cover.js
+++ tests/test_land_cover.js
@@ -35,4 +35,4 @@ Map.addLayer(lc_collection, null, 'land_cover.get_land_cover()');
 var img = ee.Image('LANDSAT/LC08/C02/T1_L2/LC08_060016_20140910');
 var img_masked_lc = land_cover.mask_land_cover(img);
 print(img_masked_lc);
-Map.addLayer(img_masked_lc, null, 'land_cover.mask_land_cover(img)');
+Map.addLayer(img_masked_lc.mask(), null, 'land_cover.mask_land_cover(img)');
