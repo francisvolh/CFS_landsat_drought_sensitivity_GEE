13798638697fdf1127e42f065ccd9dfee91f359c
robit.alec <robit.alec@gmail.com>
2022-02-12 18:39:04 +0000

    Updated tests/test_percentile.js (added 9 lines, deleted 6 lines).

--- tests/test_percentile.js
+++ tests/test_percentile.js
@@ -35,18 +35,21 @@ var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
 // Percentile list
 var percentile_list = [5, 10];
 
+// CMI viz
+var cmi_viz = {min:-30, max:30, palette: pal};
+
 // Test get_percentile
 // Usage: get_percentile(images, percentile_list)
 var percentile_images = percentile.get_percentile(ante_means, percentile_list);
-print('Antecedent means');print(ante_means);
-print('Percentile images');print(percentile_images);
-Map.addLayer(ante_means.select('CMI_ante3mo_mean'), {min:-30, max:30, palette: pal}, '2010-2015 CMI 3 month antecedent means', false);
-Map.addLayer(ante_means.select('CMI_ante3mo_mean').first(), {min:-30, max:30, palette: pal}, '2010 CMI 3 month antecedent means');
-Map.addLayer(percentile_images.select('CMI_ante3mo_mean_p10'), {min:-30, max:30, palette: pal}, '2010-2015 CMI 10th percentile');
+print('Antecedent means'); print(ante_means);
+print('Percentile images'); print(percentile_images);
+Map.addLayer(ante_means.select('CMI_ante3mo_mean'), cmi_viz, '2010-2015 CMI 3 month antecedent means', false);
+Map.addLayer(ante_means.select('CMI_ante3mo_mean').first(), cmi_viz, '2010 CMI 3 month antecedent means');
+Map.addLayer(percentile_images.select('CMI_ante3mo_mean_p10'), cmi_viz, '2010-2015 CMI 10th percentile');
 
 // Test lt_percentile
 // Usage: lt_percentile(means, percentile_images)
 var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
 print('Less than percentile'); print(lt_percent);
-// Map.addLayer(ante_means.select('CMI_ante3mo_mean'), {min:-30, max:30, palette: pal});
+// Map.addLayer(ante_means.select('CMI_ante3mo_mean'), cmi_viz);
 // Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p10').limit(-1), {min:0, max:0});
