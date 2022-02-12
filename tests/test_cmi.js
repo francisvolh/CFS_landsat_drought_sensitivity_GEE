fe4f9a608c0b160ad5b2e68f039ce894db9efd73
robit.alec <robit.alec@gmail.com>
2022-02-12 02:11:03 +0000

    Updated tests/test_cmi.js (added 1 line, deleted 1 line).

--- tests/test_cmi.js
+++ tests/test_cmi.js
@@ -21,4 +21,4 @@ var daymet = ee.ImageCollection("NASA/ORNL/DAYMET_V4")
 // Usage: calc_CMI(img)
 var cmi_daymet = cmi.calc_CMI(daymet);
 print(cmi_daymet);
-Map.addLayer(cmi_daymet.select('CMI'), {palette: palettes.colorbrewer.RdBu[5]});
+Map.addLayer(cmi_daymet.select('CMI'), {min: -30, max: 1, palette: palettes.colorbrewer.RdBu[5]});
