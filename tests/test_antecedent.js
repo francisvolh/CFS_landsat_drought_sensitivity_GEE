a23944434214a10036013853da12d6ae6fbfa744
robit.alec <robit.alec@gmail.com>
2022-02-12 02:52:58 +0000

    Created tests/test_antecedent.js.

---
+++ tests/test_antecedent.js
@@ -0,0 +1,28 @@
+/*
+Testing: modules/antecedent.js
+Alec L. Robitaille
+*/
+
+// Load antecedent module
+var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
+
+// Load palettes module
+var palettes = require('users/gena/packages:palettes');
+
+// Load get_daymet module
+var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
+
+// Set years, months
+var years = ee.List.sequence(2010, 2015);
+var months = ee.List.sequence(5, 7);
+
+// Get Daymet collection
+var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
+
+// Test antecedent_means
+// Usage: antecedent_mean(images, band, year_list)
+var ante_means = antecedent.antecedent_means(monthly_daymet, 'CMI', years);
+print(ante_means);
+Map.addLayer(ante_means.select('prcp'), {min:0, max:500});
+
+
