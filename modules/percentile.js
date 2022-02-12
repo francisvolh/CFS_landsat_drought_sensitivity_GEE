03ec52b0b9790275b9439c602635d151e4275a1b
robit.alec <robit.alec@gmail.com>
2022-02-12 17:49:17 +0000

    Updated modules/percentile.js (added 33 lines, deleted 73 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -1,80 +1,40 @@
-// Means of antecedent periods within years
-exports.antecedentMeans = function(images, band, years) {
-  // Combine yearly images into a collection
-  return ee.ImageCollection.fromImages(years.map(function(yr) {
-    // Today - July 1
-    var today = ee.Date.fromYMD(yr, 7, 1);
-
-    // Setup output band names
-    var band3mo = band + '_ante3mo';
-    // var band6mo = band + '_ante6mo';
-    var band12mo = band + '_ante12mo';
-    var band5yr = band + '_ante5yr';
-    var band5yrmean = band + '_ante5yr' + '_mean';
-
-    return ee.Image([
-      // Antecedent: 3 (months 3-6)
-      // Filter to year, and within antecedent period
-      // Reduce with mean reducer
-      images.filter(ee.Filter.date(today.advance(-3, 'month'), today))
-            .select([band], [band3mo])
-            .reduce(ee.Reducer.mean()),
-
-      // Antecedent: 6 (months 1-6)
-      // Filter to year, and within antecedent period
-      // Reduce with mean reducer
-      // images.filter(ee.Filter.date(today.advance(-6, 'month'), today))
-      //       .select([band], [band6mo])
-      //       .reduce(ee.Reducer.mean()),
-
-      // Antecedent: 12 (months 6-6 year previous)
-      // Filter to year, and within antecedent period
-      // Reduce with mean reducer
-      images.filter(ee.Filter.date(today.advance(-1, 'year'), today))
-            .select([band], [band12mo])
-            .reduce(ee.Reducer.mean()),
-
-      // Antecedent: 5 (driest in previous 5 years)
-      ee.ImageCollection([
-        images.filter(ee.Filter.date(today.advance(-1, 'year'), today))
-              .select([band], [band5yr])
-              .reduce(ee.Reducer.mean()),
-        images.filter(ee.Filter.date(today.advance(-2, 'year'), today.advance(-1, 'year')))
-              .select([band], [band5yr])
-              .reduce(ee.Reducer.mean()),
-        images.filter(ee.Filter.date(today.advance(-3, 'year'), today.advance(-2, 'year')))
-              .select([band], [band5yr])
-              .reduce(ee.Reducer.mean()),
-        images.filter(ee.Filter.date(today.advance(-4, 'year'), today.advance(-3, 'year')))
-              .select([band], [band5yr])
-              .reduce(ee.Reducer.mean()),
-        images.filter(ee.Filter.date(today.advance(-5, 'year'), today.advance(-4, 'year')))
-              .select([band], [band5yr])
-              .reduce(ee.Reducer.mean())
-        ]).reduce(ee.Reducer.min())
-      ]).set({'year': yr});
-  }));
-};
+/*
+Percentiles
+Alec L. Robitaille
+*/
 
 // Compare percentile images for each antecedent period to each image's antecedent means
-exports.ltPercentile = function(means, percentiles) {
-  var percent3 = means.select(['CMI_ante3mo_mean'], ['CMI_lt_ante3mo']).reduce(ee.Reducer.percentile(percentiles));
-  // var percent6 = means.select(['CMI_ante6mo_mean'], ['CMI_lt_ante6mo']).reduce(ee.Reducer.percentile(percentiles));
-  var percent12 = means.select(['CMI_ante12mo_mean'], ['CMI_lt_ante12mo']).reduce(ee.Reducer.percentile(percentiles));
-  var percent5 = means.select(['CMI_ante5yr_mean_min'], ['CMI_lt_ante5yr']).reduce(ee.Reducer.percentile(percentiles));
+var lt_percentile = function(means, percentile_list) {
+  var percentile_3mo = means.select(['CMI_ante3mo_mean'], ['CMI_lt_ante3mo'])
+                            .reduce(ee.Reducer.percentile(percentile_list));
 
-  return means.map(function(img){
-    return ee.Image([
+  var percentile_6mo = means.select(['CMI_ante6mo_mean'], ['CMI_lt_ante6mo'])
+                            .reduce(ee.Reducer.percentile(percentile_list));
+
+  var percentile_12mo = means.select(['CMI_ante12mo_mean'], ['CMI_lt_ante12mo'])
+                             .reduce(ee.Reducer.percentile(percentile_list));
+
+  var percentile_5yr = means.select(['CMI_ante5yr_mean_min'], ['CMI_lt_ante5yr'])
+                            .reduce(ee.Reducer.percentile(percentile_list));
+
+	var means_lt_percentiles = means.map(function(img) {
+		return ee.Image([
+
       img.select('CMI_ante3mo_mean')
-         .lt(percent3),
+         .lt(percentile_3mo),
+
       // img.select('CMI_ante6mo_mean')
-      //   .lt(percent6),
+      //   .lt(percentile_6mo),
+
       img.select('CMI_ante12mo_mean')
-         .lt(percent12),
+         .lt(percentile_12mo),
+
       img.select('CMI_ante5yr_mean_min')
-        .lt(percent5)
+        .lt(percentile_5yr)
+
       ]).copyProperties(img);
-  });
+	});
+
+	return means_lt_percentiles;
 };
-
-
+exports lt_percentile = lt_percentile;
