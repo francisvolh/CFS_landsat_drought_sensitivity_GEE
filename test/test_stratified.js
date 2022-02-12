1041e32a0f464332831fe0e104244aa7d4d9686a
robit.alec <robit.alec@gmail.com>
2022-02-12 00:56:12 +0000

    Updated test/test_stratified.js (added 2 lines, deleted 1 line).

--- test/test_stratified.js
+++ test/test_stratified.js
@@ -20,4 +20,5 @@ var geometry = ee.Geometry.Polygon([[[-96.188, 54.036], [-96.188, 51.866], [-92.
 // Test stratified_sample
 // Usage: stratified.stratified_sample(img, band, region, n_pts)
 var strat = stratified.stratified_sample(lc, 'land_cover', geometry, 100);
-
+print(strat);
+Map.addLayer(strat);
