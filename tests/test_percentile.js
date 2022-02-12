943b9dfe75ef7ac9131ea4048d0dc0f5c09eccc0
robit.alec <robit.alec@gmail.com>
2022-02-12 19:11:04 +0000

    Updated tests/test_percentile.js (added 1 line, deleted 1 line).

--- tests/test_percentile.js
+++ tests/test_percentile.js
@@ -48,7 +48,7 @@ Map.addLayer(percentile_images.select('CMI_ante3mo_mean_p10'), cmi_viz, '2010-20
 
 
 // Test lt_percentile
-// Usage: lt_percentile(means, percentile_images)
+// Usage: lt_percentile(images, percentile_images)
 var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
 print('Less than percentile'); print(lt_percent);
 Map.addLayer(lt_percent.select('CMI_ante3mo_mean_p10').first(), {min:0, max:0}, '2010 CMI lt 10th percentile 3 month antecedent');
