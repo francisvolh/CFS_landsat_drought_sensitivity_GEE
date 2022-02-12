8128df890de026264f8a4e26b84974307357fd9e
robit.alec <robit.alec@gmail.com>
2022-02-12 19:49:04 +0000

    Updated tests/test_fire.js (added 5 lines, deleted 1 line).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -8,6 +8,9 @@ Alec L. Robitaille
 // Load fire module
 var fire = require('users/robitalec/CFS:modules/fire.js');
 
+// Load land cover module
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+
 
 // Geometry
 var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
@@ -22,7 +25,8 @@ print(five_year_fires);
 
 // Test mask_five_year_fires
 // Usage: fire.mask_five_year_fires(img)
-var img = ee.Image.random().set('year', 2017);
+var img = land_cover.get_land_cover().filter(ee.Filter.eq('year', 2013)).first();
+
 var mask_five_year_fires = fire.mask_five_year_fires(img);
 print(mask_five_year_fires);
 
