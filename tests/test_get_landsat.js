cb53560cd6d0445849038c8952230b0a650c701d
robit.alec <robit.alec@gmail.com>
2022-02-12 02:15:08 +0000

    Updated tests/test_get_landsat.js (added 7 lines, deleted 7 lines).

--- tests/test_get_landsat.js
+++ tests/test_get_landsat.js
@@ -1,11 +1,11 @@
 /*
-Testing: modules/get_collection.js
+Testing: modules/get_landsat.js
 Alec L. Robitaille
 */
 
 
-// Load get_collection module
-var get_collection = require('users/robitalec/CFS:modules/get_collection.js');
+// Load get_landsat module
+var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
 
 // Geometry
 var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
@@ -13,13 +13,13 @@ var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
 
 
 // Test SR collection
-// Usage: get_collection.get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region)
-var sr_col = get_collection.get_SR(2014, 2019, '06-15', '07-15', geometry);
+// Usage: get_landsat.get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region)
+var sr_col = get_landsat.get_SR(2014, 2019, '06-15', '07-15', geometry);
 print(sr_col);
 
 // Test indices collection
-// Usage: get_collection.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region, indices)
-var indices_col = get_collection.get_indices(2014, 2019, '06-15', '07-15', geometry, ['NDVI', 'EVI']);
+// Usage: get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region, indices)
+var indices_col = get_landsat.get_indices(2014, 2019, '06-15', '07-15', geometry, ['NDVI', 'EVI']);
 print(indices_col);
 
 // Map
