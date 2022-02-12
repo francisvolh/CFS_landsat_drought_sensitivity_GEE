d7f35325491826f6e8eee917a0dc2c1256d1068b
robit.alec <robit.alec@gmail.com>
2022-02-12 18:03:12 +0000

    Updated modules/percentile.js (added 18 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -3,6 +3,24 @@ Percentiles
 Alec L. Robitaille
 */
 
+// Get percentile
+var get_percentile = function(images, percentile_list) {
+  var percentile_3mo = means.select(['CMI_ante3mo_mean'], ['CMI_ante3mo_lt'])
+                            .reduce(ee.Reducer.percentile(percentile_list));
+
+  // var percentile_6mo = means.select(['CMI_ante6mo_mean'], ['CMI_ante6mo_lt'])
+  //                           .reduce(ee.Reducer.percentile(percentile_list));
+
+  var percentile_12mo = means.select(['CMI_ante12mo_mean'], ['CMI_ante12mo_lt'])
+                             .reduce(ee.Reducer.percentile(percentile_list));
+
+  var percentile_5yr = means.select(['CMI_ante5yr_mean_min'], ['CMI_ante5yr_lt'])
+                            .reduce(ee.Reducer.percentile(percentile_list));
+                            
+  return(ee.Image([percentile_3mo, percentile_12mo, percentile_5yr]));
+};
+
+
 // Compare percentile images for each antecedent period to each image's antecedent means
 var lt_percentile = function(means, percentile_list) {
   var percentile_3mo = means.select(['CMI_ante3mo_mean'], ['CMI_ante3mo_lt'])
