f81d2e02ed411da07c7e85c9d6589f0d5b1d3298
robit.alec <robit.alec@gmail.com>
2022-02-12 02:54:02 +0000

    Updated tests/test_get_daymet.js (added 2 lines, deleted 2 lines).

--- tests/test_get_daymet.js
+++ tests/test_get_daymet.js
@@ -14,7 +14,7 @@ var reducer = ee.Reducer.mean().combine(ee.Reducer.sum(), null, true);
 
 
 // Test get_monthly_daymet
-// Usage: get_monthly_daymet(year_list, month_list, reducer)
-var monthly_daymet = get_daymet.get_monthly_daymet(years, months, reducer);
+// Usage: get_monthly_daymet(year_list, month_list)
+var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
 print(monthly_daymet);
 Map.addLayer(monthly_daymet.select('prcp'), {min:0, max:500});
