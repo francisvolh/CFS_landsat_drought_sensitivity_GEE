ffc04ba7285f37db54a8d0f1789ec5f6a270ff79
robit.alec <robit.alec@gmail.com>
2022-02-12 02:04:42 +0000

    Updated tests/test_cmi.js (added 14 lines).

--- tests/test_cmi.js
+++ tests/test_cmi.js
@@ -6,3 +6,17 @@ Alec L. Robitaille
 
 // Load CMI module
 var cmi = require('users/robitalec/CFS:modules/cmi.js');
+
+// Load palettes module
+var palettes = require('users/gena/packages:palettes');
+
+
+// Load Daymet V4
+var daymet = ee.ImageCollection("NASA/ORNL/DAYMET_V4").first();
+
+
+// Test calc_CMI
+// Usage: calc_CMI(img)
+var cmi_daymet = calc_CMI(daymet);
+print(cmi_daymet);
+Map.addLayer(cmi_daymet, {palette: palettes.colorbrewer.RdBu[5]});
