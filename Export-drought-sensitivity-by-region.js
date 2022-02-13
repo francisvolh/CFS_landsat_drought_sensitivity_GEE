58652053b6e596a6846d1779de4d53974b6aa6e3
robit.alec <robit.alec@gmail.com>
2022-02-13 06:43:17 +0000

    Updated Export-drought-sensitivity-by-region.js (added 9 lines, deleted 3 lines).

--- Export-drought-sensitivity-by-region.js
+++ Export-drought-sensitivity-by-region.js
@@ -77,6 +77,8 @@ var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
 
 var eco_ids = ecoregions.filterBounds(geometry).limit(2).aggregate_array('ECOREGI')
 
+
+
 // Asynchronously pass the object's value to the callback function
 eco_ids.evaluate(
   function(ecoreg) {
@@ -87,9 +89,13 @@ eco_ids.evaluate(
         var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id))
         var indices_col = get_landsat.get_indices(min_year, max_year, '07-01', '07-31', ft.geometry(), ['NDVI', 'EVI', 'NBR']);
         var points = stratified.stratified_sample(lc, 'land_cover', ft.geometry(), 250);
-        var join = ee.Join.simple();
-        var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
-        var sampled = ee.ImageCollection(joined).map(function(img) {return img.reduceRegions(points, ee.Reducer.mean(), 30)});
+        // var join = ee.Join.saveAll();
+        // var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
+        var joined = indices_col.merge(ante_means)
+        print(joined)
+        var sampled = ee.ImageCollection(joined).map(function(img) {
+          return img.reduceRegions(points, ee.Reducer.mean(), 30)
+        });
 
         Export.table.toDrive(sampled.flatten(), ecoreg_id, 'Batch-ecoregion-export')
       })
