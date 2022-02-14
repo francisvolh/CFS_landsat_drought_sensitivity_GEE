26ea5ffc819c41d3a6974cc64d2eaacfb6cd424f
robit.alec <robit.alec@gmail.com>
2022-02-14 21:28:23 +0000

    Updated tests/test_split_drought.js (added 1 line, deleted 1 line).

--- tests/test_split_drought.js
+++ tests/test_split_drought.js
@@ -36,7 +36,7 @@ var indices_col = get_landsat.get_indices(min_year, max_year, '06-15', '07-15', 
 
 
 // Test split_drought
-// Usage: split_drought.split_drought(image)
+// Usage: split_drought.split_drought(indices_col, lt_percent, antecedent_list, percentile_list, index_list)
 var split_drought = split_drought.split_drought(indices_col, lt_percent, antecedent_list, percentile_list, index_list);
 print('Less than percentile'); print(lt_percent);
 Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p10').first(), {min:0, max:1}, '2010 CMI lt 10th percentile 3 month antecedent');
