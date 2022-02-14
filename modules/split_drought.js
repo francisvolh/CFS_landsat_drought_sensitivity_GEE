eda2310aa4e72590f98ebc7cbe2f40f06859d664
robit.alec <robit.alec@gmail.com>
2022-02-14 21:23:35 +0000

    Updated modules/split_drought.js (added 1 line, deleted 1 line).

--- modules/split_drought.js
+++ modules/split_drought.js
@@ -22,7 +22,7 @@ var split_drought = function(images, percentile_masks, antecedent_list, percenti
           // Loop over index_list
           return index_list.map(function(index) {
             // Set up band names
-            var lt_band = 'CMI_lt_ante' + antecedent_period + '_p' + percentile;
+            var lt_band = 'CMI_ante' + antecedent_period + '_p' + percentile;
             var id = index + '_ante' + antecedent_period + '_p' + percentile;
             var drought_band = id + '_drought';
             var base_band = id + '_base';
