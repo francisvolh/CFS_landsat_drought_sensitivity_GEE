35fa374b374680acf07ce2269131531a8f75bcb6
robit.alec <robit.alec@gmail.com>
2022-02-14 20:39:44 +0000

    Updated tests/test_split_drought.js (added 5 lines, deleted 13 lines).

--- tests/test_split_drought.js
+++ tests/test_split_drought.js
@@ -23,21 +23,13 @@ var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
 
 
 
-// Get daymet
+// Processing
 var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
-
-// Calculate CMI
 var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
-
-// Antecedent means
 var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
-
-// Percentile masks
+var percentile_images = percentile.get_percentile(ante_means, percentile_list);
 var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
-
-// Get indices
 var indices_col = get_landsat.get_indices(years[1], years[2], '06-15', '07-15', geometry, index_list);
-
 
 
 
