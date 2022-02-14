08771faa58c801250c640c3c9f0dbf9703ee3141
robit.alec <robit.alec@gmail.com>
2022-02-14 21:00:23 +0000

    Updated modules/percentile.js (added 8 lines, deleted 4 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -12,12 +12,16 @@ exports.get_percentile = get_percentile;
 // Compare percentile images for each antecedent period to each image's antecedent means
 var lt_percentile = function(images, percentile_images) {
 	var images_lt_percentiles = images.map(function(img) {
-    return ee.Image([
-      percentile_images.select('CMI_ante3mo.*').gte(img.select('CMI_ante3mo_mean')),
-      // percentile_images.select('CMI_ante6mo.*').gte(img.select('CMI_ante6mo_mean')),
+	  var out = ee.Image([
+	    percentile_images.select('CMI_ante3mo.*').gte(img.select('CMI_ante3mo_mean')),
+	    // percentile_images.select('CMI_ante6mo.*').gte(img.select('CMI_ante6mo_mean')),
       percentile_images.select('CMI_ante12mo.*').gte(img.select('CMI_ante12mo_mean')),
       percentile_images.select('CMI_ante5yr.*').gte(img.select('CMI_ante5yr_mean_min'))
-     ]).copyProperties(img);
+      ]).copyProperties(img);
+    var new_names = out.bandNames();
+    new_names = new_names.replaceAll('_mean', '_lt').replaceAll('_min', '');
+    out = out.rename(new_names);
+    return out;
 	});
 	return images_lt_percentiles;
 };
