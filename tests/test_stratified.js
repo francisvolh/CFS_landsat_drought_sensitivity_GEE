4653a861a1a1f93eded73bf62d0eb8d3700c81f5
robit.alec <robit.alec@gmail.com>
2022-02-14 20:27:56 +0000

    Updated tests/test_stratified.js (added 7 lines, deleted 12 lines).

--- tests/test_stratified.js
+++ tests/test_stratified.js
@@ -1,25 +1,22 @@
 /*
 Testing: modules/stratified.js
 Alec L. Robitaille
-
 */
 
 
-// Load stratified module
+// Load modules
 var stratified = require('users/robitalec/CFS:modules/stratified.js');
-
-// Load land cover module
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
-
-// Load palettes module
 var palettes = require('users/gena/packages:palettes');
 
-// Region
+// Set variables
 var geometry = ee.Geometry.Polygon([[[-96.677, 52.700], [-96.677, 52.214], [-95.853, 52.214], [-95.853, 52.7]]]);
 
-// Load Hermosilla land cover
+// Load collection
 var hermosilla_2022 = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 var lc = hermosilla_2022.first();
+
+
 
 // Test stratified_sample
 // Usage: stratified.stratified_sample(img, band, region, n_pts)
@@ -29,7 +26,6 @@ Map.addLayer(geometry);
 Map.addLayer(lc, {palette: palettes.crameri.batlow[25]});
 Map.addLayer(strat);
 
-
 // Test stratified_sample for two classes
 // Usage: stratified.stratified_sample(img, band, region, n_pts)
 lc = lc.updateMask(lc.eq(20).or(lc.eq(220)));
@@ -38,4 +34,3 @@ print(strat);
 Map.addLayer(geometry);
 Map.addLayer(lc, {palette: palettes.crameri.batlow[25]});
 Map.addLayer(strat);
-
