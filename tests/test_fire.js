8c99ef8cc965d0f102a1c2af9c5adb2e3e940af2
robit.alec <robit.alec@gmail.com>
2022-02-14 20:25:50 +0000

    Updated tests/test_fire.js (added 6 lines, deleted 12 lines).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -1,35 +1,29 @@
 /*
 Testing: modules/fire.js
 Alec L. Robitaille
-
 */
 
 
-// Load fire module
+// Load modules
 var fire = require('users/robitalec/CFS:modules/fire.js');
-
-// Load land cover module
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 
 
-// Geometry
+// Set variables
 var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
                                     [-105.988, 58.941], [-105.988, 59.673]]]);
+
 
 
 // Test five_year_fires
 // Usage: fire.five_year_fires(year)
 var five_year_fires = fire.five_year_fires(2013);
 print(five_year_fires);
-
+Map.addLayer(five_year_fires, {min:0, max:1, palette: ['000000', 'ffc781']}, 'five_year_firest');
 
 // Test mask_five_year_fires
 // Usage: fire.mask_five_year_fires(img)
 var img = land_cover.get_land_cover().filter(ee.Filter.eq('year', 2013)).first();
 var mask_five_year_fires = fire.mask_five_year_fires(img);
 print(mask_five_year_fires);
-
-
-// Map
-Map.addLayer(five_year_fires, {min:0, max:1, palette: ['000000', 'ffc781']});
-Map.addLayer(mask_five_year_fires);
+Map.addLayer(mask_five_year_fires, null, 'mask_five_year_fires');
