d28b2f586ddc5a771b1d1ef3e7164f9ab49c1d24
robit.alec <robit.alec@gmail.com>
2022-02-15 01:00:49 +0000

    Updated modules/utils.js (added 1 line, deleted 1 line).

--- modules/utils.js
+++ modules/utils.js
@@ -13,7 +13,7 @@ exports.set_year = set_year;
 var set_date = function(img) {
 	return img.set('system:time_start', img.date().millis());
 };
-exports.set_year = set_year;
+exports.set_date = set_date;
 
 
 // Aggregate each month within each year with reducer
