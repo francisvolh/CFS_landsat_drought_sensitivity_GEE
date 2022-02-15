456e9f01825695d3d95539f823ea313b7c114107
robit.alec <robit.alec@gmail.com>
2022-02-15 00:32:17 +0000

    Updated modules/sensitivity.js (added 1 line, deleted 1 line).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -10,7 +10,7 @@ S {P,T,L} = baseline EVI{P} – drought EVI{P,T,L}
 */
 
 var sensitivity_absolute = function(split_indices, antecedent_list, percentile_list, index_list) {
-	var means = split_indices.reduce(ee.Reducer.mean());
+	var means = split_indices.mean();
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return percentile_list.map(function(percentile) {
         return index_list.map(function(index) {
