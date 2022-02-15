a6330c5f8fd175099e98faf54a939d336331730d
robit.alec <robit.alec@gmail.com>
2022-02-15 00:04:36 +0000

    Updated modules/cmi.js (deleted 31 lines).

--- modules/cmi.js
+++ modules/cmi.js
@@ -97,34 +97,3 @@ var calc_CMI = function(img) {
 	return calc_CMI_band(img);
 };
 exports.calc_CMI = calc_CMI;
-
-
-
-
-// Load Aggregate functions
-var agg = require('users/robitalec/CFS:modules/aggregate.js');
-
-// Prep Daymet for CMI
-exports.prepDaymet = function(minyear, maxyear) {
-  // Set list of years and months
-  var months = ee.List.sequence(1, 12);
-  var years = ee.List.sequence(minyear, maxyear);
-
-  // Reducer
-  // Combine both mean and sum reducers
-  var reducer = ee.Reducer.mean().combine(ee.Reducer.sum(), null, true);
-
-  // Filter daymet within years
-  var daymet = ee.ImageCollection("NASA/ORNL/DAYMET_V3")
-    .filter(ee.Filter.calendarRange(minyear, maxyear, 'year'));
-
-  // Aggregate monthly for each year
-  // Using reducer
-  var aggDaymet = agg.aggregateMY(years, months, daymet, reducer);
-
-  // Only keep tmin tmax mean and prcp sum
-  aggDaymet = aggDaymet.select(['tmin_mean', 'tmax_mean', 'prcp_sum'],
-                               ['tmin', 'tmax', 'prcp']);
-
-  return aggDaymet
-}
