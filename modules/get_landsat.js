db03e70141ca5bb68728757e488d8c78b4c8c105
robit.alec <robit.alec@gmail.com>
2022-02-15 00:59:17 +0000

    Updated modules/get_landsat.js (added 5 lines, deleted 6 lines).

--- modules/get_landsat.js
+++ modules/get_landsat.js
@@ -35,15 +35,14 @@ var get_indices = function(min_year, max_year, min_mm_dd, max_mm_dd, region, ind
 	var collection = get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region);
 
 	return(ltgee.transformSRcollection(collection, indices)
-              .map(setYear)
               // .map(utils.set_year)
               // .map(function(img) {return img.divide(1000).copyProperties(img)})
               );
 };
 exports.get_indices = get_indices;
 
-// Set year
-var setYear = function(img) {
-  return img.set('year', img.date().get('year'));
-};
-exports.setYear = setYear;
+// // Set year
+// var setYear = function(img) {
+//   return img.set('year', img.date().get('year'));
+// };
+// exports.setYear = setYear;
