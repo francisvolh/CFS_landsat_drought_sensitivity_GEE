809e764b78a46f45a1ba9e73862e34f967ac3931
robit.alec <robit.alec@gmail.com>
2022-02-15 00:13:50 +0000

    Updated tests/test_sensitivity.js (added 5 lines, deleted 1 line).

--- tests/test_sensitivity.js
+++ tests/test_sensitivity.js
@@ -39,7 +39,11 @@ var split_drought = split_drought.split_drought(indices_col, lt_percent, anteced
 // Test sensitivity_relative
 // Usage: sensitivity.sensitivity_relative(split_indices, antecedent_list, percentile_list, index_list)
 var sens_relative = sensitivity.sensitivity_relative(split_drought, antecedent_list, percentile_list, index_list);
+
+// Test sens_absolute
+// Usage: sensitivity.sens_absolute(split_indices, antecedent_list, percentile_list, index_list)
 var sens_absolute = sensitivity.sensitivity_absolute(split_drought, antecedent_list, percentile_list, index_list);
+
 print('Absolute sensitivity'); print(sens_absolute);
 print('Relative sensitivity'); print(sens_relative);
 Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p10').first(), {min:0, max:1}, '2010 CMI lt 10th percentile 3 month antecedent');
