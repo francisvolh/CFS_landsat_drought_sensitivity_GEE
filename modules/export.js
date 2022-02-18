58ac84c4ee8ae9810184a70ac118b0e4f8954ed4
robit.alec <robit.alec@gmail.com>
2022-02-18 21:20:14 +0000

    Updated modules/export.js (added 3 lines, deleted 5 lines).

--- modules/export.js
+++ modules/export.js
@@ -4,7 +4,7 @@ var export_by_ecoregion = function(ecoregions, geometry) {
     .aggregate_array('ECOREGI')
     .distinct();
     
-  ids.evaluate(function(id) {
+  id_list.evaluate(function(id) {
     // Reducer for each element of the object
     id.forEach(function(ecoreg_id) {
         var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id))
