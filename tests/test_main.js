b5897441d1417e176e1364d0ccf773dfe88aa476
robit.alec <robit.alec@gmail.com>
2022-02-18 22:03:52 +0000

    Updated tests/test_main.js (added 1 line, deleted 1 line).

--- tests/test_main.js
+++ tests/test_main.js
@@ -26,5 +26,5 @@ var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregion
 
 // Test main
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-var main_relative = main.main('relative sensitivity', ecoregions.first(), min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+var main_relative = main.main('relative sensitivity', ecoregions.limit(1), min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 Map.addLayer(main_relative)
