e3cd2131a9a63b313ea93f8c7a5e54fecd417081
robit.alec <robit.alec@gmail.com>
2022-02-18 22:02:11 +0000

    Updated modules/main.js (added 1 line).

--- modules/main.js
+++ modules/main.js
@@ -12,6 +12,7 @@ var cmi = require('users/robitalec/CFS:modules/cmi.js');
 var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
 var percentile = require('users/robitalec/CFS:modules/percentile.js');
 var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
+var split_drought = require('users/robitalec/CFS:modules/split_drought.js');
 
 
 
