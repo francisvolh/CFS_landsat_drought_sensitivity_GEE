8c58dabc7a682ff35b7504a40e26b6dd0ae4e52a
robit.alec <robit.alec@gmail.com>
2022-02-18 22:29:06 +0000

    Updated tests/test_main.js (added 3 lines, deleted 2 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -16,6 +16,7 @@ var percentile_list = [10, 20];
 var index_list = ['NDVI', 'NBR'];
 var rel_viz = {min:-50, max:50, palette: palettes.colorbrewer.RdBu[5]};
 var abs_viz = {min:-0.5, max:0.5, palette: palettes.colorbrewer.RdBu[5]};
+var cmi_viz = {min:-30, max:30, palette: palettes.colorbrewer.RdBu[5]};
 
 
 // Load an ecoregion
@@ -27,8 +28,8 @@ print(ecoregion);//.aggregate_array('ECOREG_ID'))
 // Test main - index + antecedent means
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_index_and_antecedent = main.main('vegetation index and antecedent means', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-// Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p10'), rel_viz, '2000-2015 relative drought sensitivity NDVI 10th percentile 3 month antecedent');
-print(main_index_and_antecedent);
+Map.addLayer(main_index_and_antecedent.select('CMI_ante3mo_mean').first(), cmi_viz, '2000 CMI 3 month antecedent mean');
+Map.addLayer(main_index_and_antecedent.select('NDVI').first(), {min:-1, max:1}, '2000 NDVI');
 
 // Test main - relative
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
