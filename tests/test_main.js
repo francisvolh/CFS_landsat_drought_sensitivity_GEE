2eb47d6babad5a06907618b73c366940b3c7d993
robit.alec <robit.alec@gmail.com>
2022-02-18 22:33:51 +0000

    Updated tests/test_main.js (added 1 line, deleted 1 line).

--- tests/test_main.js
+++ tests/test_main.js
@@ -33,7 +33,7 @@ Map.addLayer(main_index_and_antecedent.select('NDVI').first(), {min:-1, max:1}, 
 // Test main - relative
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_relative = main.main('relative sensitivity', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p10'), rel_viz, '2000-2015 relative drought sensitivity NDVI 10th percentile 3 month antecedent');
+Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p10'), rel_viz, '2000-2015 relative drought sensitivity NDVI 10th percentile 3 month antecedent', false);
 Map.centerObject(ecoregion);
 
 // Test main - absolute
