75e0b7c23ee0b3c553d1b4ac3213a5aa86d72bd5
robit.alec <robit.alec@gmail.com>
2022-02-18 22:04:05 +0000

    Updated modules/main.js (added 3 lines, deleted 3 lines).

--- modules/main.js
+++ modules/main.js
@@ -41,9 +41,9 @@ var main = function(output, region,
   var split = split_drought.split_drought(indices_col, lt_percent, antecedent_list, percentile_list, index_list);
 
   if (output == 'relative sensitivity') {
-    return sensitivity.sensitivity_relative(split_drought, antecedent_list, percentile_list, index_list);
+    return sensitivity.sensitivity_relative(split, antecedent_list, percentile_list, index_list);
   } else if (output == 'absolute sensitivity') {
-    return sensitivity.sensitivity_absolute(split_drought, antecedent_list, percentile_list, index_list);
+    return sensitivity.sensitivity_absolute(split, antecedent_list, percentile_list, index_list);
   } else if (output == 'vegetation index and antecedent means') {
     var join = ee.Join.inner();
     var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
