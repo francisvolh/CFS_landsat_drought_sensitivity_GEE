d45b99115ad7089451f809d6306e8236a5c2035d
robit.alec <robit.alec@gmail.com>
2022-02-18 22:02:01 +0000

    Updated tests/test_main.js (added 4 lines, deleted 1 line).

--- tests/test_main.js
+++ tests/test_main.js
@@ -5,6 +5,7 @@ Alec L. Robitaille
 
 // Load modules
 var main = require('users/robitalec/CFS:modules/main.js');
+var palettes = require('users/gena/packages:palettes');
 
 // Set variables
 var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
@@ -14,6 +15,7 @@ var min_mm_dd = '06-15';
 var max_mm_dd = '07-15';
 var percentile_list = [10, 20, 30];
 var index_list = ['NDVI', 'NBR'];
+var cmi_viz = {min:-30, max:30, palette: palettes.colorbrewer.RdBu[5]};
 
 
 // Load ecoregions
@@ -25,3 +27,4 @@ var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregion
 // Test main
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_relative = main.main('relative sensitivity', ecoregions.first(), min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+Map.addLayer(main_relative)
