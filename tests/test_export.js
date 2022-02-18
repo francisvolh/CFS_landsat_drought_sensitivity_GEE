4d894a1c58f3f673e05ab8fd268866be575ea123
robit.alec <robit.alec@gmail.com>
2022-02-18 21:14:58 +0000

    Updated tests/test_export.js (added 6 lines, deleted 17 lines).

--- tests/test_export.js
+++ tests/test_export.js
@@ -1,18 +1,7 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var geometry = 
-    /* color: #d63000 */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-128.6947831116304, 58.709490264301685],
-          [-128.6947831116304, 50.664798971876166],
-          [-111.20454873663037, 50.664798971876166],
-          [-111.20454873663037, 58.709490264301685]]], null, false);
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
 // -- Load regions, land cover
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
 
+
+var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
+          
 print(ecoregions.filterBounds(geometry).aggregate_array('ECOREGI').distinct())
