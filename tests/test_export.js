127738f6ee14a15b23b839571b83e4a2c02e926b
robit.alec <robit.alec@gmail.com>
2022-02-18 21:16:46 +0000

    Updated tests/test_export.js (added 22 lines, deleted 6 lines).

--- tests/test_export.js
+++ tests/test_export.js
@@ -1,7 +1,23 @@
-// -- Load regions, land cover
+/*
+Testing: modules/exports.js
+Alec L. Robitaille
+*/
+
+// Load modules
+var exports = require('users/robitalec/CFS:modules/exports.js');
+
+// Set variables
+var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
+
+
+
+// Load ecoregions
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
 
 
-var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
-          
 print(ecoregions.filterBounds(geometry).aggregate_array('ECOREGI').distinct())
+
+
+// Test export_by_ecoregion
+// Usage:
+
