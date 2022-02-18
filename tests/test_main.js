a2f1a49ba0b78c395dc51129e7c98d14a47d160e
robit.alec <robit.alec@gmail.com>
2022-02-18 22:10:55 +0000

    Updated tests/test_main.js (added 9 lines, deleted 7 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -8,24 +8,26 @@ var main = require('users/robitalec/CFS:modules/main.js');
 var palettes = require('users/gena/packages:palettes');
 
 // Set variables
-var min_year = 1985;
-var max_year = 2019;
+var min_year = 2000;
+var max_year = 2015;
 var min_mm_dd = '06-15';
 var max_mm_dd = '07-15';
-var percentile_list = [10, 20, 30];
+var percentile_list = [10, 20];
 var index_list = ['NDVI', 'NBR'];
-var cmi_viz = {min:-30, max:30, palette: palettes.colorbrewer.RdBu[5]};
+var rel_viz = {min:-20, max:20, palette: palettes.colorbrewer.RdBu[5]};
+var abs_viz = {min:-0.5, max:0.5, palette: palettes.colorbrewer.RdBu[5]};
 
 
 // Load an ecoregion
 var ecoregion = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada')
-  .limit(1);
-
+  .filter(ee.Filter.eq('ECOREGI', 136));
+print(ecoregion);//.aggregate_array('ECOREG_ID'))
 
 
 
 // Test main
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_relative = main.main('relative sensitivity', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-Map.addLayer(main_relative);
+Map.addLayer(ecoregion);
+Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p10'), rel_viz, '2000-2015 relative drought sensitivity NDVI 10th percentile 3 month antecedent');
 Map.centerObject(ecoregion);
