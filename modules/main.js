068b0ce7c79e192b07e725774b15042cf9b53c65
robit.alec <robit.alec@gmail.com>
2022-02-18 21:58:18 +0000

    Updated modules/main.js (deleted 31 lines).

--- modules/main.js
+++ modules/main.js
@@ -53,34 +53,3 @@ var main = function(output, region,
 
 };
 exports.main = main;
-
-
-// VEGETATION
-// get indices: min_year, max_year, min_mm_dd, max_mm_dd, region, indices)
-
-// SAMPLING
-// stratified sample: N points
-
-// CMI
-// year list
-// percentile list
-
-// RETURN
-// either antecedent means, or drought sensitivity
-
-// Daymet
-var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
-
-// Calculate CMI
-var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
-var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
-var percentile_images = percentile.get_percentile(ante_means, percentile_list);
-var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
-// drought calc
-
-
-var points = stratified.stratified_sample(lc, 'land_cover', ft.geometry(), 250);
-
-var sampled = ee.ImageCollection(joined).map(function(img) {
-  return img.reduceRegions(points, ee.Reducer.mean(), 30)
-}).flatten();
