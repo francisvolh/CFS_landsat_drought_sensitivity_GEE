fce1962b79375b7d124ffed67dbaa7909345d3c4
robit.alec <robit.alec@gmail.com>
2022-02-18 21:14:05 +0000

    Created modules/export.js.

---
+++ modules/export.js
@@ -0,0 +1,29 @@
+var export_by_ecoregion = function(ecoregions, geometry) {
+  var eco_ids = ecoregions
+    .filterBounds(geometry)
+    .aggregate_array('ECOREGI')
+    .distinct();
+
+  
+  eco_ids.evaluate(
+  function(ecoreg) {
+    // Reducer for each element of the object
+    ecoreg.forEach(
+      // Export function
+      function(ecoreg_id) {
+        var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id))
+        var indices_col = get_landsat.get_indices(min_year, max_year, '07-01', '07-31', ft.geometry(), ['NDVI', 'EVI', 'NBR']);
+        var points = stratified.stratified_sample(lc, 'land_cover', ft.geometry(), 250);
+        var join = ee.Join.inner();
+        var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
+        joined = joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))});
+        var sampled = ee.ImageCollection(joined).map(function(img) {
+          return img.reduceRegions(points, ee.Reducer.mean(), 30)
+        }).flatten();
+      
+        Export.table.toDrive(sampled, ecoreg_id, 'Batch-ecoregion-export')
+      })
+  })
+
+}
+exports.export_by_ecoregion
