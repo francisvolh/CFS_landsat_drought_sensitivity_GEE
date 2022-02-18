312909aa4752c0caf606bc924b1fc1fcc2cbb0a6
robit.alec <robit.alec@gmail.com>
2022-02-18 22:36:14 +0000

    Updated tests/test_main.js (added 1 line).

--- tests/test_main.js
+++ tests/test_main.js
@@ -27,6 +27,7 @@ var ecoregion = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions
 // Test main - index + antecedent means
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_index_and_antecedent = main.main('vegetation index and antecedent means', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+Map.setOptions('SATELLITE');
 Map.centerObject(ecoregion, 12);
 Map.addLayer(main_index_and_antecedent.select('CMI_ante3mo_mean').first(), cmi_viz, '2000 CMI 3 month antecedent mean', false);
 Map.addLayer(main_index_and_antecedent.select('NDVI').first(), {min:-1, max:1}, '2000 NDVI', false);
