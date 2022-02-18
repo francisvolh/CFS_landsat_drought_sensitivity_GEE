02f478fa2fe4ef430570a8851a6f8d40a0340cf1
robit.alec <robit.alec@gmail.com>
2022-02-18 21:45:09 +0000

    Created tests/test_main.js.

---
+++ tests/test_main.js
@@ -0,0 +1,22 @@
+/*
+Testing: modules/main.js
+Alec L. Robitaille
+*/
+
+// Load modules
+var main = require('users/robitalec/CFS:modules/main.js');
+
+// Set variables
+var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
+
+
+
+// Load ecoregions
+var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
+
+
+
+
+// Test export_by_ecoregion
+// Usage:
+
