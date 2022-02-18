bd937e4d162e1401901909b3ae6a9b2ea611ce02
robit.alec <robit.alec@gmail.com>
2022-02-18 21:48:34 +0000

    Updated modules/main.js (added 4 lines).

--- modules/main.js
+++ modules/main.js
@@ -18,6 +18,10 @@ var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
 var main = function(output, region, 
                     min_year, max_year, min_mm_dd, max_mm_dd, 
                     index_list, percentile_list) {
+  var years = ee.List.sequence(min_year, max_year);
+  var months = ee.List.sequence(1, 12);
+
+                      
   // Get Daymet
   var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
   
