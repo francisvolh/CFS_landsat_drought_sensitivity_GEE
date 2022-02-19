09bf31b1b41cce12b668badb3d9d76d857c824d7
robit.alec <robit.alec@gmail.com>
2022-02-19 01:41:50 +0000

    Updated modules/export.js (added 3 lines, deleted 3 lines).

--- modules/export.js
+++ modules/export.js
@@ -14,9 +14,9 @@ var export_by_ecoregion = function(ecoregions, geometry, output, min_year, max_y
     ecoreg_ids.forEach(function(ecoreg_id) {
         var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
         
-        var main_relative = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-        // var indices_col = get_landsat.get_indices(min_year, max_year, '07-01', '07-31', ft.geometry(), ['NDVI', 'EVI', 'NBR']);
-        // var points = stratified.stratified_sample(lc, 'land_cover', ft.geometry(), 250);
+        var output = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+
+        var points = stratified.stratified_sample(land_cover.lc_and_fire, 'land_cover', ft.geometry(), 250);
         // var join = ee.Join.inner();
         // var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
         // joined = joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))});
