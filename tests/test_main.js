027989aaf922106d3fe0dd64b36869078d88f1c2
robit.alec <robit.alec@gmail.com>
2022-02-19 17:31:47 +0000

    Updated tests/test_main.js (added 3 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -27,6 +27,7 @@ var ecoregion = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions
 // Test main - index + antecedent means
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_index_and_antecedent = main.main('vegetation index and antecedent means', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+print('veg index + antecedent means'); print(main_index_and_antecedent);
 Map.setOptions('SATELLITE');
 Map.centerObject(ecoregion, 12);
 Map.addLayer(main_index_and_antecedent.select('CMI_ante3mo_mean').first(), cmi_viz, '2000 CMI 3 month antecedent mean', false);
@@ -35,10 +36,12 @@ Map.addLayer(main_index_and_antecedent.select('NDVI').first(), {min:-1, max:1}, 
 // Test main - relative
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_relative = main.main('relative sensitivity', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+print('relative sensitivity'); print(main_relative);
 Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p10'), rel_viz, '2000-2015 relative drought sensitivity NDVI 10th percentile 3 month antecedent', false);
 
 
 // Test main - absolute
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_absolute = main.main('absolute sensitivity', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+print('absolute sensitivity'); print(main_absolute);
 Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3mo_p10'), abs_viz, '2000-2015 absolute drought sensitivity NDVI 10th percentile 3 month antecedent');
