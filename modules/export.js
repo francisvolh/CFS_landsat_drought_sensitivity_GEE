6619100a347d55f43fd63cd7ea48d0855fd5a3fe
robit.alec <robit.alec@gmail.com>
2022-02-19 01:39:38 +0000

    Updated modules/export.js (added 4 lines, deleted 2 lines).

--- modules/export.js
+++ modules/export.js
@@ -4,16 +4,18 @@ Alec L. Robitaille
 */
 
 // Export task for each ecoregion
-var export_by_ecoregion = function(ecoregions, geometry) {
+var export_by_ecoregion = function(ecoregions, geometry, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list) {
   var ecoreg_id_list = ecoregions
     .filterBounds(geometry)
     .aggregate_array('ECOREGI')
     .distinct();
     
   ecoreg_id_list.evaluate(function(ecoreg_ids) {
-    // Reducer for each element of the object
     ecoreg_ids.forEach(function(ecoreg_id) {
         var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
+        
+        var main_relative = main.main('relative sensitivity', ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+
         
         // var indices_col = get_landsat.get_indices(min_year, max_year, '07-01', '07-31', ft.geometry(), ['NDVI', 'EVI', 'NBR']);
         // var points = stratified.stratified_sample(lc, 'land_cover', ft.geometry(), 250);
