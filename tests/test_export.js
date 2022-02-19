d8e0bb1cb42c6be29d0e3310fff6215f49a2eb33
robit.alec <robit.alec@gmail.com>
2022-02-19 01:48:24 +0000

    Updated tests/test_export.js (added 6 lines, deleted 6 lines).

--- tests/test_export.js
+++ tests/test_export.js
@@ -13,10 +13,10 @@ var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.2
 
 // Load ecoregions
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
-
+ecoregions = ecoregions.filterBounds(geometry);
 
 
 
 // Test export_by_ecoregion
-// Usage:
+// Usage: export_by_ecoregion(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
 
