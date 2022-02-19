fc88542c3166b2d62d3d22dd7f2b7507eb02fdcc
robit.alec <robit.alec@gmail.com>
2022-02-19 17:39:18 +0000

    Updated tests/test_land_cover.js (added 34 lines, deleted 15 lines).

--- tests/test_land_cover.js
+++ tests/test_land_cover.js
@@ -1,3 +1,17 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #d63000 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-125.10511309395461, 58.994758102907085],
+          [-125.10511309395461, 58.62851304385702],
+          [-124.26877886543899, 58.62851304385702],
+          [-124.26877886543899, 58.994758102907085]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Testing: modules/land_cover.js
 Alec L. Robitaille
@@ -6,10 +20,16 @@ Alec L. Robitaille
 
 // Load modules
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
+
+// Variables
+var geometry =  ee.Geometry.Polygon([[[-125.10, 58.99], [-125.10, 58.62], [-124.26, 58.62], [-124.26, 58.99]]]);
 
 // Load collection
 var lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 var lc_2014 = lc.filterDate('2014-01-01', '2015-01-01').first();
+var indices_col = get_landsat.get_indices(2014, 2019, '06-15', '07-15', geometry, ['NDVI', 'EVI']);
+
 
 
 
@@ -28,15 +48,14 @@ Map.addLayer(lc_collection, null, 'land_cover.get_land_cover()');
 
 // Test mask_land_cover
 // Usage: mask_land_cover(img)
-var img = ee.Image('LANDSAT/LC08/C02/T1_L2/LC08_060016_20140910');
-var img_masked_lc = land_cover.mask_land_cover(img);
-print(img_masked_lc);
-Map.addLayer(img_masked_lc.select('SR_B1'), {palette:'#ffa18b'}, 'land_cover.mask_land_cover(img) - where pink indicates masked fire areas');
+var indices_masked_lc = indices_col.map(land_cover.mask_land_cover);
+print(indices_masked_lc);
+Map.addLayer(indices_masked_lc.select('NDVI'), {palette:'#ffa18b'}, 'land_cover.mask_land_cover(img) - where pink indicates masked fire areas');
 Map.centerObject(img);
 
 // Test mask_land_cover_and_fire
 // Usage: mask_land_cover_and_fire(img)
-var img_masked_lc_and_fire = land_cover.mask_land_cover_and_fire(img);
-print(img_masked_lc_and_fire);
-Map.addLayer(img_masked_lc_and_fire, null, 'land_cover.mask_land_cover_and_fire(img)');
+var indices_masked_lc_and_fire = indices_col.map(land_cover.mask_land_cover_and_fire);
+print(indices_masked_lc_and_fire);
+Map.addLayer(indices_masked_lc_and_fire.select('NDVI'), null, 'land_cover.mask_land_cover_and_fire(img)');
 Map.centerObject(img);
