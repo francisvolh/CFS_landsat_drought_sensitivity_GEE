6cdbaa1bcf366c3476ed2811c0bd30d86e50081e
robit.alec <robit.alec@gmail.com>
2022-02-19 01:47:33 +0000

    Updated modules/export.js (added 14 lines, deleted 14 lines).

--- modules/export.js
+++ modules/export.js
@@ -12,22 +12,22 @@ var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, 
   var ecoreg_id_list = ecoregions
     .aggregate_array('ECOREGI')
     .distinct();
-    
+
   ecoreg_id_list.evaluate(function(ecoreg_ids) {
     ecoreg_ids.forEach(function(ecoreg_id) {
-        var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
-        
-        var output = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-
-        var points = stratified.stratified_sample(land_cover.lc_and_fire, 'land_cover', ft.geometry(), n_pts);
-        
-        var sampled = ee.ImageCollection(output).map(function(img) {
-          return img.reduceRegions(points, ee.Reducer.mean(), 30);
-        }).flatten();
+      var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
+    
+      var output = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
       
-        Export.table.toDrive(sampled, ecoreg_id, folder);
-      });
+      var points = stratified.stratified_sample(land_cover.lc_and_fire, 'land_cover', ft.geometry(), n_pts);
+      
+      var sampled = ee.ImageCollection(output).map(function(img) {
+        return img.reduceRegions(points, ee.Reducer.mean(), 30);
+      }).flatten();
+    
+      var today = new Date().toJSON().slice(0, 10);
+      Export.table.toDrive(sampled, today + ecoreg_id, folder);
+    });
   });
-
 };
 exports.export_by_ecoregion = export_by_ecoregion;
