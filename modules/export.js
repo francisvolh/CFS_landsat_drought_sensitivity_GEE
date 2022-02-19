5a333fc8a45816630c1312a67a057f3fa3b626f5
robit.alec <robit.alec@gmail.com>
2022-02-19 01:42:13 +0000

    Updated modules/export.js (added 3 lines).

--- modules/export.js
+++ modules/export.js
@@ -3,6 +3,9 @@ Exports
 Alec L. Robitaille
 */
 
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+
+
 // Export task for each ecoregion
 var export_by_ecoregion = function(ecoregions, geometry, output, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list) {
   var ecoreg_id_list = ecoregions
