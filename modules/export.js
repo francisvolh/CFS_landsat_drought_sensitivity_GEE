7c2a1fb7790159352f60e9e9af4ca810f785aec8
robit.alec <robit.alec@gmail.com>
2022-02-19 18:06:01 +0000

    Updated modules/export.js (added 17 lines, deleted 9 lines).

--- modules/export.js
+++ modules/export.js
@@ -19,17 +19,25 @@ var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, 
       var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
     
       var output = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-      return output
-      // var lc_2002 = land_cover.lc_and_fire.filter(ee.Filter.eq('year', 2002)).first();
-      // var points = stratified.stratified_sample(lc_2002, 'land_cover', ft.geometry(), n_pts);
+      var lc_2002 = land_cover.lc_and_fire.filter(ee.Filter.eq('year', 2002)).first();
+      var points = stratified.stratified_sample(lc_2002, 'land_cover', ft.geometry(), n_pts);
       
-      // var sampled = ee.ImageCollection(output).map(function(img) {
-      // var sampled = output.map(function(img) {
-      //   return img.reduceRegions(points, ee.Reducer.mean(), 30);
-      // }).flatten();
+      if (output == )      
+
+      
+      if (output == 'relative sensitivity' | output == 'absolute sensitivity') {
+        var sampled = output.reduceRegions(points, ee.Reducer.mean(), 30);
+        return sampled;
+      } else if (output == 'vegetation index and antecedent means') {
+        var sampled = output.map(function(img) {
+          return img.reduceRegions(points, ee.Reducer.mean(), 30);
+        }).flatten();
+        return sampled''
+      } 
+  
     
-      // var today = new Date().toJSON().slice(0, 10);
-      // Export.table.toDrive(sampled, today + ecoreg_id, folder);
+      var today = new Date().toJSON().slice(0, 10);
+      Export.table.toDrive(sampled, today + ecoreg_id, folder);
     });
   });
 };
