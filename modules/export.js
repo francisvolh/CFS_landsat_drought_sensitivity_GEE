f88982846a188057915dc50e521e9fcc37e67443
robit.alec <robit.alec@gmail.com>
2022-02-19 01:44:34 +0000

    Updated modules/export.js (added 4 lines, deleted 6 lines).

--- modules/export.js
+++ modules/export.js
@@ -20,12 +20,10 @@ var export_by_ecoregion = function(output, folder, n_pts, ecoregions, geometry, 
         var output = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 
         var points = stratified.stratified_sample(land_cover.lc_and_fire, 'land_cover', ft.geometry(), n_pts);
-        // var join = ee.Join.inner();
-        // var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
-        // joined = joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))});
-        // var sampled = ee.ImageCollection(joined).map(function(img) {
-        //   return img.reduceRegions(points, ee.Reducer.mean(), 30)
-        // }).flatten();
+        
+        var sampled = ee.ImageCollection(output).map(function(img) {
+          return img.reduceRegions(points, ee.Reducer.mean(), 30)
+        }).flatten();
       
         Export.table.toDrive(sampled, ecoreg_id, 'Batch-ecoregion-export');
       });
