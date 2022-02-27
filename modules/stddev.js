4550d4e3a5fd64a55e765d10d026c36d7148e059
robit.alec <robit.alec@gmail.com>
2022-02-27 21:16:28 +0000

    Created modules/stddev.js.

---
+++ modules/stddev.js
@@ -0,0 +1,11 @@
+/*
+Standard devitation
+Alec L. Robitaille
+*/
+
+// Get standard deviation
+var get_stddev = function(images) {
+	return images.reduce(ee.Reducer.stdDev());
+};
+exports.get_stddev = get_stddev;
+
