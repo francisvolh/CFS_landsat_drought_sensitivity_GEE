fd43b8eef9ae0b058310c7621bbdb3a19aea1860
robit.alec <robit.alec@gmail.com>
2022-02-27 21:26:56 +0000

    Updated modules/stddev.js (added 1 line, deleted 1 line).

--- modules/stddev.js
+++ modules/stddev.js
@@ -5,7 +5,7 @@ Alec L. Robitaille
 
 // Get standard deviation
 var get_stddev = function(images) {
-	return images.reduce(ee.Reducer.sampleStdDev());
+	return images.reduce(ee.Reducer.stdDev());
 };
 exports.get_stddev = get_stddev;
 
