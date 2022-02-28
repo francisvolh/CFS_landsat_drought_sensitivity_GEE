abcf7ebcf99d1f71925f9609e8da8bfd9a1ba483
robit.alec <robit.alec@gmail.com>
2022-02-28 23:42:28 +0000

    Updated tests/test_standardize.js (added 8 lines, deleted 8 lines).

--- tests/test_standardize.js
+++ tests/test_standardize.js
@@ -1,10 +1,10 @@
 /*
-Testing: modules/stddev.js
+Testing: modules/standardize.js
 Alec L. Robitaille
 */
 
 // Load modules
-var stddev = require('users/robitalec/CFS:modules/stddev.js');
+var standardize = require('users/robitalec/CFS:modules/standardize.js');
 var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
 var cmi = require('users/robitalec/CFS:modules/cmi.js');
 var palettes = require('users/gena/packages:palettes');
@@ -15,7 +15,7 @@ var years = ee.List.sequence(2010, 2015);
 var months = ee.List.sequence(1, 12);
 var percentile_list = [5, 10];
 var cmi_viz = {min:-30, max:30, palette: palettes.colorbrewer.RdBu[5]};
-var stddev_viz = {min:-3, max:3, palette: palettes.colorbrewer.RdBu[5]};
+var standardize_viz = {min:-3, max:3, palette: palettes.colorbrewer.RdBu[5]};
 
 // Load collection
 var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
@@ -30,12 +30,12 @@ var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
 
 
 
-// Test get_stddev
-// Usage: get_stddev(images)
-var stddev_images = stddev.get_stddev(ante_means);
+// Test get_standardize
+// Usage: get_standardize(images)
+var standardize_images = standardize.get_standardize(ante_means);
 print('Antecedent means'); print(ante_means);
-print('Standard deviation images'); print(stddev_images);
+print('Standard deviation images'); print(standardize_images);
 Map.addLayer(ante_means.select('CMI_ante3mo_mean'), cmi_viz, '2010-2015 CMI 3 month antecedent means', false);
 Map.addLayer(ante_means.select('CMI_ante3mo_mean').first(), cmi_viz, '2010 CMI 3 month antecedent means');
-Map.addLayer(stddev_images.select('CMI_ante3mo_mean_stdDev'), stddev_viz, '2010-2015 CMI standard deviations');
+Map.addLayer(standardize_images.select('CMI_ante3mo_mean_standardize'), standardize_viz, '2010-2015 CMI standardized');
 
