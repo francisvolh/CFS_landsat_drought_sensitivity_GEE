d7569f1c5702512c22ae3ba9d928cf311168f30c
robit.alec <robit.alec@gmail.com>
2022-02-28 23:44:15 +0000

    Updated modules/standardize.js (added 1 line, deleted 1 line).

--- modules/standardize.js
+++ modules/standardize.js
@@ -7,7 +7,7 @@ Alec L. Robitaille
 var get_standardize = function(images) {
   var mean = images.reduce(ee.Reducer.mean());
   var stddev = images.reduce(ee.Reducer.stdDev());
-	return images.subtract(mean).divide(stddev);
+	return images.map(function(img) return img.subtract(mean).divide(stddev));
 };
 exports.get_standardize = get_standardize;
 
