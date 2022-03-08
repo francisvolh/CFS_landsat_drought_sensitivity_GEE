712329acfac3f2b140fa2987f388ebf129f22b0c
robit.alec <robit.alec@gmail.com>
2022-03-08 20:40:09 +0000

    Updated tests/test_vci.js (added 10 lines, deleted 24 lines).

--- tests/test_vci.js
+++ tests/test_vci.js
@@ -28,33 +28,19 @@ var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
 
 
 
-
-var calc_vci = function(images) {
-  var band = 'NDVI';
-  var max = images.select(band).max();
-  var min = images.select(band).min();
-  
-  return images.map(function(img) {
-    return img.expression('VCI = NDVI', {
-    NDVI: img.select(band),
-    max: max,
-    min: min
-  });
-  });
-};
-
-
-
-// Test indices collection
-// Usage: get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region, indices)
+// Get indices collection
 var indices_col = get_landsat.get_indices(2014, 2019, '06-15', '07-15', geometry, ['NDVI', 'EVI']);
+
+// Test: calc_vci
+// Usage: calc_vci(images)
+var vci_col = calc_vci(indices_col);
+
+
+// Print
 print(indices_col);
-
-
 
 
 // Map
 Map.addLayer(indices_col.select(['NDVI']), {min: -0.5, max:1}, 'NDVI collection');
-Map.addLayer(vci_col)
-
+Map.addLayer(vci_col.select(['VCI']), {min: 0, max: 100}, 'VCI collection');
 Map.centerObject(geometry);
