da0f9f5474cc974631054e195f257be6a3195a71
robit.alec <robit.alec@gmail.com>
2022-03-08 20:47:35 +0000

    Updated Export-drought-sensitivity-by-region.js (added 1 line).

--- Export-drought-sensitivity-by-region.js
+++ Export-drought-sensitivity-by-region.js
@@ -130,6 +130,7 @@ eco_ids.evaluate(
       function(ecoreg_id) {
         var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id))
         var indices_col = get_landsat.get_indices(min_year, max_year, '07-01', '07-31', ft.geometry(), ['NDVI', 'EVI', 'NBR']);
+        indices_col = vhi.calc_vci(indices_col);
         var points = stratified.stratified_sample(lc, 'land_cover', ft.geometry(), 250);
         var join = ee.Join.inner();
         var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
