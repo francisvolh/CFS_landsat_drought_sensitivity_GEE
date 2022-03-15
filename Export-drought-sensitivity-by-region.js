2c0afc05dad66a4d82770fc73b88e12c71b5fe75
robit.alec <robit.alec@gmail.com>
2022-03-15 02:39:00 +0000

    Updated Export-drought-sensitivity-by-region.js (added 16 lines, deleted 12 lines).

--- Export-drought-sensitivity-by-region.js
+++ Export-drought-sensitivity-by-region.js
@@ -1,3 +1,10 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = /* color: #d63000 */ee.Geometry.Polygon(
+        [[[-119.41006076127307, 60.05858985688522],
+          [-119.41006076127307, 55.818937456667584],
+          [-109.93984591752307, 55.818937456667584],
+          [-109.93984591752307, 60.05858985688522]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Export drought sensitivity by region
 Based on: tests/test_exports.js
@@ -8,15 +15,12 @@ Alec L. Robitaille
 var export_by = require('users/robitalec/CFS:modules/export.js');
 
 // Set variables
-var geometry = ee.Geometry.Polygon([[[-119.41006076127307, 60.05858985688522],
-          [-119.41006076127307, 55.818937456667584],
-          [-109.93984591752307, 55.818937456667584],
-          [-109.93984591752307, 60.05858985688522]]]);
+
 var min_year = 1985;
 var max_year = 2021;
 var min_mm_dd = '07-01';
 var max_mm_dd = '07-31';
-var percentile_list = [15, 85];
+var percentile_list = [15];
 var index_list = ['NDVI', 'NBR'];
 var drive_folder = 'Batch-ecoregion-export';
 var n_pts = 50;
