fe3422262a2b50d2afc5b5d16f2365c569310bd5
robit.alec <robit.alec@gmail.com>
2022-03-15 16:58:09 +0000

    Updated Export-drought-sensitivity-by-region.js (added 1 line, deleted 1 line).

--- Export-drought-sensitivity-by-region.js
+++ Export-drought-sensitivity-by-region.js
@@ -20,7 +20,7 @@ var export_by = require('users/robitalec/CFS:modules/export.js');
 // Set variables
 
 var min_year = 1985;
-var max_year = 2021;
+var max_year = 2020;
 var min_mm_dd = '07-01';
 var max_mm_dd = '07-31';
 var percentile_list = [15];
