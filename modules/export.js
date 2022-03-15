f1b498fb529f7ee21391f45a4e0267543674ac09
robit.alec <robit.alec@gmail.com>
2022-03-15 02:00:19 +0000

    Updated modules/export.js (added 1 line, deleted 1 line).

--- modules/export.js
+++ modules/export.js
@@ -20,7 +20,7 @@ var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, 
     
       var out = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
       var lc_modal = land_cover.lc_and_fire.mode();
-      var points = stratified.stratified_sample(lc_2002, 'land_cover', ft.geometry(), n_pts);
+      var points = stratified.stratified_sample(lc_modal, 'land_cover', ft.geometry(), n_pts);
       
       var sampled = ee.ImageCollection(out).map(function(img) {
         return img.reduceRegions(points, ee.Reducer.mean(), 30);
