3519eb1a88406ac07a54633f2c7fbdb338bf00a7
robit.alec <robit.alec@gmail.com>
2022-03-15 02:32:13 +0000

    Updated tests/test_percentile.js (added 4 lines, deleted 4 lines).

--- tests/test_percentile.js
+++ tests/test_percentile.js
@@ -40,13 +40,13 @@ Map.addLayer(percentile_images.select('CMI_ante3mo_mean_p15'), cmi_viz, '2010-20
 
 // Test lt_percentile
 // Usage: lt_percentile(images, percentile_images)
-// var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
-// print('Less than percentile'); print(lt_percent);
-// Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p10').first(), {min:0, max:1}, '2010 CMI lt 15th percentile 3 month antecedent');
+var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
+print('Less than percentile'); print(lt_percent);
+Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p15').first(), {min:0, max:1}, '2010 CMI lt 15th percentile 3 month antecedent');
 
 
 // Test lt_percentile_with_cap
 // Usage: lt_percentile_with_cap(images, percentile_images)
 var lt_percent_cap = percentile.lt_percentile_with_cap(ante_means, percentile_images);
 print('Less than percentile, with cap'); print(lt_percent_cap);
-// Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p10').first(), {min:0, max:1}, '2010 CMI lt 15th-85th p 3 month antecedent');
+Map.addLayer(lt_percent_cap.select('CMI_ante3mo_lt_p15_gt_p85').first(), {min:0, max:1}, '2010 CMI lt 15th-85th p 3 month antecedent');
