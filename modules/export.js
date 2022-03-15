b77542f4a9feb37283f1a0d5905100cf9e7f0e8c
robit.alec <robit.alec@gmail.com>
2022-03-15 17:34:43 +0000

    Updated modules/export.js (added 4 lines, deleted 2 lines).

--- modules/export.js
+++ modules/export.js
@@ -7,6 +7,9 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var main = require('users/robitalec/CFS:modules/main.js');
 var stratified = require('users/robitalec/CFS:modules/stratified.js');
 
+// Modal land cover
+var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
+lc_modal = lc_modal.changeProj(lc_modal.projection(), land_cover.lc_and_fire.first().projection());
 
 // Export task for each ecoregion
 var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list) {
@@ -19,8 +22,7 @@ var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, 
       var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
     
       var out = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-      var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
-      lc_modal = lc_modal.changeProj(lc_modal.projection(), land_cover.lc_and_fire.first().projection());
+
       var points = stratified.stratified_sample(lc_modal, 'land_cover_mode', 30, ft.geometry(), n_pts);
       
       var sampled = ee.ImageCollection(out).map(function(img) {
