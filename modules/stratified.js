b1379f3c67c5b6132add8207b11d894a5dac6199
robit.alec <robit.alec@gmail.com>
2022-03-15 15:51:40 +0000

    Updated modules/stratified.js (added 3 lines, deleted 2 lines).

--- modules/stratified.js
+++ modules/stratified.js
@@ -5,11 +5,12 @@ Alec L. Robitaille
 
 
 // Note unless seed is changed, always same points
-var stratified_sample = function(img, band, region, n_pts) {
+var stratified_sample = function(img, band, scale, region, n_pts) {
 	return img.addBands([ee.Image.pixelLonLat()]).stratifiedSample({
 		classBand: band,
 		numPoints: n_pts,
-		region: region
+		region: region,
+		scale: 30
 	}).map(function(ft) {
 		return ft.setGeometry(ee.Geometry.Point([ft.get('longitude'), ft.get('latitude')]));
 	});
