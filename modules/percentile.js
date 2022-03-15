cd11d3d64d30f890e974993d8b20e661e336474b
robit.alec <robit.alec@gmail.com>
2022-03-15 02:13:34 +0000

    Updated modules/percentile.js (added 29 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -13,6 +13,35 @@ exports.get_percentile = get_percentile;
 var lt_percentile = function(images, percentile_images) {
 	var images_lt_percentiles = images.map(function(img) {
     var out = ee.Image([
+      percentile_images.select('CMI_ante3mo').gte(img.select('CMI_ante3mo_mean')),
+      // percentile_images.select('CMI_ante6mo.*').gte(img.select('CMI_ante6mo_mean')),
+      percentile_images.select('CMI_ante12mo.*').gte(img.select('CMI_ante12mo_mean')),
+      percentile_images.select('CMI_ante5yr.*').gte(img.select('CMI_ante5yr_mean_min')),
+      
+      percentile_images.select('CMI_ante3mo.*').gte(img.select('CMI_ante3mo_mean')),
+      // percentile_images.select('CMI_ante6mo.*').gte(img.select('CMI_ante6mo_mean')),
+      percentile_images.select('CMI_ante12mo.*').gte(img.select('CMI_ante12mo_mean')),
+      percentile_images.select('CMI_ante5yr.*').gte(img.select('CMI_ante5yr_mean_min'))
+      
+      ]).copyProperties(img);
+    var new_names = ee.Image(out).bandNames();
+    new_names = new_names.map(function(nm) {
+      return ee.String(nm).replace('_min', '').replace('_mean', '_lt');
+    });
+    out = ee.Image(out).rename(new_names);
+    return out;
+	});
+	return images_lt_percentiles;
+};
+exports.lt_percentile = lt_percentile;
+
+
+
+// _________________________________________________________________________________________________
+// Compare percentile images for each antecedent period to each image's antecedent means
+var lt_percentile = function(images, percentile_images) {
+	var images_lt_percentiles = images.map(function(img) {
+    var out = ee.Image([
       percentile_images.select('CMI_ante3mo.*').gte(img.select('CMI_ante3mo_mean')),
       // percentile_images.select('CMI_ante6mo.*').gte(img.select('CMI_ante6mo_mean')),
       percentile_images.select('CMI_ante12mo.*').gte(img.select('CMI_ante12mo_mean')),
