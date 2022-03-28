93c1a6d7abc0fcbb1f75946f866386bad0ec4650
robit.alec <robit.alec@gmail.com>
2022-03-28 22:07:03 +0000

    Updated modules/utils.js (added 4 lines).

--- modules/utils.js
+++ modules/utils.js
@@ -15,6 +15,10 @@ var set_date = function(img) {
 };
 exports.set_date = set_date;
 
+// Add year band 
+var add_year_band = function(img) {
+  return img.addBands([ee.Image.constant(img.get('year')).rename('year')]);
+};
 
 // Aggregate each month within each year with reducer
 var aggregate_month_year = function(images, year_list, month_list, reducer) {
