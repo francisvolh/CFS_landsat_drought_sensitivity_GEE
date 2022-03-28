527e2b657d6697dc0ccb011acae3cd2af06d6c52
robit.alec <robit.alec@gmail.com>
2022-03-28 22:08:43 +0000

    Updated tests/test_utils.js (added 11 lines, deleted 4 lines).

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -6,10 +6,17 @@ Alec L. Robitaille
 // Load modules
 var utils = require('users/robitalec/CFS:modules/utils.js');
 
+var forest_lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 
 
 // Test set_year
 // Usage: utils.set_year(image)
-var forest_lc = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
 var img = forest_lc.first();
 print(utils.set_year(img));
+
+// Test add_year_band
+// Usage: utils.add_year_band(image)
+var img = forest_lc.first();
+img = utils.set_year(img);
+Map.addLayer(utils.add_year_band(img));
+
