b5b827a9c8ea1262dd608da3d8c7bf566b19a0c3
robit.alec <robit.alec@gmail.com>
2022-04-01 18:55:37 +0000

    Created test_export_img.js.

---
+++ test_export_img.js
@@ -0,0 +1,29 @@
+/*
+Testing: modules/exports.js
+Alec L. Robitaille
+*/
+
+// Load modules
+var export_by = require('users/robitalec/CFS:modules/export_by.js');
+
+// Set variables
+var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
+var min_year = 2000;
+var max_year = 2015;
+var min_mm_dd = '06-15';
+var max_mm_dd = '07-15';
+var percentile_list = [10, 20];
+var index_list = ['NDVI', 'NBR'];
+var drive_folder = 'Batch-ecoregion-export';
+var n_pts = 10;
+
+// Load ecoregions
+var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
+ecoregions = ecoregions.filterBounds(geometry).limit(3);
+
+
+
+// Test export_by_ecoregion - veg index and ante means
+// Usage: export_by_ecoregion(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
+export_by.export_by_ecoregion('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+// Map.addLayer(ecoregions)
