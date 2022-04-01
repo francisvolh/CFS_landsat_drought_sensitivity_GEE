ffa2b80cdbc6fae306f557d71a8c90b87e22043c
robit.alec <robit.alec@gmail.com>
2022-04-01 19:09:25 +0000

    Updated tests/test_export_img.js (added 8 lines, deleted 11 lines).

--- tests/test_export_img.js
+++ tests/test_export_img.js
@@ -12,18 +12,15 @@ var min_year = 2000;
 var max_year = 2015;
 var min_mm_dd = '06-15';
 var max_mm_dd = '07-15';
-var percentile_list = [10, 20];
-var index_list = ['NDVI', 'NBR'];
+var percentile = [15];
+var index = ['NDVI'];
+var 
+
+
 var drive_folder = 'Batch-ecoregion-export';
-var n_pts = 10;
-
-// Load ecoregions
-var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
-ecoregions = ecoregions.filterBounds(geometry).limit(3);
 
 
-
-// Test export_by_ecoregion - veg index and ante means
+// Test export_img
 // Usage: export_by_ecoregion(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
 export_by.export_by_ecoregion('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 // Map.addLayer(ecoregions)
