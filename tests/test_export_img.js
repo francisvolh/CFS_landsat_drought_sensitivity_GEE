96c9cbbc311a2358aa91fe7072786d9b99d89dc0
robit.alec <robit.alec@gmail.com>
2022-04-01 19:08:34 +0000

    Updated tests/test_export_img.js (added 2 lines, deleted 2 lines).

--- tests/test_export_img.js
+++ tests/test_export_img.js
@@ -1,10 +1,10 @@
 /*
-Testing: modules/exports.js
+Testing: modules/export_img.js
 Alec L. Robitaille
 */
 
 // Load modules
-var export_by = require('users/robitalec/CFS:modules/export_by.js');
+var export_img = require('users/robitalec/CFS:modules/export_by.js');
 
 // Set variables
 var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
