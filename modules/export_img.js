60158fdcc841f492f5fca0cca5594b4178252a1d
robit.alec <robit.alec@gmail.com>
2022-04-01 21:16:38 +0000

    Updated modules/export_img.js (added 14 lines, deleted 4 lines).

--- modules/export_img.js
+++ modules/export_img.js
@@ -1,5 +1,5 @@
 /*
-Export img
+Export img asset and drive
 Alec L. Robitaille
 */
 
@@ -11,9 +11,19 @@ var stratified = require('users/robitalec/CFS:modules/stratified.js');
 var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
 lc_modal = lc_modal.reproject(land_cover.lc_and_fire.first().projection());
 
-// Export img
-// output either 'relative sensitivity' or 'absolute sensitivity'
-var export_img = function(output, folder, n_pts, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
-  var out = main.main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
 
-}
+// Export img
+var export_img = function(region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
+  return main.main('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+};
+
+// Export img asset
+// output either 'relative sensitivity' or 'absolute sensitivity'
+var export_img_asset = function(asset_dir, asset_name, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
+  var out = export_img(region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+  
+  var today = new Date().toJSON().slice(0, 10);
+  
+  Export.image.toAsset(out, asset_dir + '/' + today + '_' + asset_name);
+
+};
