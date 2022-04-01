d44c93e24409cb85f18eb039abaab4c4032a351e
robit.alec <robit.alec@gmail.com>
2022-04-01 19:06:52 +0000

    Updated modules/export_img.js (added 5 lines, deleted 10 lines).

--- modules/export_img.js
+++ modules/export_img.js
@@ -12,13 +12,8 @@ var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
 lc_modal = lc_modal.reproject(land_cover.lc_and_fire.first().projection());
 
 // Export img
-var export_img = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list) {
-  var ecoreg_id_list = ecoregions
-    .aggregate_array('ECOREGI')
-    .distinct();
-
-  ecoreg_id_list.evaluate(function(ecoreg_ids) {
-    ecoreg_ids.forEach(function(ecoreg_id) {
-      var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
-    
-      var out = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+// output either 'relative sensitivity' or 'absolute sensitivity'
+var export_img = function(output, folder, n_pts, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
+  var out = main.main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+  
+}
