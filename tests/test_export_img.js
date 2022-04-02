9d44403704a4671b908d146d91caee19eaac3217
robit.alec <robit.alec@gmail.com>
2022-04-02 02:01:56 +0000

    Updated tests/test_export_img.js (added 19 lines, deleted 9 lines).

--- tests/test_export_img.js
+++ tests/test_export_img.js
@@ -7,20 +7,30 @@ Alec L. Robitaille
 var export_img = require('users/robitalec/CFS:modules/export_img.js');
 
 // Set variables
-var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
+var region = ee.FeatureCollection(geometry);
 var min_year = 2000;
 var max_year = 2015;
 var min_mm_dd = '06-15';
 var max_mm_dd = '07-15';
 var percentile = [15];
 var index = ['NDVI'];
-var 
+var antecedent = ['12mo'];
 
 
-var drive_folder = 'Batch-ecoregion-export';
 
 
-// Test export_img
-// Usage: export_by_ecoregion(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
-export_by.export_by_ecoregion('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
-// Map.addLayer(ecoregions)
+// Test export_img_asset
+// Usage: export_img_asset(asset_name, asset_path, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent)
+export_img.export_img_asset('test-export-asset', 'CFS', 30, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+
+
+
+// Test export_img_drive
+// Usage: export_img_drive(drive_name, drive_folder, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent)
+export_img.export_img_drive('test-export-drive', 'Test-export', 30, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+
+
+
+// Test export_img_cloud
+// Usage: export_img_cloud(cloud_name, cloud_bucket, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent)
+export_img.export_img_cloud('test-export-cloud', null, 30, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
