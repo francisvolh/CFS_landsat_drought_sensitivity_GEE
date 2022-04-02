3c31da2f80e5db30daa7ce9acd5f9cde5cc660c6
robit.alec <robit.alec@gmail.com>
2022-04-02 02:32:42 +0000

    Updated Export-drought-sensitivity-tiles.js (added 23 lines, deleted 23 lines).

--- Export-drought-sensitivity-tiles.js
+++ Export-drought-sensitivity-tiles.js
@@ -40,37 +40,37 @@ var scale = 30;
 
 // loop regions
 // asset_name = id
-
+tiles = tiles.map(function(ft) {return ft.set('id', ft.get('system:index'))});
 var tile_id_list = tiles.aggregate_array('id').distinct();
+
+// tile_id_list.evaluate(function(tile_ids) {
+//   tile_ids.forEach(function(tile_id) {
+//       var region = tiles.filter(ee.Filter.eq('id', tile_id));
+//       var out = main.main_cap('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
+  
+//       var today = new Date().toJSON().slice(0, 10);
+      
+//       var asset_name = 'Abs' + index + ante + percentile_low + tile_id;
+//       asset_name = today + '_' + asset_name;
+//       Export.image.toAsset({
+//         image: out, 
+//         description: asset_name, 
+//         assetId: asset_path + '/' + asset_name, 
+//         region: region, 
+//         scale: scale
+//       });
+      
+//     });
+// });
+
+
+
 
 tile_id_list.evaluate(function(tile_ids) {
     tile_ids.forEach(function(tile_id) {
-      var region = tiles.filter(ee.Filter.eq('id', tile_id));
-      var out = main.main_cap('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
-  
-      var today = new Date().toJSON().slice(0, 10);
-      
-      var asset_name = 'Abs' + index + ante + percentile_low + tile_id;
-      asset_name = today + '_' + asset_name;
-      Export.image.toAsset({
-        image: out, 
-        description: asset_name, 
-        assetId: asset_path + '/' + asset_name, 
-        region: region, 
-        scale: scale
-      });
-      
+      var ft = tiles.filter(ee.Filter.eq('id', tile_id));
+      export_img.export_img_asset_cap('Abs_' + index + '_' + antecedent + '_p' + percentile_low + '_' + tile_id, asset_path, scale, ft, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
     });
 });
-
-
-
-
-// tile_id_list.evaluate(function(tile_ids) {
-//     tile_ids.forEach(function(tile_id) {
-//       var ft = tiles.filter(ee.Filter.eq('id', tile_id));
-//       export_img.export_img_asset_cap('Abs' + index + ante + percentile_low + tile_id, asset_path, scale, ft, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
-//     });
-// });
 
 Map.addLayer(tiles, null, 'tiles');
