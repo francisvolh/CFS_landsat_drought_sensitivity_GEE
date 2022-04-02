5662e79c71ca6fd1fb3b874aedf0ceac2951cc5e
robit.alec <robit.alec@gmail.com>
2022-04-02 00:12:30 +0000

    Updated tests/test_split_drought.js (added 3 lines, deleted 3 lines).

--- tests/test_split_drought.js
+++ tests/test_split_drought.js
@@ -16,7 +16,7 @@ var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
 var min_year = 2010; var max_year = 2015;
 var years = ee.List.sequence(min_year, max_year);
 var months = ee.List.sequence(1, 12);
-var percentile_list = [15, 95];
+var percentile_list = [15, 85];
 var index_list = ['NDVI', 'EVI'];
 var antecedent_list = ['3mo', '12mo', '5yr'];
 var cmi_viz = {min:-30, max:30, palette: palettes.colorbrewer.RdBu[5]};
@@ -48,8 +48,8 @@ Map.addLayer(split_drought.select('NDVI_ante3mo_p15_base').first(),  {min: -0.5,
 
 
 // Test split_drought_cap
-// Usage: split_drought_cap.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_list, index_list)
-var split_drought_cap = split_drought_cap.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_list, index_list);
+// Usage: split_drought_cap.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_low, percentile_high, index_list)
+var split_drought_cap = split_drought_cap.split_drought_cap(indices_col, lt_percent, antecedent_list, 15, 85, index_list);
 print('Split drought  with cap'); print(split_drought_cap);
 Map.addLayer(split_drought_cap.select('NDVI_ante3mo_p15_drought').first(),  {min: -0.5, max:1}, '2010 NDVI drought 15th percentile 3 month antecedent');
 Map.addLayer(split_drought_cap.select('NDVI_ante3mo_p15_base').first(),  {min: -0.5, max:1}, '2010 NDVI baseline 15th-85th percentile 3 month antecedent', false);
