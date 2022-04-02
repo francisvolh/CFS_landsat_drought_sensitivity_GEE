516436e9383dc7513a48c131f0aaa764468a9db5
robit.alec <robit.alec@gmail.com>
2022-04-02 00:15:28 +0000

    Updated tests/test_split_drought.js (added 1 line, deleted 1 line).

--- tests/test_split_drought.js
+++ tests/test_split_drought.js
@@ -4,7 +4,7 @@ Alec L. Robitaille
 */
 
 // Load modules
-var split_drought = require('users/robitalec/CFS:modules/split_drought.js');
+var split = require('users/robitalec/CFS:modules/split_drought.js');
 var percentile = require('users/robitalec/CFS:modules/percentile.js');
 var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
 var cmi = require('users/robitalec/CFS:modules/cmi.js');
