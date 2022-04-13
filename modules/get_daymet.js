95a7f60a272f8336ef29c8d99eb8faaca4455c72
robit.alec <robit.alec@gmail.com>
2022-04-13 02:05:50 +0000

    Updated modules/get_daymet.js (added 2 lines, deleted 1 line).

--- modules/get_daymet.js
+++ modules/get_daymet.js
@@ -31,8 +31,9 @@ exports.get_monthly_daymet = get_monthly_daymet;
 
 
 // Get long term normals 
-var get_long_term_normals = function(year_list) {
+var get_mean_annual = function(year_list) {
   var annual = utils.aggregate_year(daymet, year_list, ee.Reducer.mean());
   
   return annual.reduce(ee.Reducer.mean());
 };
+exports.get_monthly_daymet = get_monthly_daymet;
