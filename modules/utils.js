0b3bbc8ec6b521af418139efb7f0ec794b517a2a
robit.alec <robit.alec@gmail.com>
2022-04-13 02:04:11 +0000

    Updated modules/utils.js (added 10 lines, deleted 17 lines).

--- modules/utils.js
+++ modules/utils.js
@@ -49,27 +49,20 @@ exports.aggregate_month_year = aggregate_month_year;
 
 
 // Aggregate each year with reducer
-var aggregate_month_year = function(images, year_list, reducer) {
+var aggregrate_year = function(images, year_list, reducer) {
   // Combine images returned for each year+month
   return ee.ImageCollection.fromImages(
     // Map over years
     year_list.map(function(yr) {
-      // Map over months
-      return month_list.map(function(mnth) {
-        // Filter images to year and month
-        // Reduce with reducer provided
-        // Set year, month and pseudo date properties
-        // Return an image for each year and month
-        return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
-                     .filter(ee.Filter.calendarRange(mnth, mnth, 'month'))
-                     .reduce(reducer)
-                     .set('year', yr)
-                     .set('month', mnth)
-                     .set('system:time_start',
-                          ee.Date.fromYMD(yr, mnth, 1).millis());
-        });
-  }).flatten()
-  );
+      // Filter images to year and month
+      // Reduce with reducer provided
+      // Set year, month and pseudo date properties
+      // Return an image for each year and month
+      return images.filter(ee.Filter.calendarRange(yr, yr, 'year'))
+                   .reduce(reducer)
+                   .set('year', yr)
+                   .set('system:time_start', ee.Date.fromYMD(yr, 1, 1).millis());
+      });
 };
 exports.aggregate_month_year = aggregate_month_year;
 
