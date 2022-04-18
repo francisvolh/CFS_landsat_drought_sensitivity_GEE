5621fa46347bc1aa4814e14384b4e96468406966
robit.alec <robit.alec@gmail.com>
2022-04-18 23:52:15 +0000

    Updated modules/get_long_term_climate.js (added 8 lines, deleted 8 lines).

--- modules/get_long_term_climate.js
+++ modules/get_long_term_climate.js
@@ -27,14 +27,14 @@ var get_long_term_climate = function(year_list) {
   var annual = utils.aggregrate_year(daymet, year_list, reducer)
                     .select(['tmin_mean', 'tmax_mean', 'prcp_sum'], 
                             ['tmin', 'tmax', 'prcp']);
-                            
-  var tmean = annual.map(function(img) {
-    return img.addBands([img.select(['tmin', 'tmax'])
-                            .reduce(ee.Reducer.mean())
-                            .rename('tmean')]);
-  });
-  annual = annual.addBands(tmean);
+  return annual                            
+  // var tmean = annual.map(function(img) {
+  //   return img.addBands([img.select(['tmin', 'tmax'])
+  //                           .reduce(ee.Reducer.mean())
+  //                           .rename('tmean')]);
+  // });
+  // annual = annual.addBands(tmean);
   
-  return annual.reduce(ee.Reducer.mean());
+  // return annual.reduce(ee.Reducer.mean());
 };
 exports.get_long_term_climate = get_long_term_climate;
