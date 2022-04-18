4035e50424b720fa34026fafb876048df438f7d4
robit.alec <robit.alec@gmail.com>
2022-04-18 23:36:37 +0000

    Created modules/get_long_term_climate.js.

---
+++ modules/get_long_term_climate.js
@@ -0,0 +1,40 @@
+/*
+Long term climate from Daymet
+Alec L. Robitaille
+
+Thornton, M.M., R. Shrestha, Y. Wei, P.E. Thornton, S. Kao, and B.E. Wilson.
+{YEAR}. Daymet: Daily Surface Weather Data on a 1-km Grid for North America,
+Version 4. ORNL DAAC, Oak Ridge, Tennessee, USA
+
+Other Citation Details - Thornton, M.M., R. Shrestha, Y. Wei, P.E. Thornton,
+S. Kao, and B.E. Wilson. 2020. Daymet: Daily Surface Weather Data on a 1-km
+Grid for North America, Version 4. ORNL DAAC, Oak Ridge, Tennessee, USA.
+doi:10.3334/ORNLDAAC/1840
+*/
+
+
+// Load modules
+var utils = require('users/robitalec/CFS:modules/utils.js');
+var daymet = require('users/robitalec/CFS:modules/daymet.js');
+
+// Load Daymet
+var daymet = ee.ImageCollection("NASA/ORNL/DAYMET_V4");
+
+// Reducer
+var reducer = ee.Reducer.mean().combine(ee.Reducer.sum(), null, true);
+
+// Get collection of Daymet images
+var get_monthly_daymet = function(year_list, month_list) {
+	return utils.aggregate_month_year(daymet, year_list, month_list, reducer)
+							.select(['tmin_mean', 'tmax_mean', 'prcp_sum'], ['tmin', 'tmax', 'prcp']);
+};
+exports.get_monthly_daymet = get_monthly_daymet;
+
+
+// Get mean annual
+var get_mean_annual = function(year_list) {
+  var annual = utils.aggregrate_year(daymet, year_list, ee.Reducer.mean());
+  
+  return annual.reduce(ee.Reducer.mean());
+};
+exports.get_mean_annual = get_mean_annual;
