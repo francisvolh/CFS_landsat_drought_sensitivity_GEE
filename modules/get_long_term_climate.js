6f5610fb3b3290a9283b2aa234c52482cc153a09
robit.alec <robit.alec@gmail.com>
2022-04-18 23:54:25 +0000

    Updated modules/get_long_term_climate.js (added 1 line, deleted 3 lines).

--- modules/get_long_term_climate.js
+++ modules/get_long_term_climate.js
@@ -32,7 +32,6 @@ var get_long_term_climate = function(year_list) {
                             .reduce(ee.Reducer.mean())
                             .rename('tmean')]);
   });
-  return annual
-  // return annual.reduce(ee.Reducer.mean());
+  return annual.reduce(ee.Reducer.mean());
 };
 exports.get_long_term_climate = get_long_term_climate;
