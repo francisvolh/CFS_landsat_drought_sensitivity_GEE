7bf11d3f70ff832ca353b4538fe6b8b73ac96195
robit.alec <robit.alec@gmail.com>
2022-04-18 23:51:48 +0000

    Updated tests/test_get_long_term_climate.js (added 3 lines, deleted 4 lines).

--- tests/test_get_long_term_climate.js
+++ tests/test_get_long_term_climate.js
@@ -9,11 +9,10 @@ var climate = require('users/robitalec/CFS:modules/get_long_term_climate.js');
 
 // Set variables
 var years = ee.List.sequence(1985, 2020);
-var months = ee.List.sequence(5, 7);
 
 
 // Test get_long_term_climate
-// Usage: get_long_term_climate(year_list, month_list)
-var long_climate = climate.get_long_term_climate(years, months);
+// Usage: get_long_term_climate(year_list)
+var long_climate = climate.get_long_term_climate(years);
 print(long_climate);
-Map.addLayer(long_climate.select('prcp'), {min:0, max:500});
+Map.addLayer(long_climate)//.select('tmean'), {min:0, max:500});
