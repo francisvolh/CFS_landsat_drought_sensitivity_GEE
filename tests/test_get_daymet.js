7814f0da06d40df8dee1ffd003f2d40210316c0d
robit.alec <robit.alec@gmail.com>
2022-04-18 23:42:05 +0000

    Updated tests/test_get_daymet.js (deleted 9 lines).

--- tests/test_get_daymet.js
+++ tests/test_get_daymet.js
@@ -18,12 +18,3 @@ var months = ee.List.sequence(5, 7);
 var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
 print(monthly_daymet);
 Map.addLayer(monthly_daymet.select('prcp'), {min:0, max:500});
-
-
-
-// Test get_mean_annual
-// Usage: get_mean_annual(year_list)
-var mean_annual_daymet = get_daymet.get_mean_annual(years);
-print(mean_annual_daymet);
-Map.addLayer(mean_annual_daymet.select('prcp_mean_mean'), {min:0, max:20});
-Map.addLayer(mean_annual_daymet.select('tmax_mean_mean'), {min:-30, max:30});
