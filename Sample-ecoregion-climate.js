bcb44e1afba0f94b0d824bdaa39900361d002e1c
robit.alec <robit.alec@gmail.com>
2022-04-19 00:13:29 +0000

    Updated Sample-ecoregion-climate.js (added 15 lines, deleted 17 lines).

--- Sample-ecoregion-climate.js
+++ Sample-ecoregion-climate.js
@@ -1,25 +1,23 @@
 // Load modules
-var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
+var climate = require('users/robitalec/CFS:modules/get_long_term_climate.js');
 
 // Set variables
 var years = ee.List.sequence(1985, 2020);
 
 
-var mean_annual_daymet = get_daymet.get_mean_annual(years);
-// print(mean_annual_daymet)
-mean_annual_daymet = mean_annual_daymet.select(['prcp_mean_mean', 'tmax_mean_mean', 'tmin_mean_mean'])
+// Load ecoregions
+var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
+ecoregions = ecoregions.filterBounds(geometry).limit(1);
 
-var ft = ee.FeatureCollection('users/robitalec/CFS/reverse-eco-points')
 
-ft = ft.randomColumn()
-       .filter(ee.Filter.lt('random', 0.01))
 
-var sample = ft.map(function(f) { 
-  return mean_annual_daymet.sampleRegions(f, null, 1000)
-}).flatten()
 
-// print(sample)
- 
-// var sample = mean_annual_daymet.sampleRegions(ft, null, 1000)
+// get_long_term_climate
+var long_climate = climate.get_long_term_climate(years)
+    .select(['tmean_mean', 'prcp_mean']);
+
+
+var sample = long_climate.reduceRegions(ecoregions, ee.Reducer.mean(), 1000);
+
 
 Export.table.toDrive(sample, 'subsample-reverse-long-term')
