5166744521f1dcc0e9a5ca1a296c1f9d337bc022
robit.alec <robit.alec@gmail.com>
2022-04-19 18:06:03 +0000

    Updated Sample-ecoregion-climate.js (added 4 lines, deleted 2 lines).

--- Sample-ecoregion-climate.js
+++ Sample-ecoregion-climate.js
@@ -21,7 +21,7 @@ var years = ee.List.sequence(1985, 2020);
 // Load ecoregions
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
 ecoregions = ecoregions.filterBounds(geometry)
-  .randomColumn().filter(ee.Filter.lt('random', 0.2));
+  // .randomColumn().filter(ee.Filter.lt('random', 0.2));
 
 
 
@@ -31,7 +31,9 @@ var long_climate = climate.get_long_term_climate(years)
     .select(['tmean_mean', 'prcp_mean']);
 
 
-var sample = long_climate.reduceRegions(ecoregions, ee.Reducer.mean(), 1000);
+var sample = ecoregions.map(function(ft) {
+  return long_climate.reduceRegions(ft, ee.Reducer.mean(), 1000);
+}).flatten();
 
 Map.addLayer(ecoregions)
 Export.table.toDrive(sample, 'ecoregion-long-term-climate');
