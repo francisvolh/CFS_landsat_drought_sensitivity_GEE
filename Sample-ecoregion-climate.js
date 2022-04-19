4e1cc9e75c26dfc9e2127e2d36e9e73010691454
robit.alec <robit.alec@gmail.com>
2022-04-19 23:09:06 +0000

    Updated Sample-ecoregion-climate.js (added 1 line, deleted 1 line).

--- Sample-ecoregion-climate.js
+++ Sample-ecoregion-climate.js
@@ -33,7 +33,7 @@ var long_climate = climate.get_long_term_climate(years)
     .select(['tmean_mean', 'prcp_mean']);
 
 var sample = ecoregions.map(function(ft) {
-  return ft.set(long_climate.reduceRegion(ee.Reducer.mean(), ft.geometry(), 1000));
+  return ft.set(long_climate.reduceRegion(ee.Reducer.mean(), ft.geometry()));
 }).flatten();
 
 
