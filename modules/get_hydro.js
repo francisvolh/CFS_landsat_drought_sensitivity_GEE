0220984797fcd31cdd10b01c249f655004255078
robit.alec <robit.alec@gmail.com>
2022-04-20 01:02:33 +0000

    Updated modules/get_hydro.js (added 1 line, deleted 1 line).

--- modules/get_hydro.js
+++ modules/get_hydro.js
@@ -35,7 +35,7 @@ var snow_mode = land_cover.hermosilla_plus_2022
                 .eq(31);
 
 var get_prop_permanent_snow = function(focal_dist) {
-  snow_mode.focalMean(focal_dist, null, 'meters');
+  return snow_mode.focalMean(focal_dist, null, 'meters');
 };
 exports.get_prop_permanent_snow = get_prop_permanent_snow;
                 
