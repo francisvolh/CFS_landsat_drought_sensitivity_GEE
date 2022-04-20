36308203f021eabc2ce81f751017a0b287876f31
robit.alec <robit.alec@gmail.com>
2022-04-20 00:59:54 +0000

    Updated tests/test_get_hydro.js (added 12 lines).

--- tests/test_get_hydro.js
+++ tests/test_get_hydro.js
@@ -25,3 +25,15 @@ Map.addLayer(hand_30_1000.select('b1'), {min:0, max:500, palette:p});
 var hand_90_1000 = get_hydro.get_hand(90, 1000);
 print(hand_90_1000);
 Map.addLayer(hand_90_1000.select('b1'), {min:0, max:500, palette:p});
+
+
+
+// Test get_prop_permanent_snow
+// Usage: get_prop_permanent_snow(focal_dist)
+var prop_snow_1000 = get_prop_permanent_snow(1000);
+print(prop_snow_1000);
+Map.addLayer(prop_snow_1000, {min:0, max:1});
+
+var prop_snow_5000 = get_prop_permanent_snow(5000);
+print(prop_snow_5000);
+Map.addLayer(prop_snow_5000, {min:0, max:1});
