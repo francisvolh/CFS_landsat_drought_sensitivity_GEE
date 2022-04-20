86efbf0010ee21adebb36c4d66a41cea84d8aab1
robit.alec <robit.alec@gmail.com>
2022-04-20 00:57:45 +0000

    Updated modules/get_hydro.js (deleted 2 lines).

--- modules/get_hydro.js
+++ modules/get_hydro.js
@@ -30,8 +30,6 @@ exports.get_hand = get_hand;
 
 
 // Get proportion glacier/permanent snow
-// 
-
 var snow_mode = land_cover.hermosilla_plus_2022()
                 .reduce(ee.Reducer.mode())
                 .eq(31);
