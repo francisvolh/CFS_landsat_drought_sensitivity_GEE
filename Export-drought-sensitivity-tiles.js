6d3742b2b7fbc8648355f0f1dfdcea97a36a7c9f
robit.alec <robit.alec@gmail.com>
2022-05-13 19:08:38 +0000

    Updated Export-drought-sensitivity-tiles.js (added 2 lines, deleted 3 lines).

--- Export-drought-sensitivity-tiles.js
+++ Export-drought-sensitivity-tiles.js
@@ -35,7 +35,7 @@ var tiles = tiler.getTilesForGeometry(geometry, 7);
 
 
 
-var asset_path = 'CFS';
+var drive_folder = 'Exports';
 var scale = 30;
 
 // loop regions
@@ -44,12 +44,11 @@ tiles = tiles.map(function(ft) {return ft.set('id', ft.get('system:index'))});
 var tile_id_list = tiles.aggregate_array('id').distinct();
 print(tile_id_list);
 
-tile_id_list = tile_id_list.slice(80, 110);
 
 tile_id_list.evaluate(function(tile_ids) {
     tile_ids.forEach(function(tile_id) {
       var ft = tiles.filter(ee.Filter.eq('id', tile_id));
-      export_img.export_img_asset_cap('Abs_' + index + '_' + antecedent + '_p' + percentile_low + '_' + tile_id, asset_path, scale, ft, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
+      export_img.export_img_asset_cap('Abs_' + index + '_' + antecedent + '_p' + percentile_low + '_' + tile_id, drive_folder, scale, ft, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
     });
 });
 
