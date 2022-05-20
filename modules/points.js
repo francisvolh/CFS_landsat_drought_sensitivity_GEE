0173bf042718e2829f93918844b0710d75c5ef29
robit.alec <robit.alec@gmail.com>
2022-05-20 19:44:25 +0000

    Updated modules/points.js (added 5 lines, deleted 6 lines).

--- modules/points.js
+++ modules/points.js
@@ -13,15 +13,14 @@ var stratified = require('users/robitalec/CFS:modules/stratified.js');
 var n_pts = 5//150;
 
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada')
+  .limit(2);
 
-.limit(2);
-
-var lc_homogeneous = land_cover.get_homogeneous_land_cover().limit(2).mode();
-
+var lc_homogeneous = land_cover.get_homogeneous_land_cover().limit(2).reduce(ee.Reducer.mode());
+Map.addLayer(lc_homogeneous)
 
 var points = ecoregions.map(function(ft) {
-  return stratified.stratified_sample(lc_homogeneous, 'land_cover', 30, ft.geometry(), n_pts);
-}).flatten();
+  return stratified.stratified_sample(lc_homogeneous, 'land_cover_mode', 30, ft.geometry(), n_pts);
+})//.flatten();
 
 print(points)
 
