3db941274148dda3b007cc248a092a7a9038cc09
robit.alec <robit.alec@gmail.com>
2022-05-20 19:11:52 +0000

    Updated modules/get_hydro.js (added 8 lines, deleted 1 line).

--- modules/get_hydro.js
+++ modules/get_hydro.js
@@ -22,10 +22,17 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 
 
 // Get HAND
+var geometry = 
+    ee.Geometry.Polygon(
+        [[[-168.54997439051382, 71.83257848283961],
+          [-168.54997439051382, 38.21307697867719],
+          [-79.25309939051384, 38.21307697867719],
+          [-79.25309939051384, 71.83257848283961]]], null, false);
 var get_hand = function(resolution, threshold) {
   if (resolution == 30 & threshold == 100) {
     // Note: image collection vs image
-    return ee.ImageCollection("users/gena/global-hand/hand-100").select(['b1'], ['hand-30-100']).mosaic();
+    return ee.ImageCollection("users/gena/global-hand/hand-100").select(['b1'], ['hand-30-100'])
+        .filterBounds(geometry).mosaic();
   } else if (resolution == 30 & threshold == 1000) {
     return ee.Image("users/gena/GlobalHAND/30m/hand-1000").select(['b1'], ['hand-30-1000']);
   } else if (resolution == 90 & threshold == 1000) {
