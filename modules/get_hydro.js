73f36e72b67f01121b07806775fa8a03f6e381b8
robit.alec <robit.alec@gmail.com>
2022-05-20 17:48:27 +0000

    Updated modules/get_hydro.js (added 2 lines, deleted 2 lines).

--- modules/get_hydro.js
+++ modules/get_hydro.js
@@ -25,9 +25,9 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var get_hand = function(resolution, threshold) {
   if (resolution == 30 & threshold == 100) {
     // Note: image collection vs image
-    return ee.ImageCollection("users/gena/global-hand/hand-100");
+    return ee.ImageCollection("users/gena/global-hand/hand-100").select(['b1'], ['hand-30-100']);
   } else if (resolution == 30 & threshold == 1000) {
-    return ee.Image("users/gena/GlobalHAND/30m/hand-1000");
+    return ee.Image("users/gena/GlobalHAND/30m/hand-1000").select(['b1'], ['hand-30-1000']);
   } else if (resolution == 90 & threshold == 1000) {
     return ee.Image("users/gena/GlobalHAND/90m-global/hand-1000").select(['b1'], ['hand-90-1000']);
   }
