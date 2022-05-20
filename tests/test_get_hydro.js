07a47c1d57ad289862ce14795b47ab7785d28dbe
robit.alec <robit.alec@gmail.com>
2022-05-20 19:05:16 +0000

    Updated tests/test_get_hydro.js (added 4 lines, deleted 4 lines).

--- tests/test_get_hydro.js
+++ tests/test_get_hydro.js
@@ -20,11 +20,11 @@ Map.addLayer(hand_30_100, {min:0, max:500, palette:p}, 'hand 30 100');
 
 var hand_30_1000 = get_hydro.get_hand(30, 1000);
 print(hand_30_1000);
-Map.addLayer(hand_30_1000, {min:0, max:500, palette:p}, 'hand 30 1000');
+Map.addLayer(hand_30_1000, {min:0, max:500, palette:p}, 'hand 30 1000', false);
 
 var hand_90_1000 = get_hydro.get_hand(90, 1000);
 print(hand_90_1000);
-Map.addLayer(hand_90_1000, {min:0, max:500, palette:p}, 'hand 90 1000');
+Map.addLayer(hand_90_1000, {min:0, max:500, palette:p}, 'hand 90 1000', false);
 
 
 
@@ -32,7 +32,7 @@ Map.addLayer(hand_90_1000, {min:0, max:500, palette:p}, 'hand 90 1000');
 // Usage: get_prop_permanent_snow(focal_dist)
 var prop_snow_1000 = get_hydro.get_prop_permanent_snow(1000);
 print(prop_snow_1000);
-Map.addLayer(prop_snow_1000, {min:0, max:1}, 'prop snow 1000');
+Map.addLayer(prop_snow_1000, {min:0, max:1}, 'prop snow 1000', false);
 
 var prop_snow_5000 = get_hydro.get_prop_permanent_snow(5000);
 print(prop_snow_5000);
@@ -54,7 +54,7 @@ Map.addLayer(dist_major_lake_500, {min:0, max:10000}, 'dist lakes > 500');
 
 var dist_major_lake_1000 = get_hydro.get_dist_major_lakes(1000);
 print(dist_major_lake_1000);
-Map.addLayer(dist_major_lake_1000, {min:0, max:10000}, 'dist lakes > 1000');
+Map.addLayer(dist_major_lake_1000, {min:0, max:10000}, 'dist lakes > 1000', false);
 
 
 // Test get_col
