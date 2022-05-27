d7d88d510e08d98c42b924345feb83d5510a015e
robit.alec <robit.alec@gmail.com>
2022-05-27 17:19:16 +0000

    Updated modules/main.js (added 3 lines, deleted 1 line).

--- modules/main.js
+++ modules/main.js
@@ -22,7 +22,9 @@ var main = function(output, region,
   var years = ee.List.sequence(min_year, max_year);
   var months = ee.List.sequence(1, 12);
   
-  if (typeof(antecedent_list)==='undefined') var antecedent_list = ['3mo', '12mo', '5yr'];
+  if (typeof(antecedent_list)==='undefined') d{
+    var antecedent_list = ['3mo', '12mo', '5yr'];
+  }
   // var antecedent_list = ['3mo', '12mo', '5yr'];
 
   // Collections
