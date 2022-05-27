ba986914cb23162dd274862fe68795f05e92c897
robit.alec <robit.alec@gmail.com>
2022-05-27 22:16:41 +0000

    Updated tests/test_export_img.js (added 4 lines, deleted 3 lines).

--- tests/test_export_img.js
+++ tests/test_export_img.js
@@ -24,6 +24,7 @@ var export_img = require('users/robitalec/CFS:modules/export_img.js');
 var region = ee.FeatureCollection(geometry);
 var min_year = 2000;
 var max_year = 2015;
+var scale = 30;
 var min_mm_dd = '06-15';
 var max_mm_dd = '07-15';
 var percentile = [15];
@@ -37,19 +38,19 @@ var antecedent = ['12mo'];
 
 // Test export_img_asset
 // Usage: export_img_asset(asset_name, asset_path, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent)
-export_img.export_img_asset('test-export-asset', 'CFS', 30, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+export_img.export_img_asset('test-export-asset', 'CFS', scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
 
 
 
 // Test export_img_drive
 // Usage: export_img_drive(drive_name, drive_folder, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent)
-export_img.export_img_drive('test-export-drive', 'Test-export', 30, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+export_img.export_img_drive('test-export-drive', 'Test-export', scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
 
 
 
 // Test export_img_cloud
 // Usage: export_img_cloud(cloud_name, cloud_bucket, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent)
-export_img.export_img_cloud('test-export-cloud', null, 30, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
+export_img.export_img_cloud('test-export-cloud', null, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
 
 
 // Test export_img_asset_cap
