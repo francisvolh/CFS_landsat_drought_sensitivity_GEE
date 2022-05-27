cf0d5cecc8ef2800ab0aaf0a910cf2fab0126d05
robit.alec <robit.alec@gmail.com>
2022-05-27 17:16:09 +0000

    Updated tests/test_points.js (added 11 lines, deleted 1 line).

--- tests/test_points.js
+++ tests/test_points.js
@@ -2,3 +2,13 @@
 Testing: modules/points.js
 Alec L. Robitaille
 */
+
+
+// Modules
+var points = require('users/robitalec/CFS:modules/points.js');
+
+
+// Test export_points_asset
+// Usage: export_points_asset(n_pts)
+var n_pts = 125;
+points.export_points_asset(125);
