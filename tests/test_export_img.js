4b66d78e9831d67f6345cbd05b54a1542fcf198c
robit.alec <robit.alec@gmail.com>
2022-05-27 22:19:47 +0000

    Updated tests/test_export_img.js (added 7 lines).

--- tests/test_export_img.js
+++ tests/test_export_img.js
@@ -53,6 +53,13 @@ export_img.export_img_drive('test-export-drive', 'Test-export', scale, region, m
 export_img.export_img_cloud('test-export-cloud', null, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
 
 
+// --- CAP --------------------------------------------------------------------
 // Test export_img_asset_cap
 // Usage: export_img_asset_cap(asset_name, asset_path, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent)
 export_img.export_img_asset_cap('test-export-asset-cap', 'CFS', scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
+
+
+// Test export_img_drive_cap
+// Usage: export_img_drive_cap(drive_name, drive_path, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent)
+export_img.export_img_drive_cap('test-export-drive-cap', 'CFS', scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
+
