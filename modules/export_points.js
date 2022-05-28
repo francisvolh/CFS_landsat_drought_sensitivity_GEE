dde00f5e0d809eef51abcd42d80fafa5f1a7dcb0
robit.alec <robit.alec@gmail.com>
2022-05-28 17:49:42 +0000

    Updated modules/export_points.js (added 62 lines, deleted 12 lines).

--- modules/export_points.js
+++ modules/export_points.js
@@ -1,5 +1,5 @@
 /*
-Export by
+Export points
 Alec L. Robitaille
 */
 
@@ -7,12 +7,26 @@ var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var main = require('users/robitalec/CFS:modules/main.js');
 var stratified = require('users/robitalec/CFS:modules/stratified.js');
 
-// Modal land cover
-var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
-lc_modal = lc_modal.reproject(land_cover.lc_and_fire.first().projection());
 
+// --- Sample -----------------------------------------------------------------
+var export_abs_sensitivity_cap = function(points, region, drive_name, drive_folder, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list) {
+
+	var out = main.main_cap('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list);
+
+	var sampled = ee.ImageCollection(out).map(function(img) {
+    return img.reduceRegions(points, ee.Reducer.mean(), 30);
+  }).flatten();
+
+	var today = new Date().toJSON().slice(0, 10);
+	Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + drive_name, drive_folder);
+}
+exports.export_abs_sensitivity_cap = export_abs_sensitivity_cap;
+
+
+
+// --- Export by --------------------------------------------------------------
 // Export task for each ecoregion
-var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list) {
+var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list, antecedent_list) {
   var ecoreg_id_list = ecoregions
     .aggregate_array('ECOREGI')
     .distinct();
@@ -20,18 +34,54 @@ var export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_year, 
   ecoreg_id_list.evaluate(function(ecoreg_ids) {
     ecoreg_ids.forEach(function(ecoreg_id) {
       var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
-    
-      var out = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 
+      var out = main.main(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list, antecedent_list);
+      var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
+      lc_modal = lc_modal.changeProj(lc_modal.projection(), land_cover.lc_and_fire.first().projection());
       var points = stratified.stratified_sample(lc_modal, 'land_cover_mode', 30, ft.geometry(), n_pts);
-      
+
       var sampled = ee.ImageCollection(out).map(function(img) {
         return img.reduceRegions(points, ee.Reducer.mean(), 30);
       }).flatten();
-    
+
       var today = new Date().toJSON().slice(0, 10);
       Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + ecoreg_id, folder);
     });
   });
 };
 exports.export_by_ecoregion = export_by_ecoregion;
+
+
+
+// Export task for each ecoregion (cap)
+var export_by_ecoregion_cap = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list) {
+  var ecoreg_id_list = ecoregions
+    .aggregate_array('ECOREGI')
+    .distinct();
+
+  ecoreg_id_list.evaluate(function(ecoreg_ids) {
+    ecoreg_ids.forEach(function(ecoreg_id) {
+      var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
+
+      var out = main.main_cap(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list);
+      var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
+      lc_modal = lc_modal.changeProj(lc_modal.projection(), land_cover.lc_and_fire.first().projection());
+      var points = stratified.stratified_sample(lc_modal, 'land_cover_mode', 30, ft.geometry(), n_pts);
+
+      var sampled = ee.ImageCollection(out).map(function(img) {
+        return img.reduceRegions(points, ee.Reducer.mean(), 30);
+      }).flatten();
+
+      var today = new Date().toJSON().slice(0, 10);
+      Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + ecoreg_id, folder);
+    });
+  });
+};
+exports.export_by_ecoregion_cap = export_by_ecoregion_cap;
+
+
+
+// TODO: export_sensitivity_by_ecoregion
+// TODO: export_hydro
+// TODO: export_climate
+// TODO: export_topo
