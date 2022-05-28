b3c3f7c45cb32d04ad873d1911f93171dc329cf5
robit.alec <robit.alec@gmail.com>
2022-05-28 17:54:48 +0000

    Updated tests/test_export_points.js (added 12 lines, deleted 3 lines).

--- tests/test_export_points.js
+++ tests/test_export_points.js
@@ -1,10 +1,10 @@
 /*
-Testing: modules/exports.js
+Testing: modules/exports_points.js
 Alec L. Robitaille
 */
 
 // Load modules
-var export_by = require('users/robitalec/CFS:modules/export_by.js');
+var export_points = require('users/robitalec/CFS:modules/export_points.js');
 
 // Set variables
 var geometry = ee.Geometry.Polygon([[[-128.69, 58.70], [-128.69, 50.66], [-111.20, 50.66], [-111.20, 58.70]]]);
@@ -13,6 +13,9 @@ var max_year = 2015;
 var min_mm_dd = '06-15';
 var max_mm_dd = '07-15';
 var percentile_list = [10, 20];
+var percentile_low = 15;
+var percentile_high = 85;
+var antecedent = '3mo'
 var index_list = ['NDVI', 'NBR'];
 var drive_folder = 'Batch-ecoregion-export';
 var n_pts = 10;
@@ -25,5 +28,11 @@ ecoregions = ecoregions.filterBounds(geometry).limit(3);
 
 // Test export_by_ecoregion - veg index and ante means
 // Usage: export_by_ecoregion(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
-export_by.export_by_ecoregion('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
+export_points.export_by_ecoregion('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list, antecedent);
 // Map.addLayer(ecoregions)
+
+
+// Test export_by_ecoregion_cap - veg index and ante means
+// Usage: export_by_ecoregion_cap(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
+export_points.export_by_ecoregion_cap('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent);
+
