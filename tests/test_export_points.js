eb67d2782fa5709989b505c1821f4af2615a9c8f
robit.alec <robit.alec@gmail.com>
2022-05-28 18:00:45 +0000

    Updated tests/test_export_points.js (added 8 lines).

--- tests/test_export_points.js
+++ tests/test_export_points.js
@@ -36,3 +36,11 @@ export_points.export_by_ecoregion('vegetation index and antecedent means', drive
 // Usage: export_by_ecoregion_cap(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list)
 export_points.export_by_ecoregion_cap('vegetation index and antecedent means', drive_folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent);
 
+
+
+
+// Test export_abs_sensitivity_cap
+// Usage: export_abs_sensitivity_cap(points, region, drive_name, drive_folder, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list)
+var points = ee.FeatureCollection.randomPoints(ecoregions, 10);
+export_points.export_abs_sensitivity_cap(points, ecoregions, 'test-export-abs-sens-cap', 'Exports', min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list)
+s
