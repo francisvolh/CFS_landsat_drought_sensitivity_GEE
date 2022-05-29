c57f009ee4c5e4764a8203802b6bd1f1b92283a0
robit.alec <robit.alec@gmail.com>
2022-05-29 17:17:56 +0000

    Updated modules/export_points.js (added 2 lines, deleted 2 lines).

--- modules/export_points.js
+++ modules/export_points.js
@@ -19,7 +19,7 @@ var export_abs_sensitivity_cap = function(points, region, drive_name, drive_fold
 
 	var today = new Date().toJSON().slice(0, 10);
 	Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + drive_name, drive_folder);
-}
+};
 exports.export_abs_sensitivity_cap = export_abs_sensitivity_cap;
 
 
