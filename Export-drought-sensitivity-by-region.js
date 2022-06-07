d20453e6f6304f418d3ad0ad067dd8372d961a9c
robit.alec <robit.alec@gmail.com>
2022-06-07 17:53:17 +0000

    Updated Export-drought-sensitivity-by-region.js (added 2 lines, deleted 1 line).

--- Export-drought-sensitivity-by-region.js
+++ Export-drought-sensitivity-by-region.js
@@ -50,6 +50,7 @@ var points = ee.FeatureCollection('users/robitalec/CFS/2022-05-25_sampling_point
 export_points.export_sensitivity_from_asset(points, 'sample-sensitivity', drive_folder);
 
 // Hydro
+export_points.export_hydro(points, 'hydro', drive_folder);
 
 // Climate
 
