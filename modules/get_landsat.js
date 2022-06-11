d3bea45d32bfe2b8594c17f50a89a92dfdfd37c6
robit.alec <robit.alec@gmail.com>
2022-06-11 17:36:45 +0000

    Updated modules/get_landsat.js (added 35 lines).

--- modules/get_landsat.js
+++ modules/get_landsat.js
@@ -48,3 +48,38 @@ var get_indices = function(min_year, max_year, min_mm_dd, max_mm_dd, region, ind
               }));
 };
 exports.get_indices = get_indices;
+
+
+// Get indices, qualityMosaic on NDVI
+var get_indices_greenest = function(min_year, max_year, min_mm_dd, max_mm_dd, region, indices) {
+  var collection = ltgee.getCombinedSRcollection(yr, min_mm_dd, max_mm_dd, region, mask);
+  var col_w_indices = ltgee.transformSRcollection(collection, indices);
+  
+  return col_w_indices.map(function(img) {
+    return img.mask(img.select('B1').neq(0)
+                       .and(img.select('B2').neq(0))
+                       .and(img.select('B3').neq(0))
+                       .and(img.select('NDVI').lt(0.98)));
+    })
+                .map(utils.set_year)
+                .map(function(img) {
+                          return img.divide(1000)
+                            .mask(img.select('B1').neq(0)
+                                  .and(img.select('B2').neq(0))
+                                  .and(img.select('B3').neq(0))
+                                  .and(img.select('ndvi').lt(0.98)))})
+                          // .qualityMosaic('ndvi'))
+  
+// 	var collection = ltgee.buildSRcollection(min_year, max_year, min_mm_dd, max_mm_dd, region, mask);
+
+	return(ltgee.transformSRcollection(collection, indices)
+              .map(utils.set_year)
+              .map(utils.add_year_band)
+              .map(function(img) {
+                return img.divide(1000)
+                          .set('system:time_start', img.get('system:time_start'))
+                          .copyProperties(img);
+              }));
+};
+exports.get_indices = get_indices;
+
