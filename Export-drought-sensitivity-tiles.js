062e7c992d8fad4a3507b16fd0d05d20cb66d7a1
robit.alec <robit.alec@gmail.com>
2022-06-15 00:00:48 +0000

    Updated Export-drought-sensitivity-tiles.js (added 1 line, deleted 1 line).

--- Export-drought-sensitivity-tiles.js
+++ Export-drought-sensitivity-tiles.js
@@ -74,7 +74,7 @@ tiles = tiles.map(function(ft) {return ft.set('id', ft.get('system:index'))});
 var tile_id_list = tiles.aggregate_array('id').distinct();
 print(tile_id_list);
 
-tile_id_list = tile_id_list.slice(90, 100);
+tile_id_list = tile_id_list.slice(100, 110);
 
 tile_id_list.evaluate(function(tile_ids) {
     tile_ids.forEach(function(tile_id) {
