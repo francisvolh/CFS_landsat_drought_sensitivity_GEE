50bd92347c27f33c4c846cc9835807d0fa18569c
robit.alec <robit.alec@gmail.com>
2022-06-18 13:36:38 +0000

    Updated Export-drought-sensitivity-tiles-BC.js (added 2 lines, deleted 2 lines).

--- Export-drought-sensitivity-tiles-BC.js
+++ Export-drought-sensitivity-tiles-BC.js
@@ -40,8 +40,8 @@ var min_mm_dd = '07-01';
 var max_mm_dd = '07-31';
 var percentile_low = 15;
 var percentile_high = 85;
-var index = ['NDVI'];
-var antecedent = ['3mo'];
+var index = ['NDVI', 'NBR'];
+var antecedent = ['3mo', '12mo'];
 
 // Get tiles
 var tiler = require('users/gena/packages:tiler');
