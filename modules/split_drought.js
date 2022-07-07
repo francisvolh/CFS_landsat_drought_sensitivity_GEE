e6853007ddcfb9631c095f53b12c10704d1c9072
robit.alec <robit.alec@gmail.com>
2022-07-07 16:27:11 +0000

    Updated modules/split_drought.js (added 45 lines, deleted 47 lines).

--- modules/split_drought.js
+++ modules/split_drought.js
@@ -4,53 +4,6 @@ Alec L. Robitaille
 */
 
 
-// Split vegetation indices into drought/non-drought pixels
-var zzz_split_drought = function(images, percentile_masks, antecedent_list, percentile_list, index_list) {
-  // Map over images
-  return images.map(function(img) {
-    // Get year
-    var yr = img.get('year');
-
-    // Filter percentile masks matching year
-    var percent_mask = percentile_masks.filter(ee.Filter.eq('year', yr)).first();
-
-    // Loop over antecedent_list
-    return ee.Image(antecedent_list.map(function(antecedent_period) {
-        // Loop over percentile_list
-        return percentile_list.map(function(percentile) {
-          // Loop over index_list
-          return index_list.map(function(index) {
-            // Set up band names
-            var percent_mask_band = 'CMI_ante' + antecedent_period + '_lt_p' + percentile;
-            var veg_band = index + '_ante' + antecedent_period + '_p' + percentile;
-            var drought_veg_band = veg_band + '_drought';
-            var base_veg_band = veg_band + '_base';
-
-            // Set up lt mask
-            var lt_mask = percent_mask.select(percent_mask_band);
-
-            // Baseline vegetation index
-            var baseline = img.select([index])
-                              .updateMask(lt_mask.not())
-                              .rename([base_veg_band]);
-
-            // Drought vegetation index
-            var drought = img.select([index])
-                             .updateMask(lt_mask)
-                             .rename([drought_veg_band]);
-            return [baseline, drought];
-          });
-        });
-      })
-    );
-  });
-};
-exports.zzz_split_drought = zzz_split_drought;
-
-
-
-
-
 // Split vegetation indices into drought/non-drought pixels, with cap at percentile_high
 var split_drought_cap = function(images, percentile_masks, antecedent_list, percentile_low, percentile_high, index_list) {
   // Map over images
@@ -95,3 +48,48 @@ var split_drought_cap = function(images, percentile_masks, antecedent_list, perc
 };
 exports.split_drought_cap = split_drought_cap;
  
+ 
+ 
+// ARCHIVE ---------------------------------------------------------
+// Split vegetation indices into drought/non-drought pixels
+var zzz_split_drought = function(images, percentile_masks, antecedent_list, percentile_list, index_list) {
+  // Map over images
+  return images.map(function(img) {
+    // Get year
+    var yr = img.get('year');
+
+    // Filter percentile masks matching year
+    var percent_mask = percentile_masks.filter(ee.Filter.eq('year', yr)).first();
+
+    // Loop over antecedent_list
+    return ee.Image(antecedent_list.map(function(antecedent_period) {
+        // Loop over percentile_list
+        return percentile_list.map(function(percentile) {
+          // Loop over index_list
+          return index_list.map(function(index) {
+            // Set up band names
+            var percent_mask_band = 'CMI_ante' + antecedent_period + '_lt_p' + percentile;
+            var veg_band = index + '_ante' + antecedent_period + '_p' + percentile;
+            var drought_veg_band = veg_band + '_drought';
+            var base_veg_band = veg_band + '_base';
+
+            // Set up lt mask
+            var lt_mask = percent_mask.select(percent_mask_band);
+
+            // Baseline vegetation index
+            var baseline = img.select([index])
+                              .updateMask(lt_mask.not())
+                              .rename([base_veg_band]);
+
+            // Drought vegetation index
+            var drought = img.select([index])
+                             .updateMask(lt_mask)
+                             .rename([drought_veg_band]);
+            return [baseline, drought];
+          });
+        });
+      })
+    );
+  });
+};
+exports.zzz_split_drought = zzz_split_drought;
