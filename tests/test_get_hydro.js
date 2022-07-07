814030e64fcb7625f59ff084b6c6a67ebf6c56fc
robit.alec <robit.alec@gmail.com>
2022-07-07 16:20:11 +0000

    Updated tests/test_get_hydro.js (deleted 15 lines).

--- tests/test_get_hydro.js
+++ tests/test_get_hydro.js
@@ -12,21 +12,6 @@ var palettes = require('users/gena/packages:palettes');
 var p = palettes.crameri.lajolla[50];
 
 
-// Test get_hand
-// Usage: get_hand(resolution, threshold)
-var hand_30_100 = get_hydro.get_hand(30, 100);
-print(hand_30_100);
-Map.addLayer(hand_30_100, {min:0, max:500, palette:p}, 'hand 30 100');
-
-var hand_30_1000 = get_hydro.get_hand(30, 1000);
-print(hand_30_1000);
-Map.addLayer(hand_30_1000, {min:0, max:500, palette:p}, 'hand 30 1000', false);
-
-var hand_90_1000 = get_hydro.get_hand(90, 1000);
-print(hand_90_1000);
-Map.addLayer(hand_90_1000, {min:0, max:500, palette:p}, 'hand 90 1000', false);
-
-
 
 // Test get_prop_permanent_snow
 // Usage: get_prop_permanent_snow(focal_dist)
