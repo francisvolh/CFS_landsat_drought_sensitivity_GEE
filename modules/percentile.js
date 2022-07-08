47ce3be63556052bd34e4e601f490ddbd5f41827
robit.alec <robit.alec@gmail.com>
2022-07-08 22:47:26 +0000

    Updated modules/percentile.js (added 29 lines, deleted 31 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -14,34 +14,6 @@ exports.get_percentile = get_percentile;
 
 
 // Compare percentile images for each antecedent period to each image's antecedent means
-// var lt_percentile_with_cap = function(images, percentile_images) {
-// 	var images_lt_percentiles = images.map(function(img) {
-//     var out = ee.Image([
-//       percentile_images.select('CMI_ante3mo_mean_p15').gte(img.select('CMI_ante3mo_mean')),
-//       percentile_images.select('CMI_ante12mo_mean_p15').gte(img.select('CMI_ante12mo_mean')),
-//       percentile_images.select('CMI_ante5yr_mean_min_p15').gte(img.select('CMI_ante5yr_mean_min')),
-      
-//       percentile_images.select('CMI_ante3mo_mean_p15').gte(img.select('CMI_ante3mo_mean')).where(
-//         img.select('CMI_ante3mo_mean').gt(percentile_images.select('CMI_ante3mo_mean_p85')), 0).rename('CMI_ante3mo_lt_p15_gt_p85'),
-//       percentile_images.select('CMI_ante12mo_mean_p15').gte(img.select('CMI_ante12mo_mean')).where(
-//         img.select('CMI_ante12mo_mean').gt(percentile_images.select('CMI_ante12mo_mean_p15')), 0).rename('CMI_ante6mo_lt_p15_gt_p85'),
-//       percentile_images.select('CMI_ante5yr_mean_min_p15').gte(img.select('CMI_ante5yr_mean_min')).where(
-//         img.select('CMI_ante5yr_mean_min').gt(percentile_images.select('CMI_ante5yr_mean_min_p15')), 0).rename('CMI_ante5yr_lt_p15_gt_p85')
-      
-//       ]).copyProperties(img);
-//     var new_names = ee.Image(out).bandNames();
-//     new_names = new_names.map(function(nm) {
-//       return ee.String(nm).replace('_min', '').replace('_mean', '_lt');
-//     });
-//     out = ee.Image(out).rename(new_names);
-//     return out;
-// 	});
-// 	return images_lt_percentiles;
-// };
-// exports.lt_percentile_with_cap = lt_percentile_with_cap;
-
-
-// Compare percentile images for each antecedent period to each image's antecedent means
 // Cap at 85th percentile
 var lt_percentile_cap = function(images, percentile_images) {
 	var images_lt_percentiles = images.map(function(img) {
@@ -67,8 +39,9 @@ exports.lt_percentile_cap = lt_percentile_cap;
 
 
 
+// ARCHIVE --------------------------------------------------------------------
 // Compare percentile images for each antecedent period to each image's antecedent means
-var lt_percentile = function(images, percentile_images) {
+var zzz_lt_percentile = function(images, percentile_images) {
 	var images_lt_percentiles = images.map(function(img) {
     var out = ee.Image([
       percentile_images.select('CMI_ante3mo.*').gte(img.select('CMI_ante3mo_mean')),
@@ -85,6 +58,31 @@ var lt_percentile = function(images, percentile_images) {
 	});
 	return images_lt_percentiles;
 };
-exports.lt_percentile = lt_percentile;
+exports.zzz_lt_percentile = zzz_lt_percentile;
 
+// Compare percentile images for each antecedent period to each image's antecedent means
+// var lt_percentile_with_cap = function(images, percentile_images) {
+// 	var images_lt_percentiles = images.map(function(img) {
+//     var out = ee.Image([
+//       percentile_images.select('CMI_ante3mo_mean_p15').gte(img.select('CMI_ante3mo_mean')),
+//       percentile_images.select('CMI_ante12mo_mean_p15').gte(img.select('CMI_ante12mo_mean')),
+//       percentile_images.select('CMI_ante5yr_mean_min_p15').gte(img.select('CMI_ante5yr_mean_min')),
 
+//       percentile_images.select('CMI_ante3mo_mean_p15').gte(img.select('CMI_ante3mo_mean')).where(
+//         img.select('CMI_ante3mo_mean').gt(percentile_images.select('CMI_ante3mo_mean_p85')), 0).rename('CMI_ante3mo_lt_p15_gt_p85'),
+//       percentile_images.select('CMI_ante12mo_mean_p15').gte(img.select('CMI_ante12mo_mean')).where(
+//         img.select('CMI_ante12mo_mean').gt(percentile_images.select('CMI_ante12mo_mean_p15')), 0).rename('CMI_ante6mo_lt_p15_gt_p85'),
+//       percentile_images.select('CMI_ante5yr_mean_min_p15').gte(img.select('CMI_ante5yr_mean_min')).where(
+//         img.select('CMI_ante5yr_mean_min').gt(percentile_images.select('CMI_ante5yr_mean_min_p15')), 0).rename('CMI_ante5yr_lt_p15_gt_p85')
+
+//       ]).copyProperties(img);
+//     var new_names = ee.Image(out).bandNames();
+//     new_names = new_names.map(function(nm) {
+//       return ee.String(nm).replace('_min', '').replace('_mean', '_lt');
+//     });
+//     out = ee.Image(out).rename(new_names);
+//     return out;
+// 	});
+// 	return images_lt_percentiles;
+// };
+// exports.lt_percentile_with_cap = lt_percentile_with_cap;
