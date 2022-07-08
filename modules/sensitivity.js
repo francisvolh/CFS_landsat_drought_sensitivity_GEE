5e23300fe9e4e22279bb845519dca41aad437ee4
robit.alec <robit.alec@gmail.com>
2022-07-08 22:02:45 +0000

    Updated modules/sensitivity.js (added 46 lines, deleted 4 lines).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -9,7 +9,49 @@ Absolute
 S {P,T,L} = baseline EVI{P} – drought EVI{P,T,L}
 */
 
-var sensitivity_absolute = function(split_indices, antecedent_list, percentile_list, index_list) {
+var sensitivity_absolute_cap = function(split_indices, antecedent_list, percentile_low, index_list) {
+	var means = split_indices.mean();
+  return ee.Image(antecedent_list.map(function(antecedent_period) {
+      return index_list.map(function(index) {
+          var id = index + '_ante' + antecedent_period + '_p' + percentile_low;
+          var baseline_band = id + '_base';
+          var drought_band = id + '_drought';
+          var sensitivity_band = 'Abs_sens_' + id;
+
+          return means.expression('baseline - drought', {
+            baseline: means.select(baseline_band),
+            drought: means.select(drought_band)
+          }).rename(sensitivity_band);
+      });
+    })
+  );
+};
+exports.sensitivity_absolute_cap = sensitivity_absolute_cap;
+
+var sensitivity_relative_cap = function(split_indices, antecedent_list, percentile_low, index_list) {
+	var means = split_indices.mean();
+  return ee.Image(antecedent_list.map(function(antecedent_period) {
+      return index_list.map(function(index) {
+          var id = index + '_ante' + antecedent_period + '_p' + percentile_low;
+          var baseline_band = id + '_base';
+          var drought_band = id + '_drought';
+          var sensitivity_band = 'Rel_sens_' + id;
+
+          return means.expression('((baseline - drought) / baseline) * 100', {
+            baseline: means.select(baseline_band),
+            drought: means.select(drought_band)
+          }).rename(sensitivity_band);
+        });
+      });
+    })
+  );
+};
+exports.sensitivity_relative_cap = sensitivity_relative_cap;
+
+
+
+// ARCHIVE --------------------------------------------------------------------
+var zzz_sensitivity_absolute = function(split_indices, antecedent_list, percentile_list, index_list) {
 	var means = split_indices.mean();
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return percentile_list.map(function(percentile) {
@@ -28,9 +70,9 @@ var sensitivity_absolute = function(split_indices, antecedent_list, percentile_l
     })
   );
 };
-exports.sensitivity_absolute = sensitivity_absolute;
+exports.zzz_sensitivity_absolute = zzz_sensitivity_absolute;
 
-var sensitivity_relative = function(split_indices, antecedent_list, percentile_list, index_list) {
+var zzz_sensitivity_relative = function(split_indices, antecedent_list, percentile_list, index_list) {
 	var means = split_indices.mean();
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return percentile_list.map(function(percentile) {
@@ -49,4 +91,4 @@ var sensitivity_relative = function(split_indices, antecedent_list, percentile_l
     })
   );
 };
-exports.sensitivity_relative = sensitivity_relative;
+exports.zzz_sensitivity_relative = zzz_sensitivity_relative;
