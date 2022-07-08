dc15b987f0c71c0d904375709b514c46a61a6887
robit.alec <robit.alec@gmail.com>
2022-07-08 22:48:44 +0000

    Updated modules/percentile.js (added 3 lines, deleted 3 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -18,11 +18,11 @@ exports.get_percentile = get_percentile;
 var lt_percentile_cap = function(images, percentile_images) {
 	var images_lt_percentiles = images.map(function(img) {
     var out = ee.Image([
-      percentile_images.select('CMI_ante3mo.*').gte(img.select('CMI_ante3mo_mean')).updateMask(
+      percentile_images.select('CMI_ante3mo_mean_p15').gte(img.select('CMI_ante3mo_mean')).updateMask(
         img.select('CMI_ante3mo_mean').gt(percentile_images.select('CMI_ante3mo_mean_p85'))),
-      percentile_images.select('CMI_ante12mo.*').gte(img.select('CMI_ante12mo_mean')).updateMask(
+      percentile_images.select('CMI_ante12mo_mean_p15').gte(img.select('CMI_ante12mo_mean')).updateMask(
         img.select('CMI_ante12mo_mean').gt(percentile_images.select('CMI_ante12mo_mean_p85'))),
-      percentile_images.select('CMI_ante5yr.*').gte(img.select('CMI_ante5yr_mean_min')).updateMask(
+      percentile_images.select('CMI_ante5yr_mean_min_p15.*').gte(img.select('CMI_ante5yr_mean_min')).updateMask(
         img.select('CMI_ante5yr_mean_min').gt(percentile_images.select('CMI_ante5yr_mean_min_p85')))
       ]).copyProperties(img);
     var new_names = ee.Image(out).bandNames();
