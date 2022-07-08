45d3004c16f38fea7d25223b1680243902bc4485
robit.alec <robit.alec@gmail.com>
2022-07-08 21:33:01 +0000

    Updated tests/test_main.js (added 1 line, deleted 1 line).

--- tests/test_main.js
+++ tests/test_main.js
@@ -18,7 +18,7 @@ var antecedent_list = ['3mo'];
 var ndvi_viz = {min:0.3, max:0.85};
 var rel_viz = {min:-50, max:50, palette: palettes.colorbrewer.RdBu[5]};
 var abs_viz = {min:-0.5, max:0.5, palette: palettes.colorbrewer.RdBu[5]};
-var cmi_viz = {min:-3, max:3, palette: palettes.colorbrewer.RdBu[5]};
+var cmi_viz = {min:-15, max:15, palette: palettes.colorbrewer.RdBu[5]};
 
 // Load an ecoregion
 var ecoregion = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada')
