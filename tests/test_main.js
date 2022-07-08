888a1d7849c052821c8f58fa4ade2b1abb61f7c8
robit.alec <robit.alec@gmail.com>
2022-07-08 21:16:01 +0000

    Updated tests/test_main.js (added 2 lines, deleted 2 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -38,11 +38,11 @@ Map.addLayer(main_index_and_antecedent.select('NDVI').first(), {min:-1, max:1}, 
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_relative = main.main_greenest('relative sensitivity', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, percentile_low, antecedent_list);
 print('relative sensitivity'); print(main_relative);
-Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p10'), rel_viz, '1995-2015 relative drought sensitivity NDVI 10th percentile 3 month antecedent', false);
+Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p15'), rel_viz, '1995-2015 relative drought sensitivity NDVI 15th percentile 3 month antecedent', false);
 
 
 // Test main - absolute
 // Usage: main(output, region, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_list);
 var main_absolute = main.main_greenest('absolute sensitivity', ecoregion, min_year, max_year, min_mm_dd, max_mm_dd, percentile_low, antecedent_list);
 print('absolute sensitivity'); print(main_absolute);
-Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3mo_p10'), abs_viz, '1995-2015 absolute drought sensitivity NDVI 10th percentile 3 month antecedent');
+Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3mo_p15'), abs_viz, '1995-2015 absolute drought sensitivity NDVI 15th percentile 3 month antecedent');
