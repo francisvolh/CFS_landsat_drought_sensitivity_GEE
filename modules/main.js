fb56d2c377a1b697126da3af621ce3600d630367
robit.alec <robit.alec@gmail.com>
2022-07-08 21:15:06 +0000

    Updated modules/main.js (added 2 lines, deleted 2 lines).

--- modules/main.js
+++ modules/main.js
@@ -20,11 +20,11 @@ var main_greenest = function(output, region,
                     percentile_low, antecedent_list) {
   // Variables
   var percentile_high = 85;
-  var index_list = ['NDVI', 'NBR'];
+  var index_list = ['NDVI', 'NBR']
 
   var years = ee.List.sequence(min_year, max_year);
   var months = ee.List.sequence(1, 12);
-  var percentile_list = percentile_low.add(percentile_high);
+  var percentile_list = [percentile_low, percentile_high];
 
   // Collections
   var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
