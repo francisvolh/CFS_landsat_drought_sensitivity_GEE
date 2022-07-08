b741a1f8845df2edf2d43dc63127b8039b3471d7
robit.alec <robit.alec@gmail.com>
2022-07-08 22:49:42 +0000

    Updated modules/main.js (added 1 line, deleted 1 line).

--- modules/main.js
+++ modules/main.js
@@ -47,7 +47,7 @@ var main_greenest = function(output, region,
   // Split vegetation index into baseline/drought
   var split = split_drought.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_low, percentile_high, index_list);
 
-  return percentile_images
+  return split
   // if (output == 'relative sensitivity') {
     // return sensitivity.sensitivity_relative_cap(split, antecedent_list, percentile_low, index_list);
   // } else if (output == 'absolute sensitivity') {
