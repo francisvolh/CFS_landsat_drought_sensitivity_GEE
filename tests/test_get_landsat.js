c83a3071f2a4698e6a98e16c631ad5e84617c90e
robit.alec <robit.alec@gmail.com>
2022-07-09 14:59:23 +0000

    Updated tests/test_get_landsat.js (added 7 lines, deleted 7 lines).

--- tests/test_get_landsat.js
+++ tests/test_get_landsat.js
@@ -21,13 +21,13 @@ var max_mm_dd = '07-31';
 
 // Test SR collection
 // Usage: get_landsat.get_SR(min_year, max_year, min_mm_dd, max_mm_dd, region)
-var sr_col = get_landsat.get_SR(min_year, max_year, min_mm_dd, max_mm_dd, geometry);
-print(sr_col);
+// var sr_col = get_landsat.get_SR(min_year, max_year, min_mm_dd, max_mm_dd, geometry);
+// print(sr_col);
 
 // Test indices collection
 // Usage: get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region, indices)
-var indices_col = get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, geometry, ['NDVI', 'EVI']);
-print(indices_col);
+// var indices_col = get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, geometry, ['NDVI', 'EVI']);
+// print(indices_col);
 
 // Test indices collection
 // Usage: get_landsat.get_indices_greenest(min_year, max_year, min_mm_dd, max_mm_dd, region)
@@ -37,7 +37,7 @@ print(indices_green_col);
 
 // Map
 Map.addLayer(geometry, null, 'region');
-Map.addLayer(sr_col.select(['B3', 'B2', 'B1']), {min: 0, max: 1.5}, 'RGB SR collection');
-Map.addLayer(indices_col.select(['NDVI']), {min: -0.5, max:1}, 'NDVI collection');
+// Map.addLayer(sr_col.select(['B3', 'B2', 'B1']), {min: 0, max: 1.5}, 'RGB SR collection');
+// Map.addLayer(indices_col.select(['NDVI']), {min: -0.5, max:1}, 'NDVI collection');
 Map.addLayer(indices_green_col.select(['NDVI']), {min: -0.5, max:1}, 'NDVI (greenest) collection');
-Map.centerObject(geometry);
+// Map.centerObject(geometry);
