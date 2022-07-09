dd8bd93b9f27736747242ae99f4789572876203b
robit.alec <robit.alec@gmail.com>
2022-07-09 16:44:06 +0000

    Updated modules/export_points.js (added 27 lines, deleted 37 lines).

--- modules/export_points.js
+++ modules/export_points.js
@@ -21,7 +21,6 @@ exports.export_hydro = export_hydro;
 
 
 var export_sensitivity_from_asset = function(points, drive_name, drive_folder) {
-
   // (thanks Noel https://gis.stackexchange.com/a/428747/27076)
   var assetList = ee.data.listAssets("users/robitalec/CFS/2022-06-14")['assets']
                     .map(function(d) { return d.name });
@@ -38,40 +37,6 @@ var export_sensitivity_from_asset = function(points, drive_name, drive_folder) {
 exports.export_sensitivity_from_asset = export_sensitivity_from_asset;
 
 
-
-// --- Export by --------------------------------------------------------------
-// Export task for each ecoregion (cap)
-var export_by_ecoregion_cap = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list) {
-  var ecoreg_id_list = ecoregions
-    .aggregate_array('ECOREGI')
-    .distinct();
-
-  ecoreg_id_list.evaluate(function(ecoreg_ids) {
-    ecoreg_ids.forEach(function(ecoreg_id) {
-      var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
-
-      var out = main.main_cap(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list);
-      var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
-      lc_modal = lc_modal.changeProj(lc_modal.projection(), land_cover.lc_and_fire.first().projection());
-      var points = stratified.stratified_sample(lc_modal, 'land_cover_mode', 30, ft.geometry(), n_pts);
-
-      var sampled = ee.ImageCollection(out).map(function(img) {
-        return img.reduceRegions(points, ee.Reducer.mean(), 30);
-      }).flatten();
-
-      var today = new Date().toJSON().slice(0, 10);
-      Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + ecoreg_id, folder);
-    });
-  });
-};
-exports.export_by_ecoregion_cap = export_by_ecoregion_cap;
-
-
-
-// TODO: export_sensitivity_by_ecoregion
-// TODO: export_hydro
-// TODO: export_climate
-// TODO: export_topo
 
 
 // ARCHIVE ---------------------------------------------------------
@@ -117,4 +82,29 @@ var zzz_export_by_ecoregion = function(output, folder, n_pts, ecoregions, min_ye
 };
 exports.zzz_export_by_ecoregion = zzz_export_by_ecoregion;
 
+// Export task for each ecoregion (cap)
+var zzz_export_by_ecoregion_cap = function(output, folder, n_pts, ecoregions, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list) {
+  var ecoreg_id_list = ecoregions
+    .aggregate_array('ECOREGI')
+    .distinct();
+
+  ecoreg_id_list.evaluate(function(ecoreg_ids) {
+    ecoreg_ids.forEach(function(ecoreg_id) {
+      var ft = ecoregions.filter(ee.Filter.eq('ECOREGI', ecoreg_id));
+
+      var out = main.main_cap(output, ft, min_year, max_year, min_mm_dd, max_mm_dd, index_list, percentile_low, percentile_high, antecedent_list);
+      var lc_modal = land_cover.lc_and_fire.reduce(ee.Reducer.mode());
+      lc_modal = lc_modal.changeProj(lc_modal.projection(), land_cover.lc_and_fire.first().projection());
+      var points = stratified.stratified_sample(lc_modal, 'land_cover_mode', 30, ft.geometry(), n_pts);
+
+      var sampled = ee.ImageCollection(out).map(function(img) {
+        return img.reduceRegions(points, ee.Reducer.mean(), 30);
+      }).flatten();
+
+      var today = new Date().toJSON().slice(0, 10);
+      Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + ecoreg_id, folder);
+    });
+  });
+};
+exports.zzz_export_by_ecoregion_cap = zzz_export_by_ecoregion_cap;
 
