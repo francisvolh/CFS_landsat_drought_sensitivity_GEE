c66e324efb584ab97aa6d21c763931a982277d22
robit.alec <robit.alec@gmail.com>
2022-07-09 13:36:13 +0000

    Updated modules/sensitivity.js (added 1 line, deleted 1 line).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -32,7 +32,7 @@ var sensitivity_relative_cap = function(split_indices, antecedent_list, index_li
 	var means = split_indices.mean();
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return index_list.map(function(index) {
-          var id = index + '_ante' + antecedent_period;
+          var id = index + '_ante' + antecedent_period + '_p15_p85';
           var baseline_band = id + 'wi_p15_p85_base';
           var drought_band = id + 'lte_p15_drought';
           var sensitivity_band = 'Rel_sens_' + id;
