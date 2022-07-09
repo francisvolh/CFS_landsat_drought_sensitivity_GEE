9056114800a1b7de0385058f6ce84cd42fcea8ea
robit.alec <robit.alec@gmail.com>
2022-07-09 01:57:56 +0000

    Updated modules/percentile.js (added 3 lines, deleted 3 lines).

--- modules/percentile.js
+++ modules/percentile.js
@@ -21,15 +21,15 @@ var wi_percentile = function(ante_images, percentile_images) {
       ee.Image.constant(0).where(ante_img.select('CMI_ante3mo_mean').gte(percentile_images.select('CMI_ante3mo_mean_p15')).and(
                                  ante_img.select('CMI_ante3mo_mean').lte(percentile_images.select('CMI_ante3mo_mean_p85'))),
                                  1)
-                          .rename('CMI_ante3mo_lt_p15_gt_p85'),
+                          .rename('CMI_ante3mo_wi_p15_p85'),
       ee.Image.constant(0).where(ante_img.select('CMI_ante12mo_mean').gte(percentile_images.select('CMI_ante12mo_mean_p15')).and(
                                  ante_img.select('CMI_ante12mo_mean').lte(percentile_images.select('CMI_ante12mo_mean_p85'))),
                                  1)
-                           .rename('CMI_ante12mo_lt_p15_gt_p85'),
+                           .rename('CMI_ante12mo_wi_p15_p85'),
       ee.Image.constant(0).where(ante_img.select('CMI_ante5yr_mean_min').gte(percentile_images.select('CMI_ante5yr_mean_min_p15')).and(
                                  ante_img.select('CMI_ante5yr_mean_min').lte(percentile_images.select('CMI_ante5yr_mean_min_p85'))),
                                  1)
-                           .rename('CMI_ante5yr_lt_p15_gt_p85')
+                           .rename('CMI_ante5yr_wi_p15_p85')
        ]).copyProperties(ante_img);
     return out;
 	});
