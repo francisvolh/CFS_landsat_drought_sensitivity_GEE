5c917c6cb117bb8b1199c1dd2993a78f20065eec
robit.alec <robit.alec@gmail.com>
2022-07-09 13:32:24 +0000

    Updated modules/sensitivity.js (added 8 lines, deleted 8 lines).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -9,13 +9,13 @@ Absolute
 S {P,T,L} = baseline EVI{P} – drought EVI{P,T,L}
 */
 
-var sensitivity_absolute_cap = function(split_indices, antecedent_list, percentile_low, index_list) {
+var sensitivity_absolute_cap = function(split_indices, antecedent_list, index_list) {
 	var means = split_indices.mean();
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return index_list.map(function(index) {
-          var id = index + '_ante' + antecedent_period + '_p' + percentile_low;
-          var baseline_band = id + '_base';
-          var drought_band = id + '_drought';
+          var id = index + '_ante' + antecedent_period;
+          var baseline_band = id + 'wi_p15_p85_base';
+          var drought_band = id + 'lte_p15_drought';
           var sensitivity_band = 'Abs_sens_' + id;
 
           return means.expression('baseline - drought', {
@@ -28,13 +28,13 @@ var sensitivity_absolute_cap = function(split_indices, antecedent_list, percenti
 };
 exports.sensitivity_absolute_cap = sensitivity_absolute_cap;
 
-var sensitivity_relative_cap = function(split_indices, antecedent_list, percentile_low, index_list) {
+var sensitivity_relative_cap = function(split_indices, antecedent_list, index_list) {
 	var means = split_indices.mean();
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return index_list.map(function(index) {
-          var id = index + '_ante' + antecedent_period + '_p' + percentile_low;
-          var baseline_band = id + '_base';
-          var drought_band = id + '_drought';
+          var id = index + '_ante' + antecedent_period;
+          var baseline_band = id + 'wi_p15_p85_base';
+          var drought_band = id + 'lte_p15_drought';
           var sensitivity_band = 'Rel_sens_' + id;
 
           return means.expression('((baseline - drought) / baseline) * 100', {
