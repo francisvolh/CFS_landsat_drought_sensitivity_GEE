97aa8fb43fbe9349aa885e8b455bed63d8ded9a6
robit.alec <robit.alec@gmail.com>
2022-07-09 12:39:10 +0000

    Updated tests/test_percentile.js (added 5 lines, deleted 5 lines).

--- tests/test_percentile.js
+++ tests/test_percentile.js
@@ -39,9 +39,9 @@ Map.addLayer(ante_means.select('CMI_ante3mo_mean').first(), cmi_viz, '2010 CMI 3
 Map.addLayer(percentile_images.select('CMI_ante3mo_mean_p15'), cmi_viz, '2010-2015 CMI 15th percentile');
 Map.addLayer(percentile_images.select('CMI_ante3mo_mean_p85'), cmi_viz, '2010-2015 CMI 85th percentile');
 
-// Test wi_percentile
-// Usage: wi_percentile(antecedent_images, percentile_images)
-var wi_percent = percentile.wi_percentile(ante_means, percentile_images);
-print('Within percentile'); print(wi_percent);
-Map.addLayer(wi_percent.select('CMI_ante3mo_wi_p15_p85').first(), {min:0, max:1}, '2010 CMI wi 15th-85th 3 month antecedent');
+// Test get_percentile_masks
+// Usage: get_percentile_masks(antecedent_images, percentile_images)
+var percentile_masks = percentile.get_percentile_masks(ante_means, percentile_images);
+print('Percentile masks', percentile_masks);
+Map.addLayer(percentile_masks.select('CMI_ante3mo_wi_p15_p85').first(), {min:0, max:1}, '2010 CMI wi 15th-85th 3 month antecedent');
 
