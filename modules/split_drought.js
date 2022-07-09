4049f7fb5b7ffd34dc1db4d42c365eb8abe598c0
robit.alec <robit.alec@gmail.com>
2022-07-09 02:10:01 +0000

    Updated modules/split_drought.js (added 46 lines).

--- modules/split_drought.js
+++ modules/split_drought.js
@@ -93,3 +93,49 @@ var zzz_split_drought = function(images, percentile_masks, antecedent_list, perc
   });
 };
 exports.zzz_split_drought = zzz_split_drought;
+
+
+
+// Split vegetation indices into drought/non-drought pixels, with cap at percentile_high
+var zzz_split_drought_cap = function(images, percentile_masks, antecedent_list, percentile_low, percentile_high, index_list) {
+  // Map over images
+  return images.map(function(img) {
+    // Get year
+    var yr = img.get('year');
+
+    // Filter percentile masks matching year
+    var percent_mask = percentile_masks.filter(ee.Filter.eq('year', yr)).first();
+
+    // Loop over antecedent_list
+    return ee.Image(antecedent_list.map(function(antecedent_period) {
+        // Loop over index_list
+        return index_list.map(function(index) {
+          // Set up band names
+          var percent_low_mask_band = 'CMI_ante' + antecedent_period + '_lt_p' + percentile_low;
+          var percent_high_mask_band = 'CMI_ante' + antecedent_period + '_lt_p' + percentile_high;
+          
+          var veg_band = index + '_ante' + antecedent_period + '_p' + percentile_low;
+          var drought_veg_band = veg_band + '_drought';
+          var base_veg_band = veg_band + '_base';
+
+          // Set up drought and base mask
+          var lt_low_mask = percent_mask.select(percent_low_mask_band);
+          var lt_high_mask = percent_mask.select(percent_high_mask_band);
+
+          // Baseline vegetation index
+          var baseline = img.select([index])
+                            .updateMask(lt_low_mask.not())
+                            .updateMask(lt_high_mask)
+                            .rename([base_veg_band]);
+
+          // Drought vegetation index
+          var drought = img.select([index])
+                           .updateMask(lt_low_mask)
+                           .rename([drought_veg_band]);
+          return [baseline, drought];
+        });
+      })
+    );
+  });
+};
+exports.zzz_split_drought_cap = zzz_split_drought_cap;
