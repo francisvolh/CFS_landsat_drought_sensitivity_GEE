0b64ea6d71ec5ff9babcd6cdcd6ce61f842ef49b
robit.alec <robit.alec@gmail.com>
2022-07-09 13:09:49 +0000

    Updated modules/split_drought.js (added 2 lines, deleted 2 lines).

--- modules/split_drought.js
+++ modules/split_drought.js
@@ -24,7 +24,7 @@ var split_drought_wi = function(images, percentile_masks, antecedent_list, index
           
           var veg_band = index + '_ante' + antecedent_period;
           var drought_veg_band = veg_band + '_drought';
-          var base_veg_band = veg_band + '_baseline';
+          var base_veg_band = veg_band + '_base';
 
           // Baseline vegetation index
           var baseline = img.select([index])
