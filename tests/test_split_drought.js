76f13f6df88cfe42a534ecbd83f4f6b3e095dbbf
robit.alec <robit.alec@gmail.com>
2022-07-09 02:18:01 +0000

    Updated tests/test_split_drought.js (added 21 lines, deleted 26 lines).

--- tests/test_split_drought.js
+++ tests/test_split_drought.js
@@ -10,12 +10,15 @@ var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
 var cmi = require('users/robitalec/CFS:modules/cmi.js');
 var palettes = require('users/gena/packages:palettes');
 var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
 
 // Set variables
 var min_year = 1985; var max_year = 2015;
 var years = ee.List.sequence(min_year, max_year);
 var months = ee.List.sequence(1, 12);
+var min_mm_dd = '07-01';
+var max_mm_dd = '07-31';
 var percentile_low = 15;
 var percentile_high = 85;
 var percentile_list = [percentile_low, percentile_high];
@@ -27,35 +30,27 @@ var geometry = ee.Geometry.Polygon([[[-125.87, 56.86], [-125.87, 54.98], [-121.8
 
 
 // Processing
+  // Collections
 var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
+var indices_col = get_landsat.get_indices_greenest(min_year, max_year, min_mm_dd, max_mm_dd, geometry);
+
+// Mask land cover and fires
+indices_col = indices_col.map(land_cover.mask_land_cover_and_fire);
+
+// CMI
 var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
-var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
-var percentile_images = percentile.get_percentile(ante_means, percentile_list);
-var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
-var indices_col = get_landsat.get_indices(min_year, max_year, '06-15', '07-15', geometry, index_list);
+
+// Define drought
+var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years); // CMI_ante3mo_mean
+var percentile_images = percentile.get_percentile(ante_means, percentile_list); //CMI_ante3mo_mean_p85 CMI_ante5yr_mean_min_p15
+  
 
 
-
-// Test split_drought
-// Usage: split_drought.split_drought(indices_col, lt_percent, antecedent_list, percentile_list, index_list)
-percentile_list = [percentile_low];
-var split_drought = split.split_drought(indices_col, lt_percent, antecedent_list, percentile_list, index_list);
-print('Less than percentile'); print(lt_percent);
-Map.addLayer(lt_percent.select('CMI_ante3mo_lt_p15').first(), {min:0, max:1}, '2010 CMI lt 15th percentile 3 month antecedent', false);
-print('Split drought'); print(split_drought);
-Map.addLayer(indices_col.select('NDVI'), null, '2010-2015 NDVI', false);
-Map.addLayer(split_drought.select('NDVI_ante3mo_p15_drought').first(),  {min: -0.5, max:1}, '2010 NDVI drought 15th percentile 3 month antecedent');
-
-
-
-// Test split_drought_cap
-// Usage: split_drought_cap.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_low, percentile_high, index_list)
-var split_drought_cap = split.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_low, percentile_high, index_list);
-print('Split drought  with cap'); print(split_drought_cap);
-Map.addLayer(split_drought_cap.select('NDVI_ante3mo_p15_drought').first(),  {min: -0.5, max:1}, '2010 NDVI drought 15th percentile 3 month antecedent');
-Map.addLayer(split_drought_cap.select('NDVI_ante3mo_p15_base'),  {min: -0.5, max:1}, '2010 NDVI baseline 15th-85th percentile 3 month antecedent', false);
+// Test split_drought_wi
+// Usage: split_drought.split_drought_wi(indices_col, wi_masks, antecedent_list, index_list)
+var split_drought_wi = split.split_drought_wi(indices_col, lt_percent, antecedent_list, percentile_low, percentile_high, index_list);
+print('Split drought withi'); print(split_drought_wi);
+Map.addLayer(split_drought_wi.select('NDVI_ante3mo_p15_drought').first(),  {min: -0.5, max:1}, '2010 NDVI drought 15th percentile 3 month antecedent');
+Map.addLayer(split_drought_wi.select('NDVI_ante3mo_p15_base'),  {min: -0.5, max:1}, '2010 NDVI baseline 15th-85th percentile 3 month antecedent', false);
 Map.centerObject(geometry);
 
-
-
-Map.addLayer(split_drought.mean().subtract(split_drought_cap.mean()), 'split drought - split drought cap')
