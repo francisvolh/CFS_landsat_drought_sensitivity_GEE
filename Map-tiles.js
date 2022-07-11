e826af22ab6f3210e181e753bd9a6380e287d607
robit.alec <robit.alec@gmail.com>
2022-07-11 13:34:35 +0000

    Updated Map-tiles.js (added 12 lines, deleted 1 line).

--- Map-tiles.js
+++ Map-tiles.js
@@ -1,4 +1,14 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = /* color: #d63000 */ee.Geometry.MultiPoint(
+        [[-122.87601510557185, 51.575257623159025],
+         [-127.6908019425976, 59.8641849594477],
+         [-127.96690934574258, 59.52208123280006],
+         [-123.13207241108223, 54.67282444419289],
+         [-106.98880981586618, 55.69926735189692]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 Map.addLayer(ee.Image.constant(1), {palette:'000', opacity:0.5})
+
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 
 var assetList = ee.data.listAssets("users/robitalec/CFS/2022-07-10")['assets']
                     .map(function(d) { return d.name });
@@ -14,6 +24,7 @@ var p = palettes.crameri.vik[10]
 
 Map.addLayer(col, {palette: p, min: -0.3, max: 0.3})
 
+Map.addLayer(land_cover.get_land_cover().filter(ee.Filter.eq('year', 2000)), null, 'lc', false);
 
 // Notes
 // Relationship between distance to climate station/consider daymet inaccuracies
