a10371027e2122a3752381edabf45bad12d1a8cf
robit.alec <robit.alec@gmail.com>
2022-07-11 13:45:01 +0000

    Updated modules/export_points.js (added 4 lines, deleted 1 line).

--- modules/export_points.js
+++ modules/export_points.js
@@ -22,7 +22,10 @@ exports.export_hydro = export_hydro;
 
 var export_sensitivity_from_asset = function(points, drive_name, drive_folder) {
   // (thanks Noel https://gis.stackexchange.com/a/428747/27076)
-  var assetList = ee.data.listAssets("users/robitalec/CFS/2022-07-10")['assets']
+  var asset_path = "users/robitalec/CFS/2022-07-10";
+  console.log('asset path: ', asset_path);
+  
+  var assetList = ee.data.listAssets(asset_path)['assets']
                     .map(function(d) { return d.name });
   var drought_sens = ee.ImageCollection(assetList);
 
