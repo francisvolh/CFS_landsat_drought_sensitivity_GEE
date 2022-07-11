38414a3ba355b1d22f5b1cef1dfcad458d97e22d
robit.alec <robit.alec@gmail.com>
2022-07-11 13:38:52 +0000

    Updated Map-tiles.js (added 1 line, deleted 1 line).

--- Map-tiles.js
+++ Map-tiles.js
@@ -24,7 +24,7 @@ var lc_p = palettes.crameri.bamako[25]
 
 
 Map.addLayer(land_cover.get_land_cover().filter(ee.Filter.eq('year', 2000)), {palette:lc_p}, 'lc');
-Map.addLayer(col, {palette: p, min: -0.3, max: 0.3})
+Map.addLayer(col, {palette: p, min: -0.3, max: 0.3}, 'absolute sensitivity')
 
 
 // Notes
