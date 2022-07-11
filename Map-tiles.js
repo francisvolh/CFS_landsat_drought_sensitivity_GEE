63400afab81106c9f4ecbd75d6d10fb9fbdf23ef
robit.alec <robit.alec@gmail.com>
2022-07-11 13:37:15 +0000

    Updated Map-tiles.js (added 3 lines, deleted 2 lines).

--- Map-tiles.js
+++ Map-tiles.js
@@ -22,9 +22,10 @@ var palettes = require('users/gena/packages:palettes');
 var p = palettes.crameri.vik[10]
 var lc_p = palettes.crameri.bamako[25]
 
+
+Map.addLayer(land_cover.get_land_cover().filter(ee.Filter.eq('year', 2000)), {palette:lc_p, opacity:0.3}, 'lc');
 Map.addLayer(col, {palette: p, min: -0.3, max: 0.3})
 
-Map.addLayer(land_cover.get_land_cover().filter(ee.Filter.eq('year', 2000)), {palette:lc_p}, 'lc');
 
 // Notes
 // Relationship between distance to climate station/consider daymet inaccuracies
