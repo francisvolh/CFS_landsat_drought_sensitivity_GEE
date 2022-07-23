8db5e9ba03ec1f67a0fe173ba5bffddba8ceb2f3
robit.alec <robit.alec@gmail.com>
2022-07-23 14:21:31 +0000

    Created tests/test_variables.js.

---
+++ tests/test_variables.js
@@ -0,0 +1,17 @@
+/*
+Testing: modules/variables.js
+Alec L. Robitaille
+*/
+
+// Load modules
+var vars = require('users/robitalec/CFS:modules/variables.js');
+
+
+// Testing
+print('Variables: index', vars.index);
+print('Variables: antecedent', vars.antecedent);
+print('Variables: min_year', vars.min_year);
+print('Variables: max_year', vars.max_year);
+print('Variables: min_mm_dd', vars.min_mm_dd);
+print('Variables: max_mm_dd', vars.max_mm_dd);
+
