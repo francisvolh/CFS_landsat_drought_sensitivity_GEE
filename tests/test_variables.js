6e082b9177853d735a69a9034a7ae94d0bb09ae2
robit.alec <robit.alec@gmail.com>
2022-07-23 14:25:11 +0000

    Updated tests/test_variables.js (added 1 line, deleted 1 line).

--- tests/test_variables.js
+++ tests/test_variables.js
@@ -8,7 +8,7 @@ var vars = require('users/robitalec/CFS:modules/variables.js');
 
 
 // Testing
-print('Variables');
+print('---- Variables -------------------------- ');
 print('Index list:', vars.index_list);
 print('Antecedent list:', vars.ante_list);
 print('Min year:', vars.min_year);
