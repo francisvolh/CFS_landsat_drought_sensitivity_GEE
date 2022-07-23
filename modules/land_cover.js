5112d2a7275fc7023a58a4886285478f8beb66f7
robit.alec <robit.alec@gmail.com>
2022-07-23 15:59:27 +0000

    Updated modules/land_cover.js (added 7 lines, deleted 2 lines).

--- modules/land_cover.js
+++ modules/land_cover.js
@@ -54,14 +54,19 @@ var fire = require('users/robitalec/CFS:modules/fire.js');
 // Data
 // Hermosilla land cover
 var hermosilla_1984_2019 = ee.ImageCollection("projects/sat-io/open-datasets/CA_FOREST_LC_VLCE2");
+
+
+
+// Add 2020
 var lc_2019 = ee.Image(hermosilla_1984_2019.filter(ee.Filter.date('2019-01-01')).first());
-
-
 var lc_2020 = lc_2019
   .set('system:time_start', ee.Date(lc_2019.get('system:time_start')).advance(1, 'year').millis())
   .set('system:time_end', ee.Date(lc_2019.get('system:time_end')).advance(1, 'year').millis());
 var hermosilla_1984_2019_plus_2020 = ee.ImageCollection(hermosilla_1984_2019.toList(50).add(lc_2020));
 exports.hermosilla_1984_2019_plus_2020 = hermosilla_1984_2019_plus_2020;
+
+
+
 
 // Mask classes
 var mask_classes = function(img) {
