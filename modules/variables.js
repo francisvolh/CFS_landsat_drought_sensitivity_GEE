b3272cc46da6923a85c33946745a57df6cbfc1ed
robit.alec <robit.alec@gmail.com>
2022-07-23 14:24:27 +0000

    Updated modules/variables.js (added 2 lines, deleted 2 lines).

--- modules/variables.js
+++ modules/variables.js
@@ -3,8 +3,8 @@ Variables
 Alec L. Robitaille
 */
 
-exports.index = ['NDVI', 'NBR'];
-exports.antecedent = ['3mo', '12mo', '5yr'];
+exports.index_list = ['NDVI', 'NBR'];
+exports.ante_list = ['3mo', '12mo', '5yr'];
 exports.min_year = 1985;
 exports.max_year = 2021;
 exports.min_mm_dd = '07-01';
