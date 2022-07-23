35f1f16ad1f96795e95f9846a1e29f1bf841e281
robit.alec <robit.alec@gmail.com>
2022-07-23 14:56:52 +0000

    Updated modules/main.js (added 6 lines, deleted 6 lines).

--- modules/main.js
+++ modules/main.js
@@ -13,16 +13,16 @@ var percentile = require('users/robitalec/CFS:modules/percentile.js');
 var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
 var split = require('users/robitalec/CFS:modules/split_drought.js');
 var sensitivity = require('users/robitalec/CFS:modules/sensitivity.js');
+var vars = require('users/robitalec/CFS:modules/variables.js');
 
 
 var main_greenest = function(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list) {
   // Variables
-  var percentile_low = 15;
-  var percentile_high = 85;
-  var index_list = ['NDVI', 'NBR'];
-
-  var years = ee.List.sequence(min_year, max_year);
-  var months = ee.List.sequence(1, 12);
+  var percentile_low = vars.percentile_low;
+  var percentile_high = vars.percentile_high;
+  var index_list = vars.index_list;
+  var years = ee.List.sequence(vars.min_year, vars.max_year);
+  var months = vars.months;
   var percentile_list = [percentile_low, percentile_high];
 
   // Collections
