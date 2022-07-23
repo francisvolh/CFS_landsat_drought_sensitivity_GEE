32ee98dd0eb16ef34dd6c53a6a32ba4a8471bd61
robit.alec <robit.alec@gmail.com>
2022-07-23 14:15:06 +0000

    Updated Export-image-drive.js (added 1 line, deleted 1 line).

--- Export-image-drive.js
+++ Export-image-drive.js
@@ -25,7 +25,7 @@ var region = ee.FeatureCollection(geometry);
 var index = ['NDVI'];
 var antecedent = ['3mo'];
 var min_year = 1985;
-var max_year = 2020;
+var max_year = 2021;
 var min_mm_dd = '07-01';
 var max_mm_dd = '07-31';
 
