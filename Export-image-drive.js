3cd8d0ab1fa6da7179156f56ed649c9cfe062142
robit.alec <robit.alec@gmail.com>
2022-07-23 14:30:12 +0000

    Updated Export-image-drive.js (added 11 lines, deleted 1 line).

--- Export-image-drive.js
+++ Export-image-drive.js
@@ -19,6 +19,10 @@ Alec L. Robitaille
 
 // Load modules
 var export_img = require('users/robitalec/CFS:modules/export_img.js');
+var vars = require('users/robitalec/CFS:modules/variables.js');
+
+
+
 
 // Set variables
 var region = ee.FeatureCollection(geometry);
@@ -28,10 +32,16 @@ var min_year = 1985;
 var max_year = 2021;
 var min_mm_dd = '07-01';
 var max_mm_dd = '07-31';
+var index_list = vars.index_list;
+var ante_list = vars.ante_list;
+var min_year =  vars.min_year;
+var max_year = vars.max_year;
+var min_mm_dd = vars.min_mm_dd;
+var max_mm_dd = vars.max_mm_dd;
 
 
 // File name
-var export_name = 'sens_' + index + '_' + antecedent + '_p15_p85';
+var export_name = 'sens_p15_p85';
 
 // Export image to drive
 export_img.export_img_drive_greenest(export_name, 'Exports', 30, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
