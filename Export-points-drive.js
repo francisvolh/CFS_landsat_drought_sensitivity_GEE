7106a8d21144e5b9da3b157a763f0915e62bf3e0
robit.alec <robit.alec@gmail.com>
2022-07-23 14:37:39 +0000

    Updated Export-points-drive.js (added 4 lines, deleted 19 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -1,19 +1,3 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var geometry = 
-    /* color: #d63000 */
-    /* shown: false */
-    ee.Geometry.Polygon(
-        [[[-141.4430528814123, 68.27919277463084],
-          [-141.13300806589254, 64.04137893026935],
-          [-139.73426050644431, 60.04815657459371],
-          [-126.34684688322079, 49.10211106287488],
-          [-108.90484061318891, 49.055202844054236],
-          [-91.79138600476233, 48.67346079998252],
-          [-95.41638550817073, 59.939877470328504],
-          [-99.80739862269407, 62.42033801780019],
-          [-108.28910573882132, 65.53581583394379],
-          [-128.65907556424258, 68.27919277463084]]]);
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Export points drive
 Based on: tests/test_export_points.js
@@ -30,13 +14,13 @@ var export_points = require('users/robitalec/CFS:modules/export_points.js');
 
 // Variables ------------------------------------------------------------------
 var drive_folder = 'Exports';
-
+var western = vars.western;
 
 
 // Data -----------------------------------------------------------------------
 // Load ecoregions
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada');
-ecoregions = ecoregions.filterBounds(geometry);
+ecoregions = ecoregions.filterBounds(western);
 
 // Load points
 var points = ee.FeatureCollection('users/robitalec/CFS/2022-05-25_sampling_points_n125');
@@ -55,3 +39,4 @@ export_points.export_hydro(points, 'hydro', drive_folder);
 
 // Topo
 
+Map.addLayer(geometry)
