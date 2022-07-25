a96149fe6cb6082cf258ad227ae2e51723b15815
robit.alec <robit.alec@gmail.com>
2022-07-25 20:18:19 +0000

    Updated Check-landsat-check-period-strip.js (deleted 4 lines).

--- Check-landsat-check-period-strip.js
+++ Check-landsat-check-period-strip.js
@@ -13,10 +13,6 @@ var palettes = require('users/gena/packages:palettes');
 var blend = require('users/jja/public:blend.js');
 
 
-// Set variables
-var geometry = ee.Geometry.Polygon([[[-107.279, 59.673], [-107.279, 58.941],
-                                    [-105.988, 58.941], [-105.988, 59.673]]]);
-
 var min_year = 2015;
 var max_year = min_year + 1;
 
