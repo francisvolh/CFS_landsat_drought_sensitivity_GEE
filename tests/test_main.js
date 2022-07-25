a31eab73583b17997e0561d0557d9fe6a3a2fd12
robit.alec <robit.alec@gmail.com>
2022-07-25 18:51:13 +0000

    Updated tests/test_main.js (added 4 lines, deleted 4 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -49,20 +49,20 @@ var geometry = ee.Geometry.Polygon([[[-125.87, 56.86], [-125.87, 54.98], [-121.8
 var main_index_and_antecedent = main.main_greenest('vegetation index and antecedent means', geometry, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 print('veg index + antecedent means'); print(main_index_and_antecedent);
 Map.setOptions('SATELLITE');
-Map.addLayer(main_index_and_antecedent.select('CMI_ante3mo_mean').first(), cmi_viz, '1995 CMI 3 month antecedent mean', false);
-Map.addLayer(main_index_and_antecedent.select('NDVI').first(), ndvi_viz, '1995 NDVI', false);
+Map.addLayer(main_index_and_antecedent.select('CMI_ante3mo_mean').first(), cmi_viz, 'CMI 3 month antecedent mean', false);
+Map.addLayer(main_index_and_antecedent.select('NDVI').first(), ndvi_viz, 'NDVI', false);
 
 // Test main - relative
 // Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 var main_relative = main.main_greenest('relative sensitivity', geometry, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 print('relative sensitivity'); print(main_relative);
-Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p15_p85'), rel_viz, '1995-2015 relative drought sensitivity NDVI p15-85 3 month antecedent', false);
+Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p15_p85'), rel_viz, 'relative drought sensitivity NDVI p15-85 3 month antecedent', false);
 
 
 // Test main - absolute
 // Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 var main_absolute = main.main_greenest('absolute sensitivity', geometry, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 print('absolute sensitivity'); print(main_absolute);
-Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3mo_p15_p85'), abs_viz, '1995-2015 absolute drought sensitivity NDVI p15-85  3 month antecedent');
+Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3mo_p15_p85'), abs_viz, 'absolute drought sensitivity NDVI p15-85  3 month antecedent');
 
 Map.centerObject(geometry);
