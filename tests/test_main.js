86d091246585bf22996928ee7c9e1d3a0128e7ab
robit.alec <robit.alec@gmail.com>
2022-07-25 18:50:19 +0000

    Updated tests/test_main.js (added 4 lines, deleted 4 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -28,10 +28,10 @@ var main = require('users/robitalec/CFS:modules/main.js');
 var palettes = require('users/gena/packages:palettes');
 
 // Set variables
-var min_year = 1995;
-var max_year = 2015;
-var min_mm_dd = '06-15';
-var max_mm_dd = '07-15';
+var min_year = 1985;
+var max_year = 2021;
+var min_mm_dd = '07-01';
+var max_mm_dd = '07-31';
 var percentile_low = 15;
 var antecedent_list = ['3mo'];
 
