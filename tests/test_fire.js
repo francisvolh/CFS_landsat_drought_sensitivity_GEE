91f81bef598b6c6b3922680f6f3ea2dea01728af
robit.alec <robit.alec@gmail.com>
2022-07-25 19:00:26 +0000

    Updated tests/test_fire.js (added 16 lines, deleted 3 lines).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -1,3 +1,17 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #d63000 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-128.7288102930123, 63.004155935418076],
+          [-128.7288102930123, 60.074728761978506],
+          [-122.84013841801229, 60.074728761978506],
+          [-122.84013841801229, 63.004155935418076]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Testing: modules/fire.js
 Alec L. Robitaille
@@ -8,7 +22,6 @@ Alec L. Robitaille
 var fire = require('users/robitalec/CFS:modules/fire.js');
 
 
-
 // Test five_year_fires
 // Usage: fire.five_year_fires(year)
 var five_year_fires = fire.five_year_fires(2021);
@@ -17,7 +30,7 @@ Map.addLayer(five_year_fires, {min:0, max:1, palette: ['000000', 'ffc781']}, 'fi
 
 // Test mask_five_year_fires
 // Usage: fire.mask_five_year_fires(img)
-var img = ee.Image.random(42, 'normal').set('year', 2021);
+var img = ee.Image.random(42, 'normal').clip(geometry).set('year', 2021);
 var mask_five_year_fires = fire.mask_five_year_fires(img);
 print(mask_five_year_fires);
 Map.addLayer(mask_five_year_fires, null, 'mask_five_year_fires');
