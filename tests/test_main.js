d612cb3609feb3c56cee7d80e3f16b0037c1748d
robit.alec <robit.alec@gmail.com>
2022-07-26 13:55:11 +0000

    Updated tests/test_main.js (added 2 lines, deleted 2 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -10,8 +10,8 @@ var palettes = require('users/gena/packages:palettes');
 // Set variables
 var min_year = 1985;
 var max_year = 2021;
-var min_mm_dd = '07-01';
-var max_mm_dd = '07-31';
+var min_mm_dd = '06-01';
+var max_mm_dd = '09-30';
 var percentile_low = 15;
 var antecedent_list = ['3mo'];
 
