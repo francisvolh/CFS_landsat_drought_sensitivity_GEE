d024297a163274456c60fd54298d5db48a957cab
robit.alec <robit.alec@gmail.com>
2022-07-26 19:01:55 +0000

    Updated modules/nobs.js (added 8 lines).

--- modules/nobs.js
+++ modules/nobs.js
@@ -2,3 +2,11 @@
 Number of observations
 Alec L. Robitaille
 */
+
+
+var count_nobs = function(split_indices, sensitivity) {
+  var count = split_indices
+    .reduce(ee.Reducer.count())
+
+  return sensitivity.addBands(count);
+}
