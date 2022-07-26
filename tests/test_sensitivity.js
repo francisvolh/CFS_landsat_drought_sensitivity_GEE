c375c45df7af50241b5523737bec91468ece7d99
robit.alec <robit.alec@gmail.com>
2022-07-26 18:24:43 +0000

    Updated tests/test_sensitivity.js (added 7 lines, deleted 2 lines).

--- tests/test_sensitivity.js
+++ tests/test_sensitivity.js
@@ -13,6 +13,7 @@ var palettes = require('users/gena/packages:palettes');
 var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
 var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+var fire = require('users/robitalec/CFS:modules/fire.js');
 
 // Set variables
 var min_year = 1985; var max_year = 2015;
@@ -36,7 +37,10 @@ var geometry = ee.Geometry.Polygon([[[-125.87, 56.86], [-125.87, 54.98], [-121.8
 // Processing
 var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
 var indices_col = get_landsat.get_indices_greenest(min_year, max_year, min_mm_dd, max_mm_dd, geometry);
-indices_col = indices_col.map(land_cover.mask_land_cover_and_fire);
+var lc_mask = land_cover.get_lc_count_mask();
+indices_col = indices_col.map(function(img) {
+  return fire.mask_five_year_fires(img.updateMask(lc_mask));
+});
 var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
 var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
 var percentile_images = percentile.get_percentile(ante_means, percentile_list); 
@@ -58,6 +62,7 @@ print('Relative sensitivity', sens_relative);
 Map.addLayer(percentile_masks.select('CMI_ante3mo_lte_p15').first(), {min:0, max:1}, '2010 CMI lte p15th 3 month antecedent');
 Map.addLayer(split_drought_wi.select('NDVI_ante3mo_lte_p15_drought').mean(),  {min: -0.5, max:1}, '2010-2015 mean NDVI drought lte 15th 3 month antecedent');
 Map.addLayer(split_drought_wi.select('NDVI_ante3mo_wi_p15_p85_base').mean(),  {min: -0.5, max:1}, '2010-2015 mean NDVI baseline wi p15-85 3 month antecedent', false);
+Map.addLayer(split_drought_wi.reduce(ee.Reducer.count()))
 Map.addLayer(sens_relative.select('Rel_sens_NDVI_ante3mo_p15_p85'), rel_viz, '2010-2015 relative drought sensitivity NDVI p15-85 3 month antecedent', false);
 Map.addLayer(sens_absolute.select('Abs_sens_NDVI_ante3mo_p15_p85'), abs_viz, '2010-2015 absolute drought sensitivity NDVI p15-85 3 month antecedent');
 
