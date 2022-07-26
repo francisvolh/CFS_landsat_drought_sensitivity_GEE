07d69194ba7ff4591ebe0d48ff01ecb86cc408eb
robit.alec <robit.alec@gmail.com>
2022-07-26 18:20:34 +0000

    Updated tests/test_sensitivity.js (added 2 lines).

--- tests/test_sensitivity.js
+++ tests/test_sensitivity.js
@@ -61,5 +61,7 @@ Map.addLayer(split_drought_wi.select('NDVI_ante3mo_wi_p15_p85_base').mean(),  {m
 Map.addLayer(sens_relative.select('Rel_sens_NDVI_ante3mo_p15_p85'), rel_viz, '2010-2015 relative drought sensitivity NDVI p15-85 3 month antecedent', false);
 Map.addLayer(sens_absolute.select('Abs_sens_NDVI_ante3mo_p15_p85'), abs_viz, '2010-2015 absolute drought sensitivity NDVI p15-85 3 month antecedent');
 
+Map.addLayer(sens_absolute.select('NDVI_ante3mo_lte_p15_drought_count'),  null, '2010-2015 drought count', false);
+Map.addLayer(sens_absolute.select('NDVI_ante3mo_wi_p15_p85_base_count'),  null, '2010-2015 baseline count', false);
 
 
