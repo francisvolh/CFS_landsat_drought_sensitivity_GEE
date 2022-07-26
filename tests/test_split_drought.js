d80b746b763f9a2f797ae7b9d51522bf9ecb653c
robit.alec <robit.alec@gmail.com>
2022-07-26 18:06:05 +0000

    Updated tests/test_split_drought.js (added 2 lines).

--- tests/test_split_drought.js
+++ tests/test_split_drought.js
@@ -57,5 +57,7 @@ var split_drought_wi = split.split_drought_wi(indices_col, percentile_masks, ant
 print('Split drought within', split_drought_wi);
 Map.addLayer(split_drought_wi.select('NDVI_ante3mo_lte_p15_drought'),  {min: -0.5, max:1}, '2010 NDVI drought 15-85th 3 month antecedent');
 Map.addLayer(split_drought_wi.select('NDVI_ante3mo_wi_p15_p85_base'),  {min: -0.5, max:1}, '2010 NDVI baseline 15th-85th 3 month antecedent', false);
+Map.addLayer(split_drought_wi.select('NDVI_ante3mo_lte_p15_drought_count'),  null, '2010 NDVI drought 15th-85th count', false);
+Map.addLayer(split_drought_wi.select('NDVI_ante3mo_wi_p15_p85_base_count'),  null, '2010 NDVI baseline 15th-85th count', false);
 Map.centerObject(geometry);
 
