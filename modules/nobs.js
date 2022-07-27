1e1b713688c8721e31d727d0c01a833d388ef125
robit.alec <robit.alec@gmail.com>
2022-07-27 18:56:26 +0000

    Updated modules/nobs.js (added 2 lines, deleted 2 lines).

--- modules/nobs.js
+++ modules/nobs.js
@@ -11,11 +11,11 @@ var count_nobs = function(split_indices, sensitivity) {
 };
 exports.count_nobs = count_nobs;
 
-var mask_nobs = function(counts, antecedent_list, index_list) {
+var mask_nobs = function(type, counts, antecedent_list, index_list) {
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return index_list.map(function(index) {
           var id = index + '_ante' + antecedent_period;
-          var sensitivity_band = 'Abs_sens_' + id + '_p15_p85';
+          var sensitivity_band = type + '_sens_' + id + '_p15_p85';
           var baseline_count_band = id + '_wi_p15_p85_base' + '_count';
           var drought_count_band = id + '_lte_p15_drought' + '_count';
           
