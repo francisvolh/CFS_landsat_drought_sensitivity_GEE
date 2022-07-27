8436410e9d9bf3dd7cd370e8df0b6b5df1e9afcc
robit.alec <robit.alec@gmail.com>
2022-07-27 18:57:15 +0000

    Updated modules/main.js (added 2 lines, deleted 2 lines).

--- modules/main.js
+++ modules/main.js
@@ -50,12 +50,12 @@ var main_greenest = function(output, region, min_year, max_year, min_mm_dd, max_
     var sens_rel = sensitivity.sensitivity_relative_cap(split_drought_wi, antecedent_list, index_list);
 
     var counts_rel = nobs.count_nobs(split_drought_wi, sens_rel);
-    return nobs.mask_nobs(counts_rel, antecedent_list, index_list);
+    return nobs.mask_nobs('Rel', counts_rel, antecedent_list, index_list);
   } else if (output == 'absolute sensitivity') {
     var sens_abs = sensitivity.sensitivity_absolute_cap(split_drought_wi, antecedent_list, index_list);
 
     var counts_abs = nobs.count_nobs(split_drought_wi, sens_abs);
-    return nobs.mask_nobs(counts_abs, antecedent_list, index_list);
+    return nobs.mask_nobs('Abs', counts_abs, antecedent_list, index_list);
   } else if (output == 'vegetation index and antecedent means') {
     var join = ee.Join.inner();
     var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
