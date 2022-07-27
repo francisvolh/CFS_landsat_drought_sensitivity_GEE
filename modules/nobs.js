48c31aa89fc66d2d7f5ad9a2c4901a0373727df8
robit.alec <robit.alec@gmail.com>
2022-07-27 18:04:03 +0000

    Updated modules/nobs.js (added 6 lines, deleted 8 lines).

--- modules/nobs.js
+++ modules/nobs.js
@@ -15,19 +15,17 @@ var mask_nobs = function(sensitivity, antecedent_list, index_list) {
   return ee.Image(antecedent_list.map(function(antecedent_period) {
       return index_list.map(function(index) {
           var id = index + '_ante' + antecedent_period;
-          var baseline_band = id + '_wi_p15_p85_base';
-          var drought_band = id + '_lte_p15_drought';
           var sensitivity_band = 'Abs_sens_' + id + '_p15_p85';
-          var baseline_count_band = baseline_band + '_count';
-          var drought_count_band = drought_band + '_count';
+          var baseline_count_band = id + '_wi_p15_p85_base' + '_count';
+          var drought_count_band = id + '_lte_p15_drought' + '_count';
           
           var mask_baseline = sensitivity.select(baseline_count_band).gte(21);
           var mask_drought = sensitivity.select(drought_count_band).gte(3);
           
-          return ee.Image([sensitivity.select(baseline_band)
-                                      .updateMask(mask_baseline),
-                           sensitivity.select(drought_band)
-                                      .updateMask(mask_drought)]);
+          var mask = mask_baseline.and(mask_drought);
+          
+          return ee.Image([sensitivity.select(sensitivity_band)
+                                      .updateMask(mask)]);
       });
   }));
 };
