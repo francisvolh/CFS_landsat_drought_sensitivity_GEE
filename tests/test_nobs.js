56719efe3a26ea549c176242061c7ff2566090ee
robit.alec <robit.alec@gmail.com>
2022-07-27 17:51:02 +0000

    Updated tests/test_nobs.js (added 2 lines, deleted 1 line).

--- tests/test_nobs.js
+++ tests/test_nobs.js
@@ -56,7 +56,8 @@ var sens_absolute = sensitivity.sensitivity_absolute_cap(split_drought_wi, antec
 var counts = nobs.count_nobs(split_drought_wi, sens_absolute);
 
 print('Counts:', counts);
-Map.addLayer(ee.Image.constant(1), {palette: '#113355'});
+Map.addLayer(geometry, null, 'region');
+Map.addLayer(ee.Image.constant(1), {palette: '#113355'}, 'constant');
 Map.addLayer(counts.select('NDVI_ante3mo_wi_p15_p85_base_count'), {min: 0, max:30}, 'baseline count');
 Map.addLayer(counts.select('NDVI_ante3mo_wi_p15_p85_base_count').gte(10), null, 'baseline count gte 10');
 Map.addLayer(counts.select('NDVI_ante3mo_lte_p15_drought_count'), {min: 0, max:6}, 'drought count');
