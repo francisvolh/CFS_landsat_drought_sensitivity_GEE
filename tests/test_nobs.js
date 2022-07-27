85de5397bed4ccad9cd13760064851b159e2b6cc
robit.alec <robit.alec@gmail.com>
2022-07-27 18:11:07 +0000

    Updated tests/test_nobs.js (added 1 line, deleted 1 line).

--- tests/test_nobs.js
+++ tests/test_nobs.js
@@ -69,5 +69,5 @@ Map.addLayer(counts.select('NDVI_ante3mo_wi_p15_p85_base_count'), {min: 0, max:3
 Map.addLayer(counts.select('NDVI_ante3mo_wi_p15_p85_base_count').gte(21), null, 'baseline count gte 21', false);
 Map.addLayer(counts.select('NDVI_ante3mo_lte_p15_drought_count'), {min: 0, max:6}, 'drought count');
 Map.addLayer(counts.select('NDVI_ante3mo_lte_p15_drought_count').gte(3), null, 'drought count gte 3', false);
-Map.addLayer(counts.select('Abs_sens_NDVI_ante3mo_p15_p85'), null, 'sensitivity');
+Map.addLayer(counts.select('Abs_sens_NDVI_ante3mo_p15_p85'), {min:-0.3, max:0.3, palette: palettes.crameri.vik[10]}, 'sensitivity');
 
