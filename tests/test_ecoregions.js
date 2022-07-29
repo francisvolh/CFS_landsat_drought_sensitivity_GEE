35733e6b6973cfd5845f80f904137cdb444c2fab
robit.alec <robit.alec@gmail.com>
2022-07-29 20:56:48 +0000

    Updated tests/test_ecoregions.js (added 3 lines, deleted 4 lines).

--- tests/test_ecoregions.js
+++ tests/test_ecoregions.js
@@ -20,7 +20,6 @@ print('Ecoregions:', ecoregions);
 print('Ecoregion, ecozone, ecoprovince as image bands:', ecoreg_bands);
 
 Map.addLayer(ecoregions);
-var viz = {min: 0, max: 300};
-Map.addLayer(ecoreg_bands.select('ecoregion'), viz);
-Map.addLayer(ecoreg_bands.select('ecozone'), viz);
-Map.addLayer(ecoreg_bands.select('ecoprovince'), viz);
+Map.addLayer(ecoreg_bands.select('ecoregion'), {min: 1, max: 217});
+Map.addLayer(ecoreg_bands.select('ecoprovince'), {min: 1.1, max: 15.2});
+Map.addLayer(ecoreg_bands.select('ecozone'), {min: 1, max: 15});
