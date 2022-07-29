e4e0c53a88c9d209b454fc3a82c3fc62ce89fd09
robit.alec <robit.alec@gmail.com>
2022-07-29 21:04:26 +0000

    Updated Export-points-drive.js (added 1 line, deleted 1 line).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -30,7 +30,7 @@ var points = ee.FeatureCollection('users/robitalec/CFS/2022-07-29_Yukon_sampling
 
 // Sample ---------------------------------------------------------------------
 // Sensitivity
-export_points.export_sensitivity_from_asset(points, 'sample-sensitivity', drive_folder);
+export_points.export_sensitivity_from_asset(points, 'sample-sensitivity-Yukon', drive_folder);
 
 // Hydro
 // export_points.export_hydro(points, 'hydro', drive_folder);
