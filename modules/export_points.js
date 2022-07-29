751423e0f847f8a158bff895a2da0b330ebbb7a0
robit.alec <robit.alec@gmail.com>
2022-07-29 20:28:26 +0000

    Updated modules/export_points.js (added 1 line, deleted 1 line).

--- modules/export_points.js
+++ modules/export_points.js
@@ -22,7 +22,7 @@ exports.export_hydro = export_hydro;
 
 var export_sensitivity_from_asset = function(points, drive_name, drive_folder) {
   // (thanks Noel https://gis.stackexchange.com/a/428747/27076)
-  var asset_path = "users/robitalec/CFS/2022-07-10";
+  var asset_path = "users/robitalec/CFS/2022-07-28";
   print('asset path: ', asset_path);
   
   var assetList = ee.data.listAssets(asset_path)['assets']
