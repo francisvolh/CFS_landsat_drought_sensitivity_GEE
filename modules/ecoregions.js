ff5c77b68e717dd56343b689f9f66e6153f0af33
robit.alec <robit.alec@gmail.com>
2022-07-29 20:50:23 +0000

    Updated modules/ecoregions.js (added 4 lines, deleted 4 lines).

--- modules/ecoregions.js
+++ modules/ecoregions.js
@@ -5,14 +5,14 @@ Alec L. Robitaille
 
 
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada')
-export.ecoregions = ecoregions;
+exports.ecoregions = ecoregions;
 
 
 var get_eco_bands = function() {
 	return ee.Image([
-	  ecoregions.reduceToImage(['ECOPROV'], ee.Reducer.first()).rename('ECOPROV'),
-	  ecoregions.reduceToImage(['ECOREGI'], ee.Reducer.first()).rename('ECOREGI'),
-	  ecoregions.reduceToImage(['ECOZONE'], ee.Reducer.first()).rename('ECOZONE'),
+    ecoregions.reduceToImage(['ECOPROV'], ee.Reducer.first()).rename('ECOPROV'),
+    ecoregions.reduceToImage(['ECOREGI'], ee.Reducer.first()).rename('ECOREGI'),
+    ecoregions.reduceToImage(['ECOZONE'], ee.Reducer.first()).rename('ECOZONE'),
   ]);
 }
 exports.get_eco_bands = get_eco_bands;
