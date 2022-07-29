c5df1145c26a4575aff1279194241cec4881fa9c
robit.alec <robit.alec@gmail.com>
2022-07-29 19:57:10 +0000

    Updated tests/test_points.js (added 9 lines, deleted 2 lines).

--- tests/test_points.js
+++ tests/test_points.js
@@ -6,9 +6,16 @@ Alec L. Robitaille
 
 // Modules
 var points = require('users/robitalec/CFS:modules/points.js');
+var vars = require('users/robitalec/CFS:modules/variables.js');
+
+// Region
+var region = vars.yukon;
+var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada')
+  .filterBounds(region);
+
 
 
 // Test export_points_asset
-// Usage: export_points_asset(n_pts)
+// Usage: export_points_asset(n_pts, ecoregions)
 var n_pts = 125;
-points.export_points_asset(125);
+points.export_points_asset(125, ecoregions);
