3a66c7382147ae5234598bd8549b97adcaf62acd
robit.alec <robit.alec@gmail.com>
2022-07-29 19:55:57 +0000

    Updated modules/points.js (added 11 lines, deleted 13 lines).

--- modules/points.js
+++ modules/points.js
@@ -16,19 +16,17 @@ var region = vars.yukon;
 var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregions_Canada')
   .filterBounds(region);
   
-var lc_mask = land_cover.get_lc_count_mask();
-print(lc_mask)
 
 // Export points as asset
-// var export_points_asset = function(n_pts) {
-//   var lc_mask = land_cover.get_lc_count_mask();
+var export_points_asset = function(n_pts, ecoregions) {
+  var lc_mask = land_cover.get_lc_count_mask();
   
-//   var points = ecoregions.map(function(ft) {
-//     return stratified.stratified_sample(lc_mask, 'land_cover_count', 30, ft.geometry(), n_pts);
-//   }).flatten();
+  var points = ecoregions.map(function(ft) {
+    return stratified.stratified_sample(lc_mask, 'land_cover_count', 30, ft.geometry(), n_pts);
+  }).flatten();
   
-//   var today = new Date().toJSON().slice(0, 10);
-//   var filename = today + '_sampling_points_n' + n_pts;
-//   Export.table.toAsset(points, filename, 'CFS/' + filename);
-// };
-// exports.export_points_asset = export_points_asset;
+  var today = new Date().toJSON().slice(0, 10);
+  var filename = today + '_sampling_points_n' + n_pts;
+  Export.table.toAsset(points, filename, 'CFS/' + filename);
+};
+exports.export_points_asset = export_points_asset;
