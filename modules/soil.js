aa0675f51fa046cc1b74205f7d0bec2dd7cd6bcd
robit.alec <robit.alec@gmail.com>
2022-08-01 21:17:52 +0000

    Updated modules/soil.js (added 3 lines, deleted 1 line).

--- modules/soil.js
+++ modules/soil.js
@@ -25,7 +25,9 @@ var get_soil_percent = function() {
 	return ee.Image([silt, sand, clay])
     .multiply(0.1)
     .select(['sand_0-5cm_mean', 'clay_0-5cm_mean', 'silt_0-5cm_mean'],
-            ['sand_0-5cm_percent', 'clay_0-5cm_percent', 'silt_0-5cm_percent']);
+            ['sand_0-5cm_percent', 'clay_0-5cm_percent', 'silt_0-5cm_percent'])
+    .round()
+    .toInt();
 };
 exports.get_soil_percent = get_soil_percent;
 
