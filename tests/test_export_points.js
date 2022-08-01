af12ab539c00821cbc1491269a15efd86ac16cfb
robit.alec <robit.alec@gmail.com>
2022-08-01 20:21:47 +0000

    Updated tests/test_export_points.js (added 21 lines, deleted 2 lines).

--- tests/test_export_points.js
+++ tests/test_export_points.js
@@ -1,3 +1,17 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #d63000 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-139.0142730574053, 63.6423407696705],
+          [-139.0142730574053, 62.98366563970803],
+          [-137.0532134870928, 62.98366563970803],
+          [-137.0532134870928, 63.6423407696705]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Testing: modules/exports_points.js
 Alec L. Robitaille
@@ -10,7 +24,12 @@ var vars = require('users/robitalec/CFS:modules/variables.js');
 // Set variables
 var drive_folder = 'Exports';
 var n_pts = 10;
-
+var geometry = ee.Geometry.Polygon(
+        [[[-139.014, 63.642],
+          [-139.014, 62.983],
+          [-137.053, 62.983],
+          [-137.053, 63.642]]]);
+var points = ee.FeatureCollection.randomPoints(geometry, 10);
 
 
 // Test export_hydro
