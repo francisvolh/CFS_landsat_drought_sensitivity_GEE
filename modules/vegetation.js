725ebbe5ecdd214be8fc8840224928817b96f112
robit.alec <robit.alec@gmail.com>
2022-08-01 20:13:13 +0000

    Updated modules/vegetation.js (added 1 line, deleted 1 line).

--- modules/vegetation.js
+++ modules/vegetation.js
@@ -8,7 +8,7 @@ var get_canopy_height = function() {
   var ch = ee.ImageCollection("projects/sat-io/open-datasets/carbon_stocks_ca/ch");
   
   return ch.toBands()
-           .rename(['ch_85perc_height_250m_b1', 
+           .select(['ch_85perc_height_250m_b1', 
                     'ch_95perc_height_250m_b1',
                     'ch_max_height_250m_b1'],
                    ['ch_85perc_height_250m', 
