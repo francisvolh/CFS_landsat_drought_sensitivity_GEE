5d757674c00e3c9d85f20bb8dd5f792653ba607b
robit.alec <robit.alec@gmail.com>
2022-08-01 20:10:58 +0000

    Updated modules/vegetation.js (added 3 lines, deleted 1 line).

--- modules/vegetation.js
+++ modules/vegetation.js
@@ -5,7 +5,9 @@ Alec L. Robitaille
 
 
 var get_canopy_height = function() {
-  return ee.ImageCollection("projects/sat-io/open-datasets/carbon_stocks_ca/ch");
+  var ch = ee.ImageCollection("projects/sat-io/open-datasets/carbon_stocks_ca/ch");
+  
+  return ch.toBands();
 };
 exports.get_canopy_height = get_canopy_height;
 
