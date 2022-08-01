d814e4baf7732a2ef6adac2fbfd17160f962ce34
robit.alec <robit.alec@gmail.com>
2022-08-01 20:48:53 +0000

    Updated tests/test_soil.js (added 19 lines, deleted 1 line).

---
+++ tests/test_soil.js
@@ -0,0 +1,19 @@
+/*
+Soil
+Alec L. Robitaille
+*/
+
+
+var get_canopy_height = function() {
+  var ch = ee.ImageCollection("projects/sat-io/open-datasets/carbon_stocks_ca/ch");
+
+  return ch.toBands()
+           .select(['ch_85perc_height_250m_b1',
+                    'ch_95perc_height_250m_b1',
+                    'ch_max_height_250m_b1'],
+                   ['ch_85perc_height_250m',
+                    'ch_95perc_height_250m',
+                    'ch_max_height_250m']);
+};
+exports.get_canopy_height = get_canopy_height;
+
