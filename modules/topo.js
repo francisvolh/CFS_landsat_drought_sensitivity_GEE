ef1126f84fa3a0224eb4397c9a41234f20774bdf
robit.alec <robit.alec@gmail.com>
2022-08-02 15:35:30 +0000

    Updated modules/topo.js (added 19 lines, deleted 7 lines).

--- modules/topo.js
+++ modules/topo.js
@@ -1,5 +1,17 @@
+/*
+Topo
+Alec L. Robitaille
+
+
+HAND
+Donchyts, Gennadii, Hessel Winsemius, Jaap Schellekens, Tyler Erickson, Hongkai Gao, Hubert Savenije, and Nick van de Giesen. "Global 30m Height Above the Nearest Drainage (HAND)",
+Geophysical Research Abstracts, Vol. 18, EGU2016-17445-3, 2016, EGU General Assembly (2016).
+
+*/
+
+
 // Get HAND
-var geometry = 
+var geometry =
     ee.Geometry.Polygon(
         [[[-168.54997439051382, 71.83257848283961],
           [-168.54997439051382, 38.21307697867719],
@@ -7,7 +19,7 @@ var geometry =
           [-79.25309939051384, 71.83257848283961]]], null, false);
 
 
-var get_hand = function(resolution, threshold) {
+var hand = function(resolution, threshold) {
   if (resolution == 30 & threshold == 100) {
     // Note: image collection vs image
     return ee.ImageCollection("users/gena/global-hand/hand-100").select(['b1'], ['hand_30_100'])
@@ -18,7 +30,7 @@ var get_hand = function(resolution, threshold) {
     return ee.Image("users/gena/GlobalHAND/90m-global/hand-1000").select(['b1'], ['hand_90_1000']);
   }
 };
-exports.get_hand = get_hand;
+exports.hand = hand;
 
 
 
@@ -26,9 +38,9 @@ exports.get_hand = get_hand;
 var get_col = function() {
   return ee.Image([
   // TODO: which?
-  get_hand(30, 100),
-  get_hand(30, 1000),
-  get_hand(90, 1000)
+  hand(30, 100),
+  hand(30, 1000),
+  hand(90, 1000)
   ]);
 };
 exports.get_col = get_col;
