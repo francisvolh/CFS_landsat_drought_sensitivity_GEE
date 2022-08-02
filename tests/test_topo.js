364e9db7ed308d3bdb9ee60bc86df39bc527f939
robit.alec <robit.alec@gmail.com>
2022-08-02 15:35:21 +0000

    Updated tests/test_topo.js (added 7 lines, deleted 7 lines).

--- tests/test_topo.js
+++ tests/test_topo.js
@@ -1,11 +1,11 @@
 /*
-Testing: modules/get_topo.js
+Testing: modules/topo.js
 Alec L. Robitaille
 */
 
 
 // Load modules
-var get_topo = require('users/robitalec/CFS:modules/get_topo.js');
+var topo = require('users/robitalec/CFS:modules/topo.js');
 
 
 // Palette
@@ -14,16 +14,16 @@ var p = palettes.crameri.lajolla[50];
 
 
 
-// Test get_hand
-// Usage: get_hand(resolution, threshold)
-var hand_30_100 = get_topo.get_hand(30, 100);
+// Test hand
+// Usage: hand(resolution, threshold)
+var hand_30_100 = topo.hand(30, 100);
 print(hand_30_100);
 Map.addLayer(hand_30_100, {min:0, max:500, palette:p}, 'hand 30 100');
 
-var hand_30_1000 = get_topo.get_hand(30, 1000);
+var hand_30_1000 = topo.hand(30, 1000);
 print(hand_30_1000);
 Map.addLayer(hand_30_1000, {min:0, max:500, palette:p}, 'hand 30 1000', false);
 
-var hand_90_1000 = get_topo.get_hand(90, 1000);
+var hand_90_1000 = topo.hand(90, 1000);
 print(hand_90_1000);
 Map.addLayer(hand_90_1000, {min:0, max:500, palette:p}, 'hand 90 1000', false);
