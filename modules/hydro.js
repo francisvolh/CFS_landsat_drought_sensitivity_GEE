6d9070d89fa8b432e8be648a5b25b91c72f4397c
robit.alec <robit.alec@gmail.com>
2022-08-02 14:16:27 +0000

    Updated modules/hydro.js (added 3 lines, deleted 3 lines).

--- modules/hydro.js
+++ modules/hydro.js
@@ -69,10 +69,10 @@ exports.distance_water_lc = distance_water_lc;
 
 
 
-// Distance to major lakes
+// Distance major lakes
 // Note: searchRadius (Float, default: 100000):
 //       Maximum distance in meters from each pixel to look for edges. Pixels will be masked unless there are edges within this distance.
-var get_dist_major_lakes = function(min_lake_area) {
+var distance_major_lakes = function(min_lake_area) {
   var lake_poly = ee.FeatureCollection("projects/sat-io/open-datasets/HydroLakes/lake_poly_v10")
     .filter(ee.Filter.eq('Continent', 'North America'))
     .filter(ee.Filter.eq('Country', 'Canada'))
@@ -80,7 +80,7 @@ var get_dist_major_lakes = function(min_lake_area) {
   
   return lake_poly.distance().rename('dist_lake_gt_' + min_lake_area + '_sq_km');
 };
-exports.get_dist_major_lakes = get_dist_major_lakes;
+exports.distance_major_lakes = distance_major_lakes;
 
 
 
