b998d64816b4811a9ab6b9525e5a6cc31a605c2b
robit.alec <robit.alec@gmail.com>
2022-08-02 14:22:59 +0000

    Updated tests/test_hydro.js (added 4 lines, deleted 4 lines).

--- tests/test_hydro.js
+++ tests/test_hydro.js
@@ -16,11 +16,11 @@ var p = palettes.crameri.lajolla[50];
 // Test proportion_permanent_snow
 // Usage: proportion_permanent_snow(focal_dist)
 var prop_snow_1000 = hydro.proportion_permanent_snow(1000);
-print(prop_snow_1000);
+print('Proportion snow 1000 m:', prop_snow_1000);
 Map.addLayer(prop_snow_1000, {min:0, max:1}, 'prop snow 1000', false);
 
 var prop_snow_5000 = hydro.proportion_permanent_snow(5000);
-print(prop_snow_5000);
+print('Proportion snow 500 m:', prop_snow_5000);
 Map.addLayer(prop_snow_5000, {min:0, max:1}, 'prop snow 5000');
 
 
@@ -42,11 +42,11 @@ Map.addLayer(dist_water_lc, {min: 0, max: 10e3}, 'distance water lc');
 // Test distance_major_lakes
 // Usage: distance_major_lakes(min_lake_area)
 var dist_major_lake_500 = hydro.distance_major_lakes(500);
-print(dist_major_lake_500);
+print('Distance major lakes > 500 sq km:', dist_major_lake_500);
 Map.addLayer(dist_major_lake_500, {min:0, max:10000}, 'dist lakes > 500 sq km');
 
 var dist_major_lake_1000 = hydro.distance_major_lakes(1000);
-print(dist_major_lake_1000);
+print('Distance major lakes > 1000 sq km:', dist_major_lake_1000);
 Map.addLayer(dist_major_lake_1000, {min:0, max:10000}, 'dist lakes > 1000 sq km', false);
 
 
