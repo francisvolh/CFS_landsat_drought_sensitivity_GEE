6170ddc5e3155edc0e3be93ab8bcd4bd8716dff1
robit.alec <robit.alec@gmail.com>
2022-08-05 15:30:09 +0000

    Updated tests/test_soil.js (added 3 lines, deleted 3 lines).

--- tests/test_soil.js
+++ tests/test_soil.js
@@ -9,8 +9,8 @@ var soil = require('users/robitalec/CFS:modules/soil.js');
 
 
 
-// Test get_soil_percent();
-// Usage: get_soil_percent();
-var soil_percent = soil.get_soil_percent();
+// Test soil_percent();
+// Usage: soil_percent();
+var soil_percent = soil.soil_percent();
 print(soil_percent);
 Map.addLayer(soil_percent, {min: 0, max: 100});
