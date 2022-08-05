11d13c8e3da51839b45dccdc4e4e7315ac112e05
robit.alec <robit.alec@gmail.com>
2022-08-05 15:34:54 +0000

    Updated modules/soil.js (added 6 lines, deleted 5 lines).

--- modules/soil.js
+++ modules/soil.js
@@ -8,13 +8,16 @@ https://samapriya.github.io/awesome-gee-community-datasets/projects/isric/
 https://www.soilgrids.org/
 https://data.isric.org/geonetwork/srv/eng/catalog.search#/metadata/178b79f1-7471-4dd4-b41e-aba8b18b1bfe
 
- 
+
 clay 	Proportion of clay particles (< 0.002 mm) in the fine earth fraction 	g/kg 	10 	g/100g (%) 	clay_mean
 sand 	Proportion of sand particles (> 0.05 mm) in the fine earth fraction 	g/kg 	10 	g/100g (%) 	sand_mean
 silt 	Proportion of silt particles (≥ 0.002 mm and ≤ 0.05 mm) in the fine earth fraction 	g/kg 	10 	g/100g (%) 	silt_mean
 
 
 */
+
+// TODO: Hugelius peat
+
 
 
 var soil_percent = function() {
@@ -32,14 +35,12 @@ var soil_percent = function() {
 exports.soil_percent = soil_percent;
 
 
+
 var soil_carbon = function() {
   var sc = ee.ImageCollection("projects/sat-io/open-datasets/carbon_stocks_ca/sc")
     .toBands()
     .select(['sc_250m_v20_b1'], ['sc_250m_v20']);
-  
+
   return sc;
 };
 exports.soil_carbon = soil_carbon;
-
-
-// TODO: Hugelius peat
