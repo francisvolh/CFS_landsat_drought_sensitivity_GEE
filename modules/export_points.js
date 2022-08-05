3a70970835b146789622f8a9d9852e39389c9138
robit.alec <robit.alec@gmail.com>
2022-08-05 15:39:58 +0000

    Updated modules/export_points.js (added 1 line, deleted 2 lines).

--- modules/export_points.js
+++ modules/export_points.js
@@ -60,8 +60,7 @@ exports.export_vegetation = export_vegetation;
 
 
 var export_soil = function(points, drive_name, drive_folder) {
-  var veg = soil.soil_percent();
-  // TODO: add peat, soil carbon
+  var col = soil.sampling_collection();
   
 	var sampled = veg.reduceRegions(points, ee.Reducer.mean(), 30);
 	var today = new Date().toJSON().slice(0, 10);
