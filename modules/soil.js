8985196692fa1aca56915d01bc8e3fb14bfc3939
robit.alec <robit.alec@gmail.com>
2022-08-05 15:35:41 +0000

    Updated modules/soil.js (added 5 lines, deleted 1 line).

--- modules/soil.js
+++ modules/soil.js
@@ -8,11 +8,15 @@ https://samapriya.github.io/awesome-gee-community-datasets/projects/isric/
 https://www.soilgrids.org/
 https://data.isric.org/geonetwork/srv/eng/catalog.search#/metadata/178b79f1-7471-4dd4-b41e-aba8b18b1bfe
 
-
 clay 	Proportion of clay particles (< 0.002 mm) in the fine earth fraction 	g/kg 	10 	g/100g (%) 	clay_mean
 sand 	Proportion of sand particles (> 0.05 mm) in the fine earth fraction 	g/kg 	10 	g/100g (%) 	sand_mean
 silt 	Proportion of silt particles (≥ 0.002 mm and ≤ 0.05 mm) in the fine earth fraction 	g/kg 	10 	g/100g (%) 	silt_mean
 
+
+
+Sothe, C., Gonsamo, A., Arabian, J., Kurz, W. A., Finkelstein, S. A., & Snider, J. (2022). 
+Large soil carbon storage in terrestrial ecosystems of Canada. 
+Global Biogeochemical Cycles, 36, e2021GB007213. https://doi.org/10.1029/2021GB007213 
 
 */
 
