0b10c95d9b951dd4ad6a9af44457e1a7526afeea
robit.alec <robit.alec@gmail.com>
2022-08-05 15:49:51 +0000

    Updated modules/soil.js (added 2 lines, deleted 1 line).

--- modules/soil.js
+++ modules/soil.js
@@ -53,7 +53,8 @@ exports.soil_carbon = soil_carbon;
 
 
 var peat_depth = function() {
-  var peat_depth = ee.Image("");
+  var peat_depth = ee.Image('users/robitalec/CFS/Hugelius_mean_potential_peat_depth_cm')
+    .rename(['mean_potential_peat_depth_cm']);
 
   return peat_depth;
 };
