04624e3b37c996293dbe3a84afa2ceee28da7196
robit.alec <robit.alec@gmail.com>
2022-08-05 19:46:49 +0000

    Updated tests/test_utils.js (added 5 lines).

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -8,6 +8,8 @@ var utils = require('users/robitalec/CFS:modules/utils.js');
 
 // Data
 var img = ee.Image.constant(1).set('system:time_start', ee.Date.fromYMD(2020, 1, 1).millis());
+var daymet = ee.ImageCollection("NASA/ORNL/DAYMET_V4")
+  .filter(ee.Filter.calendarRange('2000-01-01', '2002-01-01'));
 
 
 // Test set_year
@@ -21,3 +23,6 @@ print('Set year', img);
 // Usage: utils.add_year_band(image)
 img = utils.add_year_band(img);
 Map.addLayer(img.select('year'));
+
+
+
