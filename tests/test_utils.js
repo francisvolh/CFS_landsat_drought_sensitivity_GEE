da12fa5df5fa6468d97dfb7d853a97830c885f7d
robit.alec <robit.alec@gmail.com>
2022-08-05 19:55:11 +0000

    Updated tests/test_utils.js (added 2 lines, deleted 2 lines).

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -37,8 +37,8 @@ print('Aggregate month year', agg_month_year);
 
 
 // Test aggregrate_year
-// Usage: aggregate_year(images, year_list, reducer);
-var agg_year = utils.aggregate_year(daymet, year_list, reducer);
+// Usage: aggregrate_year(images, year_list, reducer);
+var agg_year = utils.aggregrate_year(daymet, year_list, reducer);
 print('Aggregate year', agg_year);
 
 
