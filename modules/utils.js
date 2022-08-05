02d17e2e15e85cf1d1172c3ffde54f31a252b301
robit.alec <robit.alec@gmail.com>
2022-08-05 19:52:21 +0000

    Updated modules/utils.js (added 2 lines, deleted 1 line).

--- modules/utils.js
+++ modules/utils.js
@@ -73,8 +73,9 @@ exports.aggregrate_year = aggregrate_year;
 
 
 // Aggregate week
-var week_list = ee.List.sequence(1, 53);
 var aggregrate_week = function(images, year_list, reducer) {
+  var week_list = ee.List.sequence(1, 53);
+
   return ee.ImageCollection.fromImages(
     year_list.map(function(yr) {
       week_list.map(function(wk) {
