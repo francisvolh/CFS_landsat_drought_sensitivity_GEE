ff6fba83f1e1653ece49864dbff1d732738cf523
robit.alec <robit.alec@gmail.com>
2022-08-05 16:16:48 +0000

    Updated tests/test_vegetation.js (added 3 lines, deleted 3 lines).

--- tests/test_vegetation.js
+++ tests/test_vegetation.js
@@ -9,8 +9,8 @@ var vegetation = require('users/robitalec/CFS:modules/vegetation.js');
 
 
 
-// Test get_canopy_height();
-// Usage: get_canopy_height();
-var canopy_height = vegetation.get_canopy_height();
+// Test canopy_height();
+// Usage: canopy_height();
+var canopy_height = vegetation.canopy_height();
 print(canopy_height);
 Map.addLayer(canopy_height);
