45304060c51ece8bfbd8009618c0ed4b5d25278e
robit.alec <robit.alec@gmail.com>
2022-08-05 18:09:17 +0000

    Updated modules/export_img.js (added 2 lines, deleted 1 line).

--- modules/export_img.js
+++ modules/export_img.js
@@ -4,9 +4,10 @@ Alec L. Robitaille
 */
 
 
-
+// Modules
 var main = require('users/robitalec/CFS:modules/main.js');
 var hydro = require('users/robitalec/CFS:modules/hydro.js');
+
 
 
 // Export img asset greenest
