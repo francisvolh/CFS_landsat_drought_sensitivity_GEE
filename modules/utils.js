63c92b40a53d33fd2d624f43a37dd782fd3c6076
robit.alec <robit.alec@gmail.com>
2022-08-05 20:01:38 +0000

    Updated modules/utils.js (added 1 line, deleted 1 line).

--- modules/utils.js
+++ modules/utils.js
@@ -99,6 +99,6 @@ var aggregrate_week = function(images, year_list, reducer) {
                             .set('year', yr)
                             .set('system:time_start', mean_date);
     });
-  }));
+  }).flatten());
 };
 exports.aggregrate_week = aggregrate_week;
