bff8b78bed46f8b73e44223b12804ee1e7c0a4b3
robit.alec <robit.alec@gmail.com>
2022-08-05 19:07:39 +0000

    Updated tests/test_climate.js (added 5 lines, deleted 5 lines).

--- tests/test_climate.js
+++ tests/test_climate.js
@@ -1,20 +1,20 @@
 /*
-Testing: modules/get_climate.js
+Testing: modules/climate.js
 Alec L. Robitaille
 */
 
 
 // Load modules
-var climate = require('users/robitalec/CFS:modules/get_climate.js');
+var climate = require('users/robitalec/CFS:modules/climate.js');
 
 // Set variables
 var years = ee.List.sequence(1985, 2020);
 
 
 
-// Test get_long_term_climate
-// Usage: get_long_term_climate(year_list)
-var long_climate = climate.get_long_term_climate(years);
+// Test long_term_climate
+// Usage: long_term_climate(year_list)
+var long_climate = climate.long_term_climate(years);
 print(long_climate);
 Map.addLayer(long_climate.select('tmean_mean'), {min:-20, max:20}, 'mean annual tmean');
 Map.addLayer(long_climate.select('prcp_mean'), {min:100, max:4000}, 'mean annual sum prcp');
