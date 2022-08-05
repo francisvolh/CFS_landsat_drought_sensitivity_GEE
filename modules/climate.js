e93cd27fecb58ee6de2792b2c388ca63d67b68b9
robit.alec <robit.alec@gmail.com>
2022-08-05 20:13:21 +0000

    Updated modules/climate.js (added 1 line, deleted 6 lines).

--- modules/climate.js
+++ modules/climate.js
@@ -48,12 +48,7 @@ var weekly_daymet = function(daymet, year_list) {
 exports.weekly_daymet = weekly_daymet;
 
 
-// TODO: monthly_daymet;
-
-// // Reducer
-// var reducer = ee.Reducer.mean().combine(ee.Reducer.sum(), null, true);
-
-
+// TODO: monthly_daymet
 
 
 
