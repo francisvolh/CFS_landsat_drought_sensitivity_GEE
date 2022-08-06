d4d6bee8e564a1a49dafe96da4d27b6dcfd086d8
robit.alec <robit.alec@gmail.com>
2022-08-06 14:48:20 +0000

    Updated tests/test_climate.js (added 3 lines, deleted 3 lines).

--- tests/test_climate.js
+++ tests/test_climate.js
@@ -8,18 +8,18 @@ Alec L. Robitaille
 var climate = require('users/robitalec/CFS:modules/climate.js');
 
 // Set variables
-var year_list = ee.List.sequence(2002, 2004);
+var year_list = ee.List.sequence(2002, 2002);
 
 
 
 // Test weekly_daymet
 // Usage: weekly_daymet(year_list);
 var week_daymet = climate.weekly_daymet(year_list);
-print('Weekly daymet', week_daymet);
+print('Weekly daymet (limit 5)', week_daymet.limit(5));
 
 
 
 // Test annual_mean_temp
 // Usage: annual_mean_temp(year_list);
 var ann_mean_t = climate.annual_mean_temp(year_list);
-print('Annual mean temperature', ann_mean_t);
+// print('Annual mean temperature', ann_mean_t);
