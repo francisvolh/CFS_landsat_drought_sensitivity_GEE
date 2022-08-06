d54ba12ce019201775f270e4e8387d7644ca99d6
robit.alec <robit.alec@gmail.com>
2022-08-06 15:10:25 +0000

    Updated tests/test_climate.js (added 5 lines, deleted 6 lines).

--- tests/test_climate.js
+++ tests/test_climate.js
@@ -9,20 +9,19 @@ var climate = require('users/robitalec/CFS:modules/climate.js');
 
 // Set variables
 var year_list = ee.List.sequence(2002, 2002);
-
+var week_list = ee.List.sequence(23, 25);
 
 
 // Test daymet
 // Usage: daymet();
-var daymet = climate.daymet()
-  .filter(ee.Filter.inList('year', year_list));
-print('Daymet (filter in year list)', daymet);
+var daymet = climate.daymet();
+print('Daymet (limit 5)', daymet.limit(5));
 
 
 
 // Test weekly_daymet
-// Usage: weekly_daymet(daymet, year_list);
-var week_daymet = climate.weekly_daymet(daymet, year_list);
+// Usage: weekly_daymet(daymet, year_list, week_list);
+var week_daymet = climate.weekly_daymet(daymet, year_list, week_list);
 print('Weekly daymet (limit 2)', week_daymet.limit(2));
 
 
