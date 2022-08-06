6c90fc6b834661a5e2dd4cb7da0894734bc718da
robit.alec <robit.alec@gmail.com>
2022-08-06 15:04:29 +0000

    Updated modules/climate.js (added 2 lines, deleted 2 lines).

--- modules/climate.js
+++ modules/climate.js
@@ -46,8 +46,8 @@ exports.weekly_daymet = weekly_daymet;
 
 
 
-var annual_mean_temp = function(year_list) {
-  var weekly = weekly_daymet(year_list);
+var annual_mean_temp = function(daymet_col, year_list) {
+  var weekly = weekly_daymet(daymet_col, year_list);
 
   weekly = weekly.select('tmax_max')
                  .add(weekly.select('tmin_min')
