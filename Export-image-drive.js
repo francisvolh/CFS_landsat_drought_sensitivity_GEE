79a2a0e68dce1ea28f72b4e8054be2f28e752a3e
robit.alec <robit.alec@gmail.com>
2022-08-12 22:32:58 +0000

    Updated Export-image-drive.js (added 4 lines, deleted 3 lines).

--- Export-image-drive.js
+++ Export-image-drive.js
@@ -10,20 +10,21 @@ var vars = require('users/robitalec/CFS:modules/variables.js');
 
 
 // Set variables
-var region = vars.dawson;
+var region = vars.ak_to_mb;
 var ante_list = vars.ante_list;
 var min_year =  vars.min_year;
 var max_year = vars.max_year;
 var min_mm_dd = vars.min_mm_dd;
 var max_mm_dd = vars.max_mm_dd;
 
+var scale = 1000;
 
 
 // File name
-var export_name = 'sens_p15_p85';
+var export_name = 'Abs_sens_p15_p85_AK_to_MB';
 
 // Export image to drive
-export_img.export_img_drive_greenest(export_name, 'Exports', 30, region, min_year, max_year, min_mm_dd, max_mm_dd, ante_list);
+export_img.export_img_drive_greenest(export_name, 'Exports', scale, region, min_year, max_year, min_mm_dd, max_mm_dd, ante_list);
 
 
 Map.addLayer(region);
