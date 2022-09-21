dbf33322fa312d35bf1bd6fb457851f1e321c0dc
robit.alec <robit.alec@gmail.com>
2022-09-21 19:30:36 +0000

    Updated modules/utils.js (added 1 line, deleted 1 line).

--- modules/utils.js
+++ modules/utils.js
@@ -7,7 +7,7 @@ Alec L. Robitaille
 
 // Set year
 var set_year = function(img) {
-	return img.set('year', img.date().get('year'));
+	return img.set('year', img.date().get('year').int());
 };
 exports.set_year = set_year;
 
