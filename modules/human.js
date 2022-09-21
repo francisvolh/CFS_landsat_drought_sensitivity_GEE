7e6daa2593af8d6bd1a25ee3062dc39629c2d5f3
robit.alec <robit.alec@gmail.com>
2022-09-21 16:45:13 +0000

    Updated modules/human.js (added 15 lines, deleted 1 line).

---
+++ modules/human.js
@@ -0,0 +1,15 @@
+/*
+Human footprint mask
+Alec L. Robitaille
+
+Marconcini, M., Metz-Marconcini, A., Üreyen, S., Palacios-Lopez, D., Hanke, W.,
+Bachofer, F., Zeidler, J., Esch, T., Gorelick, N., Kakarla, A., Paganini, M.,
+Strano, E. (2020). Outlining where humans live, the World Settlement Footprint
+2015. Scientific Data, 7(1), 1-14. doi:10.1038/s41597-020-00580-5
+
+*/
+
+
+
+var wsa = ee.Image("DLR/WSF/WSF2015/v1");
+exports.world_settlement_area = wsa;
