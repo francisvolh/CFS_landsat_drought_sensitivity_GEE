f29e4c7934bb3a7f1813b800249d93389ba8ec98
robit.alec <robit.alec@gmail.com>
2022-09-21 14:56:05 +0000

    Updated tests/test_agriculture.js (added 11 lines, deleted 3 lines).

--- tests/test_agriculture.js
+++ tests/test_agriculture.js
@@ -18,10 +18,18 @@ var lc_2015 = land_cover.hermosilla_1984_2019.filter(ee.Filter.eq('year', 2015))
 
 
 // Test aafc_aci
-// Usage: agriculture.aafc_aci
+// Usage: agriculture.aafc_aci;
 var aafc_aci = agriculture.aafc_aci;
 print('AAFC ACI', aafc_aci);
-Map.addLayer(aafc_aci, null, 'AAFC ACI')
+Map.addLayer(aafc_aci, null, 'AAFC ACI');
+
+
+
+// Test masked_aci
+// Usage: agriculture.masked_aci;
+var masked_aci = agriculture.masked_aci;
+print('Masked ACI', masked_aci);
+Map.addLayer(masked_aci, null, 'Masked ACI')
 
 
 
@@ -29,4 +37,4 @@ Map.addLayer(aafc_aci, null, 'AAFC ACI')
 // Usage: agriculture.get_agriculture_mask
 var ag_mask = agriculture.get_agriculture_mask;
 print('Agriculture mask', ag_mask);
-Map.addLayer(ag_mask, null, 'Agriculture');
+Map.addLayer(ag_mask, null, 'Agriculture mask');
