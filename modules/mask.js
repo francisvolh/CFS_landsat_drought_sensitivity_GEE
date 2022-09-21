a2fd3ab7654ecabb687f4b7aba26aa04b7f1ccde
robit.alec <robit.alec@gmail.com>
2022-09-21 17:01:22 +0000

    Updated modules/mask.js (added 25 lines, deleted 1 line).

---
+++ modules/mask.js
@@ -0,0 +1,25 @@
+/*
+Mask wrapper module
+Alec L. Robitaille
+
+*/
+
+
+
+// Modules
+var human = require('users/robitalec/CFS:modules/human.js');
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+var agriculture = require('users/robitalec/CFS:modules/agriculture.js');
+var fire = require('users/robitalec/CFS:modules/fire.js');
+
+
+
+// Get masks
+var not_human_mask = human.world_settlement_area.not();
+var not_agriculture_mask = agriculture.get_agriculture_mask.not();
+var min_land_cover_mask = land_cover.get_lc_count_mask();
+
+var atemporal_mask = not_human_mask.and(not_agriculture_mask).and(min_land_cover_mask);
+exports.atemporal_mask = atemporal_mask;
+// Function to update mask that will be mapped
+
