c9ccf9e23a5ead6dcddc27f9f9bf288ff8e6fb40
robit.alec <robit.alec@gmail.com>
2022-09-21 17:01:26 +0000

    Updated tests/test_mask.js (added 20 lines, deleted 1 line).

---
+++ tests/test_mask.js
@@ -0,0 +1,20 @@
+/*
+Testing: modules/mask.js
+Alec L. Robitaille
+*/
+
+
+
+
+// Load modules
+var mask = require('users/robitalec/CFS:modules/mask.js');
+
+
+
+// Variables
+var img = ee.Image.constant(1);
+
+
+
+// Test
+Map.addLayer(img.updateMask(mask.atemporal_mask)
