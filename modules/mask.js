e54aca53c277f6e58a48e2f9b0fe232be62fc92e
robit.alec <robit.alec@gmail.com>
2022-09-21 17:12:38 +0000

    Updated modules/mask.js (added 3 lines, deleted 1 line).

--- modules/mask.js
+++ modules/mask.js
@@ -19,7 +19,9 @@ var human_mask = human.world_settlement_area;
 var agriculture_mask = agriculture.get_agriculture_mask;
 var min_land_cover_mask = land_cover.get_lc_count_mask().not();
 
-var atemporal_mask = human_mask.or(agriculture_mask).or(min_land_cover_mask);
+var atemporal_mask = min_land_cover_mask
+	.updateMask(human_mask.not())
+	.updateMask(agriculture_mask.not());
 exports.atemporal_mask = atemporal_mask;
 // Function to update mask that will be mapped
 
