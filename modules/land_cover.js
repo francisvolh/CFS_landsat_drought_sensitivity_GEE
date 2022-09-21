85de8a1061a201e4a72ccb4e6feb1ac613461cb9
robit.alec <robit.alec@gmail.com>
2022-09-21 19:01:31 +0000

    Updated modules/land_cover.js (added 1 line, deleted 1 line).

--- modules/land_cover.js
+++ modules/land_cover.js
@@ -65,7 +65,7 @@ var get_lc_count_mask = function() {
     .map(utils.set_year)
     .map(mask_classes);
   var lc_masked_count = lc_masked.reduce(ee.Reducer.count()).eq(lc_masked.size());
-  return hermosille_1984_2019.mask().updateMask(lc_masked_count);
+  return hermosilla_1984_2019.mask().updateMask(lc_masked_count);
 };
 exports.get_lc_count_mask = get_lc_count_mask;
 
