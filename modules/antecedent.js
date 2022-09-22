78b36594650c5864bcc44c99857718d5fcfc73e3
robit.alec <robit.alec@gmail.com>
2022-09-22 14:18:52 +0000

    Updated modules/antecedent.js (added 31 lines, deleted 4 lines).

--- modules/antecedent.js
+++ modules/antecedent.js
@@ -15,16 +15,20 @@ var antecedent_means = function(images, band, year_list) {
     var band3mo = band + '_ante3mo';
     var band12mo = band + '_ante12mo';
     var band3yr = band + '_ante3yr';
+    var band1lag = band + '_ante1lag';
+    var band2lag = band + '_ante2lag';
+    var band3lag = band + '_ante3lag';
+
 
     return ee.Image([
-      // Antecedent: 3 (months 3-6)
+      // Antecedent: 3 months 3-6
       // Filter to year, and within antecedent period
       // Reduce with mean reducer
       images.filter(ee.Filter.date(today.advance(-3, 'month'), today))
             .select([band], [band3mo])
             .reduce(ee.Reducer.mean()),
 
-      // Antecedent: 12 (months 6-6 year previous)
+      // Antecedent: 12 months 6-6
       // Filter to year, and within antecedent period
       // Reduce with mean reducer
       images.filter(ee.Filter.date(today.advance(-1, 'year'), today))
@@ -32,12 +36,35 @@ var antecedent_means = function(images, band, year_list) {
             .reduce(ee.Reducer.mean()),
 
 
-      // Antecedent: 3 (years 6-6 year previous)
+      // Antecedent: 3 years 6-6
       // Filter to year, and within antecedent period
       // Reduce with mean reducer
       images.filter(ee.Filter.date(today.advance(-3, 'year'), today))
             .select([band], [band3yr])
-            .reduce(ee.Reducer.mean())
+            .reduce(ee.Reducer.mean()),
+
+
+      // Antecedent: 12 month, 1 year lag
+      // Filter to year, and within antecedent period
+      // Reduce with mean reducer
+      images.filter(ee.Filter.date(today.advance(-2, 'year'), today.advance(-1, 'year')))
+            .select([band], [band1lag])
+            .reduce(ee.Reducer.mean()),
+
+      // Antecedent: 12 month, 2 year lag
+      // Filter to year, and within antecedent period
+      // Reduce with mean reducer
+      images.filter(ee.Filter.date(today.advance(-3, 'year'), today.advance(-2, 'year')))
+            .select([band], [band2lag])
+            .reduce(ee.Reducer.mean()),
+
+      // Antecedent: 12 month, 3 year lag
+      // Filter to year, and within antecedent period
+      // Reduce with mean reducer
+      images.filter(ee.Filter.date(today.advance(-4, 'year'), today.advance(-3, 'year')))
+            .select([band], [band3lag])
+            .reduce(ee.Reducer.mean()),
+
       ]).set({'year': yr});
   }));
 };
