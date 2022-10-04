8a949f956c7d6462551eb266a495aa6d1b11f08c
robit.alec <robit.alec@gmail.com>
2022-10-04 14:53:56 +0000

    Updated tests/test_main.js (added 153 lines, deleted 33 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -14,40 +14,160 @@ var geometry =
           [-138.7155528504698, 64.75408317982838]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
-Testing: modules/main.js
+Main
 Alec L. Robitaille
 */
 
 // Load modules
-var main = require('users/robitalec/CFS:modules/main.js');
-var palettes = require('users/gena/packages:palettes');
-
-// Palettes
-var p = palettes.crameri.vik[10];
-var ndvi_viz = {min:0.3, max:0.85};
-var rel_viz = {min:-20, max:20, palette: p};
-var abs_viz = {min:-0.2, max:0.2, palette: p};
-var cmi_viz = {min:-15, max:15, palette: p};
-
-
-
-// Test main - index + antecedent means
-// Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
-var main_index_and_antecedent = main.main_greenest('vegetation index and antecedent means', geometry);
-print('veg index + antecedent means'); print(main_index_and_antecedent);
-Map.setOptions('SATELLITE');
-Map.addLayer(main_index_and_antecedent.select('CMI_ante3mo_mean').first(), cmi_viz, 'CMI 3 month antecedent mean', false);
-Map.addLayer(main_index_and_antecedent.select('NDVI').first(), ndvi_viz, 'NDVI', false);
-
-// Test main - relative
-// Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
-var main_relative = main.main_greenest('relative sensitivity', geometry);
-print('relative sensitivity'); print(main_relative);
-Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3mo_p15_p85'), rel_viz, 'relative drought sensitivity NDVI p15-85 3 month antecedent', false);
+var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
+var mask = require('users/robitalec/CFS:modules/mask.js');
+var cmi = require('users/robitalec/CFS:modules/cmi.js');
+var daymet = require('users/robitalec/CFS:modules/daymet.js');
+var percentile = require('users/robitalec/CFS:modules/percentile.js');
+var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
+var split = require('users/robitalec/CFS:modules/split_drought.js');
+var sensitivity = require('users/robitalec/CFS:modules/sensitivity.js');
+var vars = require('users/robitalec/CFS:modules/variables.js');
+var nobs = require('users/robitalec/CFS:modules/nobs.js');
 
 
-// Test main - absolute
-// Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
-var main_absolute = main.main_greenest('absolute sensitivity', geometry);
-print('absolute sensitivity'); print(main_absolute);
-Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3mo_p15_p85'), abs_viz, 'absolute drought sensitivity NDVI p15-85  3 month antecedent');
+var main_greenest = function(output, region) {
+  // Variables
+  var index_list = vars.index_list;
+	var antecedent_list = vars.ante_list;
+	var min_year_daymet =  vars.min_year_daymet;
+	var min_year_landsat =  vars.min_year_landsat;
+	var max_year = vars.max_year;
+	var min_mm_dd = vars.min_mm_dd;
+	var max_mm_dd = vars.max_mm_dd;
+	var percentile_low = vars.percentile_low;
+	var percentile_high = vars.percentile_high;
+	var months = vars.months;
+  var years = ee.List.sequence(min_year_daymet, max_year);
+  var percentile_list = [percentile_low, percentile_high];
+
+  // Collections
+  var monthly_daymet = daymet.monthly_daymet(years, months);
+  var indices_col = get_landsat.get_indices_greenest(min_year_landsat, max_year, min_mm_dd, max_mm_dd, region);
+
+  // Apply mask
+  indices_col = mask.apply_mask(indices_col);
+
+
+  // CMI
+  var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
+
+  // Drought/baseline
+  var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
+  var percentile_images = percentile.get_percentile(ante_means, percentile_list);
+  var percentile_masks = percentile.get_percentile_masks(ante_means, percentile_images);
+  var split_drought_wi = split.split_drought_wi(indices_col, percentile_masks, antecedent_list, index_list);
+
+  if (output == 'relative sensitivity') {
+    var sens_rel = sensitivity.sensitivity_relative_cap(split_drought_wi, antecedent_list, index_list);
+
+    var counts_rel = nobs.count_nobs(split_drought_wi, sens_rel);
+    return nobs.mask_nobs('Rel', counts_rel, antecedent_list, index_list);
+  } else if (output == 'absolute sensitivity') {
+    var sens_abs = sensitivity.sensitivity_absolute_cap(split_drought_wi, antecedent_list, index_list);
+
+    var counts_abs = nobs.count_nobs(split_drought_wi, sens_abs);
+    return nobs.mask_nobs('Abs', counts_abs, antecedent_list, index_list);
+  } else if (output == 'vegetation index and antecedent means') {
+    var join = ee.Join.inner();
+    var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
+    joined = ee.ImageCollection(joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))}));
+    return joined;
+  }
+
+};
+exports.main_greenest = main_greenest;
+
+
+
+
+
+// ARCHIVE --------------------------------------------------------------------
+var zzz_main = function(output, region,
+                    min_year, max_year, min_mm_dd, max_mm_dd,
+                    index_list, percentile_list, antecedent_list) {
+  // Variables
+  var years = ee.List.sequence(min_year, max_year);
+  var months = ee.List.sequence(1, 12);
+
+  // Collections
+  var monthly_daymet = daymet.monthly_daymet(years, months);
+  var indices_col = get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region.geometry(), index_list);
+
+  // Mask land cover and fires
+  indices_col = indices_col.map(land_cover.mask_land_cover_and_fire);
+
+  // CMI
+  var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
+
+  // Define drought
+  var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
+  var percentile_images = percentile.get_percentile(ante_means, percentile_list);
+  var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
+
+  // Split vegetation index into baseline/drought
+  var split = split_drought.split_drought(indices_col, lt_percent, antecedent_list, percentile_list, index_list);
+
+  if (output == 'relative sensitivity') {
+    return sensitivity.sensitivity_relative(split, antecedent_list, percentile_list, index_list);
+  } else if (output == 'absolute sensitivity') {
+    return sensitivity.sensitivity_absolute(split, antecedent_list, percentile_list, index_list);
+  } else if (output == 'vegetation index and antecedent means') {
+    var join = ee.Join.inner();
+    var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
+    joined = ee.ImageCollection(joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))}));
+
+    return joined;
+  }
+
+};
+exports.zzz_main = zzz_main;
+
+
+
+var zzz_main_cap = function(output, region,
+                    min_year, max_year, min_mm_dd, max_mm_dd,
+                    index_list, percentile_low, percentile_high, antecedent_list) {
+  // Variables
+  var years = ee.List.sequence(min_year, max_year);
+  var months = ee.List.sequence(1, 12);
+  var percentile_list = [percentile_low, percentile_high];
+
+  // Collections
+  var monthly_daymet = daymet.monthly_daymet(years, months);
+  var indices_col = get_landsat.get_indices(min_year, max_year, min_mm_dd, max_mm_dd, region.geometry(), index_list);
+
+  // Mask land cover and fires
+  indices_col = indices_col.map(land_cover.mask_land_cover_and_fire);
+
+  // CMI
+  var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
+
+  // Define drought
+  var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
+  var percentile_images = percentile.get_percentile(ante_means, percentile_list);
+  var lt_percent = percentile.lt_percentile(ante_means, percentile_images);
+
+  // Split vegetation index into baseline/drought
+  var split = split_drought.split_drought_cap(indices_col, lt_percent, antecedent_list, percentile_low, percentile_high, index_list);
+
+  percentile_list = [percentile_low];
+  if (output == 'relative sensitivity') {
+    return sensitivity.sensitivity_relative(split, antecedent_list, percentile_list, index_list);
+  } else if (output == 'absolute sensitivity') {
+    return sensitivity.sensitivity_absolute(split, antecedent_list, percentile_list, index_list);
+  } else if (output == 'vegetation index and antecedent means') {
+    var join = ee.Join.inner();
+    var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
+    joined = ee.ImageCollection(joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))}));
+    return joined;
+  }
+
+};
+exports.zzz_main_cap = zzz_main_cap;
+
