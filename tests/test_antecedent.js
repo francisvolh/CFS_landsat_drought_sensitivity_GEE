80773e187f1ad91677e12749f669ffb4f18e8bca
robit.alec <robit.alec@gmail.com>
2022-10-05 15:36:25 +0000

    Updated tests/test_antecedent.js (added 1 line, deleted 1 line).

--- tests/test_antecedent.js
+++ tests/test_antecedent.js
@@ -28,5 +28,5 @@ var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
 // Usage: antecedent_mean(images, band, year_list)
 var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
 print(ante_means);
-Map.addLayer(ante_means.select('CMI_ante3yr_mean'), cmi_viz, 'CMI ante 3 year');
+Map.addLayer(ante_means.select('CMI_ante3yr_mvars.ean'), cmi_viz, 'CMI ante 3 year');
 Map.addLayer(ante_means.filter(ee.Filter.eq('year', 2015)).select('CMI_ante2lag_mean'), cmi_viz, 'CMI ante 2 lag');
