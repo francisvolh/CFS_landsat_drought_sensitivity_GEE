1772357e86ef2f3745c8b50f566207d1f74cc84a
robit.alec <robit.alec@gmail.com>
2022-10-05 14:56:45 +0000

    Updated tests/test_nobs.js (added 25 lines, deleted 26 lines).

--- tests/test_nobs.js
+++ tests/test_nobs.js
@@ -25,42 +25,42 @@ var percentile = require('users/robitalec/CFS:modules/percentile.js');
 var antecedent = require('users/robitalec/CFS:modules/antecedent.js');
 var cmi = require('users/robitalec/CFS:modules/cmi.js');
 var palettes = require('users/gena/packages:palettes');
-var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
+var daymet = require('users/robitalec/CFS:modules/daymet.js');
+var mask = require('users/robitalec/CFS:modules/mask.js');
 var get_landsat = require('users/robitalec/CFS:modules/get_landsat.js');
-var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
-var fire = require('users/robitalec/CFS:modules/fire.js');
-var nobs = require('users/robitalec/CFS:modules/nobs.js');
 var vars = require('users/robitalec/CFS:modules/variables.js');
 
+// Variables
+var index_list = vars.index_list;
+var antecedent_list = vars.ante_list;
+var min_year_daymet =  vars.min_year_daymet;
+var min_year_landsat =  vars.min_year_landsat;
+var max_year = vars.max_year;
+var min_mm_dd = vars.min_mm_dd;
+var max_mm_dd = vars.max_mm_dd;
+var percentile_low = vars.percentile_low;
+var percentile_high = vars.percentile_high;
+var months = vars.months;
+var years = ee.List.sequence(min_year_daymet, max_year);
+var percentile_list = [percentile_low, percentile_high];
+var region = ee.Geometry.Polygon([[[-125.87, 56.86], [-125.87, 54.98], [-121.87, 54.98], [-121.87, 56.86]]]);
 
-// Set variables
-var min_year = 1985; var max_year = 2021;
-var years = ee.List.sequence(min_year, max_year);
-var months = ee.List.sequence(1, 12);
-var min_mm_dd = '07-01';
-var max_mm_dd = '07-31';
-var percentile_list = [15, 85];
-var index_list = ['NDVI', 'NBR'];
-var antecedent_list = ['3mo', '12mo', '5yr'];
-
+// Palettes
 var p = palettes.crameri.vik[10];
 var cmi_viz = {min:-30, max:30, palette: p};
 var rel_viz = {min:-20, max:20, palette: p};
 var abs_viz = {min:-0.3, max:0.3, palette: p};
 
-// var geometry = ee.Geometry.Polygon([[[-125.87, 56.86], [-125.87, 54.98], [-121.87, 54.98], [-121.87, 56.86]]]);
 
 
 
 // Processing
-var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
-var indices_col = get_landsat.get_indices_greenest(min_year, max_year, min_mm_dd, max_mm_dd, geometry);
-var lc_mask = land_cover.get_lc_count_mask();
-indices_col = indices_col.map(function(img) {
-  return fire.mask_five_year_fires(img.updateMask(lc_mask));
-});
+var monthly_daymet = daymet.monthly_daymet(years, months);
+var indices_col = get_landsat.get_indices_greenest(min_year_landsat, max_year, min_mm_dd, max_mm_dd, region);
+indices_col = mask.apply_mask(indices_col);
 var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
 var ante_means = antecedent.antecedent_means(cmi_daymet, 'CMI', years);
+ante_means = ante_means.filter(ee.Filter.gte('year', min_year_landsat));
 var percentile_images = percentile.get_percentile(ante_means, percentile_list);
 var percentile_masks = percentile.get_percentile_masks(ante_means, percentile_images);
 var split_drought_wi = split.split_drought_wi(indices_col, percentile_masks, antecedent_list, index_list);
@@ -88,4 +88,3 @@ Map.addLayer(counts.select('NDVI_ante3mo_lte_p15_drought_count'), {min: 0, max:6
 Map.addLayer(counts.select('NDVI_ante3mo_lte_p15_drought_count').gte(vars.min_drought_nobs), null, 'drought count gte ' + vars.min_drought_nobs);
 
 Map.addLayer(mask_counts.select('Abs_sens_NDVI_ante3mo_p15_p85'), {min:-0.3, max:0.3, palette: palettes.crameri.vik[10]}, 'sensitivity');
-
