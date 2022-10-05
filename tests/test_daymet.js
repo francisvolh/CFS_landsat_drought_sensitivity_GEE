7bf03c821a98ad4fe248f5fb80698ca33ab87081
robit.alec <robit.alec@gmail.com>
2022-10-05 15:42:52 +0000

    Updated tests/test_daymet.js (added 6 lines, deleted 6 lines).

--- tests/test_daymet.js
+++ tests/test_daymet.js
@@ -1,11 +1,11 @@
 /*
-Testing: modules/daymet.js
+Testing: modules/get_daymet.js
 Alec L. Robitaille
 */
 
 
 // Load modules
-var daymet = require('users/robitalec/CFS:modules/daymet.js');
+var get_daymet = require('users/robitalec/CFS:modules/get_daymet.js');
 
 // Set variables
 var years = ee.List.sequence(1985, 2021);
@@ -13,8 +13,8 @@ var months = ee.List.sequence(5, 7);
 
 
 
-// Test monthly_daymet
-// Usage: monthly_daymet(year_list, month_list)
-var monthly_daymet = daymet.monthly_daymet(years, months);
-print('Monthly daymet', monthly_daymet);
+// Test get_monthly_daymet
+// Usage: get_monthly_daymet(year_list, month_list)
+var monthly_daymet = get_daymet.get_monthly_daymet(years, months);
+print(monthly_daymet);
 Map.addLayer(monthly_daymet.select('prcp'), {min:0, max:500});
