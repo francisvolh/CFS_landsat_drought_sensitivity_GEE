65a433ffd740ee213f6f2159c4150da3f4ece5b7
robit.alec <robit.alec@gmail.com>
2022-10-06 14:53:07 +0000

    Updated modules/points.js (added 1 line, deleted 1 line).

--- modules/points.js
+++ modules/points.js
@@ -24,6 +24,6 @@ var export_points_asset = function(n_pts, ecoregions, region_name) {
 
   var today = new Date().toJSON().slice(0, 10);
   var filename = today + '_' + region_name + '_sampling_points_n' + n_pts;
-  Export.table.toAsset(points, filename, 'CFS/' + filename);
+  Export.table.toAsset(points, filename, 'Exports/' + filename);
 };
 exports.export_points_asset = export_points_asset;
