3e551baf835444ecf70727b7727a9569da5d7ee0
robit.alec <robit.alec@gmail.com>
2022-10-06 15:01:47 +0000

    Updated tests/test_ecoregions.js (added 10 lines, deleted 4 lines).

--- tests/test_ecoregions.js
+++ tests/test_ecoregions.js
@@ -12,14 +12,20 @@ var eco = require('users/robitalec/CFS:modules/ecoregions.js');
 var ecoregions = eco.ecoregions;
 
 
+// Testing: non_arctic_ecoregions
+var non_arctic_ecoregions = eco.non_arctic_ecoregions;
+
+
 // Testing: get_eco_bands();
 var ecoreg_bands = eco.get_eco_bands();
 
 
 print('Ecoregions:', ecoregions);
+print('Non arctic ecoregions:', non_arctic_ecoregions);
 print('Ecoregion, ecozone, ecoprovince as image bands:', ecoreg_bands);
 
-Map.addLayer(ecoregions);
-Map.addLayer(ecoreg_bands.select('ecoregion'), {min: 1, max: 217});
-Map.addLayer(ecoreg_bands.select('ecoprovince'), {min: 1.1, max: 15.2});
-Map.addLayer(ecoreg_bands.select('ecozone'), {min: 1, max: 15});
+Map.addLayer(ecoregions, null, 'Ecoregions');
+Map.addLayer(non_arctic_ecoregions, null, 'Non arctic ecoregions');
+Map.addLayer(ecoreg_bands.select('ecoregion'), {min: 1, max: 217}, 'Ecoregion band');
+Map.addLayer(ecoreg_bands.select('ecoprovince'), {min: 1.1, max: 15.2}, 'Ecoprovince band');
+Map.addLayer(ecoreg_bands.select('ecozone'), {min: 1, max: 15}, 'Ecozone band');
