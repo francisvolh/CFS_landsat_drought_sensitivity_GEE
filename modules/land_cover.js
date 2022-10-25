1bd420448c8a76206fd2aadc2cc801c3e9e9e03d
robit.alec <robit.alec@gmail.com>
2022-10-25 13:16:08 +0000

    Updated modules/land_cover.js (added 9 lines, deleted 7 lines).

--- modules/land_cover.js
+++ modules/land_cover.js
@@ -75,13 +75,15 @@ var lc_transitions = function() {
     var lc_remapped = hermosilla_1984_2019
       .map(utils.set_year)
       .map(mask_classes)
-      .remap([40, 50, 
-              80, 81,
-              100, 210, 220, 230],
-              [1, 1, 
-               2, 2, 
-               3, 3, 3, 3],
-               'land_cover');
+      .map(function(img) {
+        return img.remap([40, 50, 
+                          80, 81,
+                          100, 210, 220, 230],
+                          [1, 1, 
+                           2, 2, 
+                           3, 3, 3, 3],
+                           0,
+                           'land_cover');
   var lc_transitions = lc_remapped.reduce(ee.Reducer.countDistinct()).eq(1);
   
   return lc_transitions;
