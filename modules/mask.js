26ccc60fb189e97635a4805a7b0a406bcce1a0e5
robit.alec <robit.alec@gmail.com>
2022-10-25 13:27:10 +0000

    Updated modules/mask.js (added 2 lines, deleted 1 line).

--- modules/mask.js
+++ modules/mask.js
@@ -18,6 +18,7 @@ var fire = require('users/robitalec/CFS:modules/fire.js');
 var human_mask = human.world_settlement_area;
 var agriculture_mask = agriculture.get_agriculture_mask;
 var min_land_cover_mask = land_cover.get_lc_count_mask();
+var lc_transitions = land_cover.lc_transitions();
 
 var atemporal_mask = min_land_cover_mask
 	.updateMask(human_mask.unmask().not())
