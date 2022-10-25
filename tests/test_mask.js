7f5886d167381cb048a60f6e1391ccd314dcad44
robit.alec <robit.alec@gmail.com>
2022-10-25 13:37:18 +0000

    Updated tests/test_mask.js (added 2 lines).

--- tests/test_mask.js
+++ tests/test_mask.js
@@ -41,6 +41,8 @@ Map.addLayer(ee.Image.constant(0), {palette: '#000000'}, 'Background');
 Map.addLayer(human.world_settlement_area, {palette: '#ccc848', opacity: 0.5}, 'World Settlement Area');
 Map.addLayer(agriculture.get_agriculture_mask, {palette: '#44bf3c', opacity: 0.5}, 'Agriculture');
 Map.addLayer(land_cover.get_lc_count_mask().updateMask(land_cover.get_lc_count_mask().eq(0)), {palette: ['#945cd2', '#ffffff'], opacity: 0.5}, 'Land cover (min count)');
+Map.addLayer(land_cover.lc_transitions(), null, 'Land cover transitions');
+
 
 // Atemporal mask (without fire)
 Map.addLayer(mask.atemporal_mask, null, 'Atemporal mask', false);
