c10e33986f20f7a55c27b4ded67df342acfe6d43
Alec L. Robitaille <robit.alec@gmail.com>
2022-11-14 08:37:53 -0800

    rm get from map tiles

--- Map-tiles.js
+++ Map-tiles.js
@@ -31,7 +31,7 @@ var palettes = require('users/gena/packages:palettes');
 var p = palettes.crameri.vik[10];
 var lc_p = palettes.crameri.bamako[25];
 
-Map.addLayer(land_cover.get_land_cover().filter(ee.Filter.eq('year', 2000)), {palette:lc_p}, 'lc');
+Map.addLayer(land_cover.land_cover().filter(ee.Filter.eq('year', 2000)), {palette:lc_p}, 'lc');
 Map.addLayer(col, {palette: p, min: -0.2, max: 0.2}, 'absolute sensitivity');
 
 
