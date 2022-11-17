887812b45de8f0763bb03ed3cb244a9be8f310c8
robit.alec <robit.alec@gmail.com>
2022-11-17 15:24:12 +0000

    Updated modules/assets.js (added 15 lines, deleted 7 lines).

--- modules/assets.js
+++ modules/assets.js
@@ -4,12 +4,20 @@ Alec L. Robitaille
 
 */
 
+
+
+var list_assets_in_dir = function(dir) {
+    return ee.data.listAssets(dir)['assets'];
+};
+exports.list_assets_in_dir = list_assets_in_dir;
+
+
+
 var delete_assets_in_dir = function(dir) {
-    var assetList = ee.data.listAssets(dir)['assets']
-                        .map(function(d) { 
-                            ee.data.deleteAsset(d.name)
-                            return d.name 
-                        });
-                    
+    list_assets_in_dir(dir)
+        .map(function(d) { 
+            ee.data.deleteAsset(d.name)
+            return d.name 
+        });
 }
 exports.delete_assets_in_dir = delete_assets_in_dir;
