d83f564ed5226678a8f64765e01c60a3f4418a8c
robit.alec <robit.alec@gmail.com>
2022-11-17 15:24:22 +0000

    Updated modules/assets.js (added 3 lines, deleted 3 lines).

--- modules/assets.js
+++ modules/assets.js
@@ -16,8 +16,8 @@ exports.list_assets_in_dir = list_assets_in_dir;
 var delete_assets_in_dir = function(dir) {
     list_assets_in_dir(dir)
         .map(function(d) { 
-            ee.data.deleteAsset(d.name)
-            return d.name 
+            ee.data.deleteAsset(d.name);
+            return d.name ;
         });
-}
+};
 exports.delete_assets_in_dir = delete_assets_in_dir;
