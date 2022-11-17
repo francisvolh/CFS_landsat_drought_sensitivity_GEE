a5c1780cdb9da10d19334da4eec4538dea8a66fe
robit.alec <robit.alec@gmail.com>
2022-11-17 15:28:44 +0000

    Updated modules/assets.js (added 11 lines).

--- modules/assets.js
+++ modules/assets.js
@@ -21,3 +21,14 @@ var delete_assets_in_dir = function(dir) {
         });
 };
 exports.delete_assets_in_dir = delete_assets_in_dir;
+
+
+var collect_img_assets_in_dir = function(dir) {
+    var assetList = list_assets_in_dir(dir)
+        .map(function(d) {
+            return d.name;
+        });
+    return ee.ImageCollection(assetList);
+}
+exports.collect_img_assets_in_dir = collect_img_assets_in_dir;
+
