2f0c5e17bf8c738adb047a402ddc2d8bf8aa4e28
robit.alec <robit.alec@gmail.com>
2022-11-17 16:36:02 +0000

    Updated tests/test_points.js (added 13 lines).

--- tests/test_points.js
+++ tests/test_points.js
@@ -4,9 +4,13 @@ Alec L. Robitaille
 */
 
 
+
 // Modules
 var points = require('users/robitalec/CFS:modules/points.js');
 var vars = require('users/robitalec/CFS:modules/variables.js');
+var assets= require('users/robitalec/CFS:modules/assets.js');
+
+
 
 // Region
 var region = vars.yukon;
@@ -20,3 +24,12 @@ var ecoregions = ee.FeatureCollection('users/robitalec/CFS/Terrestrial_Ecoregion
 // Usage: export_points_asset(n_pts, ecoregions, region_name);
 var n_pts = 10;
 points.export_points_asset(n_pts, ecoregions, region_name);
+
+
+
+// Test export_points_by_tile_asset
+// Usage: export_points_by_tile_asset(tiles, factor)
+var factor = 0.01;
+var dir = 'users/robitalec/CFS/2022-07-10';
+var tiles = assets.collect_img_assets_in_dir(dir);
+points.export_points_by_tile_asset(tiles, factor);
