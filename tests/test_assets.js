0f95c8fb67c4598cfce544f9cff64550ffe97056
robit.alec <robit.alec@gmail.com>
2022-11-17 15:13:31 +0000

    Updated tests/test_assets.js (added 1 line, deleted 1 line).

--- tests/test_assets.js
+++ tests/test_assets.js
@@ -10,4 +10,4 @@ var assets = require('users/robitalec/CFS:modules/assets.js');
 
 // Test delete_assets_in_dir();
 // Usage: delete_assets_in_dir(dir);
-assets.delete_assets_in_dir('users/robitalec/CFS/2022-06-14')
+assets.delete_assets_in_dir('users/robitalec/CFS/2022-07-10')
