3a4e3c8fb550323806c25b4456f23e306e52a7cd
robit.alec <robit.alec@gmail.com>
2022-11-29 19:11:42 +0000

    Updated Export-points-drive.js (added 1 line, deleted 2 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -32,13 +32,6 @@ var dir = 'users/robitalec/CFS/2022-11-19';
 var tiles = assets.collect_img_assets_in_dir(dir);
 // points.export_points_by_tile_asset(tiles, factor);
 
-var points = tiles.map(function(tile) {
-  return tile.sample({
-    scale: 30,
-    factor: factor,
-    geometries: true
-  }); 
-}).flatten();
 
 // Data -----------------------------------------------------------------------
 // Load points
