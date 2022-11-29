d60d8c024a832ad90cbedfd6381767ff355601fe
robit.alec <robit.alec@gmail.com>
2022-11-29 19:26:44 +0000

    Updated Export-points-drive.js (added 3 lines, deleted 2 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -30,7 +30,7 @@ var non_arctic_ecoregions = eco.non_arctic_ecoregions;
 var factor = 0.01;
 var dir = 'users/robitalec/CFS/2022-11-19';
 var tiles = assets.collect_img_assets_in_dir(dir);
-print(tiles)
+print(tiles);
 // tiles = tiles.limit(50, null, true);
 tiles = tiles.limit(43, null, false);
 points.export_points_by_tile_asset(tiles, factor);
