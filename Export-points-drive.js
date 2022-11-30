b75c0de158ad77c3af8c4796959d88b055f6d1c7
robit.alec <robit.alec@gmail.com>
2022-11-30 17:14:07 +0000

    Updated Export-points-drive.js (added 4 lines, deleted 2 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -42,7 +42,7 @@ var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_point
 var name = 'tiles_point1percent_0_50'
 
 // var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_50_97');
-var name = 'tiles_point1percent_50_97'
+// var name = 'tiles_point1percent_50_97'
 
 // // Sample ---------------------------------------------------------------------
 // // Sensitivity
