c07d9b0a0426a6e5c05d493bfb3a8d2d6c909eaa
robit.alec <robit.alec@gmail.com>
2022-11-30 17:14:02 +0000

    Updated Export-points-drive.js (added 4 lines, deleted 2 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -39,12 +39,14 @@ print(tiles);
 // Data -----------------------------------------------------------------------
 // Load points
 var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_0_50');
-// var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_50_97');
+var name = 'tiles_point1percent_0_50'
 
+// var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_50_97');
+var name = 'tiles_point1percent_50_97'
 
 // // Sample ---------------------------------------------------------------------
 // // Sensitivity
-// export_points.export_sensitivity_from_asset(points, 'sample-sensitivity-' + region_name, drive_folder);
+export_points.export_sensitivity_from_asset(points, 'sample-sensitivity-' + region_name, drive_folder);
 
 // // Soil
 // export_points.export_soil(points, 'sample-soil-' + region_name, 'Exports');
