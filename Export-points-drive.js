026ab6f3be6376e3c03b33431e173b03157ea984
robit.alec <robit.alec@gmail.com>
2022-11-30 20:39:40 +0000

    Updated Export-points-drive.js (added 6 lines, deleted 6 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -24,13 +24,13 @@ var non_arctic_ecoregions = eco.non_arctic_ecoregions;
 
 
 // Points ---------------------------------------------------------------------
-var factor = 0.001;
+var factor = 0.0001;
 var dir = 'users/robitalec/CFS/2022-11-19';
 var tiles = assets.collect_img_assets_in_dir(dir);
 print(tiles);
 // tiles = tiles.limit(50, null, true);
 // tiles = tiles.limit(43, null, false);
-// points.export_points_by_tile_asset(tiles, factor);
+points.export_points_by_tile_asset(tiles, factor);
 
 
 // Data -----------------------------------------------------------------------
@@ -46,17 +46,17 @@ var points_name = 'tiles_point1percent_50_97';
 // export_points.export_sensitivity_from_asset(points, 'sample-sensitivity-' + points_name, drive_folder);
 
 // Soil
-export_points.export_soil(points, 'sample-soil-' + points_name, drive_folder);
+// export_points.export_soil(points, 'sample-soil-' + points_name, drive_folder);
 
 // Vegetation
-export_points.export_vegetation(points, 'sample-vegetation-' + points_name, drive_folder);
+// export_points.export_vegetation(points, 'sample-vegetation-' + points_name, drive_folder);
 
 
 // Hydro
-export_points.export_hydro(points, 'sample-hydro-' + points_name, drive_folder);
+// export_points.export_hydro(points, 'sample-hydro-' + points_name, drive_folder);
 
 // Topo
-export_points.export_topo(points, 'sample-topo-' + points_name, drive_folder);
+// export_points.export_topo(points, 'sample-topo-' + points_name, drive_folder);
 
 
 // Climate
