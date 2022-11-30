ee58dd8981176d5811c222d8605681498113d870
robit.alec <robit.alec@gmail.com>
2022-11-30 17:20:38 +0000

    Updated Export-points-drive.js (added 4 lines, deleted 4 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -35,11 +35,11 @@ print(tiles);
 
 // Data -----------------------------------------------------------------------
 // Load points
-var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_0_50');
-var points_name = 'tiles_point1percent_0_50';
+// var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_0_50');
+// var points_name = 'tiles_point1percent_0_50';
 
-// var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_50_97');
-// var name = 'tiles_point1percent_50_97';
+var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_50_97');
+var points_name = 'tiles_point1percent_50_97';
 
 // Sample ---------------------------------------------------------------------
 // Sensitivity
