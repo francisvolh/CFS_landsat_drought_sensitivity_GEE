eb227fd8b9e625c0bb1442962e62deeaad13f27d
robit.alec <robit.alec@gmail.com>
2022-12-01 16:09:03 +0000

    Updated modules/export_points.js (added 1 line, deleted 1 line).

--- modules/export_points.js
+++ modules/export_points.js
@@ -27,7 +27,7 @@ var export_hydro = function(points, drive_name, drive_folder) {
       geometry: pt.geometry(), 
       scale: 500
     }));
-  }).flatten();
+  });//.flatten();
 
   var today = new Date().toJSON().slice(0, 10);
 	Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + drive_name, drive_folder);
