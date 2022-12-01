2ad89c6817a8eb2d738a1abc187edbe7db98dd99
robit.alec <robit.alec@gmail.com>
2022-12-01 14:53:36 +0000

    Updated modules/export_points.js (added 1 line, deleted 1 line).

--- modules/export_points.js
+++ modules/export_points.js
@@ -22,7 +22,7 @@ var export_hydro = function(points, drive_name, drive_folder) {
   var col = hydro.sampling_collection();
 	
   var sampled = points.map(function(pt) {
-    col.reduceRegion({
+    return col.reduceRegion({
       reducer: ee.Reducer.mean(), 
       geometry: pt, 
       scale: 500
