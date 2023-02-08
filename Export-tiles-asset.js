85419b150a9181b13f69979ee3a17c2539a1b5c2
robit.alec <robit.alec@gmail.com>
2023-02-08 20:21:50 +0000

    Updated Export-tiles-asset.js (added 1 line, deleted 1 line).

--- Export-tiles-asset.js
+++ Export-tiles-asset.js
@@ -80,7 +80,7 @@ tiles = tiles.map(function(ft) {return ft.set('id', ft.get('system:index'))});
 var tile_id_list = tiles.aggregate_array('id').distinct();
 // print(tile_id_list);
 
-tile_id_list = tile_id_list.slice(110, 120);
+tile_id_list = tile_id_list.slice(120, 130);
 
 tile_id_list.evaluate(function(tile_ids) {
     tile_ids.forEach(function(tile_id) {
