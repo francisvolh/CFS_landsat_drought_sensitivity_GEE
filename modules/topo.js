f5d7cd731d4e5665185c52b9d2deecae92b4ea1e
robit.alec <robit.alec@gmail.com>
2023-02-14 21:32:12 +0000

    Updated modules/topo.js (added 2 lines, deleted 2 lines).

--- modules/topo.js
+++ modules/topo.js
@@ -79,9 +79,9 @@ exports.chili_srtm = chili_srtm;
 // var dem = ee.Image("MERIT/DEM/v1_0_3");
 
 var dem = ee.ImageCollection("projects/sat-io/open-datasets/GLO-30")
+dem = dem
   .filterBounds(geometry)
-  .select(['DSM'])
-  .mosaic();
+  .mosaic().setDefaultProjection(dem.first().projection())
 
 // TAGEE
 var smooth_dem = function(dem) {
