c0f5e5191f261f0857e0f5e4f2676f7e011c711f
robit.alec <robit.alec@gmail.com>
2023-02-14 21:02:26 +0000

    Updated tests/test_topo.js (added 27 lines, deleted 11 lines).

--- tests/test_topo.js
+++ tests/test_topo.js
@@ -1,3 +1,17 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #d63000 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-136.21751476853268, 63.875723800342705],
+          [-136.21751476853268, 56.77224576663849],
+          [-106.5104835185327, 56.77224576663849],
+          [-106.5104835185327, 63.875723800342705]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Testing: modules/topo.js
 Alec L. Robitaille
@@ -42,16 +56,18 @@ Map.addLayer(topo_diversity, null, 'topo_diversity');
 
 
 // Test TAGEE
-var terr_tagee = topo.tagee_terrain();
-print('Terrain TAGEE', terr_tagee);
-Map.addLayer(terr_tagee.select(['Elevation']), {min: 0, max: 1000}, 'Terrain TAGEE: Elevation');
-Map.addLayer(terr_tagee.select(['Slope']), null, 'Terrain TAGEE: Slope');
-Map.addLayer(terr_tagee.select(['Aspect']), null, 'Terrain TAGEE: Aspect');
-Map.addLayer(terr_tagee.select(['Northness']), null, 'Terrain TAGEE: Northness');
-Map.addLayer(terr_tagee.select(['Eastness']), null, 'Terrain TAGEE: Eastness');
-Map.addLayer(terr_tagee.select(['MinimalCurvature']), null, 'Terrain TAGEE: MinimalCurvature');
-Map.addLayer(terr_tagee.select(['MaximalCurvature']), null, 'Terrain TAGEE: MaximalCurvature');
-Map.addLayer(terr_tagee.select(['ShapeIndex']), null, 'Terrain TAGEE: ShapeIndex');
+var terrain = topo.tagee_terrain();
+print('Terrain TAGEE', terrain);
+Map.addLayer(topo.tagee_viz(terrain, 'Elevation', geometry));
+
+// Map.addLayer(terr_tagee.select(['Elevation']), {min: 0, max: 1000}, 'Terrain TAGEE: Elevation');
+// Map.addLayer(terr_tagee.select(['Slope']), null, 'Terrain TAGEE: Slope');
+// Map.addLayer(terr_tagee.select(['Aspect']), null, 'Terrain TAGEE: Aspect');
+// Map.addLayer(terr_tagee.select(['Northness']), null, 'Terrain TAGEE: Northness');
+// Map.addLayer(terr_tagee.select(['Eastness']), null, 'Terrain TAGEE: Eastness');
+// Map.addLayer(terr_tagee.select(['MinimalCurvature']), null, 'Terrain TAGEE: MinimalCurvature');
+// Map.addLayer(terr_tagee.select(['MaximalCurvature']), null, 'Terrain TAGEE: MaximalCurvature');
+// Map.addLayer(terr_tagee.select(['ShapeIndex']), null, 'Terrain TAGEE: ShapeIndex');
 
 
 
