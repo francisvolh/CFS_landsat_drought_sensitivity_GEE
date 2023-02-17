3b65f40ff32dfbff62547875c8ed5d62e6d7b106
robit.alec <robit.alec@gmail.com>
2023-02-17 02:04:04 +0000

    Updated modules/anthro.js (added 7 lines).

--- modules/anthro.js
+++ modules/anthro.js
@@ -9,6 +9,13 @@ Bachofer, F., Zeidler, J., Esch, T., Gorelick, N., Kakarla, A., Paganini, M.,
 Strano, E. (2020). Outlining where humans live, the World Settlement Footprint
 2015. Scientific Data, 7(1), 1-14. doi:10.1038/s41597-020-00580-5
 
+
+Canada Landsat Derived Forest harvest disturbance 1985-2020
+Hermosilla, T., Wulder, M.A., White, J.C., Coops, N.C., Hobart, G.W., Campbell, 
+L.B., 2016. Mass data processing of time series Landsat imagery: pixels to 
+data products for forest monitoring. International Journal of Digital Earth 
+9(11), 1035-1054.
+
 */
 
 
