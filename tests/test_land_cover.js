4b17be29e93cd9cd234c0d39289860c2411cb865
robit.alec <robit.alec@gmail.com>
2023-02-17 01:48:10 +0000

    Updated tests/test_land_cover.js (added 1 line, deleted 2 lines).

--- tests/test_land_cover.js
+++ tests/test_land_cover.js
@@ -59,6 +59,5 @@ Map.addLayer(lc_collection, null, 'land_cover.land_cover()', false);
 // Usage: mask_land_cover(img)
 var indices_masked_lc = indices_green_col.map(land_cover.mask_land_cover);
 print(indices_masked_lc);
-Map.addLayer(indices_masked_lc.select('NDVI'), {palette:'#ffa18b'}, 'land_cover.mask_land_cover(img) - where pink indicates masked areas', false);
-Map.addLayer(indices_masked_lc)
+Map.addLayer(indices_masked_lc.select('NDVI'), {palette:'#ffa18b'}, 'land_cover.mask_land_cover(img)', false);
 
