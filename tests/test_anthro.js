26b923eed32b5ac374c7ca097ed5c11233867d38
robit.alec <robit.alec@gmail.com>
2023-02-17 02:36:18 +0000

    Updated tests/test_anthro.js (added 2 lines, deleted 2 lines).

--- tests/test_anthro.js
+++ tests/test_anthro.js
@@ -29,7 +29,7 @@ var landsat = require('users/robitalec/CFS:modules/landsat.js');
 
 // Data
 // NDVI example
-var ndvi = landsat.indices_greenest(2010, 2012, '06-01', '09-31', geometry);
+var ndvi = landsat.indices_greenest(2010, 2012, '06-01', '08-31', geometry);
 Map.addLayer(ndvi, {min: -0.2, max: 0.9}, 'NDVI 2010-2012', false);
 
 var lc_2015 = land_cover.hermosilla_1984_2019.filter(ee.Filter.date('2015-01-01')).first();
@@ -58,7 +58,7 @@ var harvest_any = anthro.harvest_any;
 print('Harvest mask (any)', harvest_any);
 Map.addLayer(harvest_any, {opacity: 0.3}, 'Harvest mask (any)');
 
-
+Map.addLayer(harvest_year.gte(2010), {opacity: 0.3}, 'Harvest mask (any)');
 
 
 // Test mask_harvest_year
