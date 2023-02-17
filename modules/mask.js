3cea91ff42c04ace262a8a329b5fa50a973b30b9
robit.alec <robit.alec@gmail.com>
2023-02-17 01:59:31 +0000

    Updated modules/mask.js (added 3 lines, deleted 4 lines).

--- modules/mask.js
+++ modules/mask.js
@@ -7,7 +7,7 @@ Alec L. Robitaille
 
 
 // Modules
-var human = require('users/robitalec/CFS:modules/human.js');
+var anthro = require('users/robitalec/CFS:modules/anthro.js');
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 var agriculture = require('users/robitalec/CFS:modules/agriculture.js');
 var fire = require('users/robitalec/CFS:modules/fire.js');
@@ -15,14 +15,14 @@ var fire = require('users/robitalec/CFS:modules/fire.js');
 
 
 // Get masks
-var human_mask = human.world_settlement_area;
+var wsa_mask = anthro.world_settlement_area;
 var agriculture_mask = agriculture.agriculture_mask;
 var min_land_cover_mask = land_cover.lc_count_mask();
 var lc_transitions = land_cover.lc_transitions();
 
 var atemporal_mask = min_land_cover_mask
   .updateMask(lc_transitions)
-	.updateMask(human_mask.unmask().not())
+	.updateMask(wsa_mask.unmask().not())
 	.updateMask(agriculture_mask.unmask().not())
 	.rename('atemporal_mask');
 exports.atemporal_mask = atemporal_mask;
@@ -32,7 +32,6 @@ exports.atemporal_mask = atemporal_mask;
 // Apply masks
 var apply_mask = function(images) {
 	return images.map(function(img) {
-	  // TODO: mask land cover temporal too
 		return fire.mask_five_year_fires(img.updateMask(atemporal_mask));
 	});
 };
