bf146ed72f086e435bc2d4eda22bc111e6d4203c
robit.alec <robit.alec@gmail.com>
2023-02-17 01:58:42 +0000

    Updated tests/test_anthro.js (added 4 lines, deleted 4 lines).

--- tests/test_anthro.js
+++ tests/test_anthro.js
@@ -1,12 +1,12 @@
 /*
-Testing: modules/human.js
+Testing: modules/anthro.js
 Alec L. Robitaille
 */
 
 
 
 // Load modules
-var human = require('users/robitalec/CFS:modules/human.js');
+var anthro = require('users/robitalec/CFS:modules/anthro.js');
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
 
 
@@ -18,8 +18,8 @@ Map.addLayer(lc_2015, null, 'Land cover 2015');
 
 
 // Test world_settlement_area
-// Usage: human.world_settlement_area;
-var world_settlement_area = human.world_settlement_area;
+// Usage: anthro.world_settlement_area;
+var world_settlement_area = anthro.world_settlement_area;
 print('World Settlement Area', world_settlement_area);
 Map.addLayer(world_settlement_area, {palette: '#CD6600', opacity: 0.8}, 'World Settlement Area');
 
