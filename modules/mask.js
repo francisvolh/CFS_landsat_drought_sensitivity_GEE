0e8ce763a2298ef16b41166d3e291f533f3039ff
robit.alec <robit.alec@gmail.com>
2023-02-20 19:16:41 +0000

    Updated modules/mask.js (added 3 lines, deleted 2 lines).

--- modules/mask.js
+++ modules/mask.js
@@ -28,8 +28,9 @@ exports.atemporal_mask = atemporal_mask;
 var apply_masks = function(images) {
 	return images.map(function(img) {
     img = img.updateMask(atemporal_mask.not());
-    img = fire.mask_five_year_fires(img);
-    img = anthro.mask_harvest_year(img);
+    // img = fire.mask_five_year_fires(img);
+    // img = anthro.mask_harvest_year(img);
+    // img = land_cover_
 		return img;
 	});
 };
