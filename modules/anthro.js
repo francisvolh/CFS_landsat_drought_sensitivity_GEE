7d1eb91b441a4fb9d189e29a32a77f8472ebfc24
robit.alec <robit.alec@gmail.com>
2023-02-20 21:58:25 +0000

    Updated modules/anthro.js (added 2 lines, deleted 2 lines).

--- modules/anthro.js
+++ modules/anthro.js
@@ -27,10 +27,10 @@ exports.world_settlement_area = world_settlement_area;
 
 
 var harvest_year = ee.Image("projects/sat-io/open-datasets/CA_FOREST/CA_Forest_Harvest_1985-2020")
-  .unmask(9999);
+  .unmask(0);
 exports.harvest_year = harvest_year;
 
-var harvest_any = harvest_year.lt(9999);
+var harvest_any = harvest_year.gt(0);
 exports.harvest_any = harvest_any;
 
 var mask_harvest_year = function(img) {
