9208151e5bc823c2c70bacb760ad6699174d0977
robit.alec <robit.alec@gmail.com>
2023-02-20 21:53:44 +0000

    Updated tests/test_anthro.js (added 5 lines, deleted 5 lines).

--- tests/test_anthro.js
+++ tests/test_anthro.js
@@ -29,11 +29,11 @@ var landsat = require('users/robitalec/CFS:modules/landsat.js');
 
 // Data
 // NDVI example
-var ndvi = landsat.indices_greenest(2010, 2012, '06-01', '08-31', geometry);
-Map.addLayer(ndvi.select('NDVI'), {min: -0.2, max: 0.9}, 'NDVI 2010-2012', false);
+var ndvi = landsat.indices_greenest(2019, 2021, '06-01', '08-31', geometry);
+Map.addLayer(ndvi.select('NDVI'), {min: -0.2, max: 0.9}, 'NDVI 2019-2021', false);
 
-var lc_2015 = land_cover.hermosilla_1984_2019.filter(ee.Filter.date('2015-01-01')).first();
-Map.addLayer(lc_2015, null, 'Land cover 2015', false);
+var lc_2015 = land_cover.hermosilla_1984_2019.filter(ee.Filter.date('2019-01-01')).first();
+Map.addLayer(lc_2019, null, 'Land cover 2019', false);
 
 
 
@@ -64,5 +64,5 @@ Map.addLayer(harvest_any, {opacity: 0.3}, 'Harvest mask (any)', false);
 // Usage: anthro.mask_harvest_year;
 var mask_harvest_ndvi = ndvi.map(anthro.mask_harvest_year);
 print('Mask harvest NDVI', mask_harvest_ndvi);
-Map.addLayer(harvest_year.eq(2010), {opacity: 0.8}, 'Harvest mask year 2010', false);
+Map.addLayer(harvest_year.eq(2020), {opacity: 0.8}, 'Harvest mask year 2020', false);
 Map.addLayer(mask_harvest_ndvi.select('NDVI'), {min: -0.2, max: 0.9}, 'Mask harvest NDVI');
