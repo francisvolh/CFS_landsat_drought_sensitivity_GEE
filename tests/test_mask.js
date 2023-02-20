c222a7d1d4d212f1ebfa3baa282ceec75ba2528b
robit.alec <robit.alec@gmail.com>
2023-02-20 20:28:34 +0000

    Updated tests/test_mask.js (added 1 line, deleted 1 line).

--- tests/test_mask.js
+++ tests/test_mask.js
@@ -67,7 +67,7 @@ Map.addLayer(mask.atemporal_mask.not(), {opacity: 0.3}, 'Atemporal mask', false)
 Map.addLayer(fire.five_year_fires(2011), {palette: ['#000000', '#ff5e5e'], opacity: 0.3}, '2011 fires');
 Map.addLayer(anthro.harvest_year, {palette: ['#ffc0c0','#be0900', '#000000'], min: 1985, max:2020, opacity: 0.8}, 'Harvest year', false);
 Map.addLayer(anthro.harvest_year.eq(2011), {palette: ['#000000', '#5eb5ff'], opacity: 0.3}, '2011 harvest');
-
+Map.addLayer(land_cover.land_cover().filter(ee.Filter.eq('year', 2011)), null, 'Land cover - masked');
 
 
 // Testing: apply_masks
