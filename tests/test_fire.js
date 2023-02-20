31e02d8d13998bf8a398327234526ef323829ddc
robit.alec <robit.alec@gmail.com>
2023-02-20 21:48:40 +0000

    Updated tests/test_fire.js (added 4 lines, deleted 4 lines).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -31,7 +31,7 @@ Map.addLayer(five_year_fires, {min:0, max:1, palette: ['000000', 'ffc781']}, 'fi
 
 // Test mask_five_year_fires
 // Usage: fire.mask_five_year_fires(img)
-// var img = ee.Image.random(42, 'normal').clip(geometry).set('year', 2021);
-// var mask_five_year_fires = fire.mask_five_year_fires(img);
-// print(mask_five_year_fires);
-// Map.addLayer(mask_five_year_fires, null, 'mask_five_year_fires');
+var img = ee.Image.random().clip(geometry).set('year', 2021);
+var mask_five_year_fires = fire.mask_five_year_fires(img);
+print(mask_five_year_fires);
+Map.addLayer(mask_five_year_fires, null, 'mask_five_year_fires');
