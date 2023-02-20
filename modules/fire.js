290214c38d6a850cbc189b6c20401e36219d2c72
robit.alec <robit.alec@gmail.com>
2023-02-20 21:43:09 +0000

    Updated modules/fire.js (added 1 line, deleted 1 line).

--- modules/fire.js
+++ modules/fire.js
@@ -22,7 +22,7 @@ var five_year_fires = function(yr) {
   var ymin5 = date.advance(-4, 'year').get('year');
 
   
-  if (y > max_NBAC_fires_year) {
+  if (max_NBAC_fires_year < y) {
     console.log('warning: image year beyond range of fire masks');
   } 
 
