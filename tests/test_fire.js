e3a1e4704dbeb54cd1eaca8387b18a026b733919
robit.alec <robit.alec@gmail.com>
2023-02-20 21:49:41 +0000

    Updated tests/test_fire.js (added 3 lines, deleted 2 lines).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -1,6 +1,7 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var geometry = 
     /* color: #d63000 */
+    /* shown: false */
     /* displayProperties: [
       {
         "type": "rectangle"
@@ -25,13 +26,13 @@ var fire = require('users/robitalec/CFS:modules/fire.js');
 
 // Test five_year_fires
 // Usage: fire.five_year_fires(year)
-var five_year_fires = fire.five_year_fires(2010);
+var five_year_fires = fire.five_year_fires(2020);
 print(five_year_fires);
 Map.addLayer(five_year_fires, {min:0, max:1, palette: ['000000', 'ffc781']}, 'five_year_fires');
 
 // Test mask_five_year_fires
 // Usage: fire.mask_five_year_fires(img)
-var img = ee.Image.random().clip(geometry).set('year', 2021);
+var img = ee.Image.random().clip(geometry).set('year', 2020);
 var mask_five_year_fires = fire.mask_five_year_fires(img);
 print(mask_five_year_fires);
 Map.addLayer(mask_five_year_fires, null, 'mask_five_year_fires');
