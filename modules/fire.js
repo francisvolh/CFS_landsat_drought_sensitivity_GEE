a1e0283f7ac9fed64431391ba5700c3e259be464
robit.alec <robit.alec@gmail.com>
2023-02-20 21:36:25 +0000

    Updated modules/fire.js (added 6 lines, deleted 4 lines).

--- modules/fire.js
+++ modules/fire.js
@@ -43,4 +43,5 @@ var mask_five_year_fires = function(img) {
     var fire = five_year_fires(yr).eq(0);
     return(img.updateMask(fire));
   }
+};
 exports.mask_five_year_fires = mask_five_year_fires;
