7c88c51a59692866c3507943a2ae0ba49de56727
robit.alec <robit.alec@gmail.com>
2023-02-21 21:21:02 +0000

    Updated Map-tiles.js (added 1 line, deleted 1 line).

--- Map-tiles.js
+++ Map-tiles.js
@@ -81,7 +81,7 @@ Map.addLayer(blend.multiply(col_viz, hillshade_viz), {min: 0.1, max: 0.7});
 
 var lc_viz = lc.first()
   .visualize({
-    palette: palettes.crameri.bamako[25]
+    palette: palettes.Brewer.Greys[10]
   });
 
 Map.addLayer(blend.overlay(col_viz, lc_viz), {min: 0.1, max: 0.7});
