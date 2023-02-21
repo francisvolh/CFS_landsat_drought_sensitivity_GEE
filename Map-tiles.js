80d634a3121d67006410e0904e26dbf0c2d40c79
robit.alec <robit.alec@gmail.com>
2023-02-21 21:33:43 +0000

    Updated Map-tiles.js (added 2 lines, deleted 1 line).

--- Map-tiles.js
+++ Map-tiles.js
@@ -83,5 +83,6 @@ var lc_viz = lc.first()
   .visualize({
     palette: palettes.colorbrewer.Greys[9]
   });
-Map.addLayer(lc_viz, null, 'lc viz');
+Map.addLayer(lc_viz, null, 'lc viz', false);
+
 Map.addLayer(blend.overlay(col_viz, lc_viz), {min: 0.1, max: 0.7}, 'blend sensitivity and land cover');
