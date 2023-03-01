69258b564df231b28635daed6cbd0190c7401de3
robit.alec <robit.alec@gmail.com>
2023-03-01 14:45:18 +0000

    Updated Export-points-drive.js (deleted 14 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -22,20 +22,6 @@ var drive_folder = 'Exports';
 var non_arctic_ecoregions = eco.non_arctic_ecoregions;
 
 
-
-// Points ---------------------------------------------------------------------
-var factor = 0.0001;
-var dir = 'users/robitalec/CFS/2022-11-19';
-// var tiles = assets.collect_img_assets_in_dir(dir);
-// print(tiles);
-// Map.addLayer(ee.FeatureCollection(tiles.map(function(tile){ 
-//   return ee.Feature(ee.Geometry(tile.get('system:footprint')))
-// })))
-// tiles = tiles.limit(50, null, true);
-// tiles = tiles.limit(43, null, false);
-// points.export_points_by_tile_asset(tiles, factor);
-
-
 // Data -----------------------------------------------------------------------
 // Load points
 // var points = ee.FeatureCollection('users/robitalec/CFS/2022-11-29_sampling_points_tiles_point1percent_0_50');
