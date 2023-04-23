3917d118130cbf4cf3d153d076dc1ff68ef69b77
robit.alec <robit.alec@gmail.com>
2023-04-23 02:20:40 +0000

    Updated Export-points-drive.js (added 1 line).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -28,6 +28,7 @@ var factor = 0.005;
 var col = ee.ImageCollection('users/robitalec/CFS/2023-02-21/2023-02-21_image_col');
 points.export_points_by_img_col_asset(col, factor);
 
+Map.addlayer(points)
 // Asset
 // var points = ee.FeatureCollection('users/robitalec/CFS/2023-12-21_sampling_points_tiles_0pt01percent');
 // var points_name = 'tiles_0pt01';
