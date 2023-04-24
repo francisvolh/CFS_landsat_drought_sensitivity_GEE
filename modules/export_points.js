a65c2fd08e9e292fcb6ce529ed65ab7ad8b1de91
robit.alec <robit.alec@gmail.com>
2023-04-24 19:25:19 +0000

    Updated modules/export_points.js (added 1 line, deleted 1 line).

--- modules/export_points.js
+++ modules/export_points.js
@@ -91,7 +91,7 @@ exports.export_vegetation = export_vegetation;
 
 var export_soil = function(points, drive_name, drive_folder) {
   var col = soil.sampling_collection();
-	var sampled = col.map(function(img) {return img.reduceRegions(points, ee.Reducer.mean(), 30)});
+	var sampled = points.map(function(ft){ return col.sample(ft, 30)});
 	var today = new Date().toJSON().slice(0, 10);
 	Export.table.toDrive(ee.FeatureCollection(sampled), today + '_' + drive_name, drive_folder);
 };
