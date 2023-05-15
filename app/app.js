a4d255a3199eb0486a8fc2aa651e295d23767f4b
robit.alec <robit.alec@gmail.com>
2023-05-15 23:37:02 +0000

    Updated app/app.js (added 26 lines, deleted 3 lines).

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Polygon(
+        [[[-142.55499390153335, 70.03530462152891],
+          [-141.28886260033715, 59.73133485522378],
+          [-132.97491577653335, 51.53555252654365],
+          [-124.37867426910192, 48.11740729432402],
+          [-109.40639935881254, 48.804462143547084],
+          [-94.30304077653334, 49.12365840683438],
+          [-82.70147827653334, 42.195333863545955],
+          [-71.01202515153334, 45.11944811934683],
+          [-66.96905640153334, 45.05739605590757],
+          [-65.91436890153334, 43.16449754575042],
+          [-52.203431401533344, 46.224859987659656],
+          [-52.180299631222184, 49.641565048375476],
+          [-57.209200883265744, 55.24608626628804],
+          [-62.70636108903334, 59.220495521919005],
+          [-78.92218140153334, 62.49163190377348],
+          [-103.97100952653334, 67.692445654623],
+          [-131.21710327653335, 69.08395110419174]]]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 App
 Alec L. Robitaille
@@ -34,14 +57,14 @@ col = col.select('Abs_sens_NDVI_ante12mo_p15_p85');
 var lc_filter = lc.filter(ee.Filter.eq('year', 2010));
 
 var dem_mosaic = dem
-  .filterBounds(geometry2)
+  .filterBounds(geometry)
   .mosaic()
   .setDefaultProjection(dem.first().projection());
 
 var hillshade = ee.Terrain.hillshade(dem_mosaic);
 
 var col_mosaic = col
-    //.filterBounds(geometry2)
+    //.filterBounds(geometry)
     .mosaic();
 
 // Visualize
