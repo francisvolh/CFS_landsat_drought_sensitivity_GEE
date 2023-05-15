2a1e90cc6cf8c37051df738396767d2e8f53a8be
robit.alec <robit.alec@gmail.com>
2023-05-15 23:36:17 +0000

    Updated Map-tiles.js (added 62 lines, deleted 40 lines).

--- Map-tiles.js
+++ Map-tiles.js
@@ -36,53 +36,75 @@ var geometry =
           [-103.97100952653334, 67.692445654623],
           [-131.21710327653335, 69.08395110419174]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
+/*
+App
+Alec L. Robitaille
+
+*/
+
+
+// Modules
 var blend = require('users/jja/public:blend.js');
-
-Map.addLayer(ee.Image.constant(1), {palette:'000', opacity:0.5}, 'constant');
-
 var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
-var soil = require('users/robitalec/CFS:modules/soil.js');
-
-var col = ee.ImageCollection("users/robitalec/CFS/2023-02-21/2023-02-21_image_col");
-
-print('Band names', col.first().bandNames());
-col = col.select('Abs_sens_NDVI_ante12mo_p15_p85');
-
 var palettes = require('users/gena/packages:palettes');
 
+
+
+
+// Data
+var col = ee.ImageCollection('users/robitalec/CFS/2023-02-21/2023-02-21_image_col');
+
+var lc = land_cover.land_cover();
+
+var dem = ee.ImageCollection("projects/sat-io/open-datasets/FABDEM");
+
+
+// Palettes
 var p = palettes.crameri.vik[10];
 var lc_p = palettes.crameri.bamako[25];
 
-var lc = land_cover.land_cover().filter(ee.Filter.eq('year', 2010));
-Map.addLayer(lc, {palette:lc_p}, 'lc', false);
-Map.addLayer(col, {palette: p, min: -0.2, max: 0.2}, 'absolute sensitivity', false);
 
+// Options
+print('Band names', col.first().bandNames());
+col = col.select('Abs_sens_NDVI_ante12mo_p15_p85');
 
-var col_viz = col
-  .filterBounds(geometry2)
+// Process
+var lc_filter = lc.filter(ee.Filter.eq('year', 2010));
+
+var dem_mosaic = dem
+  .filterBounds(geometry)
   .mosaic()
-  .visualize({
+  .setDefaultProjection(dem.first().projection());
+
+var hillshade = ee.Terrain.hillshade(dem_mosaic);
+
+var col_mosaic = col
+    //.filterBounds(geometry)
+    .mosaic();
+
+// Visualize
+var col_vis = col_mosaic.visualize({
     palette:p,
     min: -0.2,
     max: 0.2
 });
 
-var dem = ee.ImageCollection("projects/sat-io/open-datasets/FABDEM");
-dem = dem
-  .filterBounds(geometry2)
-  .mosaic()
-  .setDefaultProjection(dem.first().projection());
-
-var hillshade = ee.Terrain.hillshade(dem);
-// Map.addLayer(hillshade, null, 'hillshade');
 var hillshade_viz = hillshade.visualize({
-  min:0, 
-  max:250, 
-  palette: ['#000000', '#ffffff'],
-  forceRgbOutput:true
-});
-// Map.addLayer(hillshade_viz, null, 'hillshade_viz');
+    min:0,
+    max:250,
+    palette: ['#000000', '#ffffff'],
+    forceRgbOutput:true
+  });
+  
 
+// Map
+Map.addLayer(ee.Image.constant(1), {palette:'000', opacity:0.5}, 'constant');
+
+Map.addLayer(lc_filter, {palette:lc_p}, 'lc', false);
+
+
+Map.addLayer(col_mosaic, {palette: p, min: -0.2, max: 0.2}, 'absolute sensitivity', false);
+
+
+// Blend
 Map.addLayer(blend.multiply(col_viz, hillshade_viz), {min: 0.1, max: 0.75}, 'blend sensitivity and hillshade');
-
-Map.addLayer(soil.sampling_collection().select('mean_potential_peat_depth_cm'), {min: 275, max: 900}, 'mean_potential_peat_depth_cm');
