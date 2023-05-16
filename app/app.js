49ec501b8d56e837ed80cfda73de2d4b6f004b27
robit.alec <robit.alec@gmail.com>
2023-05-16 03:58:14 +0000

    Updated app/app.js (added 5 lines, deleted 10 lines).

--- app/app.js
+++ app/app.js
@@ -202,18 +202,13 @@ var hillshade_viz = hillshade.visualize({
 
 // Blend
 var blend_col_hillshade = blend.multiply(col_viz, hillshade_viz);
+// Map.addLayer(blend_col_hillshade, {min: 0.1, max: 0.75}, 'blend sensitivity and hillshade');
 
 
 
 // Map
 Map.setOptions('SATELLITE');
 Map.centerObject(col_mosaic);
-
-
-
-// Blend
-// Map.addLayer(blend_col_hillshade, {min: 0.1, max: 0.75}, 'blend sensitivity and hillshade');
-
 
 
 
