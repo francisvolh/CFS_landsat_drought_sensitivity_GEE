6a427eaa0c5d38ae42169d700edc1fd7d85ccaaa
robit.alec <robit.alec@gmail.com>
2023-05-30 02:30:46 +0000

    Updated app/app.js (added 1 line, deleted 1 line).

--- app/app.js
+++ app/app.js
@@ -336,7 +336,7 @@ var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
   .visualize({min: 0, max: 100, palette: p}));
 panel_left_bottom.add(ui.Label('-2 __________ 0 __________ 2'));
 panel_left_bottom.add(img_thumb);
-panel_left_left.add(ui.Label('Diana Stralberg, Alec Robitaille, Guillermo Castilla, Jennifer Cartwright, Mike Michaelian, and Ted Hogg'))
+panel_left_left.add(ui.Label('Diana Stralberg, Alec L. Robitaille, Guillermo Castilla, Jennifer Cartwright, Mike Michaelian, and Ted Hogg'))
 Map.add(panel_left);
 Map.add(panel_left_bottom);
 
