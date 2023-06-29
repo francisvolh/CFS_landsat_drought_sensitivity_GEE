d671fe0fc9c5fe559fa3ecaa959795bac9ed08f0
robit.alec <robit.alec@gmail.com>
2023-06-29 15:13:54 +0000

    Updated tests/test_topo.js (added 11 lines, deleted 9 lines).

--- tests/test_topo.js
+++ tests/test_topo.js
@@ -59,14 +59,16 @@ Map.addLayer(topo_diversity, null, 'topo_diversity');
 // Test TAGEE
 var terrain = topo.tagee_terrain(geometry);
 print('Terrain TAGEE', terrain);
-Map.addLayer(topo.tagee_viz(terrain, 'Elevation', geometry), null, 'Elevation');
-Map.addLayer(topo.tagee_viz(terrain, 'Slope', geometry), null, 'Slope');
-Map.addLayer(topo.tagee_viz(terrain, 'Aspect', geometry), null, 'Aspect');
-Map.addLayer(topo.tagee_viz(terrain, 'Northness', geometry), null, 'Northness');
-Map.addLayer(topo.tagee_viz(terrain, 'Eastness', geometry), null, 'Eastness');
-Map.addLayer(topo.tagee_viz(terrain, 'MinimalCurvature', geometry), null, 'MinimalCurvature');
-Map.addLayer(topo.tagee_viz(terrain, 'MaximalCurvature', geometry), null, 'MaximalCurvature');
-Map.addLayer(topo.tagee_viz(terrain, 'ShapeIndex', geometry), null, 'ShapeIndex');
+
+var zoom = 6;
+Map.addLayer(topo.tagee_viz(terrain, 'Elevation', zoom, geometry), null, 'Elevation');
+Map.addLayer(topo.tagee_viz(terrain, 'Slope', zoom, geometry), null, 'Slope');
+Map.addLayer(topo.tagee_viz(terrain, 'Aspect', zoom, geometry), null, 'Aspect');
+Map.addLayer(topo.tagee_viz(terrain, 'Northness', zoom, geometry), null, 'Northness');
+Map.addLayer(topo.tagee_viz(terrain, 'Eastness', zoom, geometry), null, 'Eastness');
+Map.addLayer(topo.tagee_viz(terrain, 'MinimalCurvature', zoom, geometry), null, 'MinimalCurvature');
+Map.addLayer(topo.tagee_viz(terrain, 'MaximalCurvature', zoom, geometry), null, 'MaximalCurvature');
+Map.addLayer(topo.tagee_viz(terrain, 'ShapeIndex', zoom, geometry), null, 'ShapeIndex');
 
 
 // Test sampling_collection
