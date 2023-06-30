58d79245aecc280d5acce5b8f733dad2b8f5fcc4
robit.alec <robit.alec@gmail.com>
2023-06-30 14:41:19 +0000

    Updated app/app.js (added 1 line, deleted 1 line).

--- app/app.js
+++ app/app.js
@@ -297,7 +297,7 @@ var bandSelect = ui.Select({
     var img = imageSelect.getValue().select(value);
     var stats = img.reduceRegion({
       reducer: ee.Reducer.percentile([5, 95]),
-      geometry : sample,
+      geometry : Map_right.getBounds(),
       scale: level_scale.get(Map_right.getZoom()),
       bestEffort: true
     });
