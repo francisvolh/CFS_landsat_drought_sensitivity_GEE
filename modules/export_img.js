dceff7268e106cb30b9c75c1c5a0a79ea6e06a39
robit.alec <robit.alec@gmail.com>
2023-07-19 18:01:05 +0000

    Updated modules/export_img.js (added 4 lines, deleted 100 lines).

--- modules/export_img.js
+++ modules/export_img.js
@@ -12,8 +12,8 @@ var hydro = require('users/robitalec/CFS:modules/hydro.js');
 
 
 // Export img asset greenest
-var export_img_asset_greenest = function(asset_name, asset_path, scale, region) {
-  var out = main.main_greenest('absolute sensitivity', region);
+var export_img_asset_greenest = function(output, asset_name, asset_path, scale, region) {
+  var out = main.main_greenest(output, region);
 
   var today = new Date().toJSON().slice(0, 10);
 
@@ -32,8 +32,8 @@ exports.export_img_asset_greenest = export_img_asset_greenest;
 
 
 // Export img drive greenest
-var export_img_drive_greenest = function(drive_name, drive_folder, scale, region) {
-  var out = main.main_greenest('absolute sensitivity', region);
+var export_img_drive_greenest = function(output, drive_name, drive_folder, scale, region) {
+  var out = main.main_greenest(output, region);
 
   var today = new Date().toJSON().slice(0, 10);
 
@@ -98,99 +98,3 @@ var export_hydro_sampling_collection = function(region, region_name, scale) {
   });
 };
 exports.export_hydro_sampling_collection = export_hydro_sampling_collection;
-
-
-
-
-
-// ARCHIVE --------------------------------------------------------------------
-// Export img asset cap
-var zzz_export_img_asset_cap = function(asset_name, asset_path, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent) {
-  var out = main.main_cap('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
-
-  var today = new Date().toJSON().slice(0, 10);
-
-  asset_name = today + '_' + asset_name;
-  Export.image.toAsset({
-    image: out,
-    description: asset_name,
-    assetId: asset_path + '/' + asset_name,
-    region: region,
-    scale: scale,
-    maxPixels: 2.5e8
-  });
-};
-exports.zzz_export_img_asset_cap = zzz_export_img_asset_cap;
-
-
-
-// Export img drive cap
-var zzz_export_img_drive_cap = function(drive_name, drive_folder, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent) {
-  var out = main.main_cap('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile_low, percentile_high, antecedent);
-
-  var today = new Date().toJSON().slice(0, 10);
-
-  Export.image.toDrive({
-    image: out,
-    description: today + '_' + drive_name,
-    folder: drive_folder,
-    region: region,
-    scale: scale
-  });
-};
-exports.zzz_export_img_drive_cap = zzz_export_img_drive_cap;
-
-// Export img asset
-var zzz_export_img_asset = function(asset_name, asset_path, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
-  var out = main.main('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
-
-  var today = new Date().toJSON().slice(0, 10);
-
-  asset_name = today + '_' + asset_name;
-  Export.image.toAsset({
-    image: out,
-    description: asset_name,
-    assetId: asset_path + '/' + asset_name,
-    region: region,
-    scale: scale
-  });
-};
-exports.zzz_export_img_asset = zzz_export_img_asset;
-
-
-
-// Export img drive
-var zzz_export_img_drive = function(drive_name, drive_folder, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
-  var out = main.main('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
-
-  var today = new Date().toJSON().slice(0, 10);
-
-  Export.image.toDrive({
-    image: out,
-    description: today + '_' + drive_name,
-    folder: drive_folder,
-    region: region,
-    scale: scale
-  });
-};
-exports.zzz_export_img_drive = zzz_export_img_drive;
-
-
-
-// Export img cloud
-var zzz_export_img_cloud = function(cloud_name, cloud_bucket, scale, region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent) {
-  var out = main.main('absolute sensitivity', region, min_year, max_year, min_mm_dd, max_mm_dd, index, percentile, antecedent);
-
-  var today = new Date().toJSON().slice(0, 10);
-
-  cloud_name = today + '_' + cloud_name;
-  Export.image.toCloudStorage({
-    image: out,
-    description: cloud_name,
-    bucket: cloud_bucket,
-    region: region,
-    scale: scale
-  });
-};
-exports.zzz_export_img_cloud = zzz_export_img_cloud;
-
