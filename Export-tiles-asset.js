12450c11a24f962c43d72b505e6df6c19b8572b4
robit.alec <robit.alec@gmail.com>
2023-07-19 19:14:04 +0000

    Updated Export-tiles-asset.js (added 3 lines, deleted 3 lines).

--- Export-tiles-asset.js
+++ Export-tiles-asset.js
@@ -84,10 +84,10 @@ tiles = tiles.map(function(ft) {return ft.set('id', ft.get('system:index'))});
 var tile_id_list = tiles.aggregate_array('id').distinct();
 print(tile_id_list);
 
-// var sub_tile_id_list = tile_id_list.slice(0, 10);
-// print(sub_tile_id_list);
+var sub_tile_id_list = tile_id_list.slice(0, 10);
+print(sub_tile_id_list);
 
-tile_id_list.evaluate(function(tile_ids) {
+sub_tile_id_list.evaluate(function(tile_ids) {
     tile_ids.forEach(function(tile_id) {
       var ft = tiles.filter(ee.Filter.eq('id', tile_id));
       export_img.export_img_asset_greenest(output, output_prefix + '_' + tile_id, asset_path, scale, ft);
