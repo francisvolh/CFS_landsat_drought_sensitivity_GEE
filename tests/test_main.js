f4c7c5087ddbd3d4881a7e581db7871086e56554
robit.alec <robit.alec@gmail.com>
2023-07-19 19:02:06 +0000

    Updated tests/test_main.js (added 3 lines, deleted 3 lines).

--- tests/test_main.js
+++ tests/test_main.js
@@ -43,21 +43,21 @@ Map.addLayer(ee.Image.constant(1), {palette:'#000'});
 // Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 var main_index_and_antecedent = main.main_greenest('vegetation index and antecedent means', geometry);
 print('veg index + antecedent means'); print(main_index_and_antecedent);
-Map.addLayer(main_index_and_antecedent.select('CMI_ante3lag_mean').first(), vars.cmi_viz, 'CMI 3 yr lag antecedent mean', false);
+Map.addLayer(main_index_and_antecedent.select('CMI_ante3yr_mean').first(), vars.cmi_viz, 'CMI 3 yr antecedent mean', false);
 Map.addLayer(main_index_and_antecedent.select('NDVI').first(), ndvi_viz, 'NDVI', false);
 
 // Test main - relative
 // Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 var main_relative = main.main_greenest('relative sensitivity', geometry);
 print('relative sensitivity'); print(main_relative);
-Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3lag_p15_p85'), vars.rel_viz, 'relative drought sensitivity NDVI p15-85 3 yr lag antecedent', false);
+Map.addLayer(main_relative.select('Rel_sens_NDVI_ante3yr_p15_p85'), vars.rel_viz, 'relative drought sensitivity NDVI p15-85 3 yr lag antecedent', false);
 
 
 // Test main - absolute
 // Usage: main_greenest(output, region, min_year, max_year, min_mm_dd, max_mm_dd, antecedent_list);
 var main_absolute = main.main_greenest('absolute sensitivity', geometry);
 print('absolute sensitivity'); print(main_absolute);
-Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3lag_p15_p85'), vars.abs_viz, 'absolute drought sensitivity NDVI p15-85  3 yr lag antecedent');
+Map.addLayer(main_absolute.select('Abs_sens_NDVI_ante3yr_p15_p85'), vars.abs_viz, 'absolute drought sensitivity NDVI p15-85  3 yr lag antecedent');
 
 
 
