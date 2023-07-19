feedd9eabab8ff5364ae83b10892e8fe209f652f
robit.alec <robit.alec@gmail.com>
2023-07-19 18:58:22 +0000

    Updated modules/main.js (added 2 lines).

--- modules/main.js
+++ modules/main.js
@@ -153,6 +153,8 @@ var zzz_main_cap = function(output, region,
     var joined = join.apply(indices_col, ante_means, ee.Filter.equals({leftField: 'year', rightField: 'year'}));
     joined = ee.ImageCollection(joined.map(function(img) {return ee.Image.cat(img.get('primary'), img.get('secondary'))}));
     return joined;
+  } else {
+  	throw "output not one of 'relative sensitivity', 'absolute sensitivity', or 'vegetation index and antecedent means'"
   }
 
 };
