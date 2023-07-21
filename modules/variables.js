0d3f7d0089652d78aacfc54db295d464b7700442
robit.alec <robit.alec@gmail.com>
2023-07-21 17:02:12 +0000

    Updated modules/variables.js (added 6 lines).

--- modules/variables.js
+++ modules/variables.js
@@ -15,6 +15,12 @@ var geometry =
 /*
 Variables
 Alec L. Robitaille
+
+Geoboundaries
+Runfola D, Anderson A, Baier H, Crittenden M, Dowker E, Fuhrig S, et al. (2020)
+geoBoundaries: A global database of political administrative boundaries. PLoS ONE 15(4):
+e0231866. https://doi.org/10.1371/journal.pone.0231866
+
 */
 
 // Modules
