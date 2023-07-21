51ed341784cbd16f3718dd2bc26b8597f1715da5
robit.alec <robit.alec@gmail.com>
2023-07-21 16:39:11 +0000

    Updated tests/test_utils.js (added 13 lines, deleted 11 lines).

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -14,46 +14,48 @@ var daymet = climate.daymet
   .filter(ee.Filter.date('2000-01-01', '2003-01-01'));
 var year_list = ee.List.sequence(2000, 2002);
 var month_list = ee.List.sequence(1, 12);
+var week_list = ee.List.sequence(20, 25);
+
 var reducer = ee.Reducer.mean().combine(ee.Reducer.sum(), null, true);
 
 
 // Test set_year
 // Usage: utils.set_year(image)
-img = utils.set_year(img);
-print('Set year', img);
+// img = utils.set_year(img);
+// print('Set year', img);
 
 
 
 // Test add_year_band
 // Usage: utils.add_year_band(image)
-img = utils.add_year_band(img);
-Map.addLayer(img.select('year'));
+// img = utils.add_year_band(img);
+// Map.addLayer(img.select('year'));
 
 
 
 // Test aggregate_month_year
 // Usage: aggregate_month_year(images, year_list, month_list, reducer);
-var agg_month_year = utils.aggregate_month_year(daymet, year_list, month_list, reducer);
-print('Aggregate month year', agg_month_year);
+// var agg_month_year = utils.aggregate_month_year(daymet, year_list, month_list, reducer);
+// print('Aggregate month year', agg_month_year);
 
 
 
 // Test aggregrate_year
 // Usage: aggregrate_year(images, year_list, reducer);
-var agg_year = utils.aggregrate_year(daymet, year_list, reducer);
-print('Aggregate year', agg_year);
+// var agg_year = utils.aggregrate_year(daymet, year_list, reducer);
+// print('Aggregate year', agg_year);
 
 
 
 // Test set_week
 // Usage: set_week(images);
-var test_week = daymet.map(utils.set_week);
-print('Set week', test_week);
+// var test_week = daymet.map(utils.set_week);
+// print('Set week', test_week);
 
 
 
 // Test aggregrate_week
-// Usage: aggregrate_week(images, year_list, reducer);
+// Usage: aggregrate_week(images, year_list, week_list, reducer);
 var agg_wk = utils.aggregrate_week(daymet, year_list, reducer);
 print('Aggregate week', agg_wk);
 
