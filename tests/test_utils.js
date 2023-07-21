d335947a9b1736798c560cb10df7a967ac49c52c
robit.alec <robit.alec@gmail.com>
2023-07-21 16:39:17 +0000

    Updated tests/test_utils.js (added 14 lines, deleted 11 lines).

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -12,6 +12,7 @@ var climate = require('users/robitalec/CFS:modules/climate.js');
 var img = ee.Image.constant(1).set('system:time_start', ee.Date.fromYMD(2020, 1, 1).millis());
 var daymet = climate.daymet
   .filter(ee.Filter.date('2000-01-01', '2003-01-01'));
+  
 var year_list = ee.List.sequence(2000, 2002);
 var month_list = ee.List.sequence(1, 12);
 var week_list = ee.List.sequence(20, 25);
