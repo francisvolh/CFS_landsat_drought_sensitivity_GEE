b2dab0c035bc1d0a75d000dd2a17e9304931f266
robit.alec <robit.alec@gmail.com>
2023-07-21 13:03:51 +0000

    Updated tests/test_nobs.js (added 2 lines, deleted 1 line).

--- tests/test_nobs.js
+++ tests/test_nobs.js
@@ -49,7 +49,8 @@ var region = ee.Geometry.Polygon([[[-125.87, 56.86], [-125.87, 54.98], [-121.87,
 
 
 // Processing
-var monthly_daymet = climate.monthly_daymet(years, months);
+var daymet = climate.daymet();
+var monthly_daymet = climate.monthly_daymet(daymet, years, months);
 var indices_col = landsat.indices_greenest(min_year_landsat, max_year, min_mm_dd, max_mm_dd, region);
 indices_col = mask.apply_masks(indices_col);
 var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
