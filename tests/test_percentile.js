e869bcab83d5eea8f8753418a45514e33503d6fe
robit.alec <robit.alec@gmail.com>
2023-07-21 14:15:45 +0000

    Updated tests/test_percentile.js (added 1 line, deleted 2 lines).

--- tests/test_percentile.js
+++ tests/test_percentile.js
@@ -20,8 +20,7 @@ var percentile_list = [15, 85];
 
 
 // Load collection
-var daymet = climate.daymet();
-var monthly_daymet = climate.monthly_daymet(daymet, years, months);
+var monthly_daymet = climate.monthly_daymet(years, months);
 
 // Calculate CMI
 var cmi_daymet = monthly_daymet.map(cmi.calc_CMI);
