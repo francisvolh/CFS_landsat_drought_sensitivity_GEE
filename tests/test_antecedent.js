388bb791ed82a5cede0bfff0001a53f702730c8b
robit.alec <robit.alec@gmail.com>
2023-07-21 14:14:42 +0000

    Updated tests/test_antecedent.js (deleted 1 line).

--- tests/test_antecedent.js
+++ tests/test_antecedent.js
@@ -16,7 +16,6 @@ var years = ee.List.sequence(2010, 2015);
 var months = ee.List.sequence(1, 12);
 
 // Load collection
-var daymet = climate.daymet;
 var monthly_daymet = climate.monthly_daymet(years, months);
 
 // Calculate CMI
