f25d422cd722a5f49413dceba20d2f7574eb5793
robit.alec <robit.alec@gmail.com>
2023-07-24 16:36:39 +0000

    Updated modules/variables.js (added 1 line, deleted 1 line).

--- modules/variables.js
+++ modules/variables.js
@@ -63,7 +63,7 @@ exports.dawson = ee.Geometry.Polygon(
 	[-137.86706931880778, 63.10300863803273],
 	[-137.86706931880778, 64.89866428936777]]]);
 
-exports.bc = bounds.filter(ee.Filter.eq('placeName', 'British Columbia'));
+exports.bc = bounds.filter(ee.Filter.eq('placeName', 'British Columbia')).geometry();
 exports.yukon = bounds.filter(ee.Filter.eq('placeName', 'Yukon'));
 
 var western_can_ls = ['Yukon', 'Northwest Territories', 'Nunavut', 
