e029f668bd04948af475b558f3392828c7419568
robit.alec <robit.alec@gmail.com>
2023-07-28 16:47:17 +0000

    Updated modules/sensitivity.js (added 19 lines).

--- modules/sensitivity.js
+++ modules/sensitivity.js
@@ -49,3 +49,22 @@ var sensitivity_relative_cap = function(split_indices, antecedent_list, index_li
 exports.sensitivity_relative_cap = sensitivity_relative_cap;
 
 
+var sensitivity_normdiff_cap = function(split_indices, antecedent_list, index_list) {
+	var means = split_indices.mean();
+  return ee.Image(antecedent_list.map(function(antecedent_period) {
+      return index_list.map(function(index) {
+          var id = index + '_ante' + antecedent_period;
+          var baseline_band = id + '_wi_p15_p85_base';
+          var drought_band = id + '_lte_p15_drought';
+          var sensitivity_band = 'ND_sens_' + id + '_p15_p85';
+
+          return means.expression('(baseline - drought) / (baseline + drought)', {
+            baseline: means.select(baseline_band),
+            drought: means.select(drought_band)
+          }).rename(sensitivity_band);
+        });
+    })
+  );
+};
+exports.sensitivity_relative_cap = sensitivity_relative_cap;
+
