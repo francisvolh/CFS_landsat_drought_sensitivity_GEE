f33843c7ef9f8261548a91c961469a8333148f00
robit.alec <robit.alec@gmail.com>
2023-07-29 12:20:22 +0000

    Updated Check-ND-sensitivity.js (added 30 lines, deleted 11 lines).

--- Check-ND-sensitivity.js
+++ Check-ND-sensitivity.js
@@ -28,12 +28,12 @@ var hillshade_viz = hillshade.visualize({
 var p = palettes.crameri.vik[10];
 
 // Options
-print('Band names (Abs/rel)', col.first().bandNames());
-col = col.select('Abs_sens_NDVI_ante12mo_p15_p85');
+// print('Band names (Abs/rel)', col.first().bandNames());
+// col = col.select('Abs_sens_NDVI_ante12mo_p15_p85');
 
 // Options
-print('Band names (ND)', col_nd.first().bandNames());
-col_nd = col_nd.select('ND_sens_NDVI_ante12mo_p15_p85');
+// print('Band names (ND)', col_nd.first().bandNames());
+// col_nd = col_nd.select('ND_sens_NDVI_ante12mo_p15_p85');
 
 
 var col_mosaic = col
@@ -46,9 +46,9 @@ var Map_right = ui.Map();
 
 // - Antecedent select
 var ante = {
-  '3 month': ['Abs_sens_NDVI_ante3mo_p15_p85'],
-  '12 month': ['Abs_sens_NDVI_ante12mo_p15_p85'],
-  '3 year': ['Abs_sens_NDVI_ante3yr_p15_p85']
+  '3 month': ['Abs_sens_NDVI_ante3mo_p15_p85', 'ND_sens_NDVI_ante3mo_p15_p85'],
+  '12 month': ['Abs_sens_NDVI_ante12mo_p15_p85', 'ND_sens_NDVI_ante12mo_p15_p85'],
+  '3 year': ['Abs_sens_NDVI_ante3yr_p15_p85', 'ND_sens_NDVI_ante3yr_p15_p85']
 };
 
 var select = ui.Select({
@@ -67,7 +67,7 @@ var select = ui.Select({
     var col_map = ui.Map.Layer(blend_col_hillshade, null, key);
     
     // Right
-    var sens_viz_nd = col_nd.select(ante[key][0]).visualize({
+    var sens_viz_nd = col_nd.mosaic().select(ante[key][1]).visualize({
       palette: p,
       min: -0.2,
       max: 0.2
@@ -80,12 +80,31 @@ var select = ui.Select({
 });
 select.setValue('3 year');
 
+// - Panel left
+var panel_left = ui.Panel();
+panel_left.style().set({
+  width: '200px',
+  position: 'top-left'
+});
+
+
 panel_left.add(ui.Label('Select antecedent period:'));
 panel_left.add(select);
 
+Map.add(panel_left);
 
-// Map.addLayer(col_mosaic, {palette: p, min: -0.2, max: 0.2}, 'absolute 12mo', false);
-// Map.addLayer(col_nd, {palette: p, min: -0.2, max: 0.2}, 'norm diff 12mo');
+// - Linker
+var linker = ui.Map.Linker([ui.root.widgets().get(0), Map_right]);
+
+var splitPanel = ui.SplitPanel({
+  firstPanel: linker.get(0),
+  secondPanel: linker.get(1),
+  orientation: 'horizontal',
+  wipe: false,
+  style: {stretch: 'both'}
+});
+
+ui.root.widgets().reset([splitPanel]);
 
 
 
