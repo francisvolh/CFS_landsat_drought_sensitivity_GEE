4a250a4dab9c66219e02f880eeacefd9839fc4c6
robit.alec <robit.alec@gmail.com>
2023-07-29 12:15:11 +0000

    Updated Check-ND-sensitivity.js (added 35 lines, deleted 1 line).

--- Check-ND-sensitivity.js
+++ Check-ND-sensitivity.js
@@ -25,6 +25,40 @@ var col_mosaic = col
     .filterBounds(col_nd.geometry())
     .mosaic();
     
-    
+
+// Side by side maps
+var Map_right = ui.Map();
+
+// - Antecedent select
+var ante = {
+  '3 month': ['Abs_sens_NDVI_ante3mo_p15_p85'],
+  '12 month': ['Abs_sens_NDVI_ante12mo_p15_p85'],
+  '3 year': ['Abs_sens_NDVI_ante3yr_p15_p85']
+};
+
+var select = ui.Select({
+  items: Object.keys(ante),
+  onChange: function(key) {
+    Map.layers().reset();
+    var sens_viz = col_mosaic.select(ante[key][0]).visualize({
+      palette: p,
+      min: -0.2,
+      max: 0.2
+    });
+    var blend_col_hillshade = blend.multiply(sens_viz, hillshade_viz);
+    var col_map = ui.Map.Layer(blend_col_hillshade, null, key);
+    Map.add(col_map);
+  }
+});
+select.setValue('3 year');
+
+panel_left.add(ui.Label('1. Antecedent period:'));
+panel_left.add(select);
+
+
 Map.addLayer(col_mosaic, {palette: p, min: -0.2, max: 0.2}, 'absolute 12mo', false);
 Map.addLayer(col_nd, {palette: p, min: -0.2, max: 0.2}, 'norm diff 12mo');
+
+
+
+
