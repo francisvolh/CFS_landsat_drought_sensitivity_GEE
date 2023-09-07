6c30930ec93bc3931a3aaa579c05e90b2013ffdb
robit.alec <robit.alec@gmail.com>
2023-09-07 14:23:37 +0000

    Updated tests/test_vegetation.js (added 8 lines).

--- tests/test_vegetation.js
+++ tests/test_vegetation.js
@@ -32,8 +32,6 @@ var forest_age = vegetation.forest_age();
 print('Forest age', forest_age);
 Map.addLayer(forest_age, {min: 0, max: 151}, 'forest_age');
 Map.addLayer(forest_age.eq(255), null, 'forest_age == 255');
-print(forest_age.reduceRegion({reducer: ee.Reducer.max(), bestEffort: true}))
-print(forest_age.reduceRegion({reducer: ee.Reducer.min(), bestEffort: true}))
 
 
 // Test sampling_collection();
