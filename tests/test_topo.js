6bf2933ad074fc6c7906718fb201faf34e6a7f9d
robit.alec <robit.alec@gmail.com>
2023-09-09 00:10:05 +0000

    Updated tests/test_topo.js (added 8 lines, deleted 8 lines).

--- tests/test_topo.js
+++ tests/test_topo.js
@@ -62,14 +62,14 @@ var terrain = topo.tagee_terrain(geometry, radius);
 print('Terrain TAGEE', terrain);
 
 var zoom = 4;
-Map.addLayer(topo.tagee_viz(terrain, 'Elevation', zoom, geometry), null, 'Elevation');
-Map.addLayer(topo.tagee_viz(terrain, 'Slope', zoom, geometry), null, 'Slope');
-Map.addLayer(topo.tagee_viz(terrain, 'Aspect', zoom, geometry), null, 'Aspect');
-Map.addLayer(topo.tagee_viz(terrain, 'Northness', zoom, geometry), null, 'Northness');
-Map.addLayer(topo.tagee_viz(terrain, 'Eastness', zoom, geometry), null, 'Eastness');
-Map.addLayer(topo.tagee_viz(terrain, 'MinimalCurvature', zoom, geometry), null, 'MinimalCurvature');
-Map.addLayer(topo.tagee_viz(terrain, 'MaximalCurvature', zoom, geometry), null, 'MaximalCurvature');
-Map.addLayer(topo.tagee_viz(terrain, 'ShapeIndex', zoom, geometry), null, 'ShapeIndex');
+Map.addLayer(topo.tagee_viz(terrain, 'Elevation_radius_' + radius, zoom, geometry), null, 'Elevation_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'Slope_radius_' + radius, zoom, geometry), null, 'Slope_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'Aspect_radius_' + radius, zoom, geometry), null, 'Aspect_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'Northness_radius_' + radius, zoom, geometry), null, 'Northness_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'Eastness_radius_' + radius, zoom, geometry), null, 'Eastness_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'MinimalCurvature_radius_' + radius, zoom, geometry), null, 'MinimalCurvature_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'MaximalCurvature_radius_' + radius, zoom, geometry), null, 'MaximalCurvature_radius_' + radius);
+Map.addLayer(topo.tagee_viz(terrain, 'ShapeIndex_radius_' + radius, zoom, geometry), null, 'ShapeIndex_radius_' + radius);
 
 
 // Test sampling_collection
