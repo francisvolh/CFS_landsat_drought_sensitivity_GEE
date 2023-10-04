fa6349da2e222615e4be8021e9a7cbacbe22b558
robit.alec <robit.alec@gmail.com>
2023-10-04 18:59:27 +0000

    Updated tests/test_topo.js (deleted 17 lines).

--- tests/test_topo.js
+++ tests/test_topo.js
@@ -57,23 +57,6 @@ Map.addLayer(topo_diversity, null, 'topo_diversity');
 
 
 
-// Test TAGEE
-var radius = 3;
-var terrain = topo.tagee_terrain(geometry, radius);
-print('Terrain TAGEE', terrain);
-
-var zoom = 4;
-Map.addLayer(topo.tagee_viz(terrain, 'Elevation_radius_' + radius, zoom, geometry), null, 'Elevation_radius_' + radius, true);
-Map.addLayer(topo.tagee_viz(terrain, 'Slope_radius_' + radius, zoom, geometry), null, 'Slope_radius_' + radius, false);
-Map.addLayer(topo.tagee_viz(terrain, 'Aspect_radius_' + radius, zoom, geometry), null, 'Aspect_radius_' + radius, false);
-Map.addLayer(topo.tagee_viz(terrain, 'Northness_radius_' + radius, zoom, geometry), null, 'Northness_radius_' + radius, false);
-Map.addLayer(topo.tagee_viz(terrain, 'Eastness_radius_' + radius, zoom, geometry), null, 'Eastness_radius_' + radius, false);
-Map.addLayer(topo.tagee_viz(terrain, 'MinimalCurvature_radius_' + radius, zoom, geometry), null, 'MinimalCurvature_radius_' + radius, false);
-Map.addLayer(topo.tagee_viz(terrain, 'MaximalCurvature_radius_' + radius, zoom, geometry), null, 'MaximalCurvature_radius_' + radius, false);
-Map.addLayer(topo.tagee_viz(terrain, 'ShapeIndex_radius_' + radius, zoom, geometry), null, 'ShapeIndex_radius_' + radius, false);
-
-
-
 // Test Geomorpho90
 var geomorpho = topo.geomorpho;
 print('Geomorpho90', geomorpho);
