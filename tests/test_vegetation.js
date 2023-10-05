a8987bba1c8ad27aaf3c9429cd1cd50ff58e12b4
robit.alec <robit.alec@gmail.com>
2023-10-05 20:02:32 +0000

    Updated tests/test_vegetation.js (added 1 line, deleted 3 lines).

--- tests/test_vegetation.js
+++ tests/test_vegetation.js
@@ -31,9 +31,7 @@ Map.addLayer(forest_carbon, {min: 0, max: 12}, 'forest carbon');
 var forest_age = vegetation.forest_age();
 print('Forest age', forest_age);
 Map.addLayer(forest_age, {min: 0, max: 151}, 'forest_age');
-Map.addLayer(forest_age.eq(255), null, 'forest_age == 255');
-// print(forest_age.reduceRegion({reducer: ee.Reducer.max(), bestEffort: true}))
-// print(forest_age.reduceRegion({reducer: ee.Reducer.min(), bestEffort: true}))
+
 
 
 // Test sampling_collection();
