aa9000e20c755ead8b53c5d4dd861ebd81633675
robit.alec <robit.alec@gmail.com>
2023-10-05 13:46:25 +0000

    Updated modules/fire.js (added 1 line, deleted 1 line).

--- modules/fire.js
+++ modules/fire.js
@@ -62,6 +62,6 @@ var sum_burned_buffer = function(focal_dist) {
   return focal_prop_fires.reduce(ee.Reducer.sum())
                    .rename('sum_burned_' + focal_dist + '_m')
                    .divide(year_list.length())
-                   .updateMask(NBAC_fires.bounds());
+                   .updateMask(vars.canada);
 };
 exports.sum_burned_buffer = sum_burned_buffer;
