e597c3fdaff0f08ac714193eaf289708df9c5b2b
robit.alec <robit.alec@gmail.com>
2023-10-05 13:54:39 +0000

    Updated tests/test_fire.js (added 2 lines, deleted 2 lines).

--- tests/test_fire.js
+++ tests/test_fire.js
@@ -27,14 +27,14 @@ var fire = require('users/robitalec/CFS:modules/fire.js');
 // Test five_year_fires
 // Usage: fire.five_year_fires(year)
 var five_year_fires = fire.five_year_fires(2020);
-print(five_year_fires);
+print('Five year fires', five_year_fires);
 Map.addLayer(five_year_fires, {min:0, max:1, palette: ['000000', 'ffc781']}, 'five_year_fires');
 
 // Test mask_five_year_fires
 // Usage: fire.mask_five_year_fires(img)
 var img = ee.Image.random().clip(geometry).set('year', 2020);
 var mask_five_year_fires = fire.mask_five_year_fires(img);
-print(mask_five_year_fires);
+print('Mask five year fires', mask_five_year_fires);
 Map.addLayer(mask_five_year_fires, null, 'mask_five_year_fires');
 
 
