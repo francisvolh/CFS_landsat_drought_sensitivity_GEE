f288a595c1a53fe2fddede7b1f107c293b1e3222
robit.alec <robit.alec@gmail.com>
2023-10-06 18:18:30 +0000

    Updated modules/export_points.js (added 1 line, deleted 1 line).

--- modules/export_points.js
+++ modules/export_points.js
@@ -80,7 +80,7 @@ exports.export_soil = export_soil;
 
 
 var export_topo = function(points, drive_name, drive_folder, type) {
-  var col = topo.sampling_collection();
+  var col = topo.sampling_collection;
   export_to_drive(col, points, 30, drive_name, drive_folder, type);
 };
 exports.export_topo = export_topo;
