0500d4347939b8a5289b7a3785cf548742639dbe
robit.alec <robit.alec@gmail.com>
2023-11-08 18:37:33 +0000

    Updated modules/land_cover.js (added 1 line, deleted 2 lines).

--- modules/land_cover.js
+++ modules/land_cover.js
@@ -106,7 +106,6 @@ exports.mask_land_cover = mask_land_cover;
 
 // Land cover mode
 var lc = hermosilla_1984_2019
-	.map(utils.set_year);
-  .mode()
+	.mode()
 	.rename('mode_land_cover');
 exports.mode_land_cover = mode_land_cover;
