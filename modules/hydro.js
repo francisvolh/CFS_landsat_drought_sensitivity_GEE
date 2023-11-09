94dc17be0eb03f26cd453b98f25a1a3616737118
robit.alec <robit.alec@gmail.com>
2023-11-09 20:38:09 +0000

    Updated modules/hydro.js (added 2 lines, deleted 2 lines).

--- modules/hydro.js
+++ modules/hydro.js
@@ -83,7 +83,7 @@ exports.distance_major_lakes = distance_major_lakes;
 
 // Get sampling collection
 var sampling_collection = ee.Image([
-  distance_water(),
-  distance_major_lakes(50)
+  proportion_permanent_snow(),
+  proportion_water()
 ]);
 exports.sampling_collection = sampling_collection;
