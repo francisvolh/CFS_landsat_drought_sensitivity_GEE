a324eecc0149c6c45357bf32bd1cecc28bd11a56
robit.alec <robit.alec@gmail.com>
2023-11-09 14:46:10 +0000

    Updated Export-points-drive.js (added 12 lines, deleted 45 lines).

--- Export-points-drive.js
+++ Export-points-drive.js
@@ -4,8 +4,6 @@ Based on: tests/test_export_points.js
 Alec L. Robitaille
 */
 
-
-
 // Modules --------------------------------------------------------------------
 // Load modules
 var export_points = require('users/robitalec/CFS:modules/export_points.js');
@@ -23,13 +21,17 @@ var non_arctic_ecoregions = eco.non_arctic_ecoregions;
 // Sensitivity 
 var sens = ee.ImageCollection('users/robitalec/CFS/2023-09-26/2023-09-26_image_col');
 
+// Sample type
+var type = 'reduceRegions';
+
+
 
 // Points ---------------------------------------------------------------------
 // Generate points
 var factor = 0.0001;
 var factor_char = '0pt01';
 
-points.export_points_by_img_col_asset(sens, factor, factor_char);
+// points.export_points_by_img_col_asset(sens, factor, factor_char);
 
 // Asset
 var points = ee.FeatureCollection('users/robitalec/CFS/2023-10-06_sampling_points_tiles_0pt01');
@@ -38,45 +40,10 @@ var points_name = 'tiles_' + factor_char;
 
 
 // Sample ---------------------------------------------------------------------
-// Split points
-points = points.randomColumn();
-var points_first = points.filter(ee.Filter.lte('random', 0.25));
-var points_second = points.filter(ee.Filter.and(ee.Filter.gt('random', 0.25), ee.Filter.lte('random', 0.5)));
-var points_third = points.filter(ee.Filter.and(ee.Filter.gt('random', 0.5), ee.Filter.lte('random', 0.75)));
-var points_fourth = points.filter(ee.Filter.gt('random', 0.75));
+var covariates = export_points.covariates;
+var res_dict = export_points.res_dict;
 
+print(covariates);
+print(res_dict);
 
-// Soil
-export_points.export_soil(points, 'sample-soil-' + points_name, drive_folder, 'reduceRegions');
-
-// Vegetation, sum proportion burned
-export_points.export_vegetation(points_first, 'sample-vegetation-' + points_name + '-1', drive_folder, 'sample');
-export_points.export_vegetation(points_second, 'sample-vegetation-' + points_name + '-2', drive_folder, 'sample');
-export_points.export_vegetation(points_third, 'sample-vegetation-' + points_name + '-3', drive_folder, 'sample');
-export_points.export_vegetation(points_fourth, 'sample-vegetation-' + points_name + '-4', drive_folder, 'sample');
-
-
-// Hydro
-export_points.export_hydro(points, 'sample-hydro-' + points_name, drive_folder, 'reduceRegions');
-
-// Topo
-export_points.export_topo(points_first, 'sample-topo-' + points_name + '-1', drive_folder, 'sample');
-export_points.export_topo(points_second, 'sample-topo-' + points_name + '-2', drive_folder, 'sample');
-export_points.export_topo(points_third, 'sample-topo-' + points_name + '-3', drive_folder, 'sample');
-export_points.export_topo(points_fourth, 'sample-topo-' + points_name + '-4', drive_folder, 'sample');
-
-
-
-// Climate
-export_points.export_climate(points, 'sample-climate-' + points_name, drive_folder, 'reduceRegions');
-
-// Lc, ecoreg, lon lat
-export_points.export_lc_and_ecoreg(points_first, 'sample-lc_and_ecoreg-' + points_name + '-1', drive_folder, 'sample');
-export_points.export_lc_and_ecoreg(points_second, 'sample-lc_and_ecoreg-' + points_name + '-2', drive_folder, 'sample');
-export_points.export_lc_and_ecoreg(points_third, 'sample-lc_and_ecoreg-' + points_name + '-3', drive_folder, 'sample');
-export_points.export_lc_and_ecoreg(points_fourth, 'sample-lc_and_ecoreg-' + points_name + '-4', drive_folder, 'sample');
-
-
-// Sensitivity
-sens = sens.mosaic();
-export_points.export_to_drive(sens, points, 30, 'sample-sensitivity-' + points_name, drive_folder, 'reduceRegions');
+export_points.sample_covariates(points, covariates, res_dict, drive_folder, type);
