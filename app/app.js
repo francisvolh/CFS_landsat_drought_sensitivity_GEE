4d9252bd5384c2b0d41b38d50beeb19f54711b9c
robit.alec <robit.alec@gmail.com>
2024-01-11 16:14:38 +0000

    Updated app/app.js (deleted 1 line).

--- app/app.js
+++ app/app.js
@@ -158,7 +158,6 @@ var blend = require('users/jja/public:blend.js');
 var palettes = require('users/gena/packages:palettes');
 
 
-
 // Data
 var col = ee.ImageCollection('users/robitalec/CFS/2023-09-26/2023-09-26_image_col');
 var lc = land_cover.land_cover();
