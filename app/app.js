d47e442a0b3f91a17275cdedebd7fdfab683977e
robit.alec <robit.alec@gmail.com>
2024-01-11 16:27:45 +0000

    Updated app/app.js (added 2 lines, deleted 1 line).

--- app/app.js
+++ app/app.js
@@ -299,7 +299,8 @@ var bandSelect = ui.Select({
 var imageSelect = ui.Select({
   items: [
     {label: 'hydro', value: hydro.sampling_collection},
-    {label: 'vegetation', value: vegetation.sampling_collection.slice(1,2)},
+    // Temporary fix: slice off prop burned since 500 m breaking when zoomed out
+    {label: 'vegetation', value: vegetation.sampling_collection.slice(1, 3)},
     {label: 'soil', value: soil.sampling_collection},
     {label: 'topo', value: topo.sampling_collection},
     {label: 'climate', value: climate.sampling_collection},
