cc1fa3a7357e53c8f4a471aad160e956afcd86d9
robit.alec <robit.alec@gmail.com>
2024-02-15 21:00:18 +0000

    Created app/climate-source-app.js.

---
+++ app/climate-source-app.js
@@ -0,0 +1,371 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Polygon(
+        [[[-142.55499390153335, 70.03530462152891],
+          [-141.28886260033715, 59.73133485522378],
+          [-132.97491577653335, 51.53555252654365],
+          [-124.37867426910192, 48.11740729432402],
+          [-109.40639935881254, 48.804462143547084],
+          [-94.30304077653334, 49.12365840683438],
+          [-82.70147827653334, 42.195333863545955],
+          [-71.01202515153334, 45.11944811934683],
+          [-66.96905640153334, 45.05739605590757],
+          [-65.91436890153334, 43.16449754575042],
+          [-52.203431401533344, 46.224859987659656],
+          [-52.180299631222184, 49.641565048375476],
+          [-57.209200883265744, 55.24608626628804],
+          [-62.70636108903334, 59.220495521919005],
+          [-78.92218140153334, 62.49163190377348],
+          [-103.97100952653334, 67.692445654623],
+          [-131.21710327653335, 69.08395110419174]]]),
+    sample = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.MultiPoint(
+        [[-138.94296914469342, 67.55466143505897],
+         [-139.29453164469342, 66.13820093330358],
+         [-139.29453164469342, 64.25880768203831],
+         [-138.85507851969342, 62.930125618113465],
+         [-138.59140664469342, 61.66369540305913],
+         [-132.96640664469342, 59.99337136638028],
+         [-136.74570351969342, 60.903613692404406],
+         [-129.89023476969342, 57.53338989878992],
+         [-128.48398476969342, 55.29774589829106],
+         [-127.95664101969342, 53.8718208448312],
+         [-127.16562539469342, 52.28817462855293],
+         [-120.13437539469342, 50.53422666116181],
+         [-118.02500039469342, 50.478328788587326],
+         [-116.35507851969342, 53.08728094503504],
+         [-122.41953164469342, 52.609572509824616],
+         [-122.94687539469342, 55.397694099646934],
+         [-120.04648476969342, 57.5805397678959],
+         [-124.96835976969342, 58.04869045389295],
+         [-125.31992226969342, 60.16872128967182],
+         [-130.94492226969342, 62.56793343556955],
+         [-130.41757851969342, 65.52651985520313],
+         [-134.19687539469342, 66.49126205027372],
+         [-129.27500039469342, 63.406171711804625],
+         [-134.19687539469342, 63.56312198944755],
+         [-120.04648476969342, 63.01001038988345],
+         [-121.80429726969342, 60.903613692404406],
+         [-112.48789101969342, 58.418849966641744],
+         [-112.31210976969342, 56.43199582665576],
+         [-103.96250039469342, 54.13011784835057],
+         [-101.58945351969342, 57.908883081310194],
+         [-105.54453164469342, 61.99563308818851],
+         [-111.43320351969342, 63.64127442105126],
+         [-115.30039101969342, 64.41107231158648],
+         [-118.55234414469342, 59.55090221548145],
+         [-96.66757851969342, 58.69394193503796],
+         [-98.60117226969342, 54.89542569102629],
+         [-95.17343789469342, 51.529100875687824],
+         [-90.60312539469342, 49.859078783121845],
+         [-88.05429726969342, 51.63832609373806],
+         [-89.28476601969342, 54.43796213447706],
+         [-83.48398476969342, 53.245350488163574],
+         [-84.27500039469342, 51.36477046622595],
+         [-83.13242226969342, 48.246464514153956],
+         [-82.25351601969342, 47.776089491405536],
+         [-79.00156289469342, 46.76227735574356],
+         [-77.94687539469342, 45.11212940025661],
+         [-75.31015664469342, 47.3609891612731],
+         [-71.17929726969342, 48.363388181004666],
+         [-69.06992226969342, 53.455205108573196],
+         [-75.48593789469342, 55.34775152963096],
+         [-73.99179726969342, 57.48617896758462],
+         [-71.09140664469342, 57.5805397678959],
+         [-66.43320351969342, 56.33467745608679],
+         [-63.26914101969342, 54.64191542734709],
+         [-63.18125039469342, 52.66291096798763],
+         [-62.74179726969342, 51.80167192389264],
+         [-59.66562539469342, 52.23438015716917],
+         [-57.29257851969342, 52.609572509824616],
+         [-62.34628945719342, 54.996385508991516],
+         [-61.55527383219342, 52.6362498677264],
+         [-56.23789101969342, 48.217191740554235],
+         [-53.24960976969342, 47.182081526244964],
+         [-64.03685218453808, 49.834878780720636],
+         [-63.487535778288084, 49.650289084976855],
+         [-63.135973278288084, 49.450714335701775],
+         [-62.630602184538084, 49.29333331229171],
+         [-63.970934215788084, 46.51020386889133],
+         [-63.553453747038084, 46.32843522993295],
+         [-63.135973278288084, 46.25251943904355],
+         [-62.740465465788084, 46.16128158613142],
+         [-60.631090465788084, 46.64613274339763],
+         [-61.180406872038084, 45.871361817803134],
+         [-61.861559215788084, 45.271500028147436],
+         [-63.333727184538084, 45.0080165475607],
+         [-65.28929359078808, 44.068343019805994],
+         [-64.45433265328808, 44.63396699529122],
+         [-66.47581702828808, 45.901951270678126],
+         [-65.35521155953808, 45.978351209605634],
+         [-66.03636390328808, 46.72150156027832],
+         [-67.13499671578808, 47.14162444798107],
+         [-67.83812171578808, 47.78040112679957],
+         [-67.17894202828808, 48.206825226057795],
+         [-64.91575843453808, 48.57160592081368],
+         [-66.05833655953808, 48.876002372034606],
+         [-66.12425452828808, 48.41142455294612],
+         [-65.92650062203808, 51.05218248897484],
+         [-68.29954749703808, 50.60807794702426],
+         [-76.31956702828808, 52.292099125162444],
+         [-77.00071937203808, 50.17381625728706],
+         [-71.57347327828808, 50.17381625728706],
+         [-73.90257484078808, 52.7959153071131],
+         [-74.69359046578808, 51.73362403462191],
+         [-76.09984046578808, 50.11331739739308],
+         [-75.30882484078808, 49.432193488917385],
+         [-70.14525062203808, 46.89750889114133],
+         [-70.60667640328808, 46.27839838285141],
+         [-71.66136390328808, 45.590742505508146],
+         [-71.77122718453808, 47.27154648267511],
+         [-73.77073890328808, 46.596387164952034],
+         [-75.35277015328808, 46.18720355839455],
+         [-78.27513343453808, 46.912520826467215],
+         [-78.64866859078808, 49.00162290972391],
+         [-80.12083655953808, 49.88733948469614],
+         [-79.41771155953808, 48.09986971563369],
+         [-77.50609046578808, 48.041139835206096],
+         [-80.97777015328808, 49.38930516875332],
+         [-82.36204749703808, 50.895891472542445],
+         [-82.03245765328808, 51.44695038807363],
+         [-118.97187083856278, 60.46280833740384],
+         [-121.60858958856278, 59.806337471912194]]),
+    center = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.Geometry.Point([-104.75810546875, 58.185009892379]);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
+/*
+App
+Alec L. Robitaille
+ 
+*/
+
+// Modules
+var land_cover = require('users/robitalec/CFS:modules/land_cover.js');
+var hydro = require('users/robitalec/CFS:modules/hydro.js');
+var eco = require('users/robitalec/CFS:modules/ecoregions.js');
+var vegetation = require('users/robitalec/CFS:modules/vegetation.js');
+var soil = require('users/robitalec/CFS:modules/soil.js');
+var topo = require('users/robitalec/CFS:modules/topo.js');
+var climate = require('users/robitalec/CFS:modules/climate.js');
+
+var blend = require('users/jja/public:blend.js');
+var palettes = require('users/gena/packages:palettes');
+
+
+// Data
+var col = ee.ImageCollection('users/robitalec/CFS/2023-09-26/2023-09-26_image_col');
+var lc = land_cover.land_cover();
+var dem = ee.Image("MERIT/DEM/v1_0_3");
+
+
+
+// Palettes
+var p = palettes.crameri.vik[10];
+var lc_p = palettes.crameri.bamako[25];
+var p_not_grey = palettes.crameri.imola[25];
+
+
+
+// Process
+var lc_filter = lc.filter(ee.Filter.eq('year', 2000)).first();
+
+var hillshade = ee.Terrain.hillshade(dem);
+
+var col_mosaic = col
+    .mosaic();
+
+
+
+// Visualize
+var hillshade_viz = hillshade.visualize({
+    min:0,
+    max:250,
+    palette: ['#000000', '#ffffff'],
+    forceRgbOutput:true
+  });
+
+
+
+// Map
+Map.setCenter(-104.76, 58.18, 3);
+Map.setOptions('SATELLITE');
+
+
+
+// UI
+// Adapted from EE docs
+// - Map right
+var Map_right = ui.Map();
+
+// - Antecedent select
+var ante = {
+  '3 month': ['ND_sens_NDVI_ante3mo_p15_p85'],
+  '12 month': ['ND_sens_NDVI_ante12mo_p15_p85'],
+  '3 year': ['ND_sens_NDVI_ante3yr_p15_p85']
+};
+
+var select = ui.Select({
+  items: Object.keys(ante),
+  onChange: function(key) {
+    Map.layers().reset();
+    var sens_viz = col_mosaic.select(ante[key][0]).visualize({
+      palette: p,
+      min: -0.2,
+      max: 0.2
+    });
+    var blend_col_hillshade = blend.multiply(sens_viz, hillshade_viz);
+    var col_map = ui.Map.Layer(blend_col_hillshade, null, key);
+    Map.add(col_map);
+  }
+});
+select.setValue('3 year');
+
+// - Panel right
+var panel_right = ui.Panel();
+panel_right.style().set({
+  width: '200px',
+  position: 'top-right'
+});
+
+var panel_right_bottom = ui.Panel();
+panel_right_bottom.style().set({
+  width: '200px',
+  position: 'bottom-right'
+});
+
+var panel_right_left_bottom = ui.Panel();
+panel_right_left_bottom.style().set({
+  width: '400px',
+  position: 'bottom-left'
+});
+
+panel_right.add(ui.Label('2. Add covariate layers:'));
+
+
+// - Band select
+// Zoom level-scale adapted from TAGEE make viz
+var level_scale = ee.List([
+  157000,
+  78000,
+  39000,
+  20000,
+  10000,
+  5000,
+  2000,
+  1000,
+  611,
+  306,
+  153,
+  76,
+  38,
+  19,
+  10,
+  5
+]);
+
+var bandSelect = ui.Select({
+  placeholder: 'Select a covariate...',
+  onChange: function(value) {
+    var img = imageSelect.getValue().select(value);
+    var stats = img.reduceRegion({
+      reducer: ee.Reducer.percentile([5, 95]),
+      geometry : ee.Geometry.Rectangle(Map_right.getBounds()),
+      scale: level_scale.get(Map_right.getZoom()),
+      bestEffort: true
+    });
+    Map_right.layers().reset();
+    panel_right_bottom.clear();
+    stats.evaluate(function(x) {
+      Map_right.addLayer(img, {min: x[value + '_p5'], max: x[value + '_p95'], palette: p_not_grey}, value);
+
+      var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
+        .clip(ee.Geometry.Rectangle({ coords: [[0, 0], [100, 7]], geodesic: false }))
+        .visualize({min: 0, max: 100, palette: p_not_grey}));
+      panel_right_bottom.add(ui.Label(value));
+      panel_right_bottom.add(ui.Label((x[value + '_p5']).toFixed(1) + ' ____________ ' + (x[value + '_p95']).toFixed(1)));
+      panel_right_bottom.add(img_thumb);
+    });
+  }
+});
+
+// - Image select
+var imageSelect = ui.Select({
+  items: [
+    {label: 'hydro', value: hydro.sampling_collection},
+    // Temporary fix: slice off prop burned since 500 m breaking when zoomed out
+    {label: 'vegetation', value: vegetation.sampling_collection.select(0,1)},
+    {label: 'soil', value: soil.sampling_collection},
+    {label: 'topo', value: topo.sampling_collection},
+    {label: 'climate', value: climate.sampling_collection},
+    {label: 'land cover (2000)', value: lc_filter}
+  ],
+  placeholder: 'Select a list of covariates...',
+  onChange: function(value) {
+    value.bandNames().evaluate(function(bands) {
+      bandSelect.items().reset(bands);
+      bandSelect.setValue(bandSelect.items().get(0));
+    });
+  }
+});
+
+// - Panel left
+var panel_left = ui.Panel();
+panel_left.style().set({
+  width: '200px',
+  position: 'top-left'
+});
+
+var panel_left_bottom = ui.Panel();
+panel_left_bottom.style().set({
+  width: '250px',
+  position: 'bottom-left'
+});
+
+
+
+
+
+panel_left.add(ui.Label('1. Antecedent period:'));
+panel_left.add(select);
+// Adapted from palettes.showPalette to fit into panel
+var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
+  .clip(ee.Geometry.Rectangle({ coords: [[0, 0], [100, 7]], geodesic: false }))
+  .visualize({min: 0, max: 100, palette: p}));
+panel_left_bottom.add(ui.Label('Drought sensitivity ='));
+panel_left_bottom.add(ui.Label('(NDVI [baseline] - NDVI [drought]) / '));
+panel_left_bottom.add(ui.Label('(NDVI [baseline] + NDVI [drought])'));
+panel_left_bottom.add(ui.Label('-0.2 ____________ 0 ____________ 0.2'));
+panel_left_bottom.add(img_thumb);
+Map.add(panel_left);
+Map.add(panel_left_bottom);
+
+
+panel_right.add(imageSelect);
+panel_right.add(bandSelect);
+panel_right.add(ui.Label('Covariate descriptions', null, 'https://docs.google.com/spreadsheets/d/1kPFTotCdNekGtq771qFAhu1y-mLa4cBonkm99f6civA/edit#gid=1308651562'));
+panel_right_left_bottom.add(ui.Label('Drought sensitivity refugia'));
+panel_right_left_bottom.add(ui.Label('Diana Stralberg, Alec L. Robitaille, Guillermo Castilla, Jennifer Cartwright, Mike Michaelian, and Ted Hogg'));
+panel_right_left_bottom.add(ui.Label('Canadian Forest Service / Natural Resources Canada / Government of Canada'));
+Map_right.add(panel_right);
+Map_right.add(panel_right_bottom);
+Map_right.add(panel_right_left_bottom);
+
+
+// - Linker
+var linker = ui.Map.Linker([ui.root.widgets().get(0), Map_right]);
+
+var splitPanel = ui.SplitPanel({
+  firstPanel: linker.get(0),
+  secondPanel: linker.get(1),
+  orientation: 'horizontal',
+  wipe: false,
+  style: {stretch: 'both'}
+});
+
+ui.root.widgets().reset([splitPanel]);
