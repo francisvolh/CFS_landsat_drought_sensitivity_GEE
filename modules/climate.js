fda256ef935dff3012465838ec7631509d705137
robit.alec <robit.alec@gmail.com>
2024-02-15 20:54:50 +0000

    Updated modules/climate.js (added 2 lines, deleted 2 lines).

--- modules/climate.js
+++ modules/climate.js
@@ -77,8 +77,8 @@ var monthly_era5 = function(year_list, month_list) {
 
 	return utils.aggregate_month_year(era5, year_list, month_list, reducer)
 							.select(['minimum_2m_air_temperature_mean', 
-							         'maximum_2m_air_temperature_mean', 
-                       'total_precipitation_sum'], 
+                      'maximum_2m_air_temperature_mean',
+                      'total_precipitation_sum'], 
                       ['tmin', 'tmax', 'prcp']);
 };
 exports.monthly_era5 = monthly_era5;
