d4dac5e241a16066f0a43ad4a21a941d6c8514df
robit.alec <robit.alec@gmail.com>
2024-02-16 01:01:03 +0000

    Updated app/climate-source-app.js (added 6 lines).

--- app/climate-source-app.js
+++ app/climate-source-app.js
@@ -176,6 +176,9 @@ Map.setCenter(-104.76, 58.18, 3);
 Map.setOptions('SATELLITE');
 var Map_right = ui.Map();
 
+var water_land = ui.Map.Layer(ee.Image('MODIS/MOD44W/MOD44W_005_2000_02_24')
+  .select('water_mask'), {min: 0, max: 1, palette: ['fff', '000'], opacity: 0.2});
+
 
 
 // Band dict
@@ -209,6 +212,8 @@ var era5_select = ui.Select({
     };
     var era5_map = ui.Map.Layer(era5.select(era5_dict[key][0]), era5_viz, key);
     Map.add(era5_map);
+    Map.add(water_land);
+
   }
 });
 
@@ -223,6 +228,7 @@ var daymet_select = ui.Select({
     };
     var daymet_map = ui.Map.Layer(daymet.select(daymet_dict[key][0]), daymet_viz, key);
     Map_right.add(daymet_map);
+    Map_right.add(water_land);
   }
 });
 
