ebe7f1a59112509f1ac00fc36b1830d9c68bda01
robit.alec <robit.alec@gmail.com>
2024-02-16 00:37:28 +0000

    Updated app/climate-source-app.js (added 2 lines, deleted 2 lines).

--- app/climate-source-app.js
+++ app/climate-source-app.js
@@ -182,8 +182,8 @@ var Map_right = ui.Map();
 var k_to_c = 273.15;
 var min_prcp = 0;
 var max_prcp = 500;
-var min_t = 250;
-var max_t = 300;
+var min_t = -20;
+var max_t = 25;
 var era5_dict = {
   'Monthly precipitation sum': ['prcp', min_prcp, max_prcp / 1000],
   'Monthly tmin mean': ['tmin', min_t + k_to_c, max_t + k_to_c],
