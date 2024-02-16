1a0b2bc2890f9d3cc1812f39c2f5bf08147e1103
robit.alec <robit.alec@gmail.com>
2024-02-16 01:37:48 +0000

    Updated modules/climate.js (added 2 lines, deleted 2 lines).

--- modules/climate.js
+++ modules/climate.js
@@ -80,8 +80,8 @@ var monthly_era5 = function(year_list, month_list) {
 
   return agg_mon_yr.map(function(img) {
     return ee.Image([
-      img.select('minimum_2m_air_temperature_mean').add(273.15).rename('tmin'),
-      img.select('maximum_2m_air_temperature_mean').add(273.15).rename('tmax'),
+      img.select('minimum_2m_air_temperature_mean').subtract(273.15).rename('tmin'),
+      img.select('maximum_2m_air_temperature_mean').subtract(273.15).rename('tmax'),
       img.select('total_precipitation_sum').divide(1000).rename('prcp')
     ]);
   });
