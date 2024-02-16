b88abeafcd5116f9cb8839f3a459ceeb563d9a93
robit.alec <robit.alec@gmail.com>
2024-02-16 01:06:51 +0000

    Updated app/climate-source-app.js (added 4 lines, deleted 4 lines).

--- app/climate-source-app.js
+++ app/climate-source-app.js
@@ -173,14 +173,14 @@ var p = palettes.crameri.imola[25];
 
 // Map
 Map.setCenter(-104.76, 58.18, 3);
-Map.setOptions('SATELLITE');
 var Map_right = ui.Map();
 
 var water_land = ee.Image('MODIS/MOD44W/MOD44W_005_2000_02_24')
   .select('water_mask');
 water_land = water_land
   .mask(water_land.eq(1));
-var water_land_viz = ui.Map.Layer(water_land);
+var water_land_viz_right = ui.Map.Layer(water_land);
+var water_land_viz_left = ui.Map.Layer(water_land);
 
 
 
@@ -215,7 +215,7 @@ var era5_select = ui.Select({
     };
     var era5_map = ui.Map.Layer(era5.select(era5_dict[key][0]), era5_viz, key);
     Map.add(era5_map);
-    Map.add(water_land_viz);
+    Map.add(water_land_viz_right);
 
   }
 });
@@ -231,7 +231,7 @@ var daymet_select = ui.Select({
     };
     var daymet_map = ui.Map.Layer(daymet.select(daymet_dict[key][0]), daymet_viz, key);
     Map_right.add(daymet_map);
-    Map_right.add(water_land_viz);
+    Map_right.add(water_land_viz_left);
   }
 });
 
