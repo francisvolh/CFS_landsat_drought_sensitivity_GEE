6725064a014b55dda2c63430364335068aab5734
robit.alec <robit.alec@gmail.com>
2024-03-04 01:14:20 +0000

    Updated app/climate-source-app.js (added 4 lines, deleted 3 lines).

--- app/climate-source-app.js
+++ app/climate-source-app.js
@@ -176,7 +176,7 @@ var generate_button = ui.Button('Sample points', function() {
   var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
   .clip(ee.Geometry.Rectangle({coords: [[daymet_dict[key_daymet][1], 0], [daymet_dict[key_daymet][2], 7]], geodesic: false}))
   .visualize({min: daymet_dict[key_daymet][1], max: daymet_dict[key_daymet][2], palette: daymet_dict[key_daymet][3]}));
-  panel_right_chart.add(ui.Label((daymet_dict[key_daymet][1]) + ' _____________________________ ' + (daymet_dict[key_daymet][2])));
+  panel_right_chart.add(ui.Label((daymet_dict[key_daymet][1]).toFixed(1) + ' ________________________________ ' + (daymet_dict[key_daymet][2]).toFixed(1)));
   panel_right_chart.add(img_thumb);
 
   Map_right.add(ui.Map.Layer(points));
@@ -201,10 +201,11 @@ var generate_button = ui.Button('Sample points', function() {
                  "vAxis": {viewWindow: {min:era5_dict[key_era5][1], max: era5_dict[key_era5][2]}}});
   panel_left_chart.add(chart);
 
+  var h = (era5_dict[key_era5][1] - era5_dict[key_era5][2]) / 10
   var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
-  .clip(ee.Geometry.Rectangle({coords: [[era5_dict[key_era5][1], 0], [era5_dict[key_era5][2], 7]], geodesic: false}))
+  .clip(ee.Geometry.Rectangle({coords: [[era5_dict[key_era5][1], 0], [era5_dict[key_era5][2], h]], geodesic: false}))
   .visualize({min: era5_dict[key_era5][1], max: era5_dict[key_era5][2], palette: era5_dict[key_era5][3]}));
-  panel_left_chart.add(ui.Label((era5_dict[key_era5][1]) + ' _____________________________ ' + (era5_dict[key_era5][2])));
+  panel_left_chart.add(ui.Label(era5_dict[key_era5][1] + ' ________________________________' + era5_dict[key_era5][2]));
   panel_left_chart.add(img_thumb);
   Map_right.add(ui.Map.Layer(points));
 
