7ec379e9b446e566ca0d24013362fa770f3c741b
robit.alec <robit.alec@gmail.com>
2024-03-04 00:58:20 +0000

    Updated app/climate-source-app.js (deleted 1 line).

--- app/climate-source-app.js
+++ app/climate-source-app.js
@@ -176,7 +176,6 @@ var generate_button = ui.Button('Sample points', function() {
   var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
   .clip(ee.Geometry.Rectangle({ coords: [[0, 0], [100, 7]], geodesic: false }))
   .visualize({min: daymet_dict[key_daymet][1], max: daymet_dict[key_daymet][2], palette: daymet_dict[key_daymet][3]}));
-  panel_right_chart.add(ui.Label(value));
   panel_right_chart.add(ui.Label((daymet_dict[key_daymet][1]).toFixed(1) + ' ____________ ' + (daymet_dict[key_daymet][2]).toFixed(1)));
   panel_right_chart.add(img_thumb);
   Map_right.add(ui.Map.Layer(points));
