3b631ec895d0a97dbfcc7f8fac745f0169c8a0aa
robit.alec <robit.alec@gmail.com>
2024-03-04 01:20:16 +0000

    Updated app/climate-source-app.js (added 2 lines, deleted 3 lines).

--- app/climate-source-app.js
+++ app/climate-source-app.js
@@ -173,11 +173,10 @@ var generate_button = ui.Button('Sample points', function() {
                  "vAxis": {viewWindow: {min:daymet_dict[key_daymet][1], max: daymet_dict[key_daymet][2]}}});
   panel_right_chart.add(chart);
 
-  var h = (daymet_dict[key_daymet][1] - daymet_dict[key_daymet][2]) / 1000
   var img_thumb = ui.Thumbnail(ee.Image.pixelLonLat().select(0)
-  .clip(ee.Geometry.Rectangle({coords: [[daymet_dict[key_daymet][1], 0], [daymet_dict[key_daymet][2], h]], geodesic: false}))
+  .clip(ee.Geometry.Rectangle({coords: [[daymet_dict[key_daymet][1], 0], [daymet_dict[key_daymet][2], 10]], geodesic: false}))
   .visualize({min: daymet_dict[key_daymet][1], max: daymet_dict[key_daymet][2], palette: daymet_dict[key_daymet][3]}));
-  panel_right_chart.add(ui.Label(daymet_dict[key_daymet][1] + ' __________________________________ ' + daymet_dict[key_daymet][2]));
+  panel_right_chart.add(ui.Label(daymet_dict[key_daymet][1] + ' __________________________________ ' + (daymet_dict[key_daymet][2]));
   panel_right_chart.add(img_thumb);
 
   Map_right.add(ui.Map.Layer(points));
