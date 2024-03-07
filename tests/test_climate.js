5f159da8f4344f55f6bc52249b3726fa31217158
robit.alec <robit.alec@gmail.com>
2024-03-07 00:51:41 +0000

    Updated tests/test_climate.js (added 4 lines, deleted 4 lines).

--- tests/test_climate.js
+++ tests/test_climate.js
@@ -30,15 +30,15 @@ print('ERA5 (limit 5)', get_era5.limit(5));
 
 // Test monthly_daymet
 // Usage: monthly_daymet(year_list, month_list)
-// var monthly_daymet = climate.monthly_daymet(year_list, month_list);
-// print('Monthly daymet', monthly_daymet);
-// Map.addLayer(monthly_daymet.select('prcp'), {min:0, max:500}, 'monthly_daymet');
+var monthly_daymet = climate.monthly_daymet(year_list, month_list);
+print('Monthly daymet', monthly_daymet);
+Map.addLayer(monthly_daymet.select('prcp'), {min:0, max:500}, 'monthly_daymet');
 
 // Test monthly_era5
 // Usage: monthly_era5(year_list, month_list)
 var monthly_era5 = climate.monthly_era5(year_list, month_list);
 print('Monthly era5', monthly_era5);
-// Map.addLayer(monthly_era5.select('prcp'), {min:0, max:500}, 'monthly_era5');
+Map.addLayer(monthly_era5.select('prcp'), {min:0, max:500}, 'monthly_era5');
 
 
 
