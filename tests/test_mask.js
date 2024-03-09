523aad7da4aa826f7736c89c0167177f4d0a2ceb
robit.alec <robit.alec@gmail.com>
2024-03-09 00:17:25 +0000

    Updated tests/test_mask.js (added 3 lines, deleted 3 lines).

--- tests/test_mask.js
+++ tests/test_mask.js
@@ -63,10 +63,10 @@ Map.addLayer(mask.atemporal_mask.not(), {opacity: 0.3}, 'Atemporal mask', false)
 
 
 // Temporal mask: fire, harvest
-Map.addLayer(fire.five_year_fires(2011), {palette: ['#000000', '#ff5e5e'], opacity: 0.3}, '2011 fire mask');
+Map.addLayer(fire.five_year_fires(2022), {palette: ['#000000', '#ff5e5e'], opacity: 0.3}, '2022 fire mask');
 Map.addLayer(anthro.harvest_year, {palette: ['#ffc0c0','#be0900', '#000000'], min: 1985, max:2020, opacity: 0.8}, 'Harvest year', false);
-Map.addLayer(anthro.harvest_year.eq(2011), {palette: ['#000000', '#5eb5ff'], opacity: 0.3}, '2011 harvest mask');
-Map.addLayer(land_cover.land_cover().filter(ee.Filter.eq('year', 2011)), null, '2011 land cover mask');
+Map.addLayer(anthro.harvest_year.eq(2022), {palette: ['#000000', '#5eb5ff'], opacity: 0.3}, '2022 harvest mask');
+Map.addLayer(land_cover.land_cover().filter(ee.Filter.eq('year', 2022)), null, '2022 land cover mask');
 
 
 // Testing: apply_masks
