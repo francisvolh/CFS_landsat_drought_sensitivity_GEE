5681f167cef3610a7592da790b11141e7d928324
robit.alec <robit.alec@gmail.com>
2024-03-12 16:29:13 +0000

    Created Export-points-asset.js.

---
+++ Export-points-asset.js
@@ -0,0 +1,16 @@
+/*
+Export points asset
+Based on: tests/test_points.js
+Alec L. Robitaille
+*/
+
+// Modules --------------------------------------------------------------------
+var points = require('users/robitalec/CFS:modules/points.js');
+
+
+
+// Sample ---------------------------------------------------------------------
+var col_date = '2024-03-09'
+var factor = 0.01;
+var col = ee.ImageCollection('users/robitalec/CFS/' + col_date + '/' + col_date + '_image_col');
+points.export_points_by_img_col_asset(col, factor);
