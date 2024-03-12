7559ed2696f78f7c1cdd9408241a7e4f04c603b5
robit.alec <robit.alec@gmail.com>
2024-03-12 16:31:12 +0000

    Updated tests/test_points.js (added 3 lines, deleted 2 lines).

--- tests/test_points.js
+++ tests/test_points.js
@@ -32,4 +32,4 @@ points.export_points_asset(n_pts, ecoregions, region_name);
 var factor = 0.01;
 var factor_char = '0pt01';
 var col = ee.ImageCollection('users/robitalec/CFS/2023-02-21/2023-02-21_image_col');
-points.export_points_by_img_col_asset(col, factor);
+points.export_points_by_img_col_asset(col, factor, factor_char);
