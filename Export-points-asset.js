90338648c0c7aa2b9abeb823c4bf05bd5175c709
robit.alec <robit.alec@gmail.com>
2024-03-16 13:08:49 +0000

    Updated Export-points-asset.js (added 5 lines, deleted 1 line).

--- Export-points-asset.js
+++ Export-points-asset.js
@@ -6,6 +6,7 @@ Alec L. Robitaille
 
 // Modules --------------------------------------------------------------------
 var points = require('users/robitalec/CFS:modules/points.js');
+var eco = require('users/robitalec/CFS:modules/ecoregions.js');
 
 
 
@@ -13,5 +14,8 @@ var points = require('users/robitalec/CFS:modules/points.js');
 var col_date = '2024-03-09';
 var factor = 0.01;
 var factor_char = '0pt01';
+var ecoregions = eco.non_arctic_ecoregions;
+
 var col = ee.ImageCollection('users/robitalec/CFS/' + col_date + '/' + col_date + '_image_col');
-points.export_points_by_img_col_asset(col, factor, factor_char);
+
+points.export_points_by_tile_asset(ecoregion, factor, factor_char);
