4646f3bb16f707975b1100ca514998cd22ee780a
robit.alec <robit.alec@gmail.com>
2024-03-27 13:06:43 +0000

    Updated Export-image-drive.js (added 1 line, deleted 1 line).

--- Export-image-drive.js
+++ Export-image-drive.js
@@ -22,7 +22,7 @@ var scale = 1000;
 
 
 // File name
-var export_name = 'ND_sens_p15_85_YT_to_MB';
+var export_name = 'ND_sens_p15_85';
 
 // Export image to drive
 export_img.export_img_drive_greenest(output, export_name, 'Exports', scale, region, min_year, max_year, min_mm_dd, max_mm_dd, ante_list);
