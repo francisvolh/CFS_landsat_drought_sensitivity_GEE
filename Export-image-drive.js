9aa1c669b771ef2fd49b700336efed83da714847
robit.alec <robit.alec@gmail.com>
2024-03-27 13:06:55 +0000

    Updated Export-image-drive.js (added 2 lines, deleted 3 lines).

--- Export-image-drive.js
+++ Export-image-drive.js
@@ -26,6 +26,5 @@ var export_name = 'ND_sens_p15_85';
 
 // Export image to drive
 export_img.export_img_drive_greenest(output, export_name, 'Exports', scale, region);
-var export_img_drive_greenest = function(output, drive_name, drive_folder, scale, region) {
 
 Map.addLayer(region);
