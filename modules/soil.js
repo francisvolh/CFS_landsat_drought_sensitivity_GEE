70e7d854602bcf25d2d66cb3764e658781ae1c70
robit.alec <robit.alec@gmail.com>
2024-04-16 20:54:40 +0000

    Updated modules/soil.js (added 2 lines, deleted 2 lines).

--- modules/soil.js
+++ modules/soil.js
@@ -33,8 +33,8 @@ var soil_percent = function() {
     .multiply(0.1)
     .select(['sand_0-5cm_mean', 'clay_0-5cm_mean', 'silt_0-5cm_mean'],
             ['sand_0_5cm_percent', 'clay_0_5cm_percent', 'silt_0_5cm_percent'])
-    .round()
-    .toInt();
+    // .round()
+    // .toInt();
 };
 exports.soil_percent = soil_percent;
 
