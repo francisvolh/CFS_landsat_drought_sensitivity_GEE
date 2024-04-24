4d70b666a3e73c878ed59f873d2a011c756f02a0
robit.alec <robit.alec@gmail.com>
2024-04-24 21:23:28 +0000

    Updated Export-image-drive.js (added 20 lines, deleted 2 lines).

--- Export-image-drive.js
+++ Export-image-drive.js
@@ -1,3 +1,17 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var geometry = 
+    /* color: #d63000 */
+    /* displayProperties: [
+      {
+        "type": "rectangle"
+      }
+    ] */
+    ee.Geometry.Polygon(
+        [[[-142.30946577233564, 73.72057482330297],
+          [-142.30946577233564, 40.963680927935904],
+          [-49.672747022335656, 40.963680927935904],
+          [-49.672747022335656, 73.72057482330297]]], null, false);
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /*
 Export single image to drive
 Alec L. Robitaille
@@ -10,7 +24,11 @@ var vars = require('users/robitalec/CFS:modules/variables.js');
 
 
 // Set variables
-var region = vars.canada;
+var region = ee.Geometry.Polygon(
+  [[[-142.30946577233564, 73.72057482330297],
+    [-142.30946577233564, 40.963680927935904],
+    [-49.672747022335656, 40.963680927935904],
+    [-49.672747022335656, 73.72057482330297]]]);
 var scale = 1e4;
 var col = ee.ImageCollection('users/robitalec/CFS/2024-03-09/2024-03-09_image_col');
 
