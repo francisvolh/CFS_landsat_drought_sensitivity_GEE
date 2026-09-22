39c3374bdb4e28b9c20da1fe72bc6aaa68b16eb6
tursiopsperuvianus <tursiopsperuvianus@gmail.com>
2026-09-22 19:27:59 +0000

    Updated modules/main.js (added 2 lines, deleted 4 lines).

--- modules/main.js
+++ modules/main.js
@@ -46,11 +46,9 @@ var main_greenest = function(output, region) {
   // Drought/baseline
   var ante_means = antecedent.antecedent_means(cmi, 'CMI', years);
   ante_means = ante_means.filter(ee.Filter.gte('year', min_year_landsat));
-
-  var percentile_images = percentile.percentile(ante_means, percentile_list);
-  var percentile_masks = percentile.percentile_masks(ante_means, percentile_images);
+var percentile_images = percentile.percentile(ante_means, percentile_list);
+var percentile_masks = percentile.percentile_masks(ante_means, percentile_images);
   var split_drought_wi = split.split_drought_wi(indices_col, percentile_masks, antecedent_list, index_list);
-
   if (output == 'relative sensitivity') {
     var sens_rel = sensitivity.sensitivity_relative_cap(split_drought_wi, antecedent_list, index_list);
 
