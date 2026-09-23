e6ee1eb95d24200093b9e0f6317c0c05b6a79476
tursiopsperuvianus <tursiopsperuvianus@gmail.com>
2026-09-23 12:13:17 +0000

    Updated tests/test_export_img.js (added 1 line, deleted 1 line).

--- tests/test_export_img.js
+++ tests/test_export_img.js
@@ -14,7 +14,7 @@ var vars = require('users/francisv/CFS:modules/variables.js');
 // Set variables
 var region = ee.Geometry.Polygon([[[-122.87, 55.86], [-122.87, 54.98], [-121.87, 54.98], [-121.87, 55.86]]]);
 var scale = 30;
-var asset_folder = 'users/francisv/CFS';
+var asset_folder = 'projects/perfect-victor-232201/assets/CFS';
 var drive_folder = 'Test-export-img-drive-from-asset';
 
 var output = 'absolute sensitivity';
