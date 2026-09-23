d1ba53e23012cb0d15d210640f2a17f641b8a09f
tursiopsperuvianus <tursiopsperuvianus@gmail.com>
2026-09-23 11:34:07 +0000

    Updated modules/assets.js (added 28 lines, deleted 33 lines).

--- modules/assets.js
+++ modules/assets.js
@@ -1,23 +1,19 @@
 /*
 Assets
 Alec L. Robitaille
-
 */
 
-
-
 var list_assets_in_dir = function(dir) {
-    return ee.data.listAssets(dir)['assets'];
+    return ee.data.listAssets(dir)['assets'] || [];
 };
 exports.list_assets_in_dir = list_assets_in_dir;
 
 
-
 var delete_assets_in_dir = function(dir) {
     list_assets_in_dir(dir)
-        .map(function(d) { 
+        .map(function(d) {
             ee.data.deleteAsset(d.name);
-            return d.name ;
+            return d.name;
         });
 };
 exports.delete_assets_in_dir = delete_assets_in_dir;
@@ -31,4 +27,3 @@ var collect_img_assets_in_dir = function(dir) {
     return ee.ImageCollection(assetList);
 };
 exports.collect_img_assets_in_dir = collect_img_assets_in_dir;
-
